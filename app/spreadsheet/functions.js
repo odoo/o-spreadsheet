4f744b410f758a0429c9d1d1049cbd04afe2ac04
Géry Debongnie <ged@odoo.com>
2020-01-06 14:55:04 +0100

    functions: sum can handle multiple arguments

--- app/spreadsheet/functions.js
+++ app/spreadsheet/functions.js
@@ -1,8 +1,8 @@
 export const functions = {
   SUM: {
     description: "Returns the sum of all values in a range",
-    compute: function(range) {
-      return range.reduce((a, b) => a + b, 0);
+    compute: function(...args) {
+      return args.flat().reduce((a, b) => a + b, 0);
     }
   }
 };
