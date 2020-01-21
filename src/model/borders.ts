997746c6b9fa1b5aa74f7b8589bd6e31de0dec68
Géry Debongnie <ged@odoo.com>
2020-01-21 12:53:43 +0100

    [IMP] implement border tools

--- src/model/borders.ts
+++ src/model/borders.ts
@@ -1,33 +1,119 @@
-import { BorderCommand, GridState, Border } from "./state";
+import { BorderCommand, GridState, Border, Zone } from "./state";
 import { toXC, stringify } from "../helpers";
 import { getCell, addCell, deleteCell } from "./core";
 
 // ---------------------------------------------------------------------------
 // Borders
 // ---------------------------------------------------------------------------
+const commandToSides = {
+  top: ["top"],
+  left: ["left"],
+  right: ["right"],
+  bottom: ["bottom"],
+  all: ["top", "left", "bottom", "right"]
+};
+
+function getTargetZone(zone: Zone, side: string): Zone {
+  const { left, right, top, bottom } = zone;
+  switch (side) {
+    case "left":
+      return { left, top, right: left, bottom };
+    case "top":
+      return { left, top, right, bottom: top };
+    case "right":
+      return { left: right, top, right, bottom };
+    case "bottom":
+      return { left, top: bottom, right, bottom };
+  }
+  return zone;
+}
 export function setBorder(state: GridState, command: BorderCommand) {
-  if (command === "top") {
-    const border: Border = { top: ["thin", "#000"] };
-    for (let zone of state.selection.zones) {
+  for (let zone of state.selection.zones) {
+    setBorderToZone(state, zone, command);
+  }
+}
+
+function setBorderToZone(state: GridState, zone: Zone, command: BorderCommand) {
+  if (command === "clear") {
+    for (let row = zone.top; row <= zone.bottom; row++) {
       for (let col = zone.left; col <= zone.right; col++) {
-        setBorderToCell(state, col, zone.top, border);
+        clearBorder(state, col, row);
       }
     }
+    return;
   }
-  if (command === "clear") {
-    clearBorder(state, state.activeCol, state.activeRow);
+  if (command === "external") {
+    setBorderToZone(state, zone, "left");
+    setBorderToZone(state, zone, "right");
+    setBorderToZone(state, zone, "top");
+    setBorderToZone(state, zone, "bottom");
+    return;
+  }
+  if (command === "hv") {
+    setBorderToZone(state, zone, "h");
+    setBorderToZone(state, zone, "v");
+    return;
+  }
+  const { left, top, right, bottom } = zone;
+  if (command === "h") {
+    for (let r = top + 1; r <= bottom; r++) {
+      setBorderToZone(state, { left, top: r, right, bottom: r }, "top");
+    }
+    return;
+  }
+  if (command === "v") {
+    for (let c = left + 1; c <= right; c++) {
+      setBorderToZone(state, { left: c, top, right: c, bottom }, "left");
+    }
+    return;
+  }
+
+  const border: Border = {};
+  for (let side of commandToSides[command]) {
+    border[side] = ["thin", "#000"];
+  }
+
+  const target = getTargetZone(zone, command);
+  for (let row = target.top; row <= target.bottom; row++) {
+    for (let col = target.left; col <= target.right; col++) {
+      setBorderToCell(state, col, row, border);
+    }
   }
 }
 
 function clearBorder(state: GridState, col: number, row: number) {
   const cell = getCell(state, col, row);
-  if (!cell) {
-    return;
+  if (cell) {
+    if (!cell.content && !cell.style) {
+      deleteCell(state, cell.xc);
+    } else {
+      delete cell.border;
+    }
   }
-  if (!cell.content && !cell.style) {
-    deleteCell(state, toXC(col, row));
-  } else {
-    delete cell.border;
+  if (col > 0) {
+    clearSide(state, col - 1, row, "right");
+  }
+  if (row > 0) {
+    clearSide(state, col, row - 1, "bottom");
+  }
+  clearSide(state, col + 1, row, "left");
+  clearSide(state, col, row + 1, "top");
+}
+
+function clearSide(state: GridState, col: number, row: number, side: string) {
+  const cell = getCell(state, col, row);
+  if (cell && cell.border) {
+    const border = state.borders[cell.border];
+    if (side in border) {
+      const newBorder = Object.assign({}, border);
+      delete newBorder[side];
+      if (!cell.content && !cell.style && Object.keys(newBorder).length === 0) {
+        deleteCell(state, cell.xc);
+      } else {
+        const id = registerBorder(state, newBorder);
+        cell.border = id;
+      }
+    }
   }
 }
 
--- tests/model/borders_test.ts
+++ tests/model/borders_test.ts
@@ -6,19 +6,38 @@ function getBorder(state: GridState, xc: string) {
 }
 
 describe("borders", () => {
-  test("can add and remove a top border, on empty cell", () => {
+  test("can add and remove a border, on empty cell", () => {
     const model = new GridModel({});
 
-    // select B2
+    // select B2, set its top border, then clear it
     model.selectCell(1, 1);
-
-    // set a border top
     model.setBorder("top");
-
     expect(model.state.cells.B2.border).toBeDefined();
     expect(getBorder(model.state, "B2")).toEqual({ top: ["thin", "#000"] });
+    model.setBorder("clear");
+    expect(model.state.cells.B2).not.toBeDefined();
 
-    // clear borders
+    // select B2, set its left border, then clear it
+    model.selectCell(1, 1);
+    model.setBorder("left");
+    expect(model.state.cells.B2.border).toBeDefined();
+    expect(getBorder(model.state, "B2")).toEqual({ left: ["thin", "#000"] });
+    model.setBorder("clear");
+    expect(model.state.cells.B2).not.toBeDefined();
+
+    // select B2, set its bottom border, then clear it
+    model.selectCell(1, 1);
+    model.setBorder("bottom");
+    expect(model.state.cells.B2.border).toBeDefined();
+    expect(getBorder(model.state, "B2")).toEqual({ bottom: ["thin", "#000"] });
+    model.setBorder("clear");
+    expect(model.state.cells.B2).not.toBeDefined();
+
+    // select B2, set its right border, then clear it
+    model.selectCell(1, 1);
+    model.setBorder("right");
+    expect(model.state.cells.B2.border).toBeDefined();
+    expect(getBorder(model.state, "B2")).toEqual({ right: ["thin", "#000"] });
     model.setBorder("clear");
     expect(model.state.cells.B2).not.toBeDefined();
   });
@@ -55,5 +74,151 @@ describe("borders", () => {
     expect(getBorder(model.state, "B2")).toEqual({ top: ["thin", "#000"] });
     expect(model.state.cells.C2.border).toBeDefined();
     expect(getBorder(model.state, "C2")).toEqual({ top: ["thin", "#000"] });
+  });
+
+  test("can clear a zone", () => {
+    const model = new GridModel({});
+
+    // select C3 and add a border
+    model.selectCell(2, 2);
+    model.setBorder("top");
+    expect(model.state.cells.C3.border).toBeDefined();
+
+    // select A1:E6
+    model.selectCell(0, 0);
+    model.updateSelection(5, 5);
+
+    // clear all borders
+    model.setBorder("clear");
+
+    expect(model.state.cells.C3).not.toBeDefined();
+  });
+
+  test("can set all borders in a zone", () => {
+    const model = new GridModel({});
+
+    // select B2, then expand selection to B2:C3
+    model.selectCell(1, 1);
+    model.updateSelection(2, 2);
+
+    // set all borders
+    model.setBorder("all");
+    const all = {
+      left: ["thin", "#000"],
+      top: ["thin", "#000"],
+      bottom: ["thin", "#000"],
+      right: ["thin", "#000"]
+    };
+    expect(getBorder(model.state, "B2")).toEqual(all);
+    expect(getBorder(model.state, "B3")).toEqual(all);
+    expect(getBorder(model.state, "C2")).toEqual(all);
+    expect(getBorder(model.state, "C3")).toEqual(all);
+  });
+
+  test("setting top border in a zone only set top row", () => {
+    const model = new GridModel({});
+
+    // select B2, then expand selection to B2:C3
+    model.selectCell(1, 1);
+    model.updateSelection(2, 2);
+
+    // set all borders
+    model.setBorder("top");
+    const border = {
+      top: ["thin", "#000"]
+    };
+    expect(getBorder(model.state, "B2")).toEqual(border);
+    expect(getBorder(model.state, "C2")).toEqual(border);
+    expect(model.state.cells.B3).not.toBeDefined();
+    expect(model.state.cells.C3).not.toBeDefined();
+  });
+
+  test("clearing a common border in a neighbour cell", () => {
+    const model = new GridModel({});
+
+    // select B2, then set its right border
+    model.selectCell(1, 1);
+    model.setBorder("right");
+    expect(getBorder(model.state, "B2")).toEqual({ right: ["thin", "#000"] });
+
+    // select C2 then clear it
+    model.selectCell(2, 1);
+    model.setBorder("clear");
+    expect(model.state.cells.B2).not.toBeDefined();
+  });
+
+  test("setting external border in a zone works", () => {
+    const model = new GridModel({});
+
+    // select B2, then expand selection to B2:D4
+    model.selectCell(1, 1);
+    model.updateSelection(3, 3);
+
+    // set external borders
+    model.setBorder("external");
+    const s = ["thin", "#000"];
+    expect(getBorder(model.state, "B2")).toEqual({ top: s, left: s });
+    expect(getBorder(model.state, "C2")).toEqual({ top: s });
+    expect(getBorder(model.state, "D2")).toEqual({ top: s, right: s });
+    expect(getBorder(model.state, "B3")).toEqual({ left: s });
+    expect(model.state.cells.C3).not.toBeDefined();
+    expect(getBorder(model.state, "D3")).toEqual({ right: s });
+    expect(getBorder(model.state, "B4")).toEqual({ bottom: s, left: s });
+    expect(getBorder(model.state, "C4")).toEqual({ bottom: s });
+    expect(getBorder(model.state, "D4")).toEqual({ bottom: s, right: s });
+  });
+
+  test("setting internal horizontal borders in a zone works", () => {
+    const model = new GridModel({});
+
+    // select B2, then expand selection to B2:C4
+    model.selectCell(1, 1);
+    model.updateSelection(2, 3);
+
+    // set external borders
+    model.setBorder("h");
+    const s = ["thin", "#000"];
+    expect(model.state.cells.B2).not.toBeDefined();
+    expect(model.state.cells.C2).not.toBeDefined();
+    expect(getBorder(model.state, "B3")).toEqual({ top: s });
+    expect(getBorder(model.state, "C3")).toEqual({ top: s });
+    expect(getBorder(model.state, "B4")).toEqual({ top: s });
+    expect(getBorder(model.state, "C4")).toEqual({ top: s });
+  });
+
+  test("setting internal vertical borders in a zone works", () => {
+    const model = new GridModel({});
+
+    // select B2, then expand selection to B2:C4
+    model.selectCell(1, 1);
+    model.updateSelection(2, 3);
+
+    // set external borders
+    model.setBorder("v");
+    const s = ["thin", "#000"];
+    expect(model.state.cells.B2).not.toBeDefined();
+    expect(model.state.cells.B3).not.toBeDefined();
+    expect(model.state.cells.B4).not.toBeDefined();
+    expect(getBorder(model.state, "C2")).toEqual({ left: s });
+    expect(getBorder(model.state, "C3")).toEqual({ left: s });
+    expect(getBorder(model.state, "C4")).toEqual({ left: s });
+  });
+
+  test("setting internal  borders in a zone works", () => {
+    const model = new GridModel({});
+
+    // select B2, then expand selection to B2:C4
+    model.selectCell(1, 1);
+    model.updateSelection(2, 3);
+
+    // set external borders
+    model.setBorder("hv");
+    const s = ["thin", "#000"];
+    expect(model.state.cells.B2).not.toBeDefined();
+    expect(getBorder(model.state, "C2")).toEqual({ left: s });
+    expect(getBorder(model.state, "B3")).toEqual({ top: s });
+    expect(getBorder(model.state, "C3")).toEqual({ top: s, left: s });
+    expect(getBorder(model.state, "B4")).toEqual({ top: s });
+    expect(getBorder(model.state, "C4")).toEqual({ top: s, left: s });
   });
 });
