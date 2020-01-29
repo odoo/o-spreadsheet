37fecec23b1fd2bf21c447274790937f9b59c567
Géry Debongnie <ged@odoo.com>
2020-01-29 16:20:54 +0100

    [REF] reorder function descriptions to make them easier to read

--- src/functions/logical.ts
+++ src/functions/logical.ts
@@ -3,60 +3,60 @@ import { FunctionMap, args } from "./functions";
 export const functions: FunctionMap = {
   WAIT: {
     description: "Wait",
+    args: args`ms (number,cell) wait time in milliseconds`,
+    returns: ["ANY"],
+    async: true,
     compute: function(...args) {
       return new Promise(function(resolve, reject) {
         setTimeout(function() {
           resolve(args[0]);
         }, 2000);
       });
-    },
-    async: true,
-    args: args`ms (number,cell) wait time in milliseconds`,
-    returns: ["ANY"]
+    }
   },
   AND: {
     description: "Returns true if all the arguments are true, false otherwise.",
+    args: args`logicalPart (boolean,cell,repeating) logical part`,
+    returns: ["BOOLEAN"],
     compute: function(...args) {
       return Boolean(args.flat().reduce((a, b) => a && b, true));
-    },
-    args: args`logicalPart (boolean,cell,repeating) logical part`,
-    returns: ["BOOLEAN"]
+    }
   },
   OR: {
     description: "Returns true if at least one of the arguments are true, false otherwise.",
+    args: args`logicalPart (boolean,cell,repeating) logical part`,
+    returns: ["BOOLEAN"],
     compute: function(...args) {
       return Boolean(args.flat().reduce((a, b) => a || b, false));
-    },
-    args: args`logicalPart (boolean,cell,repeating) logical part`,
-    returns: ["BOOLEAN"]
+    }
   },
   XOR: {
     description:
       "Returns true if an odd number of the provided arguments are true, false otherwise.",
+    args: args`logicalPart (boolean,cell,repeating) logical part`,
+    returns: ["BOOLEAN"],
     compute: function(...args) {
       return Boolean(args.flat().filter(a => a).length % 2 !== 0);
-    },
-    args: args`logicalPart (boolean,cell,repeating) logical part`,
-    returns: ["BOOLEAN"]
+    }
   },
   NOT: {
     description: "Returns true if the argument is false, false otherwise.",
+    args: args`XXX (boolean,cell) logical part`,
+    returns: ["BOOLEAN"],
     compute: function(...args) {
       return Boolean(!args[0]);
-    },
-    args: args`XXX (boolean,cell) logical part`,
-    returns: ["BOOLEAN"]
+    }
   },
   IF: {
     description: "Returns the second argument if the first one is true, the third otherwise.",
-    compute: function(condition, valueTrue, valueFalse) {
-      return condition ? valueTrue : valueFalse;
-    },
     args: args`
       condition (boolean,cell) logical part
       valueTrue (any) the value of the cell if the condition is true
       valueFalse (any) the value of the cell if the condition is false
     `,
-    returns: ["ANY"]
+    returns: ["ANY"],
+    compute: function(condition, valueTrue, valueFalse) {
+      return condition ? valueTrue : valueFalse;
+    }
   }
 };
--- src/functions/math.ts
+++ src/functions/math.ts
@@ -7,43 +7,43 @@ function toNumber(n: any) {
 export const functions: FunctionMap = {
   SUM: {
     description: "Returns the sum of all values in a range.",
-    compute: function(...args) {
-      return args.flat().reduce((a, b) => a + toNumber(b), 0);
-    },
     args: args`
         number (number,cell,range)
         numbers (number,cell,range,optional,repeating)
     `,
-    returns: ["NUMBER"]
+    returns: ["NUMBER"],
+    compute: function(...args) {
+      return args.flat().reduce((a, b) => a + toNumber(b), 0);
+    }
   },
   RAND: {
     description: "Returns a random number between 0 and 1",
+    args: [],
+    returns: ["NUMBER"],
     compute: function() {
       return Math.random();
-    },
-    args: [],
-    returns: ["NUMBER"]
+    }
   },
   MIN: {
     description: "Returns the minimum value.",
-    compute: function(...args) {
-      return Math.min(...args);
-    },
     args: args`
         number (number,cell,range)
         numbers (number,cell,range,optional,repeating)
     `,
-    returns: ["NUMBER"]
+    returns: ["NUMBER"],
+    compute: function(...args) {
+      return Math.min(...args);
+    }
   },
   MAX: {
     description: "Returns the maximum value.",
-    compute: function(...args) {
-      return Math.max(...args);
-    },
     args: args`
         number (number,cell,range)
         numbers (number,cell,range,optional,repeating)
     `,
-    returns: ["NUMBER"]
+    returns: ["NUMBER"],
+    compute: function(...args) {
+      return Math.max(...args);
+    }
   }
 };
