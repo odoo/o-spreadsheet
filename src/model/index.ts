8c1b1364a3169fb905300046fc90fb63b57ca3d1
Géry Debongnie <ged@odoo.com>
2020-02-01 09:52:05 +0100

    [REF] rename model/resize => model/resizing

--- src/model/index.ts
+++ src/model/index.ts
@@ -5,7 +5,7 @@ import * as core from "./core";
 import * as merges from "./merges";
 import * as history from "./history";
 import * as selection from "./selection";
-import * as expand from "./resize";
+import * as expand from "./resizing";
 import {
   activateSheet,
   addSheet,
