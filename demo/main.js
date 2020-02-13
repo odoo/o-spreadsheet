24949d58d4465f2c93ebad53b334376d3e313a9d
Pierre Rousseau <pro@odoo.com>
2020-02-13 14:45:14 +0100

    [IMP] top_bar: allow to save and load a file
    Adds two buttons to save to a json file or load the data from a json file.

--- demo/main.js
+++ demo/main.js
@@ -25,12 +25,56 @@ class App extends Component {
   notifyUser(ev) {
     window.alert(ev.detail.content);
   }
+
+  onSave(ev) {
+    const data = ev.detail.data;
+    const downloadObjectAsJson = function(exportObj, exportName) {
+      const dataStr =
+        "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(exportObj));
+      const downloadAnchorNode = document.createElement("a");
+      downloadAnchorNode.setAttribute("href", dataStr);
+      downloadAnchorNode.setAttribute("download", exportName + ".json");
+      document.body.appendChild(downloadAnchorNode); // required for firefox
+      downloadAnchorNode.click();
+      downloadAnchorNode.remove();
+    };
+    downloadObjectAsJson(data, "export");
+  }
+
+  onLoad(ev) {
+    // <input type="file" id="selectFiles" value="Import" /><br /> <button id="import">Import</button> <textarea id="result"></textarea>
+    let data = false;
+    const uploadAnchorNode = document.createElement("input");
+    uploadAnchorNode.setAttribute("type", "file");
+    uploadAnchorNode.setAttribute("id", "selectFiles");
+    document.body.appendChild(uploadAnchorNode);
+    debugger;
+    uploadAnchorNode.addEventListener("change", () => {
+      const files = document.getElementById("selectFiles").files;
+      if (files.length <= 0) {
+        return false;
+      }
+      const fr = new FileReader();
+      fr.onload = e => {
+        data = JSON.parse(e.target.result);
+        this.data = data;
+        ev.detail.loadData(this.data);
+        uploadAnchorNode.remove();
+      };
+      fr.readAsText(files.item(0));
+    });
+    uploadAnchorNode.click();
+  }
 }
 
 App.template = xml`
-  <Spreadsheet data="data"
-    t-on-ask-confirmation="askConfirmation"
-    t-on-notify-user="notifyUser"/>`;
+  <div>
+    <Spreadsheet data="data"
+      t-on-ask-confirmation="askConfirmation"
+      t-on-notify-user="notifyUser"
+      t-on-save-content="onSave"
+      t-on-load-content="onLoad"/>
+  </div>`;
 App.style = css`
   html {
     height: 100%;
