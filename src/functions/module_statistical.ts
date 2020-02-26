4616499fe36d086614487e6e41e3ad6eca7f11c3
Alexis Lacroix <laa@odoo.com>
2020-02-26 22:24:17 +0100

    [IMP] functions: add AVERAGE.WEIGHTED function

--- src/functions/module_statistical.ts
+++ src/functions/module_statistical.ts
@@ -35,5 +35,80 @@ export const functions: FunctionMap = {
       }
       return sum / count;
     }
+  },
+
+  "AVERAGE.WEIGHTED": {
+    description: `Weighted average.`,
+    args: args`
+      values (number, range<number>) Values to average.
+      weights (number, range<number>) Weights for each corresponding value.
+      additional_values (number, range<number>, optional, repeating) Additional values to average with weights.
+    `,
+    // @compatibility: on google sheets, args difinitions are next:
+    // additional_values (number, range<number>, optional, repeating) Additional values to average.
+    // additional_weights (number, range<number>, optional, repeating) Additional weights.
+    returns: ["NUMBER"],
+    compute: function(...args: NumberOrRange[]): number {
+      let sum = 0;
+      let count = 0;
+      let value;
+      let weight;
+      const rangeError = `
+          AVERAGE.WEIGHTED has mismatched range sizes.
+      `;
+      const negativeWeightError = `
+          AVERAGE.WEIGHTED expects the weight to be positive or equal to 0.
+      `;
+      if (args.length % 2 === 1) {
+        throw new Error(`
+          Wrong number of arguments. Expected an even number of arguments.
+      `);
+      }
+      for (let n = 0; n < args.length - 1; n += 2) {
+        value = args[n];
+        weight = args[n + 1];
+        if (typeof value != typeof weight) {
+          throw new Error(rangeError);
+        }
+        if (Array.isArray(value)) {
+          let dimColValue = value.length;
+          let dimLinValue = value[0].length;
+          if (dimColValue !== weight.length || dimLinValue != weight[0].length) {
+            throw new Error(rangeError);
+          }
+          for (let i = 0; i < dimColValue; i++) {
+            for (let j = 0; j < dimLinValue; j++) {
+              let subValue = value[i][j];
+              let subWeight = weight[i][j];
+              // typeof subValue or subWeight can be 'number' or 'undefined'
+              if (typeof subValue !== typeof subWeight) {
+                throw new Error(`
+                  AVERAGE.WEIGHTED expects number values.
+                `);
+              }
+              if (typeof subValue !== "undefined") {
+                if (subWeight < 0) {
+                  throw new Error(negativeWeightError);
+                }
+                sum += subValue * subWeight;
+                count += subWeight;
+              }
+            }
+          }
+        } else {
+          if (weight < 0) {
+            throw new Error(negativeWeightError);
+          }
+          sum += value * weight;
+          count += weight;
+        }
+      }
+      if (count === 0) {
+        throw new Error(`
+          Evaluation of function AVERAGE.WEIGHTED caused a divide by zero error.
+        `);
+      }
+      return sum / count;
+    }
   }
 };
--- tests/functions/module_statistical_test.ts
+++ tests/functions/module_statistical_test.ts
@@ -40,7 +40,7 @@ describe("statistical", () => {
   });
 
   test("AVERAGE: functional tests on range arguments", () => {
-    const grid_average = {
+    const gridAverage = {
       A1: "=average(B2:D4)",
       A2: "=average(B2:C3, B4:C4, D2:D3, D4)",
       B2: "42.2",
@@ -53,7 +53,222 @@ describe("statistical", () => {
       C4: "0",
       D4: "0"
     };
-    expect(evaluateCell("A1", grid_average)).toEqual(8);
-    expect(evaluateCell("A2", grid_average)).toEqual(8);
+    expect(evaluateCell("A1", gridAverage)).toEqual(8);
+    expect(evaluateCell("A2", gridAverage)).toEqual(8);
+  });
+
+  //----------------------------------------------------------------------------
+  // AVERAGE.WEIGHTED
+  //----------------------------------------------------------------------------
+
+  test("AVERAGE.WEIGHTED: functional tests on simple arguments", () => {
+    expect(evaluateCell("A1", { A1: "=average.weighted( ,  )" })).toEqual("#ERROR"); // @compatibility: on google sheets, return #DIV/0!
+    expect(evaluateCell("A1", { A1: "=average.weighted(0, 0)" })).toEqual("#ERROR"); // @compatibility: on google sheets, return #DIV/0!
+    expect(evaluateCell("A1", { A1: "=average.weighted(0, 1)" })).toBe(0);
+    expect(evaluateCell("A1", { A1: "=average.weighted(1, 1, 3)" })).toEqual("#ERROR"); // @compatibility: on google sheets, return #N/A
+    expect(evaluateCell("A1", { A1: "=average.weighted(1, 1, 3, 3)" })).toBe(2.5);
+    expect(evaluateCell("A1", { A1: "=average.weighted( , 1, 3, 3)" })).toBe(2.25);
+    expect(evaluateCell("A1", { A1: "=average.weighted(1, 1, 3,  )" })).toBe(1);
+    expect(evaluateCell("A1", { A1: "=average.weighted(1,  , 3,  )" })).toEqual("#ERROR"); // @compatibility: on google sheets, return #DIV/0!
+    expect(evaluateCell("A1", { A1: "=average.weighted(1.5, 1, 2.5, 4)" })).toBe(2.3);
+    expect(evaluateCell("A1", { A1: "=average.weighted(4, 1.5, 2, 2.5)" })).toBe(2.75);
+    expect(evaluateCell("A1", { A1: "=average.weighted(-10, 1, 20, 2)" })).toBe(10);
+    expect(evaluateCell("A1", { A1: "=average.weighted(1, -1, 3, 3)" })).toEqual("#ERROR"); // @compatibility: on google sheets, return #VALUE!
+  });
+
+  test("AVERAGE.WEIGHTED: casting tests on simple arguments", () => {
+    expect(evaluateCell("A1", { A1: "=average.weighted('-10', '1', '20', '2')" })).toBe(10);
+    expect(evaluateCell("A1", { A1: "=average.weighted(TRUE, FALSE)" })).toEqual("#ERROR"); // @compatibility: on google sheets, return #VALUE!
+    expect(evaluateCell("A1", { A1: "=average.weighted(FALSE, TRUE)" })).toBe(0);
+    expect(evaluateCell("A1", { A1: "=average.weighted('@#%!*', '@#%!*', '20', '2')" })).toEqual(
+      "#ERROR"
+    ); // @compatibility: on google sheets, return #VALUE!
+  });
+
+  test("AVERAGE.WEIGHTED: functional tests on cell arguments", () => {
+    expect(evaluateCell("A1", { A1: "=average.weighted(A2, A3)", A2: "", A3: "" })).toEqual(
+      "#ERROR"
+    ); // @compatibility: on google sheets, return #DIV/0!
+    expect(evaluateCell("A1", { A1: "=average.weighted(A2, A3)", A2: "0", A3: "0" })).toEqual(
+      "#ERROR"
+    ); // @compatibility: on google sheets, return #DIV/0!
+    expect(evaluateCell("A1", { A1: "=average.weighted(A2, A3)", A2: "0", A3: "1" })).toBe(0);
+    expect(
+      evaluateCell("A1", { A1: "=average.weighted(A2, A3, A4)", A2: "1", A3: "1", A4: "3" })
+    ).toEqual("#ERROR"); // @compatibility: on google sheets, return #N/A
+    expect(
+      evaluateCell("A1", {
+        A1: "=average.weighted(A2, A3, A4, A5)",
+        A2: "1",
+        A3: "1",
+        A4: "3",
+        A5: "3"
+      })
+    ).toBe(2.5);
+    expect(
+      evaluateCell("A1", {
+        A1: "=average.weighted(A2, A3, A4, A5)",
+        A2: "",
+        A3: "1",
+        A4: "3",
+        A5: "3"
+      })
+    ).toEqual("#ERROR"); // @compatibility: on google sheets, return #VALUE!
+    expect(
+      evaluateCell("A1", {
+        A1: "=average.weighted(A2, A3, A4, A5)",
+        A2: "1",
+        A3: "1",
+        A4: "3",
+        A5: ""
+      })
+    ).toEqual("#ERROR"); // @compatibility: on google sheets, return #VALUE!
+    expect(
+      evaluateCell("A1", {
+        A1: "=average.weighted(A2, A3, A4, A5)",
+        A2: "1.5",
+        A3: "1",
+        A4: "2.5",
+        A5: "4"
+      })
+    ).toBe(2.3);
+    expect(
+      evaluateCell("A1", {
+        A1: "=average.weighted(A2, A3, A4, A5)",
+        A2: "4",
+        A3: "1.5",
+        A4: "2",
+        A5: "2.5"
+      })
+    ).toBe(2.75);
+    expect(
+      evaluateCell("A1", {
+        A1: "=average.weighted(A2, A3, A4, A5)",
+        A2: "-10",
+        A3: "1",
+        A4: "20",
+        A5: "2"
+      })
+    ).toBe(10);
+    expect(
+      evaluateCell("A1", {
+        A1: "=average.weighted(A2, A3, A4, A5)",
+        A2: "1",
+        A3: "-1",
+        A4: "3",
+        A5: "3"
+      })
+    ).toEqual("#ERROR"); // @compatibility: on google sheets, return #VALUE!
+  });
+
+  test("AVERAGE.WEIGHTED: casting tests on cell arguments", () => {
+    expect(
+      evaluateCell("A1", {
+        A1: "=average.weighted(A2, A3, A4, A5)",
+        A2: "'2'",
+        A3: "'1'",
+        A4: "'6'",
+        A5: "'1'"
+      })
+    ).toEqual("#ERROR"); // @compatibility: on google sheets, return #DIV/0!
+    expect(
+      evaluateCell("A1", {
+        A1: "=average.weighted(A2, A3, A4, A5)",
+        A2: "'2'",
+        A3: "'1'",
+        A4: "6",
+        A5: "1"
+      })
+    ).toBe(6);
+    expect(
+      evaluateCell("A1", {
+        A1: "=average.weighted(A2, A3, A4, A5)",
+        A2: "'2'",
+        A3: "1",
+        A4: "6",
+        A5: "1"
+      })
+    ).toEqual("#ERROR"); // @compatibility: on google sheets, return #VALUE!
+    expect(
+      evaluateCell("A1", {
+        A1: "=average.weighted(A2, A3, A4, A5)",
+        A2: "2",
+        A3: "'1'",
+        A4: "6",
+        A5: "'1'"
+      })
+    ).toEqual("#ERROR"); // @compatibility: on google sheets, return #VALUE!
+    expect(
+      evaluateCell("A1", {
+        A1: "=average.weighted(A2, A3, A4, A5)",
+        A2: "FALSE",
+        A3: "TRUE",
+        A4: "FALSE",
+        A5: "TRUE"
+      })
+    ).toEqual("#ERROR"); // @compatibility: on google sheets, return #DIV/0!
+    expect(
+      evaluateCell("A1", {
+        A1: "=average.weighted(A2, A3, A4, A5)",
+        A2: "FALSE",
+        A3: "TRUE",
+        A4: "6",
+        A5: "TRUE"
+      })
+    ).toEqual("#ERROR"); // @compatibility: on google sheets, return #VALUE!
+    expect(
+      evaluateCell("A1", {
+        A1: "=average.weighted(A2, A3, A4, A5)",
+        A2: "FALSE",
+        A3: "TRUE",
+        A4: "6",
+        A5: "1"
+      })
+    ).toBe(6);
+  });
+
+  test("AVERAGE.WEIGHTED: functional tests on range arguments", () => {
+    const gridAverage = {
+      A1: "=average.weighted(C1, C3, D1, D3, E1, E3, F1, F3)",
+      A2: "=average.weighted(C1:F1, C3:F3)",
+      A3: "=average.weighted(C1:D1, C3:D3, E1:F1, E3:F3)",
+      A4: "=average.weighted(C1:F1, B5:B8)",
+      A5: "=average.weighted(C1, B5, D1, B6, E1, B7, F1, B8)",
+      A6: "=average.weighted(C2, C3, D2, D3, E2, E3, F2, F3)",
+      A7: "=average.weighted(C2:F2, C3:F3)",
+      A8: "=average.weighted(C2, C4, D2, D4, E2, E4, F2, F4)",
+      A9: "=average.weighted(C2:D2, C4:D4, E2:F2, E4:F4)",
+      A10: "=average.weighted(C2:F2, C4:F4)",
+      B5: "2",
+      B6: "4",
+      B7: "2",
+      B8: "2",
+      C1: "0",
+      D1: "10",
+      E1: "20",
+      F1: "40",
+      C2: "42.2",
+      D2: "TRUE",
+      E2: "Jean Bambois",
+      F2: "-10.2",
+      C3: "2",
+      D3: "4",
+      E3: "2",
+      F3: "2",
+      C4: "2",
+      D4: "Jean Bonbeurre",
+      E4: "",
+      F4: "2"
+    };
+    expect(evaluateCell("A1", gridAverage)).toBe(16);
+    expect(evaluateCell("A2", gridAverage)).toBe(16);
+    expect(evaluateCell("A3", gridAverage)).toBe(16);
+    expect(evaluateCell("A4", gridAverage)).toEqual("#ERROR"); // @compatibility: on google sheets, return #VALUE!
+    expect(evaluateCell("A5", gridAverage)).toBe(16);
+    expect(evaluateCell("A6", gridAverage)).toEqual("#ERROR"); // @compatibility: on google sheets, return #VALUE!
+    expect(evaluateCell("A7", gridAverage)).toEqual("#ERROR"); // @compatibility: on google sheets, return #VALUE!
+    expect(evaluateCell("A8", gridAverage)).toBe(16);
+    expect(evaluateCell("A9", gridAverage)).toBe(16);
+    expect(evaluateCell("A10", gridAverage)).toBe(16);
   });
 });
