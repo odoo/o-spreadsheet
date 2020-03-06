37ee2697e9084740eaff55899cff93611667b3b5
Vincent Schippefilt <vsc@odoo.com>
2020-03-06 11:11:01 +0100

    [FIX] helper: convert a string to a Zone

--- src/helpers.ts
+++ src/helpers.ts
@@ -81,6 +81,37 @@ export function toXC(col: number, row: number): string {
   return numberToLetters(col) + String(row + 1);
 }
 
+/**
+ * Convert from a cartesian reference to a Zone
+ *
+ * Examples:
+ *    "A1" ==> Top 0, Bottom 0, Left: 0, Right: 0
+ *    "B1:B3" ==> Top 0, Bottom 3, Left: 1, Right: 1
+ *
+ *  TODO VSC: Support 'Sheet!' reference
+ */
+export function toZone(xc: string): Zone {
+  const ranges = xc.replace("$", "").split(":");
+  let top: number, bottom: number, left: number, right: number;
+
+  let c = toCartesian(ranges[0].trim());
+  left = right = c[0];
+  top = bottom = c[1];
+  if (ranges.length === 2) {
+    let d = toCartesian(ranges[1].trim());
+    right = d[0];
+    bottom = d[1];
+    if (right < left) {
+      [right, left] = [left, right];
+    }
+    if (bottom < top) {
+      [bottom, top] = [top, bottom];
+    }
+  }
+
+  return { top, bottom, left, right };
+}
+
 //------------------------------------------------------------------------------
 // Zones
 //------------------------------------------------------------------------------
