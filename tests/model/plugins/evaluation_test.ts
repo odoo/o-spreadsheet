5e65d20f2bcb140c1ef26f92ba2a14c89e6637e2
Géry Debongnie <ged@odoo.com>
2020-04-02 16:58:53 +0200

    [IMP] evaluation: allow to evaluate a formula outside of a cell
    co-authored-by: ged@odoo.com
    co-authored-by: vincent.schippefilt@gmail.com

---
+++ tests/model/plugins/evaluation_test.ts
@@ -0,0 +1,36 @@
+import { Model } from "../../../src/model";
+
+import "../../canvas.mock";
+
+let model: Model = new Model();
+beforeEach(() => {
+  model = new Model();
+});
+
+describe("evaluate formula getter", () => {
+  test("a ref in the current sheet", () => {
+    model.dispatch({ type: "SET_VALUE", xc: "A1", text: "12" });
+    expect(model.getters.evaluateFormula("=A1")).toBe(12);
+  });
+
+  test("in another sheet", () => {
+    model.dispatch({ type: "CREATE_SHEET" });
+    model.dispatch({ type: "SET_VALUE", xc: "A1", text: "11", sheet: "Sheet2" });
+    expect(model.getters.evaluateFormula("=Sheet2!A1")).toBe(11);
+  });
+
+  // i think these formulas should throw
+  test("in a not existing sheet", () => {
+    expect(() => model.getters.evaluateFormula("=Sheet99!A1")).toThrow();
+  });
+
+  test.skip("evaluate a cell in error", () => {
+    model.dispatch({ type: "SET_VALUE", xc: "A1", text: "=mqsdlkjfqsdf(((--" });
+    expect(() => model.getters.evaluateFormula("=A1")).toThrow();
+  });
+
+  test.skip("evaluate a pending cell (async)", () => {
+    model.dispatch({ type: "SET_VALUE", xc: "A1", text: "=wait(99999)" });
+    expect(() => model.getters.evaluateFormula("=A1")).toThrow();
+  });
+});
