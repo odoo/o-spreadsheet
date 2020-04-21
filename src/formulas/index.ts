3ffa4690b1a8c474f516c9e17a7e726e2523bff5
Géry Debongnie <ged@odoo.com>
2020-04-21 22:58:34 +0200

    [DOC] add some documentation

--- src/formulas/index.ts
+++ src/formulas/index.ts
@@ -1,3 +1,12 @@
+/**
+ * The formulas module provides all functionality related to manipulating
+ * formulas:
+ *
+ * - tokenization (transforming a string into a list of tokens)
+ * - parsing (same, but into an AST (Abstract Syntax Tree))
+ * - compiler (getting an executable function representing a formula)
+ */
+
 export { tokenize, Token } from "./tokenizer";
 export { composerTokenize, ComposerToken } from "./composer_tokenizer";
 export { parse, rangeReference } from "./parser";
--- src/registry.ts
+++ src/registry.ts
@@ -1,11 +1,33 @@
+/**
+ * Registry
+ *
+ * The Registry class is basically just a mapping from a string key to an object.
+ * It is really not much more than an object. It is however useful for the
+ * following reasons:
+ *
+ * 1. it let us react and execute code when someone add something to the registry
+ *   (for example, the FunctionRegistry subclass this for this purpose)
+ * 2. it throws an error when the get operation fails
+ * 3. it provides a chained API to add items to the registry.
+ */
+
 export class Registry<T> {
   content: { [key: string]: T } = {};
 
+  /**
+   * Add an item to the registry
+   *
+   * Note that this also returns the registry, so another add method call can
+   * be chained
+   */
   add(key: string, value: T): Registry<T> {
     this.content[key] = value;
     return this;
   }
 
+  /**
+   * Get an item from the registry
+   */
   get(key: string): T {
     if (!(key in this.content)) {
       throw new Error(`Cannot find ${key} in this registry!`);
@@ -13,6 +35,9 @@ export class Registry<T> {
     return this.content[key];
   }
 
+  /**
+   * Get a list of all elements in the registry
+   */
   getAll(): T[] {
     return Object.values(this.content);
   }
