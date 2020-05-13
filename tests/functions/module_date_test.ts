a88ccb26c7ead98d005d2a4e761d84d716db7f3b
Alexis Lacroix <laa@odoo.com>
2020-05-13 13:13:12 +0200

    [IMP] functions: accept numbers on date functions
    Supose:
    A1: "=MONTH(B1)", B1: "5/13/1954"
    A2: "=MONTH(B2)", B2: "43964"
    
    Before:
    A1 --> 5
    A2 --> #ERROR
    
    Now:
    A1 --> 5
    A2 --> 5

--- tests/functions/module_date_test.ts
+++ tests/functions/module_date_test.ts
@@ -7,5 +7,12 @@ describe("date", () => {
 
   test("MONTH: functional tests on simple arguments", () => {
     expect(evaluateCell("A1", { A1: "=MONTH(A2)", A2: "1/2/1954" })).toBe(1);
+    expect(evaluateCell("A1", { A1: "=MONTH(A2)", A2: "5/13/1954" })).toBe(5);
+    expect(evaluateCell("A1", { A1: "=MONTH(A2)", A2: "43964" })).toBe(5); // 43964 corespond to 5/13/195
+    expect(evaluateCell("A1", { A1: "=MONTH(A2)", A2: "0" })).toBe(12); // 0 corespond to 12/30/1899
+    expect(evaluateCell("A1", { A1: "=MONTH(A2)", A2: "1" })).toBe(12); // 1 corespond to 12/31/1899
+    expect(evaluateCell("A1", { A1: "=MONTH(A2)", A2: "2" })).toBe(1); // 2 corespond to 1/1/1900
+    expect(evaluateCell("A1", { A1: "=MONTH(A2)", A2: '="43964"' })).toBe(5);
+    expect(evaluateCell("A1", { A1: "=MONTH(A2)", A2: "TRUE" })).toBe(12);
   });
 });
