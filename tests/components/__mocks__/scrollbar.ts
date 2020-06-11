33ec2fd6bfa598073ebf2fe38226850b02725d86
Lucas Lefèvre <lul@odoo.com>
2020-06-11 11:09:10 +0200

    [FIX] components: Add touch support to move the grid
    Currently, the grid cannot be moved/scrolled on mobile
    devices (with touch screens).
    
    This commit adds touch support to navigate on the grid.

---
+++ tests/components/__mocks__/scrollbar.ts
@@ -0,0 +1,18 @@
+import { ScrollDirection } from "../../../src/components/scrollbar";
+
+export class ScrollBar {
+  private scrollValue: number;
+  el: HTMLElement;
+  constructor(el: HTMLElement | null, direction: ScrollDirection) {
+    this.el = el!;
+    this.scrollValue = 0;
+  }
+
+  get scroll(): number {
+    return this.scrollValue;
+  }
+
+  set scroll(value: number) {
+    this.scrollValue = value;
+  }
+}
--- tests/helpers.ts
+++ tests/helpers.ts
@@ -267,3 +267,45 @@ export function zone(str: string): Zone {
 export function target(str: string): Zone[] {
   return str.split(",").map(zone);
 }
+
+/**
+ * The Touch API is currently experimental (mid 2020).
+ * This implementation is used in test to easily trigger TouchEvents.
+ * (TouchEvent is not experimental and supported by all major browsers.)
+ *
+ * https://developer.mozilla.org/en-US/docs/Web/API/Touch
+ */
+export class Touch {
+  readonly altitudeAngle: number;
+  readonly azimuthAngle: number;
+  readonly clientX: number;
+  readonly clientY: number;
+  readonly force: number;
+  readonly identifier: number;
+  readonly pageX: number;
+  readonly pageY: number;
+  readonly radiusX: number;
+  readonly radiusY: number;
+  readonly rotationAngle: number;
+  readonly screenX: number;
+  readonly screenY: number;
+  readonly target: EventTarget;
+  readonly touchType: TouchType;
+  constructor(touchInitDict: TouchInit) {
+    this.identifier = touchInitDict.identifier;
+    this.target = touchInitDict.target;
+    this.altitudeAngle = touchInitDict.altitudeAngle || 0;
+    this.azimuthAngle = touchInitDict.azimuthAngle || 0;
+    this.clientX = touchInitDict.clientX || 0;
+    this.clientY = touchInitDict.clientY || 0;
+    this.force = touchInitDict.force || 0;
+    this.pageX = touchInitDict.pageX || 0;
+    this.pageY = touchInitDict.pageY || 0;
+    this.radiusX = touchInitDict.radiusX || 0;
+    this.radiusY = touchInitDict.radiusY || 0;
+    this.rotationAngle = touchInitDict.rotationAngle || 0;
+    this.screenX = touchInitDict.screenX || 0;
+    this.screenY = touchInitDict.screenY || 0;
+    this.touchType = touchInitDict.touchType || "direct";
+  }
+}
