a42b905ae058bbf8c2b29583b88c2357ab7e9f42
Vincent Schippefilt <vsc@odoo.com>
2020-07-09 13:52:03 +0200

    [FIX] renderer: Delete the last row or columns
    if it contains the active cell causes a crash.
    
    closes : #442

--- src/components/grid.ts
+++ src/components/grid.ts
@@ -348,23 +348,19 @@ export class Grid extends Component<{ model: Model }, SpreadsheetEnv> {
     this.snappedViewport = this.getters.snapViewportToCell(this.viewport);
   }
 
-  checkChanges(): { sheetChanged: boolean; positionChanged: boolean } {
+  checkChanges(): boolean {
     const [col, row] = this.getters.getPosition();
     const [curCol, curRow] = this.currentPosition;
     const currentSheet = this.getters.getActiveSheet();
-    const changes = {
-      sheetChanged: currentSheet !== this.currentSheet,
-      positionChanged: col !== curCol || row !== curRow,
-    };
-    if (changes.positionChanged) {
+    const changed = currentSheet !== this.currentSheet || col !== curCol || row !== curRow
+    if (changed) {
       this.currentPosition = [col, row];
     }
-    if (changes.sheetChanged) {
-      changes.positionChanged = true;
+    if (currentSheet !== this.currentSheet) {
       this.focus();
       this.currentSheet = currentSheet;
     }
-    return changes;
+    return changed;
   }
 
   getAutofillPosition() {
@@ -394,20 +390,15 @@ export class Grid extends Component<{ model: Model }, SpreadsheetEnv> {
     this.viewport.offsetX = this.hScrollbar.scroll;
     this.viewport.offsetY = this.vScrollbar.scroll;
 
-    const changes = this.checkChanges();
-
-    if (changes.sheetChanged) {
-      // needed to reset the bottom and the right on the current viewport to the one of the new active sheet
-      this.viewport = this.getters.adjustViewportZone(this.viewport);
-    }
+    // needed to reset the bottom and the right on the current viewport to the one of the new
+    // active sheet or in any case, the number of cols & rows might have changed.
+    this.viewport = this.getters.adjustViewportZone(this.viewport);
 
     // check for position changes
-    if (changes.positionChanged) {
+    if (this.checkChanges()) {
       this.viewport = this.getters.adjustViewportPosition(this.viewport);
       this.hScrollbar.scroll = this.viewport.offsetX;
       this.vScrollbar.scroll = this.viewport.offsetY;
-    } else {
-      this.viewport = this.getters.adjustViewportZone(this.viewport);
     }
     this.snappedViewport = this.getters.snapViewportToCell(this.snappedViewport);
 
