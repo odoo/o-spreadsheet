odoo/o-spreadsheet

commit 8f77582260ed4d81f42a2d1107f9a80ae52f6153

[FIX] conditional_format: Add rule to unactive sheet
Adding a conditional formatting rule to a sheet that was never activated crashes.

Initializing the cf data structure for the sheet should be done at sheet
creation, not at sheet activation.

However, plugins (except `core`) don't know the newly created sheet id when
the `CREATE_SHEET` command is dispatched.

This commit changes the command parameters. It now requires the id.
This change would have been required anyway when mutli users is implemented
where commands will be shared across multiple spreadsheet instances.

Co-authored-by: fleodoo <fle@odoo.com>

src/components/bottom_bar.ts
@@ -4,6 +4,7 @@ import { SpreadsheetEnv } from "../types";
 import { PLUS } from "./icons";
 import { MenuState, Menu } from "./menu";
 import { sheetMenuRegistry } from "../registries/index";
+import { uuidv4 } from "../helpers";
 const { Component } = owl;
 const { xml, css } = owl.tags;
 const { useState } = owl.hooks;
@@ -104,7 +105,7 @@ export class BottomBar extends Component<{}, SpreadsheetEnv> {
   menuState: MenuState = useState({ isOpen: false, position: null, menuItems: [] });
 
   addSheet() {
-    this.env.dispatch("CREATE_SHEET", { activate: true });
+    this.env.dispatch("CREATE_SHEET", { activate: true, id: uuidv4() });
   }
 
   activateSheet(name: string) {

src/plugins/conditional_format.ts
@@ -44,7 +44,10 @@ export class ConditionalFormatPlugin extends BasePlugin {
       case "ACTIVATE_SHEET":
         const activeSheet = cmd.to;
         this.computedStyles[activeSheet] = this.computedStyles[activeSheet] || {};
-        this.cfRules[activeSheet] = this.cfRules[activeSheet] || [];
+        this.isStale = true;
+        break;
+      case "CREATE_SHEET":
+        this.cfRules[cmd.id] = [];
         this.isStale = true;
         break;
       case "ADD_CONDITIONAL_FORMAT":

src/plugins/core.ts
@@ -12,7 +12,6 @@ import {
   sanitizeSheet,
   toCartesian,
   toXC,
-  uuidv4,
 } from "../helpers/index";
 import {
   Cell,
@@ -91,6 +90,7 @@ export class CorePlugin extends BasePlugin {
         break;
       case "CREATE_SHEET":
         const sheet = this.createSheet(
+          cmd.id,
           cmd.name || `Sheet${this.workbook.visibleSheets.length + 1}`,
           cmd.cols || 26,
           cmd.rows || 100
@@ -750,9 +750,9 @@ export class CorePlugin extends BasePlugin {
     this.history.updateSheet(_sheet, ["rows", row, "cells", col], cell);
   }
 
-  private createSheet(name: string, cols: number, rows: number): string {
+  private createSheet(id: string, name: string, cols: number, rows: number): string {
     const sheet: Sheet = {
-      id: uuidv4(),
+      id,
       name,
       cells: {},
       colNumber: cols,

src/registries/menus/menu_items_actions.ts
@@ -1,5 +1,5 @@
 import { Style, SpreadsheetEnv } from "../../types/index";
-import { numberToLetters } from "../../helpers/index";
+import { numberToLetters, uuidv4 } from "../../helpers/index";
 import { _lt } from "../../translation";
 
 //------------------------------------------------------------------------------
@@ -350,7 +350,7 @@ export const INSERT_COLUMNS_AFTER_ACTION = (env: SpreadsheetEnv) => {
 //------------------------------------------------------------------------------
 
 export const CREATE_SHEET_ACTION = (env: SpreadsheetEnv) => {
-  env.dispatch("CREATE_SHEET", { activate: true });
+  env.dispatch("CREATE_SHEET", { activate: true, id: uuidv4() });
 };
 
 //------------------------------------------------------------------------------

src/types/commands.ts
@@ -64,6 +64,7 @@ export interface ResizeRowsCommand extends BaseCommand {
  */
 export interface CreateSheetCommand extends BaseCommand {
   type: "CREATE_SHEET";
+  id: string;
   name?: string;
   cols?: number;
   rows?: number;

tests/components/bottom_bar_test.ts
@@ -1,9 +1,10 @@
 import { Component, hooks, tags } from "@odoo/owl";
 import { BottomBar } from "../../src/components/bottom_bar";
 import { Model } from "../../src/model";
-import { makeTestFixture, nextTick } from "../helpers";
+import { makeTestFixture, nextTick, mockUuidV4To } from "../helpers";
 import { triggerMouseEvent } from "../dom_helper";
 import { CommandResult } from "../../src/types";
+jest.mock("../../src/helpers/uuid", () => require("../__mocks__/uuid"));
 
 const { xml } = tags;
 const { useSubEnv } = hooks;
@@ -53,8 +54,9 @@ describe("BottomBar component", () => {
     const parent = new Parent(new Model());
     await parent.mount(fixture);
 
+    mockUuidV4To(42);
     triggerMouseEvent(".o-add-sheet", "click");
-    expect(parent.env.dispatch).toHaveBeenCalledWith("CREATE_SHEET", { activate: true });
+    expect(parent.env.dispatch).toHaveBeenCalledWith("CREATE_SHEET", { activate: true, id: "42" });
   });
 
   test("Can activate a sheet", async () => {

tests/menu_items_registry_test.ts
@@ -2,7 +2,8 @@ import { Model } from "../src";
 import { fontSizes } from "../src/fonts";
 import { FullMenuItem, topbarMenuRegistry } from "../src/registries/index";
 import { CommandResult, SpreadsheetEnv } from "../src/types";
-import { GridParent, makeTestFixture, nextTick } from "./helpers";
+import { GridParent, makeTestFixture, nextTick, mockUuidV4To } from "./helpers";
+jest.mock("../src/helpers/uuid", () => require("./__mocks__/uuid"));
 
 function getNode(_path: string[]): FullMenuItem {
   const path = [..._path];
@@ -386,8 +387,9 @@ describe("Menu Item actions", () => {
   });
 
   test("Insert -> new sheet", () => {
+    mockUuidV4To(42);
     doAction(["insert", "insert_sheet"], env);
-    expect(env.dispatch).toHaveBeenCalledWith("CREATE_SHEET", { activate: true });
+    expect(env.dispatch).toHaveBeenCalledWith("CREATE_SHEET", { activate: true, id: "42" });
   });
 
   describe("Format -> numbers", () => {

tests/plugins/clipboard_test.ts
@@ -86,7 +86,7 @@ describe("clipboard", () => {
     model.dispatch("SET_VALUE", { xc: "A1", text: "a1" });
     model.dispatch("CUT", { target: target("A1") });
     const to = model.getters.getActiveSheet();
-    model.dispatch("CREATE_SHEET", { activate: true });
+    model.dispatch("CREATE_SHEET", { activate: true, id: "42" });
     const from = model.getters.getActiveSheet();
     model.dispatch("SET_VALUE", { xc: "A1", text: "a1Sheet2" });
     model.dispatch("PASTE", { target: target("B2") });

commit 4ecf756f0c6ba575b32511f54bd96c2284dcc504

[FIX] bottom bar: fix wrong import
8f77582 broke the build

src/components/bottom_bar.ts
@@ -4,7 +4,7 @@ import { SpreadsheetEnv } from "../types";
 import { PLUS } from "./icons";
 import { MenuState, Menu } from "./menu";
 import { sheetMenuRegistry } from "../registries/index";
-import { uuidv4 } from "../helpers";
+import { uuidv4 } from "../helpers/index";
 const { Component } = owl;
 const { xml, css } = owl.tags;
 const { useState } = owl.hooks;