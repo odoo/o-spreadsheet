4c7e9bfa0b66d3a650f81a7b5e4bd689c31b3d35
Géry Debongnie <ged@odoo.com>
2020-08-12 15:56:55 +0200

    [FIX] make drag and drop more robust
    A pretty interesting issue came up while testing drag and dropping
    figures: sometimes, when clicking and moving the cursor quickly, the
    figure did not stop moving like expected.  The reason was that the
    mouseup event was not fired, even though we kind of expect it when
    we release the mouse button.
    
    The issue was that the browser interpreted the situation as an attempt
    to start a drag and drop operation.  In that case, it will not fire the
    mouseup event.
    
    But in our case, we know that when the startDnD helper is called, we do
    not want to use the native drag and drop operations, we want to use our
    own.  So, the solution is to simply disable the dragstart event, which
    makes the browser fire the mouseup event again and solve our issue.

--- src/components/helpers/drag_and_drop.ts
+++ src/components/helpers/drag_and_drop.ts
@@ -3,8 +3,15 @@ type EventFn = (ev: MouseEvent) => void;
 export function startDnd(onMouseMove: EventFn, onMouseUp: EventFn) {
   const _onMouseUp = (ev: MouseEvent) => {
     onMouseUp(ev);
+    window.removeEventListener("mouseup", _onMouseUp);
+    window.removeEventListener("dragstart", _onDragStart);
     window.removeEventListener("mousemove", onMouseMove);
   };
+  function _onDragStart(ev: DragEvent) {
+    ev.preventDefault();
+  }
+
+  window.addEventListener("mouseup", _onMouseUp);
+  window.addEventListener("dragstart", _onDragStart);
   window.addEventListener("mousemove", onMouseMove);
-  window.addEventListener("mouseup", _onMouseUp, { once: true });
 }
