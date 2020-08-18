b282cd5c26e4bb999ad45ef68f1b206b173a7fa3
Florent Lejoly <fle@odoo.com>
2020-08-18 12:06:44 +0200

    [FIX] border: adapt borders when merging/unmerging
    Currently when applying a border to a merge, the border was wrongly drawn.
    There were also issues with the borders when unmerging:
    only the topleft cell would keep the border.
    
    Closes #449
    
    Co-authored-by: fleodoo <fle@odoo.com>
    Co-authored-by: LucasLefevre <lul@odoo.com>

--- src/plugins/merge.ts
+++ src/plugins/merge.ts
@@ -245,6 +245,7 @@ export class MergePlugin extends BasePlugin {
         this.history.updateState(["sheets", sheetId, "mergeCellMap", xc], id);
       }
     }
+    this.applyBorderMerge(zone, sheetId);
     for (let m of previousMerges) {
       const { top, bottom, left, right } = sheet.merges[m];
       for (let r = top; r <= bottom; r++) {
@@ -261,6 +262,51 @@ export class MergePlugin extends BasePlugin {
         }
       }
       this.history.updateState(["sheets", sheetId, "merges", m], undefined);
+    }
+  }
+
+  private applyBorderMerge(zone: Zone, sheet: string) {
+    const { left, right, top, bottom } = zone;
+    const topLeft = this.getters.getCell(left, top);
+    const bottomRight = this.getters.getCell(right, bottom) || topLeft;
+    const bordersTopLeft = topLeft ? this.getters.getCellBorder(topLeft) : null;
+    const bordersBottomRight = bottomRight ? this.getters.getCellBorder(bottomRight) : null;
+    this.dispatch("SET_FORMATTING", {
+      sheet,
+      target: [{ left, right, top, bottom }],
+      border: "clear",
+    });
+    if (bordersBottomRight && bordersBottomRight.right) {
+      const zone = [{ left: right, right, top, bottom }];
+      this.dispatch("SET_FORMATTING", {
+        sheet,
+        target: zone,
+        border: "right",
+      });
+    }
+    if (bordersTopLeft && bordersTopLeft.left) {
+      const zone = [{ left, right: left, top, bottom }];
+      this.dispatch("SET_FORMATTING", {
+        sheet,
+        target: zone,
+        border: "left",
+      });
+    }
+    if (bordersTopLeft && bordersTopLeft.top) {
+      const zone = [{ left, right, top, bottom: top }];
+      this.dispatch("SET_FORMATTING", {
+        sheet,
+        target: zone,
+        border: "top",
+      });
+    }
+    if (bordersBottomRight && bordersBottomRight.bottom) {
+      const zone = [{ left, right, top: bottom, bottom }];
+      this.dispatch("SET_FORMATTING", {
+        sheet,
+        target: zone,
+        border: "bottom",
+      });
     }
   }
 
