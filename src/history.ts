f515cfe96bbfcf7d32d455e5d0ec4803621438a6
Pierre Rousseau <pro@odoo.com>
2020-09-29 08:11:30 +0200

    [REF] history: move specific code for active sheet to core plugin

--- src/history.ts
+++ src/history.ts
@@ -74,26 +74,13 @@ export class WHistory implements WorkbookHistoryNonLocal, CommandHandler {
 
   finalize() {
     if (this.current && this.current.length) {
-      if (!this.isActiveSheet()) {
-        // We do not want to save the ACTIVATE_SHEET command if triggered
-        // standalone
-        this.undoStack.push(this.current);
-      }
+      this.undoStack.push(this.current);
       this.redoStack = [];
       this.current = null;
       if (this.undoStack.length > MAX_HISTORY_STEPS) {
         this.undoStack.shift();
       }
     }
-  }
-
-  isActiveSheet() {
-    return (
-      this.current &&
-      this.current.length === 1 &&
-      this.current[0].path.length === 1 &&
-      this.current[0].path[0] === "activeSheet"
-    );
   }
 
   undo() {
