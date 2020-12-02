73f17a28f3cab17a0dba23eaf1b869f0347d9c62
Pierre Rousseau <pro@odoo.com>
2020-12-02 12:02:59 +0100

    [FIX] history: create empty structure on redo
    Co-authored-by: LucasLefevre <lul@odoo.com>

--- src/history.ts
+++ src/history.ts
@@ -166,10 +166,29 @@ export class WHistory implements CommandHandler {
     }
   }
 
+  /**
+   * Create an empty structure according to the type of the node key:
+   * string: object
+   * number: array
+   */
+  private createEmptyStructure(node: string | number | any) {
+    if (typeof node === "string") {
+      return {};
+    } else if (typeof node === "number") {
+      return [];
+    }
+    throw new Error(`Cannot create new node`);
+  }
+
   private applyChange(change: HistoryChange, target: "before" | "after") {
     let val = change.root as any;
     let key = change.path[change.path.length - 1];
-    for (let p of change.path.slice(0, -1)) {
+    for (let pathIndex = 0; pathIndex < change.path.slice(0, -1).length; pathIndex++) {
+      const p = change.path[pathIndex];
+      if (val[p] === undefined) {
+        const nextPath = change.path[pathIndex + 1];
+        val[p] = this.createEmptyStructure(nextPath);
+      }
       val = val[p];
     }
     if (change[target] === undefined) {
@@ -188,13 +207,7 @@ export class WHistory implements CommandHandler {
       const p = path[pathIndex];
       if (value[p] === undefined) {
         const nextPath = path[pathIndex + 1];
-        if (typeof nextPath === "string") {
-          value[p] = {};
-        } else if (typeof nextPath === "number") {
-          value[p] = [];
-        } else {
-          throw new Error(`Cannot create new path: ${path}`);
-        }
+        value[p] = this.createEmptyStructure(nextPath);
       }
       value = value[p];
     }
