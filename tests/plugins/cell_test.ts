2cee787df25068a3511331df2df8765db0c4c800
Vincent Schippefilt <vsc@odoo.com>
2021-01-08 10:57:58 +0100

    [FIX] core: handle undefined as value in getCellText
    OPW-2376306
    FWP of #681

---
+++ tests/plugins/cell_test.ts
@@ -0,0 +1,26 @@
+import { Model } from "../../src";
+import { CellType } from "../../src/types";
+import "../canvas.mock";
+
+describe("getCellText", () => {
+  test.each([
+    [undefined, ""],
+    [{ hello: 1 }, "0"],
+    [{ hello: 1, toString: () => "hello" }, "0"],
+    [null, "0"],
+  ])("getCellText of cell with %j value", (a, expected) => {
+    const model = new Model();
+    const sheetId = model.getters.getActiveSheetId();
+    expect(
+      model.getters.getCellText(
+        {
+          id: "42",
+          value: a,
+          type: CellType.text,
+          content: "text",
+        },
+        sheetId
+      )
+    ).toBe(expected);
+  });
+});
