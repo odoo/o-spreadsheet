c0421dd52afccb3bdf0600ad2f8a2cb076ae14d3
Pierre Rousseau <pro@odoo.com>
2021-02-17 15:50:00 +0100

    [MOV] sheet: move helpers to dedicated file

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -3,5 +3,6 @@ export * from "./coordinates";
 export * from "./grid_manipulation";
 export * from "./misc";
 export * from "./numbers";
+export * from "./sheet";
 export * from "./uuid";
 export * from "./zones";
---
+++ src/helpers/sheet.ts
@@ -0,0 +1,94 @@
+import { numberToLetters } from ".";
+import { DEFAULT_CELL_HEIGHT, DEFAULT_CELL_WIDTH } from "../constants";
+import { Col, HeaderData, Row } from "../types";
+
+export function createDefaultCols(colNumber: number): Col[] {
+  const cols: Col[] = [];
+  let current = 0;
+  for (let i = 0; i < colNumber; i++) {
+    const size = DEFAULT_CELL_WIDTH;
+    const col = {
+      start: current,
+      end: current + size,
+      size: size,
+      name: numberToLetters(i),
+    };
+    cols.push(col);
+    current = col.end;
+  }
+  return cols;
+}
+
+export function createDefaultRows(rowNumber: number): Row[] {
+  const rows: Row[] = [];
+  let current = 0;
+  for (let i = 0; i < rowNumber; i++) {
+    const size = DEFAULT_CELL_HEIGHT;
+    const row = {
+      start: current,
+      end: current + size,
+      size: size,
+      name: String(i + 1),
+      cells: {},
+    };
+    rows.push(row);
+    current = row.end;
+  }
+  return rows;
+}
+
+export function createCols(savedCols: { [key: number]: HeaderData }, colNumber: number): Col[] {
+  const cols: Col[] = [];
+  let current = 0;
+  for (let i = 0; i < colNumber; i++) {
+    const size = savedCols[i] ? savedCols[i].size || DEFAULT_CELL_WIDTH : DEFAULT_CELL_WIDTH;
+    const col = {
+      start: current,
+      end: current + size,
+      size: size,
+      name: numberToLetters(i),
+    };
+    cols.push(col);
+    current = col.end;
+  }
+  return cols;
+}
+
+export function createRows(savedRows: { [key: number]: HeaderData }, rowNumber: number): Row[] {
+  const rows: Row[] = [];
+  let current = 0;
+  for (let i = 0; i < rowNumber; i++) {
+    const size = savedRows[i] ? savedRows[i].size || DEFAULT_CELL_HEIGHT : DEFAULT_CELL_HEIGHT;
+    const row = {
+      start: current,
+      end: current + size,
+      size: size,
+      name: String(i + 1),
+      cells: {},
+    };
+    rows.push(row);
+    current = row.end;
+  }
+  return rows;
+}
+export function exportCols(cols: Col[]): { [key: number]: HeaderData } {
+  const exportedCols: { [key: number]: HeaderData } = {};
+  for (let i in cols) {
+    const col = cols[i];
+    if (col.size !== DEFAULT_CELL_WIDTH) {
+      exportedCols[i] = { size: col.size };
+    }
+  }
+  return exportedCols;
+}
+
+export function exportRows(rows: Row[]): { [key: number]: HeaderData } {
+  const exportedRows: { [key: number]: HeaderData } = {};
+  for (let i in rows) {
+    const row = rows[i];
+    if (row.size !== DEFAULT_CELL_HEIGHT) {
+      exportedRows[i] = { size: row.size };
+    }
+  }
+  return exportedRows;
+}
