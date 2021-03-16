29699286cc7f6b4bf0e7d3c3aea1be1bc17f2196
Pierre Rousseau <pro@odoo.com>
2021-03-16 14:41:38 +0100

    [FIX] cell: do not override format when specified
    Before this commit, a update cell with a content tried to compute the
    format, even if the format was given in the command

--- tests/plugins/cell_test.ts
+++ tests/plugins/cell_test.ts
@@ -1,6 +1,7 @@
 import { Model } from "../../src";
 import { CellType } from "../../src/types";
 import "../canvas.mock";
+import { getCell } from "../helpers";
 
 describe("getCellText", () => {
   test.each([
@@ -22,5 +23,34 @@ describe("getCellText", () => {
         sheetId
       )
     ).toBe(expected);
+  });
+
+  test("Update cell with a format is correctly set", () => {
+    const model = new Model();
+    const sheetId = model.getters.getActiveSheetId();
+    model.dispatch("UPDATE_CELL", {
+      sheetId,
+      col: 0,
+      row: 0,
+      content: "5%",
+      format: "bla",
+    });
+    model.dispatch("UPDATE_CELL", {
+      sheetId,
+      col: 1,
+      row: 1,
+      content: "12/30/1899",
+      format: "bla",
+    });
+    model.dispatch("UPDATE_CELL", {
+      sheetId,
+      col: 2,
+      row: 2,
+      content: "=DATE(2021,1,1)",
+      format: "bla",
+    });
+    expect(getCell(model, "A1")?.format).toBe("bla");
+    expect(getCell(model, "B2")?.format).toBe("bla");
+    expect(getCell(model, "C3")?.format).toBe("bla");
   });
 });
