4d9e58f39f42e0275e54562bbc8eb443271c3596
Lucas Lefèvre <lul@odoo.com>
2021-04-22 10:12:59 +0200

    [IMP] helpers: add a step parameter to the range helper
    This commit adds a third parameter to the `range` helper which specifies the
    incremental step (defaults to 1).
    
    It can be usefull to generate a sequence in the reverse order with a step of -1
    
    The API is similar to the python `range` API.
    
    https://docs.python.org/3.3/library/stdtypes.html?highlight=range#ranges

--- src/helpers/misc.ts
+++ src/helpers/misc.ts
@@ -49,16 +49,21 @@ export function computeTextWidth(context: CanvasRenderingContext2D, text: string
 }
 
 /**
- * Create a range from start (included) to end (excluded)
+ * Create a range from start (included) to end (excluded).
  * range(10, 13) => [10, 11, 12]
+ * range(2, 8, 2) => [2, 4, 6]
  */
-export function range(start: number, end: number) {
-  if (end <= start) {
+export function range(start: number, end: number, step = 1) {
+  if (end <= start && step > 0) {
     return [];
   }
-  const array: number[] = Array(end - start);
-  for (let i = 0; i < end - start; i++) {
-    array[i] = start + i;
+  if (step === 0) {
+    throw new Error("range() step must not be zero");
+  }
+  const length = Math.ceil(Math.abs((end - start) / step));
+  const array: number[] = Array(length);
+  for (let i = 0; i < length; i++) {
+    array[i] = start + i * step;
   }
   return array;
 }
--- tests/helpers/misc.test.ts
+++ tests/helpers/misc.test.ts
@@ -4,9 +4,19 @@ describe("Misc", () => {
   test("range", () => {
     expect(range(0, 1)).toEqual([0]);
     expect(range(0, 0)).toEqual([]);
+    expect(range(1, 1)).toEqual([]);
     expect(range(10, 1)).toEqual([]);
     expect(range(10, 13)).toEqual([10, 11, 12]);
     expect(range(-2, 2)).toEqual([-2, -1, 0, 1]);
+    expect(range(2, -2, -1)).toEqual([2, 1, 0, -1]);
+    expect(range(-2, 3, 2)).toEqual([-2, 0, 2]);
+    expect(range(-2, 4, 2)).toEqual([-2, 0, 2]);
+    expect(range(-2, 5, 2)).toEqual([-2, 0, 2, 4]);
+    expect(range(2, -3, -2)).toEqual([2, 0, -2]);
+    expect(range(2, -4, -2)).toEqual([2, 0, -2]);
+    expect(range(2, -5, -2)).toEqual([2, 0, -2, -4]);
+    expect(range(10, 0, 1)).toEqual([]);
+    expect(() => range(0, 10, 0)).toThrow();
   });
 
   test("groupConsecutive", () => {
