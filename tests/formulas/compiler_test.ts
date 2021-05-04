2ecf82526ac92c09e9884f22eb71fa78b4503f07
Alexis Lacroix <laa@odoo.com>
2021-05-04 20:53:24 +0200

    [FIX] compiler: add missing tests for function arguments compilation

--- tests/formulas/compiler_test.ts
+++ tests/formulas/compiler_test.ts
@@ -1,5 +1,7 @@
 import { compile } from "../../src/formulas";
 import { functionCache } from "../../src/formulas/compiler";
+import { functionRegistry } from "../../src/functions/index";
+import { evaluateCell } from "../helpers";
 
 function compiledBaseFunction(formula: string): string {
   for (let f in functionCache) {
@@ -75,5 +77,118 @@ describe("expression compiler", () => {
     expect(() => compiledBaseFunction("=sum(1)")).not.toThrowError();
     expect(() => compiledBaseFunction("=sum(1,2)")).not.toThrowError();
     expect(() => compiledBaseFunction("=sum(1,2,3)")).not.toThrowError();
+  });
+});
+
+describe("compile functions", () => {
+  describe("check number of arguments", () => {
+    test("with basic arguments", () => {
+      functionRegistry.add("ANYFUNCTION", {
+        description: "any function",
+        compute: () => {
+          return true;
+        },
+        args: [
+          { name: "arg1", description: "", type: ["ANY"] },
+          { name: "arg2", description: "", type: ["ANY"] },
+        ],
+        returns: ["ANY"],
+      });
+      expect(() => compiledBaseFunction("=ANYFUNCTION()")).toThrow();
+      expect(() => compiledBaseFunction("=ANYFUNCTION(1)")).toThrow();
+      expect(() => compiledBaseFunction("=ANYFUNCTION(1,2)")).not.toThrow();
+      expect(() => compiledBaseFunction("=ANYFUNCTION(1,2,3)")).toThrow();
+    });
+
+    test("with optional argument", () => {
+      functionRegistry.add("OPTIONAL", {
+        description: "function with optional argument",
+        compute: (arg) => {
+          return true;
+        },
+        args: [
+          { name: "arg1", description: "", type: ["ANY"] },
+          { name: "arg2", description: "", type: ["ANY"], optional: true },
+        ],
+        returns: ["ANY"],
+      });
+      expect(() => compiledBaseFunction("=OPTIONAL(1)")).not.toThrow();
+      expect(() => compiledBaseFunction("=OPTIONAL(1,2)")).not.toThrow();
+      expect(() => compiledBaseFunction("=OPTIONAL(1,2,3)")).toThrow();
+    });
+
+    test("with repeatable argument", () => {
+      functionRegistry.add("REPEATABLE", {
+        description: "function with repeatable argument",
+        compute: (arg) => {
+          return true;
+        },
+        args: [
+          { name: "arg1", description: "", type: ["ANY"] },
+          { name: "arg2", description: "", type: ["ANY"], optional: true, repeating: true },
+        ],
+        returns: ["ANY"],
+      });
+      expect(() => compiledBaseFunction("=REPEATABLE(1)")).not.toThrow();
+      expect(() => compiledBaseFunction("=REPEATABLE(1,2)")).not.toThrow();
+      expect(() => compiledBaseFunction("=REPEATABLE(1,2,3,4,5,6)")).not.toThrow();
+    });
+  });
+
+  describe("with lazy arguments", () => {
+    // this tests performs controls inside formula functions. For this reason, we
+    // don't use mocked functions. Errors would be caught during evaluation of
+    // formulas and not during the tests. So here we use a simple counter
+
+    let count = 0;
+
+    beforeAll(() => {
+      functionRegistry.add("ANYFUNCTION", {
+        description: "any function",
+        compute: () => {
+          count += 1;
+          return true;
+        },
+        args: [],
+        returns: ["ANY"],
+      });
+
+      functionRegistry.add("USELAZYARG", {
+        description: "function with a lazy argument",
+        compute: (arg) => {
+          count *= 42;
+          return arg();
+        },
+        args: [{ name: "lazyArg", description: "", type: ["ANY"], lazy: true }],
+        returns: ["ANY"],
+      });
+
+      functionRegistry.add("NOTUSELAZYARG", {
+        description: "any function",
+        compute: (arg) => {
+          count *= 42;
+          return arg;
+        },
+        args: [{ name: "any", description: "", type: ["ANY"] }],
+        returns: ["ANY"],
+      });
+    });
+
+    test("with function as argument --> change the order in which functions are evaluated ", () => {
+      count = 0;
+      evaluateCell("A1", { A1: "=USELAZYARG(ANYFUNCTION())" });
+      expect(count).toBe(1);
+      count = 0;
+      evaluateCell("A2", { A2: "=NOTUSELAZYARG(ANYFUNCTION())" });
+      expect(count).toBe(42);
+    });
+
+    test.each(["=USELAZYARG(24)", "=USELAZYARG(1/0)"])(
+      "functions call requesting lazy parameters",
+      (formula) => {
+        const compiledFormula = compiledBaseFunction(formula);
+        expect(compiledFormula.toString()).toMatchSnapshot();
+      }
+    );
   });
 });
