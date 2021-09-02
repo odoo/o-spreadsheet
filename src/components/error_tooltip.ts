a1d6df5ea2563f6999aee7fdf0d557178bd96a48
Pierre Rousseau <pro@odoo.com>
2021-09-02 09:23:27 +0200

    [FIX] error_tooltip: adapt with popover
    task-id 2636118

--- src/components/error_tooltip.ts
+++ src/components/error_tooltip.ts
@@ -11,10 +11,8 @@ const TEMPLATE = xml/* xml */ `
 
 const CSS = css/* scss */ `
   .o-error-tooltip {
-    position: absolute;
     font-size: 13px;
     background-color: white;
-    box-shadow: 0 1px 4px 3px rgba(60, 64, 67, 0.15);
     border-left: 3px solid red;
     padding: 10px;
   }
