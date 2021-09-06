233642b632a4692a824d733fdff87a3479f9a9c1
Lucas Lefèvre <lul@odoo.com>
2021-09-06 09:47:31 +0200

    [FIX] menus: disable text selection
    The user should not be able to select the text of menus
    
    Task 2637279

--- src/components/menu.ts
+++ src/components/menu.ts
@@ -67,6 +67,7 @@ const CSS = css/* scss */ `
       white-space: nowrap;
       text-overflow: ellipsis;
       cursor: pointer;
+      user-select: none;
 
       &:hover {
         background-color: #ebebeb;
