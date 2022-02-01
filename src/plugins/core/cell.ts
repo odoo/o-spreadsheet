3f6e20cc7145e03024d9e88e197f391e9238882d
Lucas Lefèvre <lul@odoo.com>
2022-02-01 15:49:01 +0100

    [FIX] collaborative: export cell styles in the same order
    When exporting cells, the iteration order depends on key insertion order.
    In a collaborative context, it may happen that several users locally inserted
    some cell in a different order, resulting in a different iteration order.
    Since style ids are auto incremented ids, their value could be different.
    
    It doesn't make any difference in practice, but to keep things consistent and
    allow to compare exported data in tests this commit ensures all users export
    cells and styles in the same order.

--- src/plugins/core/cell.ts
+++ src/plugins/core/cell.ts
@@ -15,7 +15,6 @@ import {
   Cell,
   CellData,
   CellDependencies,
-  CellPosition,
   CellValueType,
   CommandResult,
   CoreCommand,
@@ -382,9 +381,12 @@ export class CellPlugin extends CorePlugin<CoreState> implements CoreState {
 
     for (let _sheet of data.sheets) {
       const cells: { [key: string]: CellData } = {};
-      for (let [cellId, cell] of Object.entries(this.cells[_sheet.id] || {})) {
-        let position: CellPosition = this.getters.getCellPosition(cellId);
-        let xc = toXC(position.col, position.row);
+      const positions = Object.keys(this.cells[_sheet.id] || {})
+        .map((cellId) => this.getters.getCellPosition(cellId))
+        .sort((a, b) => (a.col === b.col ? a.row - b.row : a.col - b.col));
+      for (const { col, row } of positions) {
+        const cell = this.getters.getCell(_sheet.id, col, row)!;
+        const xc = toXC(col, row);
 
         cells[xc] = {
           style: cell.style && getStyleId(cell.style),
