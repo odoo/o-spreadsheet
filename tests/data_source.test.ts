odoo/o-spreadsheet

commit 3eadb34b2133ec1c4f6c07e7814563f3cf0e344c

[FIX] evaluation: limit re-evaluations triggered by datasources
Currently, datasources are able to notify that they have updated their
data and each of these notification will trigger a full evaluation of
the current sheet.

Unfortunately, we encounter situations where the number of notifications
explodes.
E.g. in [1], each call to this function will trigger an evaluation. Said
evaluation could handle a `PIVOT.HEADER` formula, which will in turn
trigger an evaluation if the cache is not updated yet, etc ...

Concretely, if the triggers occur in batch, only the last one is relevant.
This commit therefore adds a debounce to the event 'data-loaded' triggered
by the dataSource registry, which will  in turn drastically reduce the number
of evaluations.

In case of [1], the number of evaluations is now equal to the number
of groupBys in the pivot.

[1]: https://github.com/odoo/enterprise/blob/15.0/documents_spreadsheet/static/src/js/o_spreadsheet/plugins/ui/pivot_structure_plugin.js#L84-L97

task 2752589

tests/data_source.test.ts
@@ -1,3 +1,4 @@
+import * as owl from "@odoo/owl";
 import { DataSource, UIPlugin } from "../src";
 import { DataSourceRegistry } from "../src/data_source";
 import { args, functionRegistry } from "../src/functions";
@@ -22,7 +23,7 @@ class StringDataSource extends DataSource<string, string> {
 class DataSourcePlugin extends UIPlugin {
   static getters = ["get"];
 
-  dataSource: DataSourceRegistry<string, string>;
+  dataSources: DataSourceRegistry<string, string>;
 
   constructor(
     getters: Getters,
@@ -31,15 +32,15 @@ class DataSourcePlugin extends UIPlugin {
     config: ModelConfig
   ) {
     super(getters, state, dispatch, config);
-    this.dataSource = config.dataSources;
+    this.dataSources = config.dataSources;
   }
 
   addDataSource(id: UID, dataSource: DataSource<string, string>) {
-    this.dataSource.add(id, dataSource);
+    this.dataSources.add(id, dataSource);
   }
 
   get(id: UID): DataSource<string, string> {
-    return this.dataSource.get(id);
+    return this.dataSources.get(id);
   }
 }
 
@@ -100,11 +101,17 @@ describe("DataSource", () => {
     expect(dataSourcePlugin.get("2").getSync()).toBe("data");
   });
 
-  test("Functions can rely on dataSource, and are evaluated as soon as the dataSource is ready", async () => {
-    dataSourcePlugin.addDataSource("1", new StringDataSource());
+  test("Functions can rely on dataSource, and are evaluated right after the dataSource is ready", async () => {
+    jest.useFakeTimers();
+    jest.spyOn(owl.browser, "setTimeout").mockImplementation(window.setTimeout.bind(window));
+    jest.spyOn(owl.browser, "clearTimeout").mockImplementation(window.clearTimeout.bind(window));
+    const stringDs = new StringDataSource();
+    dataSourcePlugin.addDataSource("1", stringDs);
     setCellContent(model, "A1", "=WAIT2(1)");
     expect(getCellContent(model, "A1")).toBe("LOADING...");
-    await model.waitForIdle();
+    // ready the dataSource
+    await stringDs.get();
+    jest.advanceTimersByTime(2);
     expect(getCellContent(model, "A1")).toBe("data");
   });
 });

src/data_source.ts
@@ -20,7 +20,11 @@ export class DataSourceRegistry<M, D> extends owl.core.EventBus {
    */
   add(key: string, value: DataSource<M, D>): DataSourceRegistry<M, D> {
     this.registry.add(key, value);
-    value.on("data-loaded", this, () => this.trigger("data-loaded", { id: key }));
+    const debouncedLoaded: Function = owl.utils.debounce(
+      () => this.trigger("data-loaded", { id: key }),
+      0
+    );
+    value.on("data-loaded", this, () => debouncedLoaded());
     value.loadMetadata();
     return this;
   }

commit 59a861146a4235f2cd500cd68735c7dd69876a5c

[FIX] evaluation: evaluate all sheets forces the evaluation
- In odoo, insert a pivot in a spreadsheet.
- From the Documents app kanban view, select the spreadsheet and
  click on the download button
=> all pivot formulas are "Loading..."

Bug introduced by 3eadb34
Before that commit, `await dataSources.waitForReady()` would wait "data
sources are loaded" which had the (synchronous) side effect of forcing an
evaluation.
Since 3eadb34, the side effect is no longer synchronous because the event
which triggers the evaluation is no longer triggered synchronously.

The command "EVALUATE_ALL_SHEETS" should evaluate a sheet even if it's
marked as "up to date".

src/plugins/ui/evaluation.ts
@@ -312,10 +312,8 @@ export class EvaluationPlugin extends UIPlugin {
    */
   private evaluateAllSheets() {
     for (const sheetId of this.getters.getVisibleSheets()) {
-      if (!this.isUpToDate.has(sheetId)) {
-        this.evaluate(sheetId);
-        this.isUpToDate.add(sheetId);
-      }
+      this.evaluate(sheetId);
+      this.isUpToDate.add(sheetId);
     }
   }
 }

tests/data_source.test.ts
@@ -114,4 +114,13 @@ describe("DataSource", () => {
     jest.advanceTimersByTime(2);
     expect(getCellContent(model, "A1")).toBe("data");
   });
+
+  test("evaluate all sheets forces evaluation", async () => {
+    dataSourcePlugin.addDataSource("1", new StringDataSource());
+    setCellContent(model, "A1", "=WAIT2(1)");
+    expect(getCellContent(model, "A1")).toBe("LOADING...");
+    await model.waitForIdle();
+    model.dispatch("EVALUATE_ALL_SHEETS");
+    expect(getCellContent(model, "A1")).toBe("data");
+  });
 });