odoo/o-spreadsheet

commit 39f674372c89b4d6e1f8dc5dcaeaee9a947c1bc7

[FIX] import: can import cells outside sheet size
We have a case where `rowNumber` is wrong in the exported json data.
It says there are 63 rows, but there are cells below that row: in A64, A65, ...
Importing those cells crashes and the spreadsheet can't be opened anymore.

With this commit, the import tries to save the day by checking the max col/row
of cells and use that number if it's it's bigger.

We don't know how the data was corrupted in the first place. We have a bug
lurking somewhere... but let's fix the customer's spreadsheet first

opw 2746389

closes odoo/o-spreadsheet#1176

X-original-commit: cf955d1aff72e053ec3d6cdb215c6e3f396aab78
Signed-off-by: Rémi Rahir (rar) <rar@odoo.com>
Signed-off-by: Lucas Lefèvre (lul) <lul@odoo.com>

src/plugins/core/sheet.ts
@@ -13,6 +13,7 @@ import {
   isZoneValid,
   numberToLetters,
   positions,
+  toCartesian,
 } from "../../helpers/index";
 import { _lt, _t } from "../../translation";
 import {
@@ -27,6 +28,7 @@ import {
   RenameSheetCommand,
   Row,
   Sheet,
+  SheetData,
   UID,
   UpdateCellPositionCommand,
   WorkbookData,
@@ -210,11 +212,12 @@ export class SheetPlugin extends CorePlugin<SheetState> implements SheetState {
 
     for (let sheetData of data.sheets) {
       const name = sheetData.name || _t("Sheet") + (Object.keys(this.sheets).length + 1);
+      const { colNumber, rowNumber } = this.getImportedSheetSize(sheetData);
       const sheet: Sheet = {
         id: sheetData.id,
         name: name,
-        cols: createCols(sheetData.cols || {}, sheetData.colNumber),
-        rows: createRows(sheetData.rows || {}, sheetData.rowNumber),
+        cols: createCols(sheetData.cols || {}, colNumber),
+        rows: createRows(sheetData.rows || {}, rowNumber),
         hiddenColsGroups: [],
         hiddenRowsGroups: [],
         areGridLinesVisible:
@@ -965,6 +968,14 @@ export class SheetPlugin extends CorePlugin<SheetState> implements SheetState {
       });
     }
     this.history.update("sheets", sheet.id, "rows", rows);
+  }
+
+  private getImportedSheetSize(data: SheetData): { rowNumber: number; colNumber: number } {
+    const positions = Object.keys(data.cells).map(toCartesian);
+    return {
+      rowNumber: Math.max(data.rowNumber, ...positions.map(([col, row]) => row + 1)),
+      colNumber: Math.max(data.colNumber, ...positions.map(([col, row]) => col + 1)),
+    };
   }
 
   // ----------------------------------------------------

tests/plugins/import_export.test.ts
@@ -6,6 +6,7 @@ import {
   DEFAULT_REVISION_ID,
   FORBIDDEN_SHEET_CHARS,
 } from "../../src/constants";
+import { toCartesian } from "../../src/helpers";
 import { CURRENT_VERSION } from "../../src/migrations/data";
 import { Model } from "../../src/model";
 import { corePluginRegistry } from "../../src/plugins";
@@ -573,6 +574,29 @@ test("complete import, then export", () => {
   // We test here a that two import with the same data give the same result.
   const model2 = new Model(modelData);
   expect(model2.exportData()).toEqual(modelData);
+});
+
+test("can import cells outside sheet size", () => {
+  const sheetId = "someuuid";
+  const modelData = {
+    version: CURRENT_VERSION,
+    sheets: [
+      {
+        id: sheetId,
+        colNumber: 10,
+        rowNumber: 10,
+        cols: {},
+        rows: {},
+        cells: {
+          Z100: { content: "hello" },
+        },
+      },
+    ],
+  };
+  const model = new Model(modelData);
+  expect(model.getters.getNumberRows(sheetId)).toBe(100);
+  expect(model.getters.getNumberCols(sheetId)).toBe(26);
+  expect(model.getters.getCell(sheetId, ...toCartesian("Z100"))?.content).toBe("hello");
 });
 
 test("Data of a duplicate sheet are correctly duplicated", () => {

commit e7188fc79d4b08c82c87003f65f080eea9da2320

[FIX] import: can import sheets with more than 125k cells
Bug introduced by 39f6743

There's a limit to the number of function arguments. In Chrome, it's a bit more
than 125k.

Since the mentioned commit, loading a spreadsheet with more than 125k cells
crashes with `RangeError: Maximum call stack size exceeded`

This commit mitigate the issue. Now the limit before a crash is
125k columns or 125k rows instead of 125k cells.

src/plugins/core/sheet.ts
@@ -945,8 +945,8 @@ export class SheetPlugin extends CorePlugin<SheetState> implements SheetState {
   private getImportedSheetSize(data: SheetData): { rowNumber: number; colNumber: number } {
     const positions = Object.keys(data.cells).map(toCartesian);
     return {
-      rowNumber: Math.max(data.rowNumber, ...positions.map(([col, row]) => row + 1)),
-      colNumber: Math.max(data.colNumber, ...positions.map(([col, row]) => col + 1)),
+      rowNumber: Math.max(data.rowNumber, ...new Set(positions.map(([col, row]) => row + 1))),
+      colNumber: Math.max(data.colNumber, ...new Set(positions.map(([col, row]) => col + 1))),
     };
   }
 