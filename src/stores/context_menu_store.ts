1f9a1eeb7b7eef630ce2e0f0c3de32532e62c134
Lucas Lefèvre <lul@odoo.com>
2022-04-06 14:44:53 +0200

    storification

--- src/stores/context_menu_store.ts
+++ src/stores/context_menu_store.ts
@@ -1,4 +1,5 @@
-import { FullMenuItem } from "../registries";
+import { reactive } from "@odoo/owl";
+import { FullMenuItem, MenuItem } from "../registries";
 import { DOMCoordinates } from "../types";
 
 interface MenuActionHandlers {
@@ -6,19 +7,79 @@ interface MenuActionHandlers {
   onMenuClicked?: (menuActionResult: any) => void;
 }
 
-// class ContextMenuState {
-//   menuItems: FullMenuItem[] = [];
-//   position: DOMCoordinates = { x: 0, y: 0 };
+class MenuActions {
+  constructor(private state: MenuInternalState) {}
 
-//   get isOpen(): boolean {
-//     return this.menuItems.length !== 0;
-//   }
-// }
+  open(menuItems) {
+    this.state.menuItems = menuItems;
+  }
+
+  openSubMenu(menuItems) {
+    if (this.state.subMenu === undefined) {
+      this.state.subMenu = MenuStore();
+    }
+    this.state.subMenu.notify.open(menuItems);
+  }
+}
+
+interface OpenedMenu {
+  isOpen: true;
+  menuItems: MenuItem[];
+  subMenu: Menu;
+}
+
+interface ClosedMenu {
+  isOpen: false;
+}
+
+type Menu = OpenedMenu | ClosedMenu;
+
+interface MenuInternalState {
+  menuItems: any[];
+  subMenu?: Store<Menu, MenuActions>;
+}
+
+type Store<State, Actions> = {
+  state: State;
+  notify: Actions;
+};
+
+const MenuStore: () => Store<Menu, MenuActions> = () =>
+  store<MenuInternalState, Menu>(
+    {
+      menuItems: [],
+    },
+    MenuActions,
+    (state) => {
+      if (state.menuItems.length === 0) {
+        return { isOpen: false };
+      }
+      return {
+        isOpen: true,
+        menuItems: [],
+        subMenu: state.subMenu ? state.subMenu.state : { isOpen: false },
+      };
+    }
+  );
+
+function store<S, C>(state: S, actionsConstructor, computePublicState: (state: S) => C) {
+  let computedState = computePublicState(state);
+  const reactiveState = reactive(state as Object, () => {
+    computedState = computePublicState(state);
+  });
+  const actions = new actionsConstructor(reactiveState);
+  return {
+    get state() {
+      return computedState;
+    },
+    notify: actions,
+  };
+}
 
 export class ContextMenu {
   menuItems: FullMenuItem[] = [];
   position: DOMCoordinates = { x: 0, y: 0 };
-  subMenu: ContextMenu = new ContextMenu(); // no, it breaks all good design properties
+  subMenu: ContextMenu = new ContextMenu(); // no, it breaks all good design properties => should be intrinsic to the store
   scrollOffset: number = 0;
 
   get isOpen(): boolean {
