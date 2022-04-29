2b12ba5d95a50b90619d05d609362f3ad077baa8
Rémi Rahir <rar@odoo.com>
2022-04-29 15:32:41 +0200

    [FIX] config: update rollup path
    The fix introduced in 457fe704 did not account for the updated path of
    the files compiled by `tsc`.

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import { version } from "./package.json";
 import git from "git-rev-sync";
 
 export default {
-  input: "dist/js/src/index.js",
+  input: "dist/js/index.js",
   external: ["@odoo/owl"],
   output: {
     file: "dist/o_spreadsheet.js",
