odoo/o-spreadsheet

commit a709ca182d3cb856ea81bbebf45909464a2121e6

[IMP] spreadsheet: introduce dashboard mode
This commit introduces a new mode of o-spreadsheet: `dashboard`.
This mode is an extension of the behavior of `readonly`, with some
addition, notably:
* Headers are not displayed
* Selection is disabled
* Menu item not available are hidden
* Grid lines are hidden

Task-id 2799599

closes odoo/o-spreadsheet#1260

Signed-off-by: Rémi Rahir (rar) <rar@odoo.com>
Co-authored-by: Lucas Lefèvre <lul@odoo.com>

src/components/figures/container/container.ts
@@ -1,5 +1,5 @@
 import { Component, onMounted, useState } from "@odoo/owl";
-import { HEADER_HEIGHT, HEADER_WIDTH, SELECTION_BORDER_COLOR } from "../../../constants";
+import { SELECTION_BORDER_COLOR } from "../../../constants";
 import { figureRegistry } from "../../../registries/index";
 import { Figure, SpreadsheetChildEnv } from "../../../types/index";
 import { css } from "../../helpers/css";
@@ -134,23 +134,17 @@ export class FiguresContainer extends Component<Props, SpreadsheetChildEnv> {
     const { offsetX, offsetY } = this.env.model.getters.getActiveSnappedViewport();
     const target = figure.id === (isSelected && this.dnd.figureId) ? this.dnd : figure;
     const { width, height } = target;
-    let x = target.x - offsetX + HEADER_WIDTH - 1;
-    let y = target.y - offsetY + HEADER_HEIGHT - 1;
-    // width and height of wrapper need to be adjusted so we do not overlap
-    // with headers
-    const correctionX = Math.max(0, HEADER_WIDTH - x);
-    x += correctionX;
-    const correctionY = Math.max(0, HEADER_HEIGHT - y);
-    y += correctionY;
+    let x = target.x - offsetX - 1;
+    let y = target.y - offsetY - 1;
 
     if (width < 0 || height < 0) {
       return `position:absolute;display:none;`;
     }
     const offset =
       ANCHOR_SIZE + ACTIVE_BORDER_WIDTH + (isSelected ? ACTIVE_BORDER_WIDTH : BORDER_WIDTH);
-    return `position:absolute; top:${y + 1}px; left:${x + 1}px; width:${
-      width - correctionX + offset
-    }px; height:${height - correctionY + offset}px`;
+    return `position:absolute; top:${y + 1}px; left:${x + 1}px; width:${width + offset}px; height:${
+      height + offset
+    }px`;
   }
 
   setup() {
@@ -210,7 +204,7 @@ export class FiguresContainer extends Component<Props, SpreadsheetChildEnv> {
   }
 
   onMouseDown(figure: Figure, ev: MouseEvent) {
-    if (ev.button > 0) {
+    if (ev.button > 0 || this.env.model.getters.isReadonly()) {
       // not main button, probably a context menu
       return;
     }

tests/components/figure.test.ts
@@ -191,7 +191,7 @@ describe("figures", () => {
   test("Cannot select/move figure in readonly mode", async () => {
     const figureId = "someuuid";
     createFigure(model, { id: figureId, y: 200 });
-    model.updateReadOnly(true);
+    model.updateMode("readonly");
     await nextTick();
     const figure = fixture.querySelector(".o-figure")!;
     await simulateClick(".o-figure");

demo/main.js
@@ -14,7 +14,7 @@ const {
 } = owl;
 
 const { Spreadsheet, Model } = o_spreadsheet;
-const { topbarMenuRegistry, currenciesRegistry } = o_spreadsheet.registries;
+const { topbarMenuRegistry, dashboardMenuRegistry } = o_spreadsheet.registries;
 
 const uuidGenerator = new o_spreadsheet.helpers.UuidGenerator();
 
@@ -55,18 +55,28 @@ class Demo extends Component {
     topbarMenuRegistry.addChild("readonly", ["file"], {
       name: "Open in read-only",
       sequence: 11,
-      action: async (env) => {
-        this.model.updateReadOnly(true);
-      },
+      action: () => this.model.updateMode("readonly"),
+    });
+
+    topbarMenuRegistry.addChild("dashboard", ["file"], {
+      name: "Open in dashboard",
+      sequence: 12,
+      isReadonlyAllowed: true,
+      action: () => this.model.updateMode("dashboard"),
     });
 
     topbarMenuRegistry.addChild("read_write", ["file"], {
       name: "Open with write access",
-      sequence: 12,
+      sequence: 13,
       isReadonlyAllowed: true,
-      action: async (env) => {
-        this.model.updateReadOnly(false);
-      },
+      action: () => this.model.updateMode("normal"),
+    });
+
+    dashboardMenuRegistry.add("open normal", {
+      name: "Normal mode",
+      sequence: 10,
+      isReadonlyAllowed: true,
+      action: () => this.model.updateMode("normal"),
     });
 
     useSubEnv({
@@ -108,7 +118,7 @@ class Demo extends Component {
         evalContext: { env: this.env },
         transportService: this.transportService,
         client: this.client,
-        isReadonly: false,
+        mode: "normal",
       },
       this.stateUpdateMessages
     );

src/components/figures/container/container.xml
@@ -5,7 +5,7 @@
         <div
           class="o-figure-wrapper"
           t-att-style="getStyle(info)"
-          t-on-mousedown="(ev) => this.onMouseDown(info.figure, ev)">
+          t-on-mousedown.stop="(ev) => this.onMouseDown(info.figure, ev)">
           <div
             class="o-figure"
             t-att-class="{active: info.isSelected, 'o-dragging': info.id === dnd.figureId}"

src/components/grid/grid.ts
@@ -27,6 +27,7 @@ import { interactivePaste } from "../../helpers/ui/paste";
 import { ComposerSelection } from "../../plugins/ui/edition";
 import { cellMenuRegistry } from "../../registries/menus/cell_menu_registry";
 import { colMenuRegistry } from "../../registries/menus/col_menu_registry";
+import { dashboardMenuRegistry } from "../../registries/menus/dashboard_menu_registry";
 import { rowMenuRegistry } from "../../registries/menus/row_menu_registry";
 import {
   CellValueType,
@@ -64,12 +65,13 @@ import { ScrollBar } from "../scrollbar";
  * - a vertical resizer (same, for rows)
  */
 
-export type ContextMenuType = "ROW" | "COL" | "CELL";
+export type ContextMenuType = "ROW" | "COL" | "CELL" | "DASHBOARD";
 
 const registries = {
   ROW: rowMenuRegistry,
   COL: colMenuRegistry,
   CELL: cellMenuRegistry,
+  DASHBOARD: dashboardMenuRegistry,
 };
 
 // copy and paste are specific events that should not be managed by the keydown event,
@@ -207,7 +209,6 @@ css/* scss */ `
       z-index: 2;
       &.vertical {
         right: 0;
-        top: ${HEADER_HEIGHT}px;
         bottom: ${SCROLLBAR_WIDTH}px;
         width: ${SCROLLBAR_WIDTH}px;
         overflow-x: hidden;
@@ -216,17 +217,12 @@ css/* scss */ `
         bottom: 0;
         height: ${SCROLLBAR_WIDTH}px;
         right: ${SCROLLBAR_WIDTH + 1}px;
-        left: ${HEADER_WIDTH}px;
         overflow-y: hidden;
       }
     }
 
     .o-grid-overlay {
       position: absolute;
-      top: ${HEADER_HEIGHT}px;
-      left: ${HEADER_WIDTH}px;
-      height: calc(100% - ${HEADER_HEIGHT}px);
-      width: calc(100% - ${HEADER_WIDTH}px);
       outline: none;
     }
   }
@@ -327,6 +323,25 @@ export class Grid extends Component<Props, SpreadsheetChildEnv> {
     this.focus();
     this.resizeGrid();
     this.drawGrid();
+  }
+
+  get gridOverlayStyle() {
+    return `
+      top: ${this.env.isDashboard() ? 0 : HEADER_HEIGHT}px;
+      left: ${this.env.isDashboard() ? 0 : HEADER_WIDTH}px;
+      height: calc(100% - ${this.env.isDashboard() ? 0 : HEADER_HEIGHT}px);
+      width: calc(100% - ${this.env.isDashboard() ? 0 : HEADER_WIDTH}px);
+    `;
+  }
+
+  get vScrollbarStyle() {
+    return `
+      top: ${this.env.isDashboard() ? 0 : HEADER_HEIGHT}px;`;
+  }
+
+  get hScrollbarStyle() {
+    return `
+      left: ${this.env.isDashboard() ? 0 : HEADER_WIDTH}px;`;
   }
 
   get errorTooltip() {
@@ -544,8 +559,8 @@ export class Grid extends Component<Props, SpreadsheetChildEnv> {
       this.env.model.getters.getViewportDimensionWithHeaders();
     if (currentHeight != viewportHeight || currentWidth !== viewportWidth) {
       this.env.model.dispatch("RESIZE_VIEWPORT", {
-        height: currentHeight - HEADER_HEIGHT,
-        width: currentWidth - HEADER_WIDTH,
+        height: currentHeight - (this.env.isDashboard() ? 0 : HEADER_HEIGHT),
+        width: currentWidth - (this.env.isDashboard() ? 0 : HEADER_WIDTH),
       });
     }
   }
@@ -663,7 +678,7 @@ export class Grid extends Component<Props, SpreadsheetChildEnv> {
   }
 
   onMouseDown(ev: MouseEvent) {
-    if (ev.button > 0) {
+    if (ev.button > 0 || this.env.isDashboard()) {
       // not main button, probably a context menu
       return;
     }
@@ -832,6 +847,9 @@ export class Grid extends Component<Props, SpreadsheetChildEnv> {
   }
 
   onKeydown(ev: KeyboardEvent) {
+    if (this.env.isDashboard()) {
+      return;
+    }
     if (ev.key.startsWith("Arrow")) {
       this.processArrows(ev);
       return;
@@ -892,6 +910,9 @@ export class Grid extends Component<Props, SpreadsheetChildEnv> {
 
   toggleContextMenu(type: ContextMenuType, x: number, y: number) {
     this.closeLinkEditor();
+    if (this.env.model.getters.isDashboard()) {
+      type = "DASHBOARD";
+    }
     this.menuState.isOpen = true;
     this.menuState.position = { x, y };
     this.menuState.menuItems = registries[type]

src/components/grid/grid.xml
@@ -12,13 +12,22 @@
         t-ref="gridOverlay"
         tabindex="-1"
         class="o-grid-overlay"
+        t-att-style="gridOverlayStyle"
         t-on-mousedown="onMouseDown"
         t-on-dblclick="onDoubleClick"
-        t-on-contextmenu="onCanvasContextMenu"
+        t-on-contextmenu="onCanvasContextMenu">
+        <FiguresContainer
+          model="props.model"
+          sidePanelIsOpen="props.sidePanelIsOpen"
+          onFigureDeleted="() => this.focus()"
+        />
+      </div>
+      <HeadersOverlay
+        t-if="!env.isDashboard()"
+        onOpenContextMenu="(type, x, y) => this.toggleContextMenu(type, x, y)"
       />
-      <HeadersOverlay onOpenContextMenu="(type, x, y) => this.toggleContextMenu(type, x, y)"/>
 
-      <t t-if="env.model.getters.getEditionMode() !== 'inactive'">
+      <t t-if="env.model.getters.getEditionMode() !== 'inactive' and !env.isDashboard()">
         <GridComposer
           onComposerUnmounted="() => this.focus()"
           onComposerContentFocused="props.onComposerContentFocused"
@@ -27,6 +36,7 @@
       </t>
       <canvas t-ref="canvas"/>
       <t
+        t-if="!env.isDashboard()"
         t-foreach="env.model.getters.getClientsToDisplay()"
         t-as="client"
         t-key="getClientPositionKey(client)">
@@ -56,7 +66,7 @@
         <LinkDisplay cellPosition="activeCellPosition"/>
       </Popover>
       <Popover
-        t-if="props.linkEditorIsOpen"
+        t-if="props.linkEditorIsOpen and !env.isDashboard()"
         position="popoverPosition.position"
         flipHorizontalOffset="-popoverPosition.cellWidth"
         flipVerticalOffset="-popoverPosition.cellHeight"
@@ -67,13 +77,13 @@
           onLinkEditorClosed="props.onLinkEditorClosed"
         />
       </Popover>
-      <t t-if="env.model.getters.getEditionMode() === 'inactive'">
+      <t t-if="env.model.getters.getEditionMode() === 'inactive' and !env.isDashboard()">
         <Autofill
           position="getAutofillPosition()"
           getGridBoundingClientRect="() => this.getGridBoundingClientRect()"
         />
       </t>
-      <t t-if="env.model.getters.getEditionMode() !== 'inactive'">
+      <t t-if="env.model.getters.getEditionMode() !== 'inactive' and !env.isDashboard()">
         <t t-foreach="env.model.getters.getHighlights()" t-as="highlight" t-key="highlight_index">
           <t t-if="highlight.sheetId === env.model.getters.getActiveSheetId()">
             <Highlight zone="highlight.zone" color="highlight.color"/>
@@ -90,15 +100,18 @@
         t-set="gridSize"
         t-value="env.model.getters.getMaxViewportSize(env.model.getters.getActiveSheet())"
       />
-      <FiguresContainer
-        model="props.model"
-        sidePanelIsOpen="props.sidePanelIsOpen"
-        onFigureDeleted="() => this.focus()"
-      />
-      <div class="o-scrollbar vertical" t-on-scroll="onScroll" t-ref="vscrollbar">
+      <div
+        class="o-scrollbar vertical"
+        t-on-scroll="onScroll"
+        t-ref="vscrollbar"
+        t-att-style="vScrollbarStyle">
         <div t-attf-style="width:1px;height:{{gridSize.height}}px"/>
       </div>
-      <div class="o-scrollbar horizontal" t-on-scroll="onScroll" t-ref="hscrollbar">
+      <div
+        class="o-scrollbar horizontal"
+        t-on-scroll="onScroll"
+        t-ref="hscrollbar"
+        t-att-style="hScrollbarStyle">
         <div t-attf-style="height:1px;width:{{gridSize.width}}px"/>
       </div>
     </div>

src/components/menu/menu.ts
@@ -128,9 +128,13 @@ export class Menu extends Component<Props, SpreadsheetChildEnv> {
 
   get popover() {
     const isRoot = this.props.depth === 1;
+    let marginTop = 6;
+    if (this.env.isDashboard()) {
+      marginTop += TOPBAR_HEIGHT + HEADER_HEIGHT;
+    }
     return {
       // some margin between the header and the component
-      marginTop: HEADER_HEIGHT + 6 + TOPBAR_HEIGHT,
+      marginTop,
       flipHorizontalOffset: MENU_WIDTH * (this.props.depth - 1),
       flipVerticalOffset: isRoot ? 0 : MENU_ITEM_HEIGHT,
     };

src/components/menu/menu.xml
@@ -1,6 +1,7 @@
 <templates>
   <t t-name="o-spreadsheet.Menu" owl="1">
     <Popover
+      t-if="props.menuItems.length"
       position="props.position"
       childWidth="MENU_WIDTH"
       childHeight="menuHeight"

commit e171687bc2e7c6e74c34bcd48c3b7c81594d488e

[FIX] Figure container: Crop figures to avoid overlap with headers
The introduction of dashboard mode in a709ca18 mistakenly removed the
cropping of figures when they overlap on headers. This commit fixes that
issue.

Task 2889602

Co-authored-by: Anthony Hendrickx <anhe@odoo.com>

src/components/figures/container/container.ts
@@ -137,14 +137,20 @@ export class FiguresContainer extends Component<Props, SpreadsheetChildEnv> {
     let x = target.x - offsetX - 1;
     let y = target.y - offsetY - 1;
 
+    // width and height of wrapper need to be adjusted so we do not overlap
+    // with headers
+    const correctionX = this.env.isDashboard() ? 0 : Math.max(0, -x);
+    x += correctionX;
+    const correctionY = this.env.isDashboard() ? 0 : Math.max(0, -y);
+    y += correctionY;
     if (width < 0 || height < 0) {
       return `position:absolute;display:none;`;
     }
     const offset =
       ANCHOR_SIZE + ACTIVE_BORDER_WIDTH + (isSelected ? ACTIVE_BORDER_WIDTH : BORDER_WIDTH);
-    return `position:absolute; top:${y + 1}px; left:${x + 1}px; width:${width + offset}px; height:${
-      height + offset
-    }px`;
+    return `position:absolute; top:${y + 1}px; left:${x + 1}px; width:${
+      width - correctionX + offset
+    }px; height:${height - correctionY + offset}px`;
   }
 
   setup() {

tests/components/figure.test.ts
@@ -1,5 +1,6 @@
 import { App, Component, xml } from "@odoo/owl";
 import { Model, Spreadsheet } from "../../src";
+import { DEFAULT_CELL_HEIGHT, DEFAULT_CELL_WIDTH } from "../../src/constants";
 import { figureRegistry } from "../../src/registries";
 import { CreateFigureCommand, Figure, SpreadsheetChildEnv, UID } from "../../src/types";
 import {
@@ -201,5 +202,21 @@ describe("figures", () => {
     triggerMouseEvent(figure, "mousedown", 300, 200);
     await nextTick();
     expect(figure.classList).not.toContain("o-dragging");
+  });
+
+  test("Figures are cropped to avoid overlap with headers", async () => {
+    const figureId = "someuuid";
+    createFigure(model, { id: figureId, x: 100, y: 20, height: 200, width: 100 });
+    await nextTick();
+    const figure = fixture.querySelector(".o-figure-wrapper")!;
+    expect(window.getComputedStyle(figure).width).toBe("111px"); // width + borders
+    expect(window.getComputedStyle(figure).height).toBe("211px"); // height + borders
+    model.dispatch("SET_VIEWPORT_OFFSET", {
+      offsetX: 2 * DEFAULT_CELL_WIDTH,
+      offsetY: 3 * DEFAULT_CELL_HEIGHT,
+    });
+    await nextTick();
+    expect(window.getComputedStyle(figure).width).toBe("18px"); // width + borders - 2 * DEFAULT_CELL_WIDTH
+    expect(window.getComputedStyle(figure).height).toBe("161px"); // height + offset - 2 * DEFAULT_CELL_WIDTH
   });
 });