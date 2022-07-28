e2dd4affd4d01ea94bddd0afdedf93ddd38eadf6
Adrien Minne <adrm@odoo.com>
2022-07-28 11:32:39 +0200

    [FIX] merge: cells overridden by merge should lose their style
    The cells overridden by merges didn't loose their styles if the top-left
    cell of the cell had no style. The expected behaviour is to have all
    the cells of the merge have the same style as the top-left cell.
    
    Odoo task 2766282

--- src/plugins/core/merge.ts
+++ src/plugins/core/merge.ts
@@ -388,7 +388,7 @@ export class MergePlugin extends CorePlugin<MergeState> implements MergeState {
             sheetId: sheet.id,
             col,
             row,
-            style: topLeft ? topLeft.style : undefined,
+            style: topLeft ? topLeft.style : null,
             content: undefined,
           });
         }
