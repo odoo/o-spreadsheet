d0aa2e23026c733f2c9530a9af87b62fa4e9de60
Lucas Lefèvre <lul@odoo.com>
2022-08-19 08:51:45 +0000

    [MOV] tests: header visibility tests to their own file
    header visibility tests currently are in grid_manipulation.test file.
    
    They are moved to their own file because
    - they are hard to find there
    - the file is already huuuge
    - those tests depends on global data defined for all tests in the file. This
      data is not relevent to those tests (?) and it makes the tests harder to read
      (not self-contained) and you don't know what the test is actually testing.
    
    Part-of: odoo/o-spreadsheet#1587

--- tests/plugins/grid_manipulation.test.ts
+++ tests/plugins/grid_manipulation.test.ts
@@ -14,8 +14,6 @@ import {
   deleteCells,
   deleteColumns,
   deleteRows,
-  hideColumns,
-  hideRows,
   insertCells,
   merge,
   redo,
@@ -23,8 +21,6 @@ import {
   setCellContent,
   setSelection,
   undo,
-  unhideColumns,
-  unhideRows,
 } from "../test_helpers/commands_helpers";
 import {
   getBorder,
@@ -190,71 +186,6 @@ describe("Clear rows", () => {
 });
 
 //------------------------------------------------------------------------------
-// Hide/unhide
-//------------------------------------------------------------------------------
-describe("Hide Columns", () => {
-  const sheetId = "1";
-  beforeEach(() => {
-    model = new Model({
-      sheets: [
-        {
-          id: sheetId,
-          colNumber: 6,
-          rowNumber: 2,
-        },
-      ],
-    });
-  });
-  test("hide single column", () => {
-    hideColumns(model, ["B"]);
-    expect(model.getters.getHiddenColsGroups(sheetId)).toEqual([[1]]);
-  });
-  test("hide multiple columns", () => {
-    hideColumns(model, ["B", "E", "F"]);
-    expect(model.getters.getHiddenColsGroups(sheetId)).toEqual([[1], [4, 5]]);
-  });
-  test("unhide columns", () => {
-    hideColumns(model, ["B", "E", "F"]);
-    unhideColumns(model, ["F"]);
-    expect(model.getters.getHiddenColsGroups(sheetId)).toEqual([[1], [4]]);
-  });
-  test("Cannot hide columns on invalid sheetId", () => {
-    expect(hideColumns(model, ["A"], "INVALID")).toBeCancelledBecause(CommandResult.InvalidSheetId);
-  });
-});
-
-describe("Hide Rows", () => {
-  const sheetId = "2";
-  beforeEach(() => {
-    model = new Model({
-      sheets: [
-        {
-          id: sheetId,
-          colNumber: 2,
-          rowNumber: 6,
-        },
-      ],
-    });
-  });
-  test("hide single row", () => {
-    hideRows(model, [1]);
-    expect(model.getters.getHiddenRowsGroups(sheetId)).toEqual([[1]]);
-  });
-  test("hide multiple rows", () => {
-    hideRows(model, [1, 4, 5]);
-    expect(model.getters.getHiddenRowsGroups(sheetId)).toEqual([[1], [4, 5]]);
-  });
-  test("unhide rows", () => {
-    hideRows(model, [1, 4, 5]);
-    unhideRows(model, [5]);
-    expect(model.getters.getHiddenRowsGroups(sheetId)).toEqual([[1], [4]]);
-  });
-  test("Cannot hide rows on invalid sheetId", () => {
-    expect(hideRows(model, [0], "INVALID")).toBeCancelledBecause(CommandResult.InvalidSheetId);
-  });
-});
-
-//------------------------------------------------------------------------------
 // Columns
 //------------------------------------------------------------------------------
 
@@ -347,37 +278,6 @@ describe("Columns", () => {
       expect(addColumns(model, "after", "A", 1, sheetId)).toBeCancelledBecause(
         CommandResult.InvalidSheetId
       );
-    });
-
-    test("On hide/unhide Column on small sheet", () => {
-      model = new Model({
-        sheets: [
-          {
-            colNumber: 5,
-            rowNumber: 1,
-          },
-        ],
-      });
-      const sheet = model.getters.getActiveSheet();
-      const dimensions = model.getters.getMaxViewportSize(sheet);
-      hideColumns(model, ["B", "C", "D"], sheet.id);
-      let dimensions2 = model.getters.getMaxViewportSize(sheet);
-      expect(dimensions2.width).toEqual(dimensions.width - 3 * DEFAULT_CELL_WIDTH);
-      unhideColumns(model, ["D"], sheet.id);
-      dimensions2 = model.getters.getMaxViewportSize(sheet);
-      expect(dimensions2.width).toEqual(dimensions.width - 2 * DEFAULT_CELL_WIDTH);
-    });
-
-    test("On Hide/ unhide Column on big sheet", () => {
-      model = new Model();
-      const sheet = model.getters.getActiveSheet();
-      const dimensions = model.getters.getMaxViewportSize(sheet);
-      hideColumns(model, ["B", "C", "D"], sheet.id);
-      let dimensions2 = model.getters.getMaxViewportSize(sheet);
-      expect(dimensions2.width).toEqual(dimensions.width - 3 * DEFAULT_CELL_WIDTH);
-      unhideColumns(model, ["D"], sheet.id);
-      dimensions2 = model.getters.getMaxViewportSize(sheet);
-      expect(dimensions2.width).toEqual(dimensions.width - 2 * DEFAULT_CELL_WIDTH);
     });
   });
 
@@ -941,25 +841,6 @@ describe("Columns", () => {
       undo(model);
       expect(model).toExport(beforeAdd);
     });
-    test("On hiding", () => {
-      model = new Model(fullData);
-      const beforeHidden = model.exportData();
-      hideColumns(model, ["B"]);
-      const afterHidden1 = model.exportData();
-      unhideColumns(model, ["B"]);
-      const afterUnhidden1 = model.exportData();
-      hideColumns(model, ["D"]);
-      const afterHidden2 = model.exportData();
-      undo(model);
-      expect(model).toExport(afterUnhidden1);
-      redo(model);
-      expect(model).toExport(afterHidden2);
-      undo(model);
-      undo(model);
-      expect(model).toExport(afterHidden1);
-      undo(model);
-      expect(model).toExport(beforeHidden);
-    });
   });
 
   describe("Correctly update selection", () => {
@@ -993,19 +874,6 @@ describe("Columns", () => {
       expect(model.getters.getSelectedZone()).toEqual(toZone("B1:D3"));
       addColumns(model, "after", "B", 3);
       expect(model.getters.getSelectedZone()).toEqual(toZone("B1:G3"));
-    });
-    test("On hide 1", () => {
-      model = new Model(fullData);
-      setSelection(model, ["E1:E4"]);
-      expect(model.getters.getSelectedZone()).toEqual(toZone("E1:E4"));
-      hideColumns(model, ["E"]);
-      expect(model.getters.getSelectedZone()).toEqual(toZone("E1:E4"));
-    });
-    test("On hide multiple", () => {
-      model = new Model(fullData);
-      setSelection(model, ["A1:A4", "E1:E4"]);
-      hideColumns(model, ["A", "E"]);
-      expect(model.getters.getSelectedZones()).toEqual([toZone("A1:D4"), toZone("E1:E4")]);
     });
   });
 });
@@ -1192,37 +1060,6 @@ describe("Rows", () => {
       activateSheet(model, to);
       dimensions = model.getters.getMaxViewportSize(model.getters.getActiveSheet());
       expect(dimensions).toMatchObject({ width: 192, height: 124 });
-    });
-
-    test("On hide/unhide Row on small sheet", () => {
-      model = new Model({
-        sheets: [
-          {
-            colNumber: 1,
-            rowNumber: 5,
-          },
-        ],
-      });
-      const sheet = model.getters.getActiveSheet();
-      const dimensions = model.getters.getMaxViewportSize(sheet);
-      hideRows(model, [1, 2, 3], sheet.id);
-      let dimensions2 = model.getters.getMaxViewportSize(sheet);
-      expect(dimensions2.height).toEqual(dimensions.height - 3 * DEFAULT_CELL_HEIGHT);
-      unhideRows(model, [3], sheet.id);
-      dimensions2 = model.getters.getMaxViewportSize(sheet);
-      expect(dimensions2.height).toEqual(dimensions.height - 2 * DEFAULT_CELL_HEIGHT);
-    });
-
-    test("On Hide/ unhide Row on big sheet", () => {
-      model = new Model();
-      const sheet = model.getters.getActiveSheet();
-      const dimensions = model.getters.getMaxViewportSize(sheet);
-      hideRows(model, [1, 2, 3], sheet.id);
-      let dimensions2 = model.getters.getMaxViewportSize(sheet);
-      expect(dimensions2.height).toEqual(dimensions.height - 3 * DEFAULT_CELL_HEIGHT);
-      unhideRows(model, [3], sheet.id);
-      dimensions2 = model.getters.getMaxViewportSize(sheet);
-      expect(dimensions2.height).toEqual(dimensions.height - 2 * DEFAULT_CELL_HEIGHT);
     });
   });
 
@@ -1747,25 +1584,6 @@ describe("Rows", () => {
       undo(model);
       expect(model).toExport(beforeAdd);
     });
-    test("On hiding", () => {
-      model = new Model(fullData);
-      const beforeHidden = model.exportData();
-      hideRows(model, [1]);
-      const afterHidden1 = model.exportData();
-      unhideRows(model, [1]);
-      const afterUnhidden1 = model.exportData();
-      hideRows(model, [3]);
-      const afterHidden2 = model.exportData();
-      undo(model);
-      expect(model).toExport(afterUnhidden1);
-      redo(model);
-      expect(model).toExport(afterHidden2);
-      undo(model);
-      undo(model);
-      expect(model).toExport(afterHidden1);
-      undo(model);
-      expect(model).toExport(beforeHidden);
-    });
   });
 
   describe("Correctly update selection", () => {
@@ -1796,20 +1614,6 @@ describe("Rows", () => {
       expect(model.getters.getSelectedZone()).toEqual(toZone("C1:D2"));
       addRows(model, "after", 0, 3);
       expect(model.getters.getSelectedZone()).toEqual(toZone("C1:D5"));
-    });
-    test("On hide 1", () => {
-      model = new Model(fullData);
-      setSelection(model, ["A3:D3"]);
-      hideRows(model, [2]);
-      expect(model.getters.getSelectedZone()).toEqual(toZone("A3:D3"));
-    });
-    test("On hide multiple", () => {
-      model = new Model(fullData);
-      const zone1 = toZone("A1:D2");
-      const zone2 = toZone("A3:D3");
-      setSelection(model, ["A1:D1", "A3:D3"]);
-      hideRows(model, [0, 2]);
-      expect(model.getters.getSelectedZones()).toEqual([zone1, zone2]);
     });
   });
 
