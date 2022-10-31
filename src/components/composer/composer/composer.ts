98f81f6f881bca3332a719e471ce0a865d34e5f6
Rémi Rahir <rar@odoo.com>
2022-10-31 08:32:40 +0000

    [FIX] selection: Altering selection should scroll the viewport
    Commit b437f446b introduced an error. while fixing the viewport
    scrolling while in autofill (it shouldn't happen) it also prevent an
    change of selection (not an extension) to properly scroll the viewport.
    
    The problem ultimately lies in the fact that the autofill was using the
    selectionProcessor to override its selection instead of extending it.
    
    In order to fix this, it was necessary to alter the API of the
    selection processor to allow more granularity in the steps to extend a
    zone selection: i.e. instead of a step of 1 or jump to the end of a zone,
    we need to support all POSITIVE values of steps.
    
    Task 3050101
    
    X-original-commit: b167b3e6d9f1eb5421f6fe5d3df5d83e50e008c5

--- src/components/composer/composer/composer.ts
+++ src/components/composer/composer/composer.ts
@@ -267,7 +267,7 @@ export class Composer extends Component<Props, SpreadsheetChildEnv> {
 
     const direction = ev.shiftKey ? "left" : "right";
     this.env.model.dispatch("STOP_EDITION");
-    this.env.model.selection.moveAnchorCell(direction, "one");
+    this.env.model.selection.moveAnchorCell(direction, 1);
   }
 
   private processEnterKey(ev: KeyboardEvent) {
@@ -284,7 +284,7 @@ export class Composer extends Component<Props, SpreadsheetChildEnv> {
     }
     this.env.model.dispatch("STOP_EDITION");
     const direction = ev.shiftKey ? "up" : "down";
-    this.env.model.selection.moveAnchorCell(direction, "one");
+    this.env.model.selection.moveAnchorCell(direction, 1);
   }
 
   private processEscapeKey() {
