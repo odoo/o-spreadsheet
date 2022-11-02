763b9490af3b2df6153072cd937918d8ee65c5d7
Rémi Rahir <rar@odoo.com>
2022-11-02 09:20:43 +0000

    [FIX] sheet: prevent creation of sheet with the same id
    A user could create a sheet with the same id as an existing one,
    effectively erasing the first sheet of the mapping.
    
    closes odoo/o-spreadsheet#1798
    
    Signed-off-by: Pierre Rousseau (pro) <pro@odoo.com>
    Signed-off-by: Rémi Rahir (rar) <rar@odoo.com>

--- src/types/commands.ts
+++ src/types/commands.ts
@@ -1043,6 +1043,7 @@ export const enum CommandResult {
   NotEnoughSheets,
   MissingSheetName,
   DuplicatedSheetName,
+  DuplicatedSheetId,
   ForbiddenCharactersInSheetName,
   WrongSheetMove,
   WrongSheetPosition,
