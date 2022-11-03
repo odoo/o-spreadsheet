a3e75a55ecd15714212b5e060756a45eb89fd512
Rémi Rahir <rar@odoo.com>
2022-11-03 16:23:14 +0100

    [FIX] config: Allow to build outside of git repository
    The lib used to extract the hash commit will raise when run outside a
    git repository. Since we need to perform this action on runbot, we catch
    the error in that specific case.

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,14 @@
 import { version } from "./package.json";
 import git from "git-rev-sync";
 
+let commitHash = "";
+
+try {
+  commitHash = git.short();
+}
+catch(_) {
+}
+
 export default {
   input: "dist/js/index.js",
   external: ["@odoo/owl"],
@@ -10,6 +18,6 @@ export default {
     name: "o_spreadsheet",
     extend: true,
     globals: { "@odoo/owl": "owl" /*, "chart.js": "chart_js" */ },
-    outro: `exports.__info__.version = '${version}';\nexports.__info__.date = '${new Date().toISOString()}';\nexports.__info__.hash = '${git.short()}';`,
+    outro: `exports.__info__.version = '${version}';\nexports.__info__.date = '${new Date().toISOString()}';\nexports.__info__.hash = '${commitHash}';`,
   },
 };
