12449c0605f1bf4db853bad48c7783c0420f1d8f
Rémi Rahir <rar@odoo.com>
2022-11-14 17:13:28 +0100

    [FIX] functions: fix error message formatting in lookup module

--- src/translation.ts
+++ src/translation.ts
@@ -15,7 +15,7 @@ let _t: TranslationFunction = (s) => s;
 function sprintf(s: string, ...values: string[] | [{ [key: string]: string }]): string {
   if (values.length === 1 && typeof values[0] === "object") {
     const valuesDict = values[0] as { [key: string]: string };
-    s = s.replace(/\%\(?([^\)]+)\)s/g, (match, value) => valuesDict[value]);
+    s = s.replace(/%\(?([^\)]+)\)s/g, (match, value) => valuesDict[value]);
   } else if (values.length > 0) {
     s = s.replace(/\%s/g, () => values.shift() as string);
   }
