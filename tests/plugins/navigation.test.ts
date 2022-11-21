odoo/o-spreadsheet

commit b437f446b2353c01c4304fc9fd7dd0746f33ebc3

[FIX] selection: don't reset selection on Autofill
How to reproduce:
-----------------

- extend your selection while edgescrolling in order to have a scrolled viewport
- try to autofill the selection

=> the viewport is scrolled back to the top of the sheet.

Part of task 3032860

Part-of: odoo/o-spreadsheet#1740

src/components/grid.ts
@@ -854,7 +854,11 @@ export class Grid extends Component<Props, SpreadsheetChildEnv> {
       const newZone = this.env.model.getters.getSelectedZone();
       const viewport = this.env.model.getters.getActiveSnappedViewport();
       const sheet = this.env.model.getters.getActiveSheet();
-      let [col, row] = findCellInNewZone(oldZone, newZone);
+      let [col, row] = findCellInNewZone(
+        oldZone,
+        newZone,
+        this.env.model.getters.getActiveSnappedViewport()
+      );
       col = Math.min(col, sheet.cols.length - 1);
       row = Math.min(row, sheet.rows.length - 1);
       const { left, right, top, bottom, offsetX, offsetY } = viewport;

src/helpers/zones.ts
@@ -1,4 +1,4 @@
-import { Position, Zone, ZoneDimension } from "../types";
+import { Position, Viewport, Zone, ZoneDimension } from "../types";
 import { toCartesian, toXC } from "./coordinates";
 import { range } from "./misc";
 
@@ -484,7 +484,11 @@ export function mergeOverlappingZones(zones: Zone[]) {
  * This function will compare the modifications of selection to determine
  * a cell that is part of the new zone and not the previous one.
  */
-export function findCellInNewZone(oldZone: Zone, currentZone: Zone): [number, number] {
+export function findCellInNewZone(
+  oldZone: Zone,
+  currentZone: Zone,
+  viewport: Viewport
+): [number, number] {
   let col: number, row: number;
   const { left: oldLeft, right: oldRight, top: oldTop, bottom: oldBottom } = oldZone!;
   const { left, right, top, bottom } = currentZone;
@@ -493,14 +497,16 @@ export function findCellInNewZone(oldZone: Zone, currentZone: Zone): [number, nu
   } else if (right != oldRight) {
     col = right;
   } else {
-    col = left;
+    // left and right don't change
+    col = viewport.left > left || left > viewport.right ? viewport.left : left;
   }
   if (top != oldTop) {
     row = top;
   } else if (bottom != oldBottom) {
     row = bottom;
   } else {
-    row = top;
+    // top and bottom don't change
+    row = viewport.top > top || top > viewport.bottom ? viewport.top : top;
   }
   return [col, row];
 }

src/plugins/ui/viewport.ts
@@ -82,7 +82,11 @@ export class ViewportPlugin extends UIPlugin {
       case "ZonesSelected":
         // altering a zone should not move the viewport
         const sheet = this.getters.getActiveSheet();
-        let [col, row] = findCellInNewZone(event.previousAnchor.zone, event.anchor.zone);
+        let [col, row] = findCellInNewZone(
+          event.previousAnchor.zone,
+          event.anchor.zone,
+          this.getActiveSnappedViewport()
+        );
         col = Math.min(col, sheet.cols.length - 1);
         row = Math.min(row, sheet.rows.length - 1);
         this.refreshViewport(this.getters.getActiveSheetId(), { col, row });

tests/components/autofill.test.ts
@@ -1,8 +1,8 @@
 import { App, Component, xml } from "@odoo/owl";
 import { Spreadsheet } from "../../src";
-import { HEADER_HEIGHT, HEADER_WIDTH } from "../../src/constants";
+import { DEFAULT_CELL_WIDTH, HEADER_HEIGHT, HEADER_WIDTH } from "../../src/constants";
 import { Model } from "../../src/model";
-import { setCellContent } from "../test_helpers/commands_helpers";
+import { setCellContent, setSelection } from "../test_helpers/commands_helpers";
 import { triggerMouseEvent } from "../test_helpers/dom_helper";
 import { makeTestFixture, mountSpreadsheet, nextTick, spyDispatch } from "../test_helpers/helpers";
 
@@ -274,5 +274,25 @@ describe("Autofill component", () => {
     );
     await nextTick();
     expect(dispatch).not.toHaveBeenCalled();
+  });
+
+  test("Autofill does not reset the viewport position", async () => {
+    setSelection(parent.model, ["A1:A100"]);
+    parent.model.dispatch("SET_VIEWPORT_OFFSET", { offsetX: 400, offsetY: 400 });
+    const firstViewport = parent.model.getters.getActiveSnappedViewport();
+    console.log(firstViewport);
+    const autofill = fixture.querySelector(".o-autofill");
+    triggerMouseEvent(autofill, "mousedown", 4, 4);
+    await nextTick();
+    const newX =
+      HEADER_WIDTH +
+      parent.model.getters.getCol(parent.model.getters.getActiveSheetId(), 0)!.start +
+      2 * DEFAULT_CELL_WIDTH;
+    triggerMouseEvent(autofill, "mousemove", newX, HEADER_HEIGHT + 4);
+    await nextTick();
+    triggerMouseEvent(autofill, "mouseup", newX, HEADER_HEIGHT + 4);
+    await nextTick();
+    console.log(parent.model.getters.getActiveSnappedViewport());
+    expect(firstViewport).toMatchObject(parent.model.getters.getActiveSnappedViewport());
   });
 });

commit c39291e9c98db05164442bc5c9598b30272ed0f1

[FIX] selection: Altering selection should scroll the viewport
Commit b437f446b introduced an error. while fixing the viewport
scrolling while in autofill (it shouldn't happen) it also prevent an
change of selection (not an extension) to properly scroll the viewport.

The problem ultimately lies in the fact that the autofill was using the
selectionProcessor to override its selection instead of extending it.

In order to fix this, it was necessary to alter the API of the
selection processor to allow more granularity in the steps to extend a
zone selection: i.e. instead of a step of 1 or jump to the end of a zone,
we need to support all POSITIVE values of steps.

Task 3050101

src/components/composer/composer.ts
@@ -294,7 +294,7 @@ export class Composer extends Component<Props, SpreadsheetChildEnv> {
 
     const direction = ev.shiftKey ? "left" : "right";
     this.env.model.dispatch("STOP_EDITION");
-    this.env.model.selection.moveAnchorCell(direction, "one");
+    this.env.model.selection.moveAnchorCell(direction, 1);
   }
 
   private processEnterKey(ev: KeyboardEvent) {
@@ -310,7 +310,7 @@ export class Composer extends Component<Props, SpreadsheetChildEnv> {
     }
     this.env.model.dispatch("STOP_EDITION");
     const direction = ev.shiftKey ? "up" : "down";
-    this.env.model.selection.moveAnchorCell(direction, "one");
+    this.env.model.selection.moveAnchorCell(direction, 1);
   }
 
   private processEscapeKey() {

src/components/grid.ts
@@ -20,7 +20,6 @@ import {
   TOPBAR_HEIGHT,
 } from "../constants";
 import {
-  findCellInNewZone,
   findVisibleHeader,
   getNextVisibleCellCoords,
   isInside,
@@ -468,8 +467,8 @@ export class Grid extends Component<Props, SpreadsheetChildEnv> {
         ? this.props.onGridComposerCellFocused()
         : this.props.onComposerContentFocused();
     },
-    TAB: () => this.env.model.selection.moveAnchorCell("right", "one"),
-    "SHIFT+TAB": () => this.env.model.selection.moveAnchorCell("left", "one"),
+    TAB: () => this.env.model.selection.moveAnchorCell("right", 1),
+    "SHIFT+TAB": () => this.env.model.selection.moveAnchorCell("left", 1),
     F2: () => {
       const cell = this.env.model.getters.getActiveCell();
       !cell || cell.isEmpty()
@@ -843,37 +842,16 @@ export class Grid extends Component<Props, SpreadsheetChildEnv> {
     ev.stopPropagation();
     this.closeLinkEditor();
     const arrowMap = {
-      ArrowDown: { direction: "down", delta: [0, 1] },
-      ArrowLeft: { direction: "left", delta: [-1, 0] },
-      ArrowRight: { direction: "right", delta: [1, 0] },
-      ArrowUp: { direction: "up", delta: [0, -1] },
+      ArrowDown: "down",
+      ArrowLeft: "left",
+      ArrowRight: "right",
+      ArrowUp: "up",
     };
-    const { direction, delta } = arrowMap[ev.key];
+    const direction = arrowMap[ev.key];
     if (ev.shiftKey) {
-      const oldZone = this.env.model.getters.getSelectedZone();
-      this.env.model.selection.resizeAnchorZone(direction, ev.ctrlKey ? "end" : "one");
-      const newZone = this.env.model.getters.getSelectedZone();
-      const viewport = this.env.model.getters.getActiveSnappedViewport();
-      const sheet = this.env.model.getters.getActiveSheet();
-      let [col, row] = findCellInNewZone(
-        oldZone,
-        newZone,
-        this.env.model.getters.getActiveSnappedViewport()
-      );
-      col = Math.min(col, sheet.cols.length - 1);
-      row = Math.min(row, sheet.rows.length - 1);
-      const { left, right, top, bottom, offsetX, offsetY } = viewport;
-      const newOffsetX =
-        col < left || col > right - 1 ? sheet.cols[left + delta[0]].start : offsetX;
-      const newOffsetY = row < top || row > bottom - 1 ? sheet.rows[top + delta[1]].start : offsetY;
-      if (newOffsetX !== offsetX || newOffsetY !== offsetY) {
-        this.env.model.dispatch("SET_VIEWPORT_OFFSET", {
-          offsetX: newOffsetX,
-          offsetY: newOffsetY,
-        });
-      }
+      this.env.model.selection.resizeAnchorZone(direction, ev.ctrlKey ? "end" : 1);
     } else {
-      this.env.model.selection.moveAnchorCell(direction, ev.ctrlKey ? "end" : "one");
+      this.env.model.selection.moveAnchorCell(direction, ev.ctrlKey ? "end" : 1);
     }
 
     if (this.env.model.getters.isPaintingFormat()) {

src/helpers/zones.ts
@@ -1,4 +1,4 @@
-import { Position, Viewport, Zone, ZoneDimension } from "../types";
+import { Position, Zone, ZoneDimension } from "../types";
 import { toCartesian, toXC } from "./coordinates";
 import { range } from "./misc";
 
@@ -484,11 +484,7 @@ export function mergeOverlappingZones(zones: Zone[]) {
  * This function will compare the modifications of selection to determine
  * a cell that is part of the new zone and not the previous one.
  */
-export function findCellInNewZone(
-  oldZone: Zone,
-  currentZone: Zone,
-  viewport: Viewport
-): [number, number] {
+export function findCellInNewZone(oldZone: Zone, currentZone: Zone): [number, number] {
   let col: number, row: number;
   const { left: oldLeft, right: oldRight, top: oldTop, bottom: oldBottom } = oldZone!;
   const { left, right, top, bottom } = currentZone;
@@ -498,7 +494,7 @@ export function findCellInNewZone(
     col = right;
   } else {
     // left and right don't change
-    col = viewport.left > left || left > viewport.right ? viewport.left : left;
+    col = left;
   }
   if (top != oldTop) {
     row = top;
@@ -506,7 +502,7 @@ export function findCellInNewZone(
     row = bottom;
   } else {
     // top and bottom don't change
-    row = viewport.top > top || top > viewport.bottom ? viewport.top : top;
+    row = top;
   }
   return [col, row];
 }

src/plugins/ui/autofill.ts
@@ -1,4 +1,4 @@
-import { clip, isInside, toCartesian, toXC, union } from "../../helpers/index";
+import { clip, isInside, toCartesian, toXC } from "../../helpers/index";
 import { Mode } from "../../model";
 import { autofillModifiersRegistry, autofillRulesRegistry } from "../../registries/index";
 import {
@@ -85,6 +85,7 @@ export class AutofillPlugin extends UIPlugin {
   static modes: Mode[] = ["normal"];
 
   private autofillZone: Zone | undefined;
+  private steps: number | undefined;
   private lastCellSelected: { col?: number; row?: number } = {};
   private direction: DIRECTION | undefined;
   private tooltip: Tooltip | undefined;
@@ -167,7 +168,7 @@ export class AutofillPlugin extends UIPlugin {
    *              useful to set it to false when we need to fill the tooltip
    */
   private autofill(apply: boolean) {
-    if (!this.autofillZone || this.direction === undefined) {
+    if (!this.autofillZone || !this.steps || this.direction === undefined) {
       this.tooltip = undefined;
       return;
     }
@@ -226,12 +227,12 @@ export class AutofillPlugin extends UIPlugin {
     }
 
     if (apply) {
-      const zone = union(this.getters.getSelectedZone(), this.autofillZone);
       this.autofillZone = undefined;
+      this.selection.resizeAnchorZone(this.direction, this.steps);
       this.lastCellSelected = {};
       this.direction = undefined;
+      this.steps = 0;
       this.tooltip = undefined;
-      this.selection.selectZone({ cell: { col: zone.left, row: zone.top }, zone });
     }
   }
 
@@ -248,15 +249,19 @@ export class AutofillPlugin extends UIPlugin {
     switch (this.direction) {
       case DIRECTION.UP:
         this.saveZone(row, source.top - 1, source.left, source.right);
+        this.steps = source.top - row;
         break;
       case DIRECTION.DOWN:
         this.saveZone(source.bottom + 1, row, source.left, source.right);
+        this.steps = row - source.bottom;
         break;
       case DIRECTION.LEFT:
         this.saveZone(source.top, source.bottom, col, source.left - 1);
+        this.steps = source.left - col;
         break;
       case DIRECTION.RIGHT:
         this.saveZone(source.top, source.bottom, source.right + 1, col);
+        this.steps = col - source.right;
         break;
     }
     this.autofill(false);

src/plugins/ui/viewport.ts
@@ -80,13 +80,22 @@ export class ViewportPlugin extends UIPlugin {
       case "AlterZoneCorner":
         break;
       case "ZonesSelected":
-        // altering a zone should not move the viewport
+        let [col, row] = findCellInNewZone(event.previousAnchor.zone, event.anchor.zone);
+
+        if (event.mode === "updateAnchor") {
+          const oldZone = event.previousAnchor.zone;
+          const newZone = event.anchor.zone;
+          // altering a zone should not move the viewport in a dimension that wasn't changed
+          const { top, bottom, left, right } = this.getters.getActiveSnappedViewport();
+          if (oldZone.left === newZone.left && oldZone.right === newZone.right) {
+            col = left > col || col > right ? left : col;
+          }
+          if (oldZone.top === newZone.top && oldZone.bottom === newZone.bottom) {
+            row = top > row || row > bottom ? top : row;
+          }
+        }
+
         const sheet = this.getters.getActiveSheet();
-        let [col, row] = findCellInNewZone(
-          event.previousAnchor.zone,
-          event.anchor.zone,
-          this.getActiveSnappedViewport()
-        );
         col = Math.min(col, sheet.cols.length - 1);
         row = Math.min(row, sheet.rows.length - 1);
         this.refreshViewport(this.getters.getActiveSheetId(), { col, row });

src/selection_stream/selection_stream_processor.ts
@@ -142,7 +142,10 @@ export class SelectionStreamProcessor
   /**
    * Set the selection to one of the cells adjacent to the current anchor cell.
    */
-  moveAnchorCell(direction: SelectionDirection, step: SelectionStep = "one"): DispatchResult {
+  moveAnchorCell(direction: SelectionDirection, step: SelectionStep = 1): DispatchResult {
+    if (step !== "end" && step <= 0) {
+      return new DispatchResult(CommandResult.InvalidSelectionStep);
+    }
     const { col, row } = this.getNextAvailablePosition(direction, step);
     return this.selectCell(col, row);
   }
@@ -188,7 +191,10 @@ export class SelectionStreamProcessor
    * The anchor cell remains where it is. It's the opposite side
    * of the anchor zone which moves.
    */
-  resizeAnchorZone(direction: SelectionDirection, step: SelectionStep = "one"): DispatchResult {
+  resizeAnchorZone(direction: SelectionDirection, step: SelectionStep = 1): DispatchResult {
+    if (step !== "end" && step <= 0) {
+      return new DispatchResult(CommandResult.InvalidSelectionStep);
+    }
     const sheet = this.getters.getActiveSheet();
     const anchor = this.anchor;
     const { col: anchorCol, row: anchorRow } = anchor.cell;
@@ -378,7 +384,7 @@ export class SelectionStreamProcessor
    */
   private getNextAvailablePosition(
     direction: SelectionDirection,
-    step: SelectionStep = "one"
+    step: SelectionStep = 1
   ): Position {
     const { col, row } = this.anchor.cell;
     const delta = this.deltaToTarget({ col, row }, direction, step);
@@ -468,20 +474,20 @@ export class SelectionStreamProcessor
   ): [number, number] {
     switch (direction) {
       case "up":
-        return step === "one"
-          ? [0, -1]
+        return step !== "end"
+          ? [0, -step]
           : [0, this.getEndOfCluster(position, "rows", -1) - position.row];
       case "down":
-        return step === "one"
-          ? [0, 1]
+        return step !== "end"
+          ? [0, step]
           : [0, this.getEndOfCluster(position, "rows", 1) - position.row];
       case "left":
-        return step === "one"
-          ? [-1, 0]
+        return step !== "end"
+          ? [-step, 0]
           : [this.getEndOfCluster(position, "cols", -1) - position.col, 0];
       case "right":
-        return step === "one"
-          ? [1, 0]
+        return step !== "end"
+          ? [step, 0]
           : [this.getEndOfCluster(position, "cols", 1) - position.col, 0];
     }
   }

src/types/commands.ts
@@ -991,6 +991,7 @@ export const enum CommandResult {
   FigureDoesNotExist,
   InvalidConditionalFormatId,
   DuplicatedFigureId,
+  InvalidSelectionStep,
 }
 
 export interface CommandHandler<T> {

src/types/misc.ts
@@ -180,10 +180,10 @@ export type BorderCommand =
 export type BorderDescription = { vertical?: BorderDescr; horizontal?: BorderDescr } | undefined;
 
 export const enum DIRECTION {
-  UP,
-  DOWN,
-  LEFT,
-  RIGHT,
+  UP = "up",
+  DOWN = "down",
+  LEFT = "left",
+  RIGHT = "right",
 }
 
 export type ChangeType = "REMOVE" | "RESIZE" | "MOVE" | "CHANGE" | "NONE";

src/types/selection.ts
@@ -1,3 +1,3 @@
 export type SelectionDirection = "up" | "down" | "left" | "right";
 
-export type SelectionStep = "one" | "end";
+export type SelectionStep = number | "end";

tests/components/autofill.test.ts
@@ -280,7 +280,6 @@ describe("Autofill component", () => {
     setSelection(parent.model, ["A1:A100"]);
     parent.model.dispatch("SET_VIEWPORT_OFFSET", { offsetX: 400, offsetY: 400 });
     const firstViewport = parent.model.getters.getActiveSnappedViewport();
-    console.log(firstViewport);
     const autofill = fixture.querySelector(".o-autofill");
     triggerMouseEvent(autofill, "mousedown", 4, 4);
     await nextTick();
@@ -292,7 +291,6 @@ describe("Autofill component", () => {
     await nextTick();
     triggerMouseEvent(autofill, "mouseup", newX, HEADER_HEIGHT + 4);
     await nextTick();
-    console.log(parent.model.getters.getActiveSnappedViewport());
     expect(firstViewport).toMatchObject(parent.model.getters.getActiveSnappedViewport());
   });
 });

tests/plugins/navigation.test.ts
@@ -392,4 +392,25 @@ describe("Navigation starting from hidden cells", () => {
       expect(model.getters.getPosition()).toEqual(toCartesian(endPosition));
     }
   );
+
+  test("Cannot from zero or negative steps does nothing", () => {
+    const model = new Model({
+      sheets: [
+        {
+          colNumber: 5,
+          rowNumber: 2,
+        },
+      ],
+    });
+    selectCell(model, "C1");
+    hideColumns(model, ["C"]);
+    const move1 = moveAnchorCell(model, "down", 0);
+    expect(move1).toBeCancelledBecause(CommandResult.InvalidSelectionStep);
+    const move2 = moveAnchorCell(model, "up", -1);
+    expect(move2).toBeCancelledBecause(CommandResult.InvalidSelectionStep);
+    const move3 = moveAnchorCell(model, "right", -2);
+    expect(move3).toBeCancelledBecause(CommandResult.InvalidSelectionStep);
+    const move4 = moveAnchorCell(model, "left", -3);
+    expect(move4).toBeCancelledBecause(CommandResult.InvalidSelectionStep);
+  });
 });

tests/plugins/viewport.test.ts
@@ -118,7 +118,7 @@ describe("Viewport of Simple sheet", () => {
 
   test("Add columns doesn't affect offset", () => {
     selectCell(model, "P1");
-    const currentViewport = model.getters.getActiveViewport();
+    const currentViewport = { ...model.getters.getActiveViewport() };
     addColumns(model, "after", "P", 30);
     expect(model.getters.getActiveViewport()).toMatchObject(currentViewport);
     undo(model);
@@ -535,6 +535,26 @@ describe("Viewport of Simple sheet", () => {
     expect(model.getters.getActiveViewport()).toMatchObject(
       model.getters.getActiveSnappedViewport()
     );
+  });
+
+  describe("Cross Move Position with selection outside the viewport affects offset", () => {
+    test("Move horizontally a cell which row is outside the viewport", () => {
+      const { bottom } = model.getters.getActiveSnappedViewport();
+      selectCell(model, toXC(0, bottom + 3));
+      const viewport = { ...model.getters.getActiveSnappedViewport() };
+      model.dispatch("SET_VIEWPORT_OFFSET", { offsetX: 0, offsetY: 0 });
+      moveAnchorCell(model, "right");
+      expect(model.getters.getActiveSnappedViewport()).toMatchObject(viewport);
+    });
+
+    test("Move vertically a cell which col is outside the viewport", () => {
+      const { right } = model.getters.getActiveSnappedViewport();
+      selectCell(model, toXC(right + 3, 0));
+      const viewport = { ...model.getters.getActiveSnappedViewport() };
+      model.dispatch("SET_VIEWPORT_OFFSET", { offsetX: 0, offsetY: 0 });
+      moveAnchorCell(model, "down");
+      expect(model.getters.getActiveSnappedViewport()).toMatchObject(viewport);
+    });
   });
 
   test("Move position on cells that are taller than the client's height", () => {

tests/test_helpers/commands_helpers.ts
@@ -350,7 +350,7 @@ export function selectCell(model: Model, xc: string): DispatchResult {
 export function moveAnchorCell(
   model: Model,
   direction: SelectionDirection,
-  step: SelectionStep = "one"
+  step: SelectionStep = 1
 ): DispatchResult {
   return model.selection.moveAnchorCell(direction, step);
 }
@@ -358,7 +358,7 @@ export function moveAnchorCell(
 export function resizeAnchorZone(
   model: Model,
   direction: SelectionDirection,
-  step: SelectionStep = "one"
+  step: SelectionStep = 1
 ): DispatchResult {
   return model.selection.resizeAnchorZone(direction, step);
 }