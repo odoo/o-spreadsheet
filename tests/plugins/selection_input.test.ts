odoo/o-spreadsheet

commit 3660bf27522121e720212f90d15309ba61ae510f

[REF] components: enforce component autonomy during keyboard event handling
With this commit, each component have now the responsibility to handle
its proper keyboards event, stopping events when it handles an event but
let bubbles the others.

closes odoo/o-spreadsheet#1803

Signed-off-by: Lucas Lefèvre (lul) <lul@odoo.com>
Co-authored-by: Rémi Rahir <rar@odoo.com>

src/plugins/ui/selection_inputs_manager.ts
@@ -1,4 +1,4 @@
-import { positionToZone, rangeReference } from "../../helpers/index";
+import { positionToZone, rangeReference, toZone } from "../../helpers/index";
 import { ModelConfig } from "../../model";
 import { SelectionStreamProcessor } from "../../selection_stream/selection_stream_processor";
 import { StateObserver } from "../../state_observer";
@@ -82,13 +82,25 @@ export class SelectionInputsManagerPlugin extends UIPlugin {
 
       case "ADD_EMPTY_RANGE":
       case "REMOVE_RANGE":
-      case "FOCUS_RANGE":
-      case "CHANGE_RANGE":
         if (cmd.id !== this.focusedInputId) {
           const input = this.inputs[cmd.id];
           this.selection.capture(
             input,
             { cell: { col: 0, row: 0 }, zone: positionToZone({ col: 0, row: 0 }) },
+            { handleEvent: input.handleEvent.bind(input) }
+          );
+          this.focusedInputId = cmd.id;
+        }
+        break;
+      case "FOCUS_RANGE":
+      case "CHANGE_RANGE":
+        if (cmd.id !== this.focusedInputId) {
+          const input = this.inputs[cmd.id];
+          const range = input.ranges.find((range) => range.id === cmd.rangeId);
+          const zone = toZone(range?.xc || "A1");
+          this.selection.capture(
+            input,
+            { cell: { col: zone.left, row: zone.top }, zone },
             { handleEvent: input.handleEvent.bind(input) }
           );
           this.focusedInputId = cmd.id;

tests/components/selection_input.test.ts
@@ -16,7 +16,7 @@ let model: Model;
 let fixture: HTMLElement;
 
 function focus(index = 0) {
-  fixture.querySelectorAll("input")[index].dispatchEvent(new Event("focus"));
+  fixture.querySelectorAll("input")[index].focus();
 }
 
 async function writeInput(index: number, text: string) {
@@ -250,6 +250,24 @@ describe("Selection Input", () => {
     expect(model.getters.getSelectionInput(id)[0].xc).toBe("C2");
     expect(fixture.querySelectorAll("input")[1].value).toBe("A1");
     expect(model.getters.getSelectionInput(id)[1].xc).toBe("A1");
+    app.destroy();
+  });
+
+  test("F2 alters edition mode", async () => {
+    const { app } = await createSelectionInput({ initialRanges: ["C2"] });
+    const selectionInputEl: HTMLInputElement = fixture.querySelector(".o-selection-input input")!;
+    focus(0);
+    await nextTick();
+    expect(document.activeElement).toBe(selectionInputEl);
+    selectionInputEl.dispatchEvent(new KeyboardEvent("keydown", { key: "ArrowLeft" }));
+    await nextTick();
+    expect(document.activeElement).toBe(selectionInputEl);
+    expect(selectionInputEl?.value).toEqual("B2");
+    selectionInputEl.dispatchEvent(new KeyboardEvent("keydown", { key: "F2" }));
+    selectionInputEl.dispatchEvent(new KeyboardEvent("keydown", { key: "ArrowLeft" }));
+    await nextTick();
+    expect(document.activeElement).toBe(selectionInputEl);
+    expect(selectionInputEl?.value).toEqual("B2");
     app.destroy();
   });
 

src/components/composer/autocomplete_dropdown/autocomplete_dropdown.ts
@@ -1,34 +1,6 @@
-import { Component, onMounted, onWillUpdateProps, useState } from "@odoo/owl";
-import { functionRegistry } from "../../../functions/index";
-import { Registry } from "../../../registry";
+import { Component } from "@odoo/owl";
 import { css } from "../../helpers/css";
-const functions = functionRegistry.content;
-
-// -----------------------------------------------------------------------------
-// Autocomplete Value Providers
-// -----------------------------------------------------------------------------
-
-interface AutocompleteValue {
-  text: string;
-  description: string;
-}
-
-type AutocompleteProvider = () => AutocompleteValue[];
-
-const providerRegistry = new Registry<AutocompleteProvider>();
-
-providerRegistry.add("functions", () => {
-  return Object.keys(functions).map((key) => {
-    return {
-      text: key,
-      description: functions[key].description,
-    };
-  });
-});
-
-// -----------------------------------------------------------------------------
-// Autocomplete DropDown component
-// -----------------------------------------------------------------------------
+import { AutocompleteValue } from "../composer/composer";
 
 css/* scss */ `
   .o-autocomplete-dropdown {
@@ -57,86 +29,17 @@ css/* scss */ `
 `;
 
 interface Props {
-  provider: string;
-  filter?: (searchTerm: string, vals: AutocompleteValue[]) => AutocompleteValue[];
-  search: string;
-  borderStyle: string;
-  exposeAPI: (api: TextValueProviderApi) => void;
-  onCompleted: (text?: string) => void;
+  values: AutocompleteValue[];
+  selectedIndex: number;
+  onValueSelected: (value: string) => void;
 }
 
-export interface TextValueProviderApi {
-  moveUp: () => void;
-  moveDown: () => void;
-  getValueToFill: () => string | undefined;
-}
-
-export class TextValueProvider extends Component<Props> implements TextValueProviderApi {
+export class TextValueProvider extends Component<Props> {
   static template = "o-spreadsheet-TextValueProvider";
-  state = useState({
-    values: <AutocompleteValue[]>[],
-    selectedIndex: 0,
-  });
-
-  setup() {
-    onMounted(() => this.filter(this.props.search));
-    onWillUpdateProps((nextProps: Props) => this.checkUpdateProps(nextProps));
-    this.props.exposeAPI({
-      getValueToFill: () => this.getValueToFill(),
-      moveDown: () => this.moveDown(),
-      moveUp: () => this.moveUp(),
-    });
-  }
-
-  checkUpdateProps(nextProps: any) {
-    if (nextProps.search !== this.props.search) {
-      this.filter(nextProps.search);
-    }
-  }
-
-  async filter(searchTerm: string) {
-    const provider = providerRegistry.get(this.props.provider);
-    let values = provider();
-    if (this.props.filter) {
-      values = this.props.filter(searchTerm, values);
-    } else {
-      values = values
-        .filter((t) => t.text.toUpperCase().startsWith(searchTerm.toUpperCase()))
-        .sort((l, r) => (l.text < r.text ? -1 : l.text > r.text ? 1 : 0));
-    }
-    this.state.values = values.slice(0, 10);
-    this.state.selectedIndex = 0;
-  }
-
-  fillValue(index) {
-    this.state.selectedIndex = index;
-    this.props.onCompleted(this.getValueToFill());
-  }
-
-  moveDown() {
-    this.state.selectedIndex = (this.state.selectedIndex + 1) % this.state.values.length;
-  }
-
-  moveUp() {
-    this.state.selectedIndex--;
-    if (this.state.selectedIndex < 0) {
-      this.state.selectedIndex = this.state.values.length - 1;
-    }
-  }
-
-  getValueToFill(): string | undefined {
-    if (this.state.values.length) {
-      return this.state.values[this.state.selectedIndex].text;
-    }
-    return undefined;
-  }
 }
 
 TextValueProvider.props = {
-  provider: String,
-  filter: { type: Function, optional: true },
-  search: String,
-  borderStyle: String,
-  exposeAPI: Function,
-  onCompleted: Function,
+  values: Array,
+  selectedIndex: Number,
+  onValueSelected: Function,
 };

src/components/composer/autocomplete_dropdown/autocomplete_dropdown.xml
@@ -1,17 +1,15 @@
 <templates>
   <t t-name="o-spreadsheet-TextValueProvider" owl="1">
-    <div
-      t-att-class="{'o-autocomplete-dropdown':state.values.length}"
-      t-att-style="state.values.length > 0 ? props.borderStyle : null">
-      <t t-foreach="state.values" t-as="v" t-key="v.text">
+    <div t-att-class="{'o-autocomplete-dropdown':props.values.length}">
+      <t t-foreach="props.values" t-as="v" t-key="v.text">
         <div
-          t-att-class="{'o-autocomplete-value-focus': state.selectedIndex === v_index}"
-          t-on-click.stop.prevent="() => this.fillValue(v_index)">
+          t-att-class="{'o-autocomplete-value-focus': props.selectedIndex === v_index}"
+          t-on-click.stop.prevent="() => this.props.onValueSelected(v.text)">
           <div class="o-autocomplete-value" t-esc="v.text"/>
           <div
             class="o-autocomplete-description"
             t-esc="v.description"
-            t-if="state.selectedIndex === v_index"
+            t-if="props.selectedIndex === v_index"
           />
         </div>
       </t>

src/components/composer/composer/composer.ts
@@ -6,10 +6,8 @@ import { isEqual, rangeReference, zoneToDimension } from "../../../helpers/index
 import { ComposerSelection, SelectionIndicator } from "../../../plugins/ui/edition";
 import { DOMDimension, FunctionDescription, Rect, SpreadsheetChildEnv } from "../../../types/index";
 import { css } from "../../helpers/css";
-import {
-  TextValueProvider,
-  TextValueProviderApi,
-} from "../autocomplete_dropdown/autocomplete_dropdown";
+import { updateSelectionWithArrowKeys } from "../../helpers/selection_helpers";
+import { TextValueProvider } from "../autocomplete_dropdown/autocomplete_dropdown";
 import { ContentEditableHelper } from "../content_editable_helper";
 import { FunctionDescriptionProvider } from "../formula_assistant/formula_assistant";
 
@@ -75,6 +73,11 @@ css/* scss */ `
       margin: 4px;
       pointer-events: none;
     }
+
+    .o-autocomplete-dropdown,
+    .o-formula-assistant-container {
+      box-shadow: 0 1px 4px 3px rgba(60, 64, 67, 0.15);
+    }
   }
 
   /* Custom css to highlight topbar composer on focus */
@@ -82,6 +85,11 @@ css/* scss */ `
     border: 1px solid ${SELECTION_BORDER_COLOR};
   }
 `;
+
+export interface AutocompleteValue {
+  text: string;
+  description: string;
+}
 
 interface Props {
   inputStyle: string;
@@ -99,8 +107,8 @@ interface ComposerState {
 
 interface AutoCompleteState {
   showProvider: boolean;
-  provider: string;
-  search: string;
+  selectedIndex: number;
+  values: AutocompleteValue[];
 }
 
 interface FunctionDescriptionState {
@@ -118,7 +126,6 @@ export class Composer extends Component<Props, SpreadsheetChildEnv> {
   };
 
   composerRef = useRef("o_composer");
-  private autocompleteAPI?: TextValueProviderApi;
 
   contentHelper: ContentEditableHelper = new ContentEditableHelper(this.composerRef.el!);
 
@@ -129,8 +136,8 @@ export class Composer extends Component<Props, SpreadsheetChildEnv> {
 
   autoCompleteState: AutoCompleteState = useState({
     showProvider: false,
-    provider: "functions",
-    search: "",
+    values: [],
+    selectedIndex: 0,
   });
 
   functionDescriptionState: FunctionDescriptionState = useState({
@@ -161,8 +168,6 @@ export class Composer extends Component<Props, SpreadsheetChildEnv> {
     }
     return `width:${ASSISTANT_WIDTH}px;`;
   }
-
-  borderStyle = `box-shadow: 0 1px 4px 3px rgba(60, 64, 67, 0.15);`;
 
   // we can't allow input events to be triggered while we remove and add back the content of the composer in processContent
   shouldProcessInputEvents: boolean = false;
@@ -207,9 +212,9 @@ export class Composer extends Component<Props, SpreadsheetChildEnv> {
     if (this.env.model.getters.isSelectingForComposer()) {
       this.functionDescriptionState.showDescription = false;
       // Prevent the default content editable behavior which moves the cursor
-      // but don't stop the event and let it bubble to the grid which will
-      // update the selection accordingly
       ev.preventDefault();
+      ev.stopPropagation();
+      updateSelectionWithArrowKeys(ev, this.env.model.selection);
       return;
     }
     const content = this.env.model.getters.getCurrentContent();
@@ -221,18 +226,24 @@ export class Composer extends Component<Props, SpreadsheetChildEnv> {
       this.env.model.dispatch("STOP_EDITION");
       return;
     }
+    // All arrow keys are processed: up and down should move autocomplete, left
+    // and right should move the cursor.
     ev.stopPropagation();
+    this.handleArrowKeysForAutocomplete(ev);
+  }
+
+  private handleArrowKeysForAutocomplete(ev: KeyboardEvent) {
     // only for arrow up and down
-    if (
-      ["ArrowUp", "ArrowDown"].includes(ev.key) &&
-      this.autoCompleteState.showProvider &&
-      this.autocompleteAPI
-    ) {
+    if (["ArrowUp", "ArrowDown"].includes(ev.key) && this.autoCompleteState.showProvider) {
       ev.preventDefault();
       if (ev.key === "ArrowUp") {
-        this.autocompleteAPI.moveUp();
+        this.autoCompleteState.selectedIndex--;
+        if (this.autoCompleteState.selectedIndex < 0) {
+          this.autoCompleteState.selectedIndex = this.autoCompleteState.values.length - 1;
+        }
       } else {
-        this.autocompleteAPI.moveDown();
+        this.autoCompleteState.selectedIndex =
+          (this.autoCompleteState.selectedIndex + 1) % this.autoCompleteState.values.length;
       }
     }
   }
@@ -240,8 +251,9 @@ export class Composer extends Component<Props, SpreadsheetChildEnv> {
   private processTabKey(ev: KeyboardEvent) {
     ev.preventDefault();
     ev.stopPropagation();
-    if (this.autoCompleteState.showProvider && this.autocompleteAPI) {
-      const autoCompleteValue = this.autocompleteAPI.getValueToFill();
+    if (this.autoCompleteState.showProvider) {
+      const autoCompleteValue =
+        this.autoCompleteState.values[this.autoCompleteState.selectedIndex]?.text;
       if (autoCompleteValue) {
         this.autoComplete(autoCompleteValue);
         return;
@@ -262,8 +274,9 @@ export class Composer extends Component<Props, SpreadsheetChildEnv> {
     ev.preventDefault();
     ev.stopPropagation();
     this.isKeyStillDown = false;
-    if (this.autoCompleteState.showProvider && this.autocompleteAPI) {
-      const autoCompleteValue = this.autocompleteAPI.getValueToFill();
+    if (this.autoCompleteState.showProvider) {
+      const autoCompleteValue =
+        this.autoCompleteState.values[this.autoCompleteState.selectedIndex]?.text;
       if (autoCompleteValue) {
         this.autoComplete(autoCompleteValue);
         return;
@@ -336,8 +349,7 @@ export class Composer extends Component<Props, SpreadsheetChildEnv> {
     ev.stopPropagation();
     this.autoCompleteState.showProvider = false;
     if (ev.ctrlKey && ev.key === " ") {
-      this.autoCompleteState.search = "";
-      this.autoCompleteState.showProvider = true;
+      this.showAutocomplete("");
       this.env.model.dispatch("STOP_COMPOSER_RANGE_SELECTION");
       return;
     }
@@ -354,6 +366,22 @@ export class Composer extends Component<Props, SpreadsheetChildEnv> {
 
     this.processTokenAtCursor();
     this.processContent();
+  }
+
+  showAutocomplete(searchTerm: string) {
+    this.autoCompleteState.showProvider = true;
+    let values = Object.entries(functionRegistry.content).map(([text, { description }]) => {
+      return {
+        text,
+        description,
+      };
+    });
+
+    values = values
+      .filter((t) => t.text.toUpperCase().startsWith(searchTerm.toUpperCase()))
+      .sort((l, r) => (l.text < r.text ? -1 : l.text > r.text ? 1 : 0));
+    this.autoCompleteState.values = values.slice(0, 10);
+    this.autoCompleteState.selectedIndex = 0;
   }
 
   onMousedown(ev: MouseEvent) {
@@ -380,10 +408,6 @@ export class Composer extends Component<Props, SpreadsheetChildEnv> {
 
   onBlur() {
     this.isKeyStillDown = false;
-  }
-
-  onCompleted(text: string | undefined) {
-    text && this.autoComplete(text);
   }
 
   // ---------------------------------------------------------------------------
@@ -515,8 +539,7 @@ export class Composer extends Component<Props, SpreadsheetChildEnv> {
           (tokenAtCursor.type === "SYMBOL" && !rangeReference.test(xc))
         ) {
           // initialize Autocomplete Dropdown
-          this.autoCompleteState.search = tokenAtCursor.value;
-          this.autoCompleteState.showProvider = true;
+          this.showAutocomplete(tokenAtCursor.value);
         } else if (tokenAtCursor.functionContext && tokenAtCursor.type !== "UNKNOWN") {
           // initialize Formula Assistant
           const tokenContext = tokenAtCursor.functionContext;
@@ -546,7 +569,7 @@ export class Composer extends Component<Props, SpreadsheetChildEnv> {
         }
 
         const tokens = this.env.model.getters.getCurrentTokens();
-        if (this.autoCompleteState.provider && tokens.length) {
+        if (tokens.length) {
           value += "(";
 
           const currentTokenIndex = tokens.map((token) => token.start).indexOf(tokenAtCursor.start);

src/components/composer/composer/composer.xml
@@ -22,18 +22,15 @@
         t-att-style="assistantStyle">
         <TextValueProvider
           t-if="autoCompleteState.showProvider"
-          exposeAPI="(api) => this.autocompleteAPI = api"
-          search="autoCompleteState.search"
-          provider="autoCompleteState.provider"
-          onCompleted="(text) => this.onCompleted(text)"
-          borderStyle="borderStyle"
+          values="autoCompleteState.values"
+          selectedIndex="autoCompleteState.selectedIndex"
+          onValueSelected.bind="this.autoComplete"
         />
         <FunctionDescriptionProvider
           t-if="functionDescriptionState.showDescription"
           functionName="functionDescriptionState.functionName"
           functionDescription="functionDescriptionState.functionDescription"
           argToFocus="functionDescriptionState.argToFocus"
-          borderStyle="borderStyle"
         />
       </div>
     </div>

src/components/composer/formula_assistant/formula_assistant.ts
@@ -59,7 +59,6 @@ interface Props {
   functionName: string;
   functionDescription: FunctionDescription;
   argToFocus: number;
-  borderStyle: string;
 }
 
 interface AssistantState {
@@ -101,5 +100,4 @@ FunctionDescriptionProvider.props = {
   functionName: String,
   functionDescription: Object,
   argToFocus: Number,
-  borderStyle: String,
 };

src/components/composer/formula_assistant/formula_assistant.xml
@@ -2,7 +2,6 @@
   <t t-name="o-spreadsheet-FunctionDescriptionProvider" owl="1">
     <div
       class="o-formula-assistant-container"
-      t-att-style="props.borderStyle"
       t-att-class="{
           'o-formula-assistant-event-none': assistantState.allowCellSelectionBehind,
           'o-formula-assistant-event-auto': !assistantState.allowCellSelectionBehind

commit a228c256ed0fcec4de78ba0bef55b4cbc52954e0

[FIX] selection_input: traceback when using unbounded ranges
3660bf27 introduced a `toZone()` in the handle of `FOCUS/CHANGE_RANGE`, but
this was a place were the xc was possibly the xc of an unbounded zone, thus
creating a traceback.

Odoo task 3082065

src/plugins/ui_feature/selection_inputs_manager.ts
@@ -1,4 +1,4 @@
-import { positionToZone, rangeReference, toZone } from "../../helpers/index";
+import { positionToZone, rangeReference } from "../../helpers/index";
 import { Command, CommandResult, Highlight, LAYERS, UID } from "../../types/index";
 import { UIPlugin, UIPluginConfig } from "../ui_plugin";
 import { RangeInputValue, SelectionInputPlugin } from "./selection_input";
@@ -80,7 +80,8 @@ export class SelectionInputsManagerPlugin extends UIPlugin {
         if (cmd.id !== this.focusedInputId) {
           const input = this.inputs[cmd.id];
           const range = input.ranges.find((range) => range.id === cmd.rangeId);
-          const zone = toZone(range?.xc || "A1");
+          const sheetId = this.getters.getActiveSheetId();
+          const zone = this.getters.getRangeFromSheetXC(sheetId, range?.xc || "A1").zone;
           this.selection.capture(
             input,
             { cell: { col: zone.left, row: zone.top }, zone },

tests/plugins/selection_input.test.ts
@@ -636,4 +636,12 @@ describe("selection input plugin", () => {
     moveAnchorCell(model, "down");
     expect(highlightedZones(model)).toEqual(["A3"]);
   });
+
+  test("focus and change range with unbounded ranges", () => {
+    model.dispatch("ENABLE_NEW_SELECTION_INPUT", { id, initialRanges: ["A:A"] });
+    model.dispatch("FOCUS_RANGE", { id, rangeId: idOfRange(model, id, 0) });
+    expect(model.getters.getSelectionInput(id)[0].xc).toBe("A:A");
+    model.dispatch("CHANGE_RANGE", { id, rangeId: idOfRange(model, id, 0), value: "1:1" });
+    expect(model.getters.getSelectionInput(id)[0].xc).toBe("1:1");
+  });
 });