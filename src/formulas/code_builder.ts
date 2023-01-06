1a3a4a216704fdcceb7d4045fbaba84ae3ba2dea
Lucas Lefèvre <lul@odoo.com>
2023-01-06 15:17:03 +0100

    [REF] compiler: use code builder
    The goal here is to have more flexibility in the way we
    assign (or not) intermediate compiled AST nodes to a variable before
    using it later. This prepares the ground for automatic function argument
    casting.
    
    Currently, `compileAST` function decides to assign the result or not.
    
    Notice how `isLazy` parameter of `compileAST` is used.
    One of its effect is to assign (or not) the result to a variable
    for the caller to use. (how is that linked to laziness? 🧐)
    It should be the caller's responsibility to do it. Only the caller
    knows what it will do with the result and if it makes sense or not.
    
    This brings noise to `compileAST` with early returns and temporary
    `statement` variable.
    
    It allows to remove `isLazy` from `compileAST`'s parameters.
    That makes sense because laziness only makes sense when compiling function
    arguments. It is not a property of any AST node itself.
    Now laziness is located in `compileFunctionArgs` only.
    
    Task 3134477

---
+++ src/formulas/code_builder.ts
@@ -0,0 +1,106 @@
+/**
+ * Block of code that produces a value.
+ */
+export interface FunctionCode {
+  readonly returnExpression: string;
+  /**
+   * Return the same function code but wrapped in a closure.
+   */
+  wrapInClosure(): FunctionCode;
+  /**
+   * Return the same function code but with the return expression assigned to a variable.
+   */
+  assignResultToVariable(): FunctionCode;
+}
+
+export class FunctionCodeBuilder {
+  private code: string = "";
+
+  constructor(private scope: Scope = new Scope()) {}
+
+  append(...lines: (string | FunctionCode)[]) {
+    this.code += lines.map((line) => line.toString()).join("\n") + "\n";
+  }
+
+  return(expression: string): FunctionCode {
+    return new FunctionCodeImpl(this.scope, this.code, expression);
+  }
+
+  toString(): string {
+    return indentCode(this.code);
+  }
+}
+
+class FunctionCodeImpl implements FunctionCode {
+  private readonly code: string;
+  constructor(private readonly scope: Scope, code: string, readonly returnExpression: string) {
+    this.code = indentCode(code);
+  }
+
+  toString(): string {
+    return this.code;
+  }
+
+  wrapInClosure(): FunctionCode {
+    const closureName = this.scope.nextVariableName();
+    const code = new FunctionCodeBuilder(this.scope);
+    code.append(`const ${closureName} = () => {`);
+    code.append(this.code);
+    code.append(`return ${this.returnExpression};`);
+    code.append(`}`);
+    return code.return(closureName);
+  }
+
+  assignResultToVariable(): FunctionCode {
+    if (this.scope.isAlreadyDeclared(this.returnExpression)) {
+      return this;
+    }
+    const variableName = this.scope.nextVariableName();
+    const code = new FunctionCodeBuilder(this.scope);
+    code.append(this.code);
+    code.append(`const ${variableName} = ${this.returnExpression};`);
+    return code.return(variableName);
+  }
+}
+
+export class Scope {
+  private nextId = 1;
+  private declaredVariables: Set<string> = new Set();
+
+  nextVariableName(): string {
+    const name = `_${this.nextId++}`;
+    this.declaredVariables.add(name);
+    return name;
+  }
+
+  isAlreadyDeclared(name: string): boolean {
+    return this.declaredVariables.has(name);
+  }
+}
+
+/**
+ * Takes a list of strings that might be single or multiline
+ * and maps them in a list of single line strings.
+ */
+function splitLines(str: string): string[] {
+  return str
+    .split("\n")
+    .map((line) => line.trim())
+    .filter((line) => line !== "");
+}
+
+function indentCode(code: string): string {
+  let result = "";
+  let indentLevel = 0;
+  const lines = splitLines(code);
+  for (const line of lines) {
+    if (line.startsWith("}")) {
+      indentLevel--;
+    }
+    result += "\t".repeat(indentLevel) + line + "\n";
+    if (line.endsWith("{")) {
+      indentLevel++;
+    }
+  }
+  return result.trim();
+}
--- src/formulas/compiler.ts
+++ src/formulas/compiler.ts
@@ -4,6 +4,7 @@ import { concat, parseNumber, removeStringQuotes } from "../helpers";
 import { _lt } from "../translation";
 import { CompiledFormula } from "../types";
 import { BadExpressionError } from "../types/errors";
+import { FunctionCode, FunctionCodeBuilder, Scope } from "./code_builder";
 import { AST, ASTFuncall, parseTokens } from "./parser";
 import { rangeTokenize } from "./range_tokenizer";
 
@@ -28,39 +29,6 @@ const UNARY_OPERATOR_MAP = {
   "-": "UMINUS",
   "+": "UPLUS",
   "%": "UNARY.PERCENT",
-};
-
-/**
- * Takes a list of strings that might be single or multiline
- * and maps them in a list of single line strings.
- */
-function splitCodeLines(codeBlocks: string[]): string[] {
-  return codeBlocks
-    .join("\n")
-    .split("\n")
-    .filter((line) => line.trim() !== "");
-}
-/**
- * Used as intermediate compilation.
- * Formula `=SUM(|0|, |1|)` gives the following code.
- * ```js
- * let _2 = range(deps[0])
- * let _3 = range(deps[1])
- * ctx.__lastFnCalled = 'SUM'
- * let _1 = ctx['SUM'](_2,_3)
- * ```
- * The result id is `_1`.
- */
-type CompiledAST = {
-  /**
-   * The result of the code is stored in this identifier.
-   * Can be a variable or a primitive value.
-   */
-  id: string;
-  /**
-   * String containing the compiled code
-   */
-  code: string;
 };
 
 interface ConstantValues {
@@ -89,7 +57,7 @@ export function compile(formula: string): CompiledFormula {
   const cacheKey = compilationCacheKey(tokens, dependencies, constantValues);
   if (!functionCache[cacheKey]) {
     const ast = parseTokens([...tokens]);
-    let nextId = 1;
+    const scope = new Scope();
 
     if (ast.type === "BIN_OPERATION" && ast.value === ":") {
       throw new BadExpressionError(_lt("Invalid formula"));
@@ -98,17 +66,16 @@ export function compile(formula: string): CompiledFormula {
       throw new BadExpressionError(_lt("Invalid formula"));
     }
     const compiledAST = compileAST(ast);
-    const code = splitCodeLines([
-      `// ${cacheKey}`,
-      compiledAST.code,
-      `return ${compiledAST.id};`,
-    ]).join("\n");
+    const code = new FunctionCodeBuilder();
+    code.append(`// ${cacheKey}`);
+    code.append(compiledAST);
+    code.append(`return ${compiledAST.returnExpression};`);
     let baseFunction = new Function(
       "deps", // the dependencies in the current formula
       "ref", // a function to access a certain dependency at a given index
       "range", // same as above, but guarantee that the result is in the form of a range
       "ctx",
-      code
+      code.toString()
     );
 
     functionCache[cacheKey] = {
@@ -123,7 +90,7 @@ export function compile(formula: string): CompiledFormula {
      * the cell value into a range. This allow the grid model to differentiate
      * between a cell value and a non cell value.
      */
-    function compileFunctionArgs(ast: ASTFuncall): CompiledAST[] {
+    function compileFunctionArgs(ast: ASTFuncall): FunctionCode[] {
       const functionDefinition = functions[ast.value.toUpperCase()];
       const currentFunctionArguments = ast.args;
 
@@ -169,7 +136,7 @@ export function compile(formula: string): CompiledFormula {
         }
       }
 
-      let listArgs: CompiledAST[] = [];
+      let compiledArgs: FunctionCode[] = [];
       for (let i = 0; i < nbrArg; i++) {
         const argPosition = functionDefinition.getArgToFocus(i + 1) - 1;
         if (0 <= argPosition && argPosition < functionDefinition.args.length) {
@@ -211,15 +178,15 @@ export function compile(formula: string): CompiledFormula {
             }
           }
 
-          const compiledAST = compileAST(currentArg, isLazy, isMeta, hasRange, {
+          const compiledAST = compileAST(currentArg, isMeta, hasRange, {
             functionName: ast.value.toUpperCase(),
             paramIndex: i + 1,
           });
-          listArgs.push(compiledAST);
+          compiledArgs.push(isLazy ? compiledAST.wrapInClosure() : compiledAST);
         }
       }
 
-      return listArgs;
+      return compiledArgs;
     }
 
     /**
@@ -227,123 +194,85 @@ export function compile(formula: string): CompiledFormula {
      * executable code for the evaluation of the cells content. It uses a cash to
      * not reevaluate identical code structures.
      *
-     * The function is sensitive to two parameters “isLazy” and “isMeta”. These
-     * parameters may vary when compiling function arguments:
-     *
-     * - isLazy: In some cases the function arguments does not need to be
-     * evaluated before entering the functions. For example the IF function might
-     * take invalid arguments that do not need to be evaluate and thus should not
-     * create an error. For this we have lazy arguments.
-     *
-     * - isMeta: In some cases the function arguments expects information on the
+     * The function is sensitive to parameter “isMeta”. This
+     * parameter may vary when compiling function arguments:
+     * isMeta: In some cases the function arguments expects information on the
      * cell/range other than the associated value(s). For example the COLUMN
      * function needs to receive as argument the coordinates of a cell rather
      * than its value. For this we have meta arguments.
      */
-
     function compileAST(
       ast: AST,
-      isLazy = false,
       isMeta = false,
       hasRange = false,
       referenceVerification: {
         functionName?: string;
         paramIndex?: number;
       } = {}
-    ): CompiledAST {
-      const codeBlocks: string[] = [];
-      let id, fnName, statement;
+    ): FunctionCode {
+      const code = new FunctionCodeBuilder(scope);
       if (ast.type !== "REFERENCE" && !(ast.type === "BIN_OPERATION" && ast.value === ":")) {
         if (isMeta) {
           throw new BadExpressionError(_lt(`Argument must be a reference to a cell or range.`));
         }
       }
       if (ast.debug) {
-        codeBlocks.push("debugger;");
+        code.append("debugger;");
       }
       switch (ast.type) {
         case "BOOLEAN":
-          if (!isLazy) {
-            return { id: `{ value: ${ast.value} }`, code: "" };
-          }
-          id = nextId++;
-          statement = `{ value: ${ast.value} }`;
-          break;
+          return code.return(`{ value: ${ast.value} }`);
         case "NUMBER":
-          id = nextId++;
-          statement = `{ value: this.constantValues.numbers[${constantValues.numbers.indexOf(
-            ast.value
-          )}] }`;
-          break;
+          return code.return(
+            `{ value: this.constantValues.numbers[${constantValues.numbers.indexOf(ast.value)}] }`
+          );
         case "STRING":
-          id = nextId++;
-          statement = `{ value: this.constantValues.strings[${constantValues.strings.indexOf(
-            ast.value
-          )}] }`;
-          break;
+          return code.return(
+            `{ value: this.constantValues.strings[${constantValues.strings.indexOf(ast.value)}] }`
+          );
         case "REFERENCE":
           const referenceIndex = dependencies.indexOf(ast.value);
-          id = nextId++;
           if (hasRange) {
-            statement = `range(deps[${referenceIndex}])`;
+            return code.return(`range(deps[${referenceIndex}])`);
           } else {
-            statement = `ref(deps[${referenceIndex}], ${isMeta ? "true" : "false"}, "${
-              referenceVerification.functionName || OPERATOR_MAP["="]
-            }",  ${referenceVerification.paramIndex})`;
+            return code.return(
+              `ref(deps[${referenceIndex}], ${isMeta ? "true" : "false"}, "${
+                referenceVerification.functionName || OPERATOR_MAP["="]
+              }",  ${referenceVerification.paramIndex})`
+            );
           }
-          break;
         case "FUNCALL":
-          id = nextId++;
-          const args = compileFunctionArgs(ast);
-          codeBlocks.push(args.map((arg) => arg.code).join("\n"));
-          fnName = ast.value.toUpperCase();
-          codeBlocks.push(`ctx.__lastFnCalled = '${fnName}';`);
-          statement = `ctx['${fnName}'](${args.map((arg) => arg.id)})`;
-          break;
+          const args = compileFunctionArgs(ast).map((arg) => arg.assignResultToVariable());
+          code.append(...args);
+          const fnName = ast.value.toUpperCase();
+          code.append(`ctx.__lastFnCalled = '${fnName}';`);
+          return code.return(`ctx['${fnName}'](${args.map((arg) => arg.returnExpression)})`);
         case "UNARY_OPERATION": {
-          id = nextId++;
-          fnName = UNARY_OPERATOR_MAP[ast.value];
-          const operand = compileAST(ast.operand, false, false, false, {
+          const fnName = UNARY_OPERATOR_MAP[ast.value];
+          const operand = compileAST(ast.operand, false, false, {
             functionName: fnName,
-          });
-          codeBlocks.push(operand.code);
-          codeBlocks.push(`ctx.__lastFnCalled = '${fnName}';`);
-          statement = `ctx['${fnName}'](${operand.id})`;
-          break;
+          }).assignResultToVariable();
+          code.append(operand);
+          code.append(`ctx.__lastFnCalled = '${fnName}';`);
+          return code.return(`ctx['${fnName}'](${operand.returnExpression})`);
         }
         case "BIN_OPERATION": {
-          id = nextId++;
-          fnName = OPERATOR_MAP[ast.value];
-          const left = compileAST(ast.left, false, false, false, {
+          const fnName = OPERATOR_MAP[ast.value];
+          const left = compileAST(ast.left, false, false, {
             functionName: fnName,
-          });
-          const right = compileAST(ast.right, false, false, false, {
+          }).assignResultToVariable();
+          const right = compileAST(ast.right, false, false, {
             functionName: fnName,
-          });
-          codeBlocks.push(left.code);
-          codeBlocks.push(right.code);
-          codeBlocks.push(`ctx.__lastFnCalled = '${fnName}';`);
-          statement = `ctx['${fnName}'](${left.id}, ${right.id})`;
-          break;
+          }).assignResultToVariable();
+          code.append(left);
+          code.append(right);
+          code.append(`ctx.__lastFnCalled = '${fnName}';`);
+          return code.return(
+            `ctx['${fnName}'](${left.returnExpression}, ${right.returnExpression})`
+          );
         }
         case "UNKNOWN":
-          if (!isLazy) {
-            return { id: "undefined", code: "" };
-          }
-          id = nextId++;
-          statement = `undefined`;
-          break;
-      }
-      if (isLazy) {
-        const lazyFunction =
-          `const _${id} = () => {\n` +
-          `\t${splitCodeLines(codeBlocks).join("\n\t")}\n` +
-          `\treturn ${statement};\n` +
-          "}";
-        return { id: `_${id}`, code: lazyFunction };
-      } else {
-        codeBlocks.push(`let _${id} = ${statement};`);
-        return { id: `_${id}`, code: codeBlocks.join("\n") };
+          return code.return("undefined");
       }
     }
   }
