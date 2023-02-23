322d0b085365b81e0a2f914d91e9f7be9d67d05a
Rémi Rahir (rar) <rar@odoo.com>
2023-02-23 13:53:32 +0100

    [FIX] figure,chart: Prevent destructive creation
    The fix introduced in commit bb2d957c was incorect as The allowDispatch
    set on figures is utterly useless as we never dispatch the 'CREATE_FIGURE'
    command from outside the plugin, it's always dispatched by `ChartPlugin`ATM.
    
    This commit fixes the allowdispatch method in `FigurePlugin` for the form
    but also introduces its equivalent in `ChartPlugin`.
    
    Task 3141532

--- tests/xlsx_export.test.ts
+++ tests/xlsx_export.test.ts
@@ -754,7 +754,7 @@ describe("Test XLSX export", () => {
           labelRange: "Sheet1!A2:A4",
           type: "bar",
         },
-        "1"
+        "2"
       );
       expect(await exportPrettifiedXlsx(model)).toMatchSnapshot();
     });
