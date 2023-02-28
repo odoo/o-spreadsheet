a9e13b4070155bc732e809865c6bcb0aaefce723
Rémi Rahir <rar@odoo.com>
2023-02-28 16:37:14 +0000

    [FIX] chart: Remove forwardport comments
    In a haste to close a tiresome forward port, I forgot to address some of
    the `TODO` comments I left myself...
    
    Task: 3210689
    X-original-commit: d07a3c5e52c5e9a0c43cc4849679c7fdb2b1f614

--- src/components/side_panel/chart/main_chart_panel/main_chart_panel.ts
+++ src/components/side_panel/chart/main_chart_panel/main_chart_panel.ts
@@ -93,7 +93,6 @@ export class ChartPanel extends Component<Props, SpreadsheetChildEnv> {
       ...(this.getChartDefinition() as T),
       ...updateDefinition,
     };
-    // TODORAR add a putain de test - c'est faux de nouveau ...
     return this.env.model.dispatch("UPDATE_CHART", {
       definition,
       id: this.figureId,
@@ -107,7 +106,6 @@ export class ChartPanel extends Component<Props, SpreadsheetChildEnv> {
       throw new Error("Chart not defined.");
     }
     const definition = getChartDefinitionFromContextCreation(context, type);
-    // TODORAR add a putain de test - c'est faux de nouveau ...
     this.env.model.dispatch("UPDATE_CHART", {
       definition,
       id: this.figureId,
