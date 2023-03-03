5ff125828378c49e4f1aa60a3abfcc82fb5343e6
Adrien Minne (adrm) <adrm@odoo.com>
2023-03-03 10:54:39 +0000

    [FIX] bundle_xml: small fixes for script bundling the xmls
    Two issues :
    1) bundle_xml script had the wrong default value if the argument "--outDir"
    wasn't specified.
    2) the syntax 'build:xml ...' in package.json wasn't working in windows.
    Replaced `'build...'` by `\"build...\"` to fix it.
    
    closes odoo/o-spreadsheet#2160
    
    X-original-commit: 6a30375ad54b92dd90fe6920e47a97258faa6902
    Signed-off-by: Rémi Rahir (rar) <rar@odoo.com>
    Signed-off-by: Minne Adrien (adrm) <adrm@odoo.com>

--- tools/bundle_xml/main.js
+++ tools/bundle_xml/main.js
@@ -1,6 +1,8 @@
 const bundle = require("./bundle_xml_templates");
 
-const outDirAgIndex = process.argv.findIndex((arg) => arg === "--outDir") + 1;
-const outDir = process.argv[outDirAgIndex] || "dist";
+const DEFAULT_DIR = "dist";
 
-bundle.writeOwlTemplateBundleToFile(outDir);
+const outDirFlagIndex = process.argv.findIndex((arg) => arg === "--outDir");
+const outDir = outDirFlagIndex !== -1 ? process.argv[outDirFlagIndex + 1] : DEFAULT_DIR;
+
+bundle.writeOwlTemplateBundleToFile(outDir || DEFAULT_DIR);
