7cad278dbf87e52862181a7ef9d3f339302e42d2
Lucas Lefèvre <lul@odoo.com>
2023-03-08 14:37:19 +0000

    [FIX] package: correctly name package entry files
    Files are named o-spreadsheet[...] in package.json but
    rollup generates o_spreadsheet[...] files
    
    as a result any import fails because it's looking for a file that doesn't exist
    
    `import { Model } from @odoo/o-spreadsheet`
    
    => Error: Cannot find module '/home/odoo/odoo/upgrade-spreadsheet-data/node_modules/@odoo/o-spreadsheet/dist/o-spreadsheet.cjs.js'.
       Please verify that the package.json has a valid "main" entry
    
    X-original-commit: ebc1eedcfd086261d67fb40536db405b2d00db05

--- rollup.config.js
+++ rollup.config.js
@@ -25,7 +25,7 @@ __info__.hash = '${commitHash}';
  */
 function getConfigForFormat(format, minified = false) {
   return {
-    file: minified ? `dist/o_spreadsheet.${format}.min.js` : `dist/o_spreadsheet.${format}.js`,
+    file: minified ? `dist/o-spreadsheet.${format}.min.js` : `dist/o-spreadsheet.${format}.js`,
     format,
     name: "o_spreadsheet",
     extend: true,
@@ -78,7 +78,7 @@ export default (commandLineArgs) => {
       },
       {
         input: "dist/types/index.d.ts",
-        output: [{ file: "dist/o_spreadsheet.d.ts", format: "es" }],
+        output: [{ file: "dist/o-spreadsheet.d.ts", format: "es" }],
         plugins: [dts(), nodeResolve()],
       },
     ];
