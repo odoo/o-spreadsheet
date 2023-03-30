c712b02990660b26c15d8a9f5dbb8830ad160e5b
Adrien Minne (adrm) <adrm@odoo.com>
2023-03-30 15:14:35 +0000

    [IMP] xlsx_import: support more date formats
    We now try to parse and modify the xlsx date formats at import to get a
    format supported by o_spreadsheet.
    
    Odoo task 3222986
    
    closes odoo/o-spreadsheet#2293
    
    Signed-off-by: Lucas Lefèvre (lul) <lul@odoo.com>

--- src/constants.ts
+++ src/constants.ts
@@ -148,7 +148,7 @@ export const DEFAULT_BORDER_DESC: BorderDescr = ["thin", "#000"];
 export const DEFAULT_FILTER_BORDER_DESC: BorderDescr = ["thin", FILTERS_COLOR];
 
 // DateTimeRegex
-export const DATETIME_FORMAT = /[ymd:]/;
+export const DATETIME_FORMAT = /[ymdhs:]/;
 
 // Ranges
 export const INCORRECT_RANGE_STRING = CellErrorType.InvalidReference;
