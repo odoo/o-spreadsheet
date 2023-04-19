f8490e90ea99820138b4f05875144a888ffcaf3c
Adrien Minne (adrm) <adrm@odoo.com>
2023-04-19 13:45:04 +0000

    [IMP] *: use deepCopy instead of JSON.parse(JSON.stringify)
    closes odoo/o-spreadsheet#2375
    
    Task: 3272878
    Signed-off-by: Rémi Rahir (rar) <rar@odoo.com>
    Signed-off-by: Minne Adrien (adrm) <adrm@odoo.com>

--- src/model.ts
+++ src/model.ts
@@ -589,7 +589,7 @@ export class Model extends EventBus<any> implements CommandDispatcher {
       }
     }
     data.revisionId = this.session.getRevisionId() || DEFAULT_REVISION_ID;
-    data = JSON.parse(JSON.stringify(data));
+    data = deepCopy(data);
     return data;
   }
 
@@ -619,7 +619,7 @@ export class Model extends EventBus<any> implements CommandDispatcher {
         handler.exportForExcel(data);
       }
     }
-    data = JSON.parse(JSON.stringify(data));
+    data = deepCopy(data);
 
     return getXLSX(data);
   }
