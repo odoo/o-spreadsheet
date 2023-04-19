157a22e8e07424f8e95830d5a2e4046947f40b7e
Adrien Minne (adrm) <adrm@odoo.com>
2023-04-19 15:29:46 +0200

    rcToCellPosition ~2x faster

--- src/plugins/ui_core_views/evaluation.ts
+++ src/plugins/ui_core_views/evaluation.ts
@@ -1006,11 +1006,17 @@ export class EvaluationPlugin extends UIPlugin {
 }
 
 function cellPositionToRc(position: CellPosition): string {
-  return `${position.sheetId}!${position.col}!${position.row}`;
+  return `${position.row}!${position.col}!${position.sheetId}`;
 }
 
 function rcToCellPosition(rc: string): CellPosition {
-  const [sheetId, col, row] = rc.split("!");
+  // faster than  a split("!") by a factor 2
+  const i1 = rc.indexOf("!");
+  const row = rc.slice(0, i1);
+  const position = rc.slice(i1 + 1);
+  const i2 = position.indexOf("!");
+  const col = position.slice(0, i2);
+  const sheetId = position.slice(i2 + 1);
   return { sheetId, col: Number(col), row: Number(row) };
 }
 
