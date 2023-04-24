92e7513417ce31e6b28db36f6def1ecf12746ffd
Adrien Minne (adrm) <adrm@odoo.com>
2023-04-24 15:30:41 +0200

    [REF] misc: avoid regex in `toCartesian`
    Replace the regex.match inside `toCartesian` with a loop through the string
    to find the number/letter part of the XC. This is uglier, but ~2x faster.
    
    This saves ~100ms on loading a 26*10_000 formula sheet.

--- src/helpers/coordinates.ts
+++ src/helpers/coordinates.ts
@@ -3,7 +3,6 @@
 //------------------------------------------------------------------------------
 
 import { HeaderIndex, Position, RangePart } from "../types";
-import { cellReference } from "./references";
 
 /**
  * Convert a (col) number to the corresponding letter.
@@ -25,6 +24,14 @@ export function numberToLetters(n: number): string {
   }
 }
 
+const LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" as const;
+const LETTERS_NUMBER_MAPPING = {};
+for (const letter of LETTERS) {
+  const colIndex = letter.charCodeAt(0) - 64;
+  LETTERS_NUMBER_MAPPING[letter] = colIndex;
+  LETTERS_NUMBER_MAPPING[letter.toLowerCase()] = colIndex;
+}
+
 /**
  * Convert a string (describing a column) to its number value.
  *
@@ -38,11 +45,19 @@ export function lettersToNumber(letters: string): number {
   const l = letters.length;
   let pow = 1;
   for (let i = l - 1; i >= 0; i--) {
-    const charCode = letters[i].charCodeAt(0) - 64;
+    const charCode = LETTERS_NUMBER_MAPPING[letters[i]];
     result += charCode * pow;
     pow *= 26;
   }
   return result;
+}
+
+function isCharALetter(char: string) {
+  return (char >= "A" && char <= "Z") || (char >= "a" && char <= "z");
+}
+
+function isCharADigit(char: string) {
+  return char >= "0" && char <= "9";
 }
 
 /**
@@ -55,17 +70,56 @@ export function lettersToNumber(letters: string): number {
  * Note: it also accepts lowercase coordinates, but not fixed references
  */
 export function toCartesian(xc: string): Position {
-  xc = xc.toUpperCase().trim();
-  const match = xc.match(cellReference);
-  if (match !== null) {
-    const [m, letters, numbers] = match;
-    if (m === xc) {
-      const col = lettersToNumber(letters);
-      const row = parseInt(numbers, 10) - 1;
-      return { col, row };
+  xc = xc.trim();
+  let numberPartStart: number | undefined = undefined;
+
+  // Note: looping by hand is uglier but ~2x faster than using a regex to match number/letter parts
+  for (let i = 0; i < xc.length; i++) {
+    const char = xc[i];
+
+    // as long as we haven't found the number part, keep advancing
+    if (!numberPartStart) {
+      if ((char === "$" && i === 0) || isCharALetter(char)) {
+        continue;
+      }
+      numberPartStart = i;
+    }
+
+    // Number part
+    if (!isCharADigit(char)) {
+      if (char === "$" && i === numberPartStart) {
+        continue;
+      }
+      throw new Error(`Invalid cell description: ${xc}`);
     }
   }
-  throw new Error(`Invalid cell description: ${xc}`);
+
+  if (!numberPartStart || numberPartStart === xc.length) {
+    throw new Error(`Invalid cell description: ${xc}`);
+  }
+
+  const letterPart = xc[0] === "$" ? xc.slice(1, numberPartStart) : xc.slice(0, numberPartStart);
+  const numberPart =
+    xc[numberPartStart] === "$" ? xc.slice(numberPartStart + 1) : xc.slice(numberPartStart);
+
+  // limit to max 3 letters and 7 numbers to avoid
+  // gigantic numbers that would be a performance killer
+  // down the road
+  if (
+    letterPart.length < 1 ||
+    letterPart.length > 3 ||
+    numberPart.length < 1 ||
+    numberPart.length > 7
+  ) {
+    throw new Error(`Invalid cell description: ${xc}`);
+  }
+
+  const col = lettersToNumber(letterPart);
+  const row = Number(numberPart) - 1;
+  if (isNaN(row)) {
+    throw new Error(`Invalid cell description: ${xc}`);
+  }
+  return { col, row };
 }
 
 /**
