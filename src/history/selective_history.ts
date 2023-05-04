0394a62fbec11a82ed237a0c1202d95187ea0091
Adrien Minne (adrm) <adrm@odoo.com>
2023-05-04 10:56:51 +0200

    [IMP] selective_history: name constructor arguments
    The constructor of the SelectiveHistory takes 5 arguments, but most of
    them are functions, and it's hard to see at first glance exactly what each
    argument does. Transform the arguments into a single object to have named
    arguments and make the call to the constructor a bit more readable.
    
    Task: 3264821

--- src/history/selective_history.ts
+++ src/history/selective_history.ts
@@ -9,6 +9,12 @@ export class SelectiveHistory<T = unknown> {
   private HEAD_OPERATION: Operation<T>;
   private tree: Tree<T>;
 
+  private initialOperationId: UID;
+  private applyOperation: (data: T) => void;
+  private revertOperation: (data: T) => void;
+  private buildEmpty: (id: UID) => T;
+  private readonly buildTransformation: TransformationFactory<T>;
+
   /**
    * The selective history is a data structure used to register changes/updates of a state.
    * Each change/update is called an "operation".
@@ -29,16 +35,25 @@ export class SelectiveHistory<T = unknown> {
    *                    (used for internal implementation)
    * @param buildTransformation Factory used to build transformations
    */
-  constructor(
-    initialOperationId: UID,
-    private applyOperation: (data: T) => void,
-    private revertOperation: (data: T) => void,
-    private buildEmpty: (id: UID) => T,
-    private readonly buildTransformation: TransformationFactory<T>
-  ) {
+  constructor(args: {
+    initialOperationId: UID;
+    applyOperation: (data: T) => void;
+    revertOperation: (data: T) => void;
+    buildEmpty: (id: UID) => T;
+    readonly buildTransformation: TransformationFactory<T>;
+  }) {
+    this.applyOperation = args.applyOperation;
+    this.revertOperation = args.revertOperation;
+    this.buildEmpty = args.buildEmpty;
+    this.initialOperationId = args.initialOperationId;
+    this.buildTransformation = args.buildTransformation;
+
     this.HEAD_BRANCH = new Branch<T>(this.buildTransformation);
-    this.tree = new Tree(buildTransformation, this.HEAD_BRANCH);
-    const initial = new Operation(initialOperationId, buildEmpty(initialOperationId));
+    this.tree = new Tree(this.buildTransformation, this.HEAD_BRANCH);
+    const initial = new Operation(
+      this.initialOperationId,
+      this.buildEmpty(this.initialOperationId)
+    );
     this.tree.insertOperationLast(this.HEAD_BRANCH, initial);
     this.HEAD_OPERATION = initial;
   }
