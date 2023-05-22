6d3dee52cc96516517cb1b6c7dfffb2cf4d25d8e
anhe-odoo <anhe@odoo.com>
2023-05-22 08:43:11 +0200

    [FIX] Chart Pannel: fixes updateChart process
    Taks Description
    
    In https://github.com/odoo/o-spreadsheet/pull/1613, we tried to fix
    an issue when updating a chart's title and then switching to
    another chart in the chart pannel without confirming the title change
    (ie without pressing enter key). This was done by introducing a
    shouldUpdateChart flag that was false when we try to update the wrong
    chart.
    
    The fix was working back then but in a strange and kind of dangerous
    way, leading us to another issue nowadays. The current issue is that,
    when opening a chart pannel and then switching to another chart, we
    cannot edit a property of the 2nd chart until we click another time
    on it, as the shouldUpdateChart was set to false (and stay in this
    state untill we reclick on the chart).
    
    Related Task(s):
    
    -Task-2961701 (old task with first attempt to fix)
    -Task-3323875 (current task)
    
    X-original-commit: e05bec092cdd153da0f9b8b2bb2a4e6df856e3e3

--- tests/components/charts.test.ts
+++ tests/components/charts.test.ts
@@ -286,6 +286,43 @@ describe("charts", () => {
     expect(model.getters.getChartDefinition("2").title).toBe("old_title_2");
   });
 
+  test("selecting a chart then selecting another chart and editing property change the second chart", async () => {
+    createChart(
+      model,
+      {
+        dataSets: ["C1:C4"],
+        labelRange: "A2:A4",
+        type: "line",
+        title: "old_title_1",
+      },
+      "1"
+    );
+    await nextTick();
+    createChart(
+      model,
+      {
+        dataSets: ["C1:C4"],
+        labelRange: "A2:A4",
+        type: "line",
+        title: "old_title_2",
+      },
+      "2"
+    );
+    await nextTick();
+
+    const figures = fixture.querySelectorAll(".o-figure");
+    await simulateClick(figures[0] as HTMLElement);
+    await simulateClick(".o-figure-menu-item");
+    await simulateClick(".o-menu div[data-name='edit']");
+    await simulateClick(".o-panel .inactive");
+    await simulateClick(figures[1] as HTMLElement);
+    await simulateClick(".o-chart-title input");
+    setInputValueAndTrigger(".o-chart-title input", "new_title", "change");
+
+    expect(model.getters.getChartDefinition("1").title).toBe("old_title_1");
+    expect(model.getters.getChartDefinition("2").title).toBe("new_title");
+  });
+
   test.each(["basicChart", "scorecard"])(
     "can edit charts %s background",
     async (chartType: string) => {
