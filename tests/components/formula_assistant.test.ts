1a06a0934669310358c684fb2fefefdc297094db
Lucas Lefèvre <lul@odoo.com>
2023-06-13 14:09:54 +0000

    [FIX] functions: translate argument description
    This commit fixes https://github.com/odoo/o-spreadsheet/commit/a815a1675431aa15d3b4e9ea252f8a5e444a164c
    
    Argument descriptions were still eagerly interpolated in a template
    string, before the translation had a chance to be loaded.
    
    closes odoo/o-spreadsheet#2601
    
    Signed-off-by: Rémi Rahir (rar) <rar@odoo.com>

--- tests/components/formula_assistant.test.ts
+++ tests/components/formula_assistant.test.ts
@@ -1,5 +1,8 @@
+import { setTranslationMethod } from "../../src";
 import { arg, functionRegistry } from "../../src/functions/index";
 import { Model } from "../../src/model";
+import { _lt } from "../../src/translation";
+import { registerCleanup } from "../setup/jest.setup";
 import { keyDown, keyUp } from "../test_helpers/dom_helper";
 import {
   clearFunctions,
@@ -34,7 +37,7 @@ describe("formula assistant", () => {
     });
     functionRegistry.add("FUNC1", {
       description: "func1 def",
-      args: [arg("f1Arg1 (any)", "f1 Arg1 def"), arg("f1Arg2 (any)", "f1 Arg2 def")],
+      args: [arg("f1Arg1 (any)", "f1 Arg1 def"), arg("f1Arg2 (any)", _lt("f1 Arg2 def"))],
       compute: () => 1,
       returns: ["ANY"],
     });
@@ -251,6 +254,23 @@ describe("formula assistant", () => {
         );
         expect(fixture.querySelectorAll(".o-formula-assistant-arg div")[3].textContent).toBe(
           "f1 Arg2 def"
+        );
+      });
+
+      test("argument description is translated", async () => {
+        // set the translation method after creating the argument.
+        // This is what actually happens because translations are
+        // loaded after the function definition
+        setTranslationMethod((str, ...values) => {
+          if (str === "f1 Arg2 def") {
+            return "translated description";
+          }
+          return str;
+        });
+        await typeInComposerGrid("=FUNC1(");
+        registerCleanup(() => setTranslationMethod((str, ...values) => str));
+        expect(fixture.querySelectorAll(".o-formula-assistant-arg div")[3].textContent).toBe(
+          "translated description"
         );
       });
 
