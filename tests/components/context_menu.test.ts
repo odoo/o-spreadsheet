odoo/o-spreadsheet

commit 8a9914a29b9955e370d0b4325b92e9430725bb4e

[FIX] ActionButton: update SVG icon to display current style in top bar
Upadated the SVG icon in the top bar to reflect the current style. The icon
now correctly displays the appropriate style based on the selected style.

Task- 3341969

closes odoo/o-spreadsheet#2642

Signed-off-by: Rémi Rahir (rar) <rar@odoo.com>

src/components/menu/menu.ts
@@ -170,8 +170,8 @@ export class Menu extends Component<Props, SpreadsheetChildEnv> {
   }
 
   getIconName(menu: Action) {
-    if (menu.icon) {
-      return menu.icon;
+    if (menu.icon(this.env)) {
+      return menu.icon(this.env);
     }
     if (menu.isActive?.(this.env)) {
       return "o-spreadsheet-Icon.CHECK";

tests/components/__snapshots__/bottom_bar.test.ts.snap
@@ -13,6 +13,9 @@ exports[`BottomBar component Can open the list of statistics 1`] = `
     <div
       class="d-flex w-100"
     >
+      <div
+        class="o-menu-item-icon align-middle"
+      />
       
       <div
         class="o-menu-item-name align-middle text-truncate"
@@ -32,6 +35,9 @@ exports[`BottomBar component Can open the list of statistics 1`] = `
     <div
       class="d-flex w-100"
     >
+      <div
+        class="o-menu-item-icon align-middle"
+      />
       
       <div
         class="o-menu-item-name align-middle text-truncate"
@@ -51,6 +57,9 @@ exports[`BottomBar component Can open the list of statistics 1`] = `
     <div
       class="d-flex w-100"
     >
+      <div
+        class="o-menu-item-icon align-middle"
+      />
       
       <div
         class="o-menu-item-name align-middle text-truncate"
@@ -70,6 +79,9 @@ exports[`BottomBar component Can open the list of statistics 1`] = `
     <div
       class="d-flex w-100"
     >
+      <div
+        class="o-menu-item-icon align-middle"
+      />
       
       <div
         class="o-menu-item-name align-middle text-truncate"
@@ -89,6 +101,9 @@ exports[`BottomBar component Can open the list of statistics 1`] = `
     <div
       class="d-flex w-100"
     >
+      <div
+        class="o-menu-item-icon align-middle"
+      />
       
       <div
         class="o-menu-item-name align-middle text-truncate"
@@ -108,6 +123,9 @@ exports[`BottomBar component Can open the list of statistics 1`] = `
     <div
       class="d-flex w-100"
     >
+      <div
+        class="o-menu-item-icon align-middle"
+      />
       
       <div
         class="o-menu-item-name align-middle text-truncate"

src/actions/action.ts
@@ -36,7 +36,7 @@ export interface ActionSpec {
   /**
    * Can be defined to display an icon
    */
-  icon?: string;
+  icon?: string | ((env: SpreadsheetChildEnv) => string);
   /**
    * is the action allowed when running spreadsheet in readonly mode
    */
@@ -67,7 +67,7 @@ export interface Action {
   isVisible: (env: SpreadsheetChildEnv) => boolean;
   isEnabled: (env: SpreadsheetChildEnv) => boolean;
   isActive?: (env: SpreadsheetChildEnv) => boolean;
-  icon: string;
+  icon: (env: SpreadsheetChildEnv) => string;
   isReadonlyAllowed: boolean;
   execute?: (env: SpreadsheetChildEnv) => unknown;
   children: (env: SpreadsheetChildEnv) => Action[];
@@ -88,6 +88,7 @@ export function createAction(item: ActionSpec): Action {
   const name = item.name;
   const children = item.children;
   const description = item.description;
+  const icon = item.icon;
   return {
     id: item.id || uuidGenerator.uuidv4(),
     name: typeof name === "function" ? name : () => name,
@@ -105,7 +106,7 @@ export function createAction(item: ActionSpec): Action {
       : () => [],
     isReadonlyAllowed: item.isReadonlyAllowed || false,
     separator: item.separator || false,
-    icon: item.icon || "",
+    icon: typeof icon === "function" ? icon : () => icon || "",
     description: typeof description === "function" ? description : () => description || "",
     textColor: item.textColor,
     sequence: item.sequence || 0,

src/actions/format_actions.ts
@@ -1,8 +1,13 @@
-import { DEFAULT_FONT_SIZE, DEFAULT_VERTICAL_ALIGN, FONT_SIZES } from "../constants";
+import {
+  DEFAULT_FONT_SIZE,
+  DEFAULT_VERTICAL_ALIGN,
+  DEFAULT_WRAPPING_MODE,
+  FONT_SIZES,
+} from "../constants";
 import { formatValue, roundFormat } from "../helpers";
 import { parseLiteral } from "../helpers/cells";
 import { _lt } from "../translation";
-import { Align, DEFAULT_LOCALE, SpreadsheetChildEnv } from "../types";
+import { Align, DEFAULT_LOCALE, SpreadsheetChildEnv, VerticalAlign, Wrapping } from "../types";
 import { ActionSpec } from "./action";
 import * as ACTIONS from "./menu_items_actions";
 import { setFormatter, setStyle } from "./menu_items_actions";
@@ -192,7 +197,7 @@ export const formatAlignment: ActionSpec = {
 
 export const formatAlignmentHorizontal: ActionSpec = {
   name: _lt("Horizontal align"),
-  icon: "o-spreadsheet-Icon.ALIGN_LEFT",
+  icon: (env) => getHorizontalAlignmentIcon(env),
 };
 
 export const formatAlignmentLeft: ActionSpec = {
@@ -221,56 +226,58 @@ export const formatAlignmentRight: ActionSpec = {
 
 export const formatAlignmentVertical: ActionSpec = {
   name: _lt("Vertical align"),
-  icon: "o-spreadsheet-Icon.ALIGN_MIDDLE",
+  icon: (env) => getVerticalAlignmentIcon(env),
 };
 
 export const formatAlignmentTop: ActionSpec = {
   name: _lt("Top"),
   execute: (env) => ACTIONS.setStyle(env, { verticalAlign: "top" }),
-  isActive: (env) =>
-    (env.model.getters.getCurrentStyle().verticalAlign || DEFAULT_VERTICAL_ALIGN) === "top",
+  isActive: (env) => getVerticalAlign(env) === "top",
   icon: "o-spreadsheet-Icon.ALIGN_TOP",
 };
 
 export const formatAlignmentMiddle: ActionSpec = {
   name: _lt("Middle"),
   execute: (env) => ACTIONS.setStyle(env, { verticalAlign: "middle" }),
-  isActive: (env) =>
-    (env.model.getters.getCurrentStyle().verticalAlign || DEFAULT_VERTICAL_ALIGN) === "middle",
+  isActive: (env) => getVerticalAlign(env) === "middle",
   icon: "o-spreadsheet-Icon.ALIGN_MIDDLE",
 };
 
 export const formatAlignmentBottom: ActionSpec = {
   name: _lt("Bottom"),
   execute: (env) => ACTIONS.setStyle(env, { verticalAlign: "bottom" }),
-  isActive: (env) =>
-    (env.model.getters.getCurrentStyle().verticalAlign || DEFAULT_VERTICAL_ALIGN) === "bottom",
+  isActive: (env) => getVerticalAlign(env) === "bottom",
   icon: "o-spreadsheet-Icon.ALIGN_BOTTOM",
+};
+
+export const formatWrappingIcon: ActionSpec = {
+  name: _lt("Wrapping"),
+  icon: "o-spreadsheet-Icon.WRAPPING_OVERFLOW",
 };
 
 export const formatWrapping: ActionSpec = {
   name: _lt("Wrapping"),
-  icon: "o-spreadsheet-Icon.WRAPPING_OVERFLOW",
+  icon: (env) => getWrapModeIcon(env),
 };
 
 export const formatWrappingOverflow: ActionSpec = {
   name: _lt("Overflow"),
   execute: (env) => ACTIONS.setStyle(env, { wrapping: "overflow" }),
-  isActive: (env) => (env.model.getters.getCurrentStyle().wrapping || "overflow") === "overflow",
+  isActive: (env) => getWrappingMode(env) === "overflow",
   icon: "o-spreadsheet-Icon.WRAPPING_OVERFLOW",
 };
 
 export const formatWrappingWrap: ActionSpec = {
   name: _lt("Wrap"),
   execute: (env) => ACTIONS.setStyle(env, { wrapping: "wrap" }),
-  isActive: (env) => env.model.getters.getCurrentStyle().wrapping === "wrap",
+  isActive: (env) => getWrappingMode(env) === "wrap",
   icon: "o-spreadsheet-Icon.WRAPPING_WRAP",
 };
 
 export const formatWrappingClip: ActionSpec = {
   name: _lt("Clip"),
   execute: (env) => ACTIONS.setStyle(env, { wrapping: "clip" }),
-  isActive: (env) => env.model.getters.getCurrentStyle().wrapping === "clip",
+  isActive: (env) => getWrappingMode(env) === "clip",
   icon: "o-spreadsheet-Icon.WRAPPING_CLIP",
 };
 
@@ -335,4 +342,59 @@ function getHorizontalAlign(env: SpreadsheetChildEnv): Align {
   }
   const cell = env.model.getters.getActiveCell();
   return cell.defaultAlign;
+}
+
+function getVerticalAlign(env: SpreadsheetChildEnv): VerticalAlign {
+  const style = env.model.getters.getCurrentStyle();
+  if (style.verticalAlign) {
+    return style.verticalAlign;
+  }
+  return DEFAULT_VERTICAL_ALIGN;
+}
+
+function getWrappingMode(env: SpreadsheetChildEnv): Wrapping {
+  const style = env.model.getters.getCurrentStyle();
+  if (style.wrapping) {
+    return style.wrapping;
+  }
+  return DEFAULT_WRAPPING_MODE;
+}
+
+function getHorizontalAlignmentIcon(env: SpreadsheetChildEnv) {
+  const horizontalAlign = getHorizontalAlign(env);
+
+  switch (horizontalAlign) {
+    case "right":
+      return "o-spreadsheet-Icon.ALIGN_RIGHT";
+    case "center":
+      return "o-spreadsheet-Icon.ALIGN_CENTER";
+    default:
+      return "o-spreadsheet-Icon.ALIGN_LEFT";
+  }
+}
+
+function getVerticalAlignmentIcon(env: SpreadsheetChildEnv) {
+  const verticalAlign = getVerticalAlign(env);
+
+  switch (verticalAlign) {
+    case "top":
+      return "o-spreadsheet-Icon.ALIGN_TOP";
+    case "middle":
+      return "o-spreadsheet-Icon.ALIGN_MIDDLE";
+    default:
+      return "o-spreadsheet-Icon.ALIGN_BOTTOM";
+  }
+}
+
+function getWrapModeIcon(env: SpreadsheetChildEnv) {
+  const wrapMode = getWrappingMode(env);
+
+  switch (wrapMode) {
+    case "wrap":
+      return "o-spreadsheet-Icon.WRAPPING_WRAP";
+    case "clip":
+      return "o-spreadsheet-Icon.WRAPPING_CLIP";
+    default:
+      return "o-spreadsheet-Icon.WRAPPING_OVERFLOW";
+  }
 }

src/components/action_button/action_button.ts
@@ -54,7 +54,7 @@ export class ActionButton extends Component<Props, SpreadsheetChildEnv> {
   }
 
   get iconTitle() {
-    return this.actionButton.icon;
+    return this.actionButton.icon(this.env);
   }
 
   onClick(ev: MouseEvent) {

src/constants.ts
@@ -147,6 +147,8 @@ export const DEFAULT_FONT_SIZE = 10;
 export const HEADER_FONT_SIZE = 11;
 export const DEFAULT_FONT = "'Roboto', arial";
 export const DEFAULT_VERTICAL_ALIGN = "bottom";
+export const DEFAULT_HORIZONTAL_ALIGN = "left";
+export const DEFAULT_WRAPPING_MODE = "overflow";
 
 // Borders
 export const DEFAULT_BORDER_DESC: BorderDescr = { style: "thin", color: "#000000" };

src/registries/menus/topbar_menu_registry.ts
@@ -329,7 +329,7 @@ topbarMenuRegistry
     separator: true,
   })
   .addChild("format_wrapping", ["format"], {
-    ...ACTION_FORMAT.formatWrapping,
+    ...ACTION_FORMAT.formatWrappingIcon,
     sequence: 80,
     separator: true,
   })

tests/components/__snapshots__/spreadsheet.test.ts.snap
@@ -465,10 +465,10 @@ exports[`Simple Spreadsheet Component simple rendering snapshot 1`] = `
                 style=""
               >
                 <svg
-                  class="o-icon align-middle"
+                  class="o-icon align-bottom"
                 >
                   <path
-                    d="M12.5 3H10V0H8v3H5.5L9 6.5M5.5 15H8v3h2v-3h2.5L9 11.5M3 8v2h12V8"
+                    d="M5.5 9H8V2h2v7h2.5L9 12.5M3 14v2h12v-2"
                     fill="currentColor"
                   />
                 </svg>

commit 807be7a06210fc567a2b1c71ba92b715d6a13450

[FIX] Menu: Hide the icon slot of the menu items when absent
The refactoring of the menus, which introduced the possibility to
attach an icon, was designed so that the icon slot would not be rendered
if no menuitem had an icon. Unfortunately, this was broken when we
introduced the dynamic icons in 8a9914a2.

Task: 3476424

src/components/menu/menu.ts
@@ -166,7 +166,7 @@ export class Menu extends Component<Props, SpreadsheetChildEnv> {
   }
 
   get childrenHaveIcon(): boolean {
-    return this.props.menuItems.some((menuItem) => !!menuItem.icon || !!menuItem.isActive);
+    return this.props.menuItems.some((menuItem) => !!this.getIconName(menuItem));
   }
 
   getIconName(menu: Action) {

tests/components/__snapshots__/bottom_bar.test.ts.snap
@@ -13,9 +13,6 @@ exports[`BottomBar component Can open the list of statistics 1`] = `
     <div
       class="d-flex w-100"
     >
-      <div
-        class="o-menu-item-icon align-middle"
-      />
       
       <div
         class="o-menu-item-name align-middle text-truncate"
@@ -35,9 +32,6 @@ exports[`BottomBar component Can open the list of statistics 1`] = `
     <div
       class="d-flex w-100"
     >
-      <div
-        class="o-menu-item-icon align-middle"
-      />
       
       <div
         class="o-menu-item-name align-middle text-truncate"
@@ -57,9 +51,6 @@ exports[`BottomBar component Can open the list of statistics 1`] = `
     <div
       class="d-flex w-100"
     >
-      <div
-        class="o-menu-item-icon align-middle"
-      />
       
       <div
         class="o-menu-item-name align-middle text-truncate"
@@ -79,9 +70,6 @@ exports[`BottomBar component Can open the list of statistics 1`] = `
     <div
       class="d-flex w-100"
     >
-      <div
-        class="o-menu-item-icon align-middle"
-      />
       
       <div
         class="o-menu-item-name align-middle text-truncate"
@@ -101,9 +89,6 @@ exports[`BottomBar component Can open the list of statistics 1`] = `
     <div
       class="d-flex w-100"
     >
-      <div
-        class="o-menu-item-icon align-middle"
-      />
       
       <div
         class="o-menu-item-name align-middle text-truncate"
@@ -123,9 +108,6 @@ exports[`BottomBar component Can open the list of statistics 1`] = `
     <div
       class="d-flex w-100"
     >
-      <div
-        class="o-menu-item-icon align-middle"
-      />
       
       <div
         class="o-menu-item-name align-middle text-truncate"

tests/components/context_menu.test.ts
@@ -681,6 +681,25 @@ describe("Context Menu internal tests", () => {
     await nextTick();
     expect(fixture.querySelector("div[data-name='menuItem']")).toBeFalsy();
   });
+
+  test("Menu icon slot is visibile if one of the children has an icon", async () => {
+    let visible = false;
+    const menuItems = createActions([
+      {
+        name: "child1",
+        icon: () => (visible ? "o-spreadsheet-Icon.BOLD" : ""),
+      },
+      {
+        name: "child2",
+      },
+    ]);
+    await renderContextMenu(300, 300, { menuItems });
+    expect(fixture.querySelectorAll(".o-menu-item-icon").length).toBe(0);
+    visible = true;
+    parent.render(true);
+    await nextTick();
+    expect(fixture.querySelectorAll(".o-menu-item-icon").length).toBe(2);
+  });
 });
 
 describe("Context Menu position on large screen 1000px/1000px", () => {