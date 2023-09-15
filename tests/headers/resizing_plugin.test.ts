eb585bf458ff31f191fa523fd5070c4b9ff6d9d8
Lucas Lefèvre (lul) <lul@odoo.com>
2023-09-15 12:14:18 +0000

    [FIX] header_size: decouple added row from base row
    Note: this issue is no longer present is this version,
    but I'm still forward-porting the tests.
    
    Issue in lower versions:
    Steps to reproduce:
    - Insert a row below row 1
    - change the font size of row 1
    => rows 1 AND 2 are now bigger. The size of row 2 should not have changed
    
    That's because we fill the size array for the new row with the same
    object (by reference) as the base one. Chaning any of the two rows
    impacts the other one.
    
    We can also end up with a crash if you add a row below the last row.
    Let's say there are 10 rows, you insert a new one at the very bottom.
    Set some content in a cell of row 11 (now the size of row 10 is linked
    to the size of row 11). Now delete row 11
    => getting the size of row 10 will crash because it's linked to row 11
    which no longer exists.
    
    Task: 3510863
    X-original-commit: 5d60210a896b51c244af46c03978a62f7cf892cd

--- tests/headers/resizing_plugin.test.ts
+++ tests/headers/resizing_plugin.test.ts
@@ -263,6 +263,15 @@ describe("Model resizer", () => {
       expect(model.getters.getRowSize(sheetId, 3)).toEqual(20);
     });
 
+    test("Remove added row at the end with some content", () => {
+      let lastRow = model.getters.getNumberRows(sheetId) - 1;
+      addRows(model, "after", lastRow, 1);
+      lastRow += 1;
+      setCellContent(model, toXC(0, lastRow), "Hello");
+      deleteRows(model, [lastRow]);
+      expect(model.getters.getRowSize(sheetId, lastRow)).toEqual(DEFAULT_CELL_HEIGHT);
+    });
+
     test("Add row before", () => {
       addRows(model, "after", 0, 2, sheetId);
       expect(model.getters.getRowSize(sheetId, 2)).toEqual(DEFAULT_CELL_HEIGHT);
@@ -277,6 +286,16 @@ describe("Model resizer", () => {
       expect(model.getters.getRowSize(sheetId, 3)).toEqual(20);
       expect(model.getters.getRowSize(sheetId, 4)).toEqual(DEFAULT_CELL_HEIGHT);
       expect(model.getters.getRowSize(sheetId, 5)).toEqual(DEFAULT_CELL_HEIGHT);
+    });
+
+    test("added row is independent from base row", () => {
+      setCellContent(model, "A1", "something");
+      addRows(model, "after", 0, 1, sheetId);
+      setStyle(model, "A1", { fontSize: 36 });
+      setCellContent(model, "A2", "something");
+      const font36CellHeight = getDefaultCellHeight(getCell(model, "A1"));
+      expect(model.getters.getRowSize(sheetId, 0)).toEqual(font36CellHeight);
+      expect(model.getters.getRowSize(sheetId, 1)).toEqual(DEFAULT_CELL_HEIGHT);
     });
   });
 
