odoo/o-spreadsheet

commit b7219cedc5ceabe325c572f20298213806090d8f

[FIX] find_and_replace: disabled button css and shorten the debounce
Previously  find and replace side panel buttons did not have
the proper style when they were disabled.

This commit will fix that CSS issue.

The debounce of 400 ms made the search feature feel sluggish.
This becomes especially weird since we fixed the look of the disabled
buttons when a result in not found. The button is disabled by default,
hence will stay that way after typing a character and the first
debounce is executed.

closes odoo/o-spreadsheet#2700

Task: 3432224
Signed-off-by: Rémi Rahir (rar) <rar@odoo.com>
Co-authored-by: Rémi Rahir <rar@odoo.com>

src/components/side_panel/find_and_replace/find_and_replace.ts
@@ -1,4 +1,5 @@
 import { Component, onMounted, onWillUnmount, useEffect, useRef, useState } from "@odoo/owl";
+import { debounce } from "../../../helpers";
 import { SpreadsheetChildEnv } from "../../../types/index";
 import { css } from "../../helpers/css";
 
@@ -56,6 +57,7 @@ export class FindAndReplacePanel extends Component<Props, SpreadsheetChildEnv> {
   private state: FindAndReplaceState = useState(this.initialState());
   private debounceTimeoutId;
   private showFormulaState: boolean = false;
+  private debouncedUpdateSearch!: Function;
 
   private findAndReplaceRef = useRef("findAndReplace");
 
@@ -69,6 +71,7 @@ export class FindAndReplacePanel extends Component<Props, SpreadsheetChildEnv> {
 
   setup() {
     this.showFormulaState = this.env.model.getters.shouldShowFormulas();
+    this.debouncedUpdateSearch = debounce(this.updateSearch.bind(this), 200);
 
     onMounted(() => this.focusInput());
 
@@ -125,13 +128,6 @@ export class FindAndReplacePanel extends Component<Props, SpreadsheetChildEnv> {
       toSearch: this.state.toSearch,
       searchOptions: this.state.searchOptions,
     });
-  }
-  debouncedUpdateSearch() {
-    clearTimeout(this.debounceTimeoutId);
-    this.debounceTimeoutId = setTimeout(() => {
-      this.updateSearch();
-      this.debounceTimeoutId = undefined;
-    }, 400);
   }
 
   replace() {

tests/components/find_replace_side_panel.test.ts
@@ -152,7 +152,6 @@ describe("find and replace sidePanel component", () => {
     test("search match count is removed when input is cleared", async () => {
       setCellContent(model, "A1", "Hello");
       setInputValueAndTrigger(selectors.inputSearch, "Hel", "input");
-      await nextTick();
       expect(fixture.querySelector(".o-input-count")).toBeNull();
       jest.runOnlyPendingTimers();
       await nextTick();

src/components/menu/menu.ts
@@ -1,6 +1,6 @@
 import { Component, onWillUpdateProps, useExternalListener, useRef, useState } from "@odoo/owl";
 import {
-  MENU_ITEM_DISABLED_COLOR,
+  DISABLED_TEXT_COLOR,
   MENU_ITEM_HEIGHT,
   MENU_SEPARATOR_HEIGHT,
   MENU_VERTICAL_PADDING,
@@ -63,7 +63,7 @@ css/* scss */ `
         }
       }
       &.disabled {
-        color: ${MENU_ITEM_DISABLED_COLOR};
+        color: ${DISABLED_TEXT_COLOR};
         cursor: not-allowed;
       }
     }

src/components/side_panel/side_panel/side_panel.ts
@@ -1,5 +1,9 @@
 import { Component, onWillUpdateProps, useState } from "@odoo/owl";
-import { BACKGROUND_HEADER_COLOR, ComponentsImportance } from "../../../constants";
+import {
+  BACKGROUND_HEADER_COLOR,
+  ComponentsImportance,
+  DISABLED_TEXT_COLOR,
+} from "../../../constants";
 import { SidePanelContent, sidePanelRegistry } from "../../../registries/side_panel_registry";
 import { SpreadsheetChildEnv } from "../../../types";
 import { css } from "../../helpers/css";
@@ -98,6 +102,11 @@ css/* scss */ `
     .o-sidePanelButton:enabled {
       cursor: pointer;
     }
+
+    .o-sidePanelButton:disabled {
+      color: ${DISABLED_TEXT_COLOR};
+    }
+
     .o-sidePanelButton:last-child {
       margin-right: 0px;
     }

src/constants.ts
@@ -15,7 +15,7 @@ export const SELECTION_BORDER_COLOR = "#3266ca";
 export const HEADER_BORDER_COLOR = "#C0C0C0";
 export const CELL_BORDER_COLOR = "#E2E3E3";
 export const BACKGROUND_CHART_COLOR = "#FFFFFF";
-export const MENU_ITEM_DISABLED_COLOR = "#CACACA";
+export const DISABLED_TEXT_COLOR = "#CACACA";
 export const DEFAULT_COLOR_SCALE_MIDPOINT_COLOR = 0xb6d7a8;
 export const LINK_COLOR = "#01666b";
 export const FILTERS_COLOR = "#188038";

commit a94750a8fc5238dec011f5e3f50cc94313e2a5bd

[REV] find & replace: shorten the debounce on find search
Partially revert forward-port b7219ce which broke the
behavior of a5a67d8

Open the side panel and start typing something that matches
some cells. A quick "0 / 0" is displayed a few frames (200ms,
the debounce time) before displaying the correct match result
(e.g. "1 / 3").

With this commit, nothing is displayed until the search
result is actually done.

`debounceTimeoutId` was left unused and never correctly
assigned with the debounce timeout id.

The behavior was tested... but notice how `await nextTick()`
in `find_replace_side_panel.test.ts` was removed....

Note: the original commit 4123fd1 (merged in 16.0) was correct
because a5a67d8 only came with saas-16.1

Task: 3510813

src/components/side_panel/find_and_replace/find_and_replace.ts
@@ -1,5 +1,4 @@
 import { Component, onMounted, onWillUnmount, useEffect, useRef, useState } from "@odoo/owl";
-import { debounce } from "../../../helpers";
 import { SpreadsheetChildEnv } from "../../../types/index";
 import { css } from "../../helpers/css";
 
@@ -57,7 +56,6 @@ export class FindAndReplacePanel extends Component<Props, SpreadsheetChildEnv> {
   private state: FindAndReplaceState = useState(this.initialState());
   private debounceTimeoutId;
   private showFormulaState: boolean = false;
-  private debouncedUpdateSearch!: Function;
 
   private findAndReplaceRef = useRef("findAndReplace");
 
@@ -71,7 +69,6 @@ export class FindAndReplacePanel extends Component<Props, SpreadsheetChildEnv> {
 
   setup() {
     this.showFormulaState = this.env.model.getters.shouldShowFormulas();
-    this.debouncedUpdateSearch = debounce(this.updateSearch.bind(this), 200);
 
     onMounted(() => this.focusInput());
 
@@ -128,6 +125,14 @@ export class FindAndReplacePanel extends Component<Props, SpreadsheetChildEnv> {
       toSearch: this.state.toSearch,
       searchOptions: this.state.searchOptions,
     });
+  }
+
+  debouncedUpdateSearch() {
+    clearTimeout(this.debounceTimeoutId);
+    this.debounceTimeoutId = setTimeout(() => {
+      this.updateSearch();
+      this.debounceTimeoutId = undefined;
+    }, 200);
   }
 
   replace() {

tests/components/find_replace_side_panel.test.ts
@@ -152,6 +152,7 @@ describe("find and replace sidePanel component", () => {
     test("search match count is removed when input is cleared", async () => {
       setCellContent(model, "A1", "Hello");
       setInputValueAndTrigger(selectors.inputSearch, "Hel", "input");
+      await nextTick(); // wait the next render to check if the count is displayed
       expect(fixture.querySelector(".o-input-count")).toBeNull();
       jest.runOnlyPendingTimers();
       await nextTick();