odoo/o-spreadsheet

commit 3710b7df173fc463bf00f13fe63b13c31d7b975b

[IMP] css: use cssPropertiesToCss helper everywhere
This commit replace the old way of generating css strings by hand/with
string concatenation with the helper cssPropertiesToCss.

This have 2 main advantages:

1) It makes the code slightly more readable
2) We avoid inserting strings with newlines directly into the DOM,
which makes the DOM way easier to inspect.

closes odoo/o-spreadsheet#1996

Signed-off-by: Lucas Lefèvre (lul) <lul@odoo.com>

src/components/collaborative_client_tag/collaborative_client_tag.ts
@@ -1,7 +1,7 @@
 import { Component } from "@odoo/owl";
 import { DEFAULT_FONT_SIZE } from "../../constants";
 import { HeaderIndex, SpreadsheetChildEnv } from "../../types";
-import { css } from "../helpers/css";
+import { css, cssPropertiesToCss } from "../helpers/css";
 
 interface ClientTagProps {
   active: boolean;
@@ -33,11 +33,14 @@ export class ClientTag extends Component<ClientTagProps, SpreadsheetChildEnv> {
       right: col,
       bottom: row,
     });
-    return `bottom: ${height - y + 15}px;left: ${
-      x - 1
-    }px;border: 1px solid ${color};background-color: ${color};${
-      this.props.active ? "opacity:1 !important" : ""
-    }`;
+
+    return cssPropertiesToCss({
+      bottom: `${height - y + 15}px`,
+      left: `${x - 1}px`,
+      border: `1px solid ${color}`,
+      "background-color": color,
+      opacity: this.props.active ? "opacity:1 !important" : undefined,
+    });
   }
 }
 

src/components/color_picker/color_picker.ts
@@ -10,7 +10,7 @@ import { hslaToRGBA, isColorValid, rgbaToHex } from "../../helpers";
 import { chartFontColor } from "../../helpers/figures/charts";
 import { Color } from "../../types";
 import { SpreadsheetChildEnv } from "../../types/env";
-import { css } from "../helpers/css";
+import { css, cssPropertiesToCss } from "../helpers/css";
 
 const PICKER_PADDING = 6;
 
@@ -263,9 +263,12 @@ export class ColorPicker extends Component<ColorPickerProps, SpreadsheetChildEnv
 
   get magnifyingGlassStyle() {
     const { display, background, left, top } = this.state.style;
-    return `display:${display};${
-      display === "block" ? `background-color:${background};left:${left};top:${top};` : ""
-    }`;
+    return cssPropertiesToCss({
+      display,
+      "background-color": display === "block" ? background : undefined,
+      left,
+      top,
+    });
   }
 }
 

src/components/composer/composer/composer.ts
@@ -10,8 +10,14 @@ import {
   zoneToDimension,
 } from "../../../helpers/index";
 import { ComposerSelection } from "../../../plugins/ui_stateful/edition";
-import { DOMDimension, FunctionDescription, Rect, SpreadsheetChildEnv } from "../../../types/index";
-import { css } from "../../helpers/css";
+import {
+  CSSProperties,
+  DOMDimension,
+  FunctionDescription,
+  Rect,
+  SpreadsheetChildEnv,
+} from "../../../types/index";
+import { css, cssPropertiesToCss } from "../../helpers/css";
 import { getElementScrollTop, setElementScrollTop } from "../../helpers/dom_helpers";
 import { updateSelectionWithArrowKeys } from "../../helpers/selection_helpers";
 import { TextValueProvider } from "../autocomplete_dropdown/autocomplete_dropdown";
@@ -158,21 +164,20 @@ export class Composer extends Component<Props, SpreadsheetChildEnv> {
     if (this.props.delimitation && this.props.rect) {
       const { x: cellX, y: cellY, height: cellHeight } = this.props.rect;
       const remainingHeight = this.props.delimitation.height - (cellY + cellHeight);
-      let assistantStyle = "";
+      let assistantStyle: CSSProperties = {};
       if (cellY > remainingHeight) {
         // render top
-        assistantStyle += `
-          top: -3px;
-          transform: translate(0, -100%);
-        `;
+        assistantStyle.top = `$-3px`;
+        assistantStyle.transform = `translate(0, -100%)`;
       }
       if (cellX + ASSISTANT_WIDTH > this.props.delimitation.width) {
         // render left
-        assistantStyle += `right:0px;`;
+        assistantStyle.right = `0px`;
       }
-      return (assistantStyle += `width:${ASSISTANT_WIDTH}px;`);
+      assistantStyle.width = `${ASSISTANT_WIDTH}px`;
+      return cssPropertiesToCss(assistantStyle);
     }
-    return `width:${ASSISTANT_WIDTH}px;`;
+    return cssPropertiesToCss({ width: `${ASSISTANT_WIDTH}px` });
   }
 
   // we can't allow input events to be triggered while we remove and add back the content of the composer in processContent

src/components/composer/grid_composer/grid_composer.ts
@@ -5,7 +5,7 @@ import { deepEquals, getComposerSheetName, positionToZone, toXC } from "../../..
 import { ComposerSelection } from "../../../plugins/ui_stateful/edition";
 import { DOMDimension, Rect, Ref, SpreadsheetChildEnv, Zone } from "../../../types/index";
 import { getTextDecoration } from "../../helpers";
-import { css } from "../../helpers/css";
+import { css, cssPropertiesToCss } from "../../helpers/css";
 import { Composer } from "../composer/composer";
 import { ZoneDimension } from "./../../../types/misc";
 
@@ -117,10 +117,10 @@ export class GridComposer extends Component<Props, SpreadsheetChildEnv> {
 
   get cellReferenceStyle(): string {
     const { x: left, y: top } = this.rect;
-    return `
-      left: ${left - COMPOSER_BORDER_WIDTH}px;
-      top: ${top - GRID_CELL_REFERENCE_TOP_OFFSET}px;
-    `;
+    return cssPropertiesToCss({
+      left: `${left - COMPOSER_BORDER_WIDTH}px`,
+      top: `${top - GRID_CELL_REFERENCE_TOP_OFFSET}px`,
+    });
   }
 
   get containerStyle(): string {
@@ -155,33 +155,33 @@ export class GridComposer extends Component<Props, SpreadsheetChildEnv> {
      *
      * The +-1 are there to include cell borders in the composer sizing/positioning
      */
-    return `
-      left: ${left - 1}px;
-      top: ${top}px;
+    return cssPropertiesToCss({
+      left: `${left - 1}px`,
+      top: `${top}px`,
 
-      min-width: ${width + 1}px;
-      min-height: ${height + 1}px;
+      "min-width": `${width + 1}px`,
+      "min-height": `${height + 1}px`,
 
-      background: ${background};
-      color: ${color};
+      background,
+      color,
 
-      font-size: ${fontSizeMap[fontSize]}px;
-      font-weight: ${fontWeight};
-      font-style: ${fontStyle};
-      text-decoration: ${textDecoration};
+      "font-size": `${fontSizeMap[fontSize]}px`,
+      "font-weight": String(fontWeight),
+      "font-style": fontStyle,
+      "text-decoration": textDecoration,
 
-      text-align: ${textAlign};
-    `;
+      "text-align": textAlign,
+    });
   }
 
   get composerStyle(): string {
     const maxHeight = this.props.gridDims.height - this.rect.y;
     const maxWidth = this.props.gridDims.width - this.rect.x;
 
-    return `
-      max-width : ${maxWidth}px;
-      max-height : ${maxHeight}px;
-    `;
+    return cssPropertiesToCss({
+      "max-width": `${maxWidth}px`,
+      "max-height": `${maxHeight}px`,
+    });
   }
 }
 

src/components/dashboard/dashboard.ts
@@ -13,7 +13,7 @@ import {
 } from "../../types/index";
 import { GridOverlay } from "../grid_overlay/grid_overlay";
 import { GridPopover } from "../grid_popover/grid_popover";
-import { css } from "../helpers/css";
+import { css, cssPropertiesToCss } from "../helpers/css";
 import { useGridDrawing } from "../helpers/draw_grid_hook";
 import { useAbsolutePosition } from "../helpers/position_hook";
 import { useWheelHandler } from "../helpers/wheel_hook";
@@ -77,25 +77,23 @@ export class SpreadsheetDashboard extends Component<Props, SpreadsheetChildEnv> 
     const sheetId = this.env.model.getters.getActiveSheetId();
     const { right } = this.env.model.getters.getSheetZone(sheetId);
     const { end } = this.env.model.getters.getColDimensions(sheetId, right);
-    return `
-      max-width: ${end}px;
-    `;
+    return cssPropertiesToCss({ "max-width": `${end}px` });
   }
 
   get gridOverlayDimensions() {
-    return `
-      height: 100%;
-      width: 100%
-    `;
+    return cssPropertiesToCss({
+      height: "100%",
+      width: "100%",
+    });
   }
 
   getCellClickableStyle(coordinates: Rect) {
-    return `
-      top: ${coordinates.y}px;
-      left: ${coordinates.x}px;
-      width: ${coordinates.width}px;
-      height: ${coordinates.height}px;
-    `;
+    return cssPropertiesToCss({
+      top: `${coordinates.y}px`,
+      left: `${coordinates.x}px`,
+      width: `${coordinates.width}px`,
+      height: `${coordinates.height}px`,
+    });
   }
 
   /**

src/components/figures/chart/scorecard/chart_scorecard.ts
@@ -155,16 +155,14 @@ export class ScorecardChart extends Component<Props, SpreadsheetChildEnv> {
   }
 
   get chartStyle() {
-    return `
-      padding:${this.chartPadding}px;
-      background:${this.backgroundColor};
-    `;
+    return cssPropertiesToCss({
+      padding: `${this.chartPadding}px`,
+      background: this.backgroundColor,
+    });
   }
 
   get chartContentStyle() {
-    return `
-      height:${this.getDrawableHeight()}px;
-    `;
+    return cssPropertiesToCss({ height: `${this.getDrawableHeight()}px` });
   }
 
   get chartPadding() {

src/components/figures/figure/figure.ts
@@ -5,8 +5,8 @@ import {
   SELECTION_BORDER_COLOR,
 } from "../../../constants";
 import { figureRegistry } from "../../../registries/index";
-import { Figure, Pixel, SpreadsheetChildEnv, UID } from "../../../types/index";
-import { css } from "../../helpers/css";
+import { CSSProperties, Figure, Pixel, SpreadsheetChildEnv, UID } from "../../../types/index";
+import { css, cssPropertiesToCss } from "../../helpers/css";
 import { gridOverlayPosition } from "../../helpers/dom_helpers";
 import { startDnd } from "../../helpers/drag_and_drop";
 
@@ -243,12 +243,12 @@ export class FigureComponent extends Component<Props, SpreadsheetChildEnv> {
     const top = figureY >= y ? y : 0;
     const height = viewHeight - top;
 
-    return `
-      left: ${left}px;
-      top: ${top}px;
-      width: ${width}px;
-      height: ${height}px
-    `;
+    return cssPropertiesToCss({
+      left: `${left}px`,
+      top: `${top}px`,
+      width: `${width}px`,
+      height: `${height}px`,
+    });
   }
 
   get inverseViewportPositionStyle(): string {
@@ -259,10 +259,10 @@ export class FigureComponent extends Component<Props, SpreadsheetChildEnv> {
     const left = figureX >= x ? -(x + offsetX) : 0;
     const top = figureY >= y ? -(y + offsetY) : 0;
 
-    return `
-      left: ${left}px;
-      top: ${top}px;
-    `;
+    return cssPropertiesToCss({
+      left: `${left}px`,
+      top: `${top}px`,
+    });
   }
 
   private getBorderWidth(): Pixel {
@@ -275,34 +275,34 @@ export class FigureComponent extends Component<Props, SpreadsheetChildEnv> {
 
   get wrapperStyle() {
     const { x, y, width, height } = this.displayedFigure;
-    return (
-      `top:${y}px;` +
-      `left:${x}px;` +
-      `width:${width}px;` +
-      `height:${height}px;` +
-      `z-index: ${ComponentsImportance.Figure + (this.isSelected ? 1 : 0)}`
-    );
+    return cssPropertiesToCss({
+      left: `${x}px`,
+      top: `${y}px`,
+      width: `${width}px`,
+      height: `${height}px`,
+      "z-index": String(ComponentsImportance.Figure + (this.isSelected ? 1 : 0)),
+    });
   }
 
-  getResizerPosition(resizer: ResizeAnchor) {
+  getResizerPosition(resizer: ResizeAnchor): string {
     const anchorCenteringOffset = (ANCHOR_SIZE - ACTIVE_BORDER_WIDTH) / 2;
-    let style = "";
+    let style: CSSProperties = {};
     if (resizer.includes("top")) {
-      style += `top: ${-anchorCenteringOffset}px;`;
+      style.top = `${-anchorCenteringOffset}px`;
     } else if (resizer.includes("bottom")) {
-      style += `bottom: ${-anchorCenteringOffset}px;`;
+      style.bottom = `${-anchorCenteringOffset}px;`;
     } else {
-      style += ` bottom: calc(50% - ${anchorCenteringOffset}px);`;
+      style.bottom = `calc(50% - ${anchorCenteringOffset}px)`;
     }
 
     if (resizer.includes("left")) {
-      style += `left: ${-anchorCenteringOffset}px;`;
+      style.left = `${-anchorCenteringOffset}px;`;
     } else if (resizer.includes("right")) {
-      style += `right: ${-anchorCenteringOffset}px;`;
+      style.right += `${-anchorCenteringOffset}px;`;
     } else {
-      style += ` right: calc(50% - ${anchorCenteringOffset}px);`;
+      style.right += ` calc(50% - ${anchorCenteringOffset}px);`;
     }
-    return style;
+    return cssPropertiesToCss(style);
   }
 
   resize(dirX: number, dirY: number, ev: MouseEvent) {

src/components/grid/grid.ts
@@ -42,6 +42,7 @@ import { FilterIconsOverlay } from "../filters/filter_icons_overlay/fitler_icons
 import { GridOverlay } from "../grid_overlay/grid_overlay";
 import { GridPopover } from "../grid_popover/grid_popover";
 import { HeadersOverlay } from "../headers_overlay/headers_overlay";
+import { cssPropertiesToCss } from "../helpers";
 import { dragAndDropBeyondTheViewport } from "../helpers/drag_and_drop";
 import { useGridDrawing } from "../helpers/draw_grid_hook";
 import { useAbsolutePosition } from "../helpers/position_hook";
@@ -146,12 +147,12 @@ export class Grid extends Component<Props, SpreadsheetChildEnv> {
   }
 
   get gridOverlayDimensions() {
-    return `
-      top: ${HEADER_HEIGHT}px;
-      left: ${HEADER_WIDTH}px;
-      height: calc(100% - ${HEADER_HEIGHT + SCROLLBAR_WIDTH}px);
-      width: calc(100% - ${HEADER_WIDTH + SCROLLBAR_WIDTH}px);
-    `;
+    return cssPropertiesToCss({
+      top: `${HEADER_HEIGHT}px`,
+      left: `${HEADER_WIDTH}px`,
+      height: `calc(100% - ${HEADER_HEIGHT + SCROLLBAR_WIDTH}px)`,
+      width: `calc(100% - ${HEADER_WIDTH + SCROLLBAR_WIDTH}px)`,
+    });
   }
 
   onClosePopover() {

commit ea96590a9864fe3d16bcdf033a38517de7e1cbfc

[FIX] collaborative: display other users name
When there are multiple users connected on the same spreadsheet, hovering
the other user's position should display its name.
It currently broken.

The css property applied is wrong:
it end up being `opacity: opacity: 1 !important;` instead of
`opacity: 1 !important;`

Broken since 3710b7d

src/components/collaborative_client_tag/collaborative_client_tag.ts
@@ -39,7 +39,7 @@ export class ClientTag extends Component<ClientTagProps, SpreadsheetChildEnv> {
       left: `${x - 1}px`,
       border: `1px solid ${color}`,
       "background-color": color,
-      opacity: this.props.active ? "opacity:1 !important" : undefined,
+      opacity: this.props.active ? "1 !important" : undefined,
     });
   }
 }

tests/components/grid.test.ts
@@ -787,6 +787,20 @@ describe("Multi User selection", () => {
     ({ parent, fixture } = await mountSpreadsheet({ model }));
   });
 
+  test("Render collaborative user when hovering the position", async () => {
+    const sheetId = model.getters.getActiveSheetId();
+    transportService.sendMessage({
+      type: "CLIENT_JOINED",
+      version: MESSAGE_VERSION,
+      client: { id: "david", name: "David", position: { sheetId, col: 1, row: 1 } },
+    });
+    await nextTick();
+    expect(getElComputedStyle(".o-client-tag", "opacity")).toBe("0");
+    await hoverCell(model, "B2", 400);
+    expect(getElComputedStyle(".o-client-tag", "opacity")).toBe("1");
+    expect(document.querySelector(".o-client-tag")?.textContent).toBe("David");
+  });
+
   test("Do not render multi user selection with invalid sheet", async () => {
     transportService.sendMessage({
       type: "CLIENT_JOINED",