ad8323e383b6a533126bc18e31482cf3327a545b
Lucas Lefèvre (lul) <lul@odoo.com>
2023-10-26 16:24:13 +0200

    [IMP] actions: don't create actions at each rending
    With this commit, we are no longer creating the full action at every
    rendering.
    
    `createAction` has a hidden cost of generating a uuid if the action does not
    have one.
    When scrolling really fast (to trigger lots of rendering), `getRandomValues`
    was taking almost 4% of the compute time. Now it's no longer called at every
    rendering.
    
    X-original-commit: 05da23cbe50e7ea7ed18aedb5fc6f96fbc3a3351
    Part-of: odoo/o-spreadsheet#3097

--- src/components/action_button/action_button.ts
+++ src/components/action_button/action_button.ts
@@ -32,7 +32,11 @@ export class ActionButton extends Component<Props, SpreadsheetChildEnv> {
   private actionButton = createAction(this.props.action);
 
   setup() {
-    onWillUpdateProps(() => (this.actionButton = createAction(this.props.action)));
+    onWillUpdateProps((nextProps) => {
+      if (nextProps.action !== this.props.action) {
+        this.actionButton = createAction(this.props.action);
+      }
+    });
   }
 
   get isVisible() {
