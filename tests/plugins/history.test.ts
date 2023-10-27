b801a93a0ee1a9439fd1ffb94c2f43853d42ce1c
Lucas Lefèvre (lul) <lul@odoo.com>
2023-10-27 15:37:16 +0200

    [IMP] history: reduce memory allocation
    Backport of d8d943bb363638912b86d0684b52ea3a02c364c7
    
    `addChange` is used *a lot*, like *a lot, a lot*, for every mutation in core
    plugins. Its performance is key.
    
    Currently `const [root, ...path] = args` creates a new array for `path`,
    the array is allocated, and must be garbage collected later.
    
    On the large formula dataset, with 20k rows (520k cells), and adding a column
    before A, then reload the spreadsheet:
    
    (average over 5 runs)
    
    	minor GC	addChange
    before	1609ms		1168ms
    after	1094ms		753ms
    	-32%		-35%
    
    The memory usage also decreases: right after loading, memory was ~1725Mb and
    ~792Mb after running the GC. It's now ~1455Mb right after loading and ~670Mb
    after running the GC. (I'm not sure why it goes from 792Mb to 670Mb. Is it
    just thanks to the removed "root" key)
    
    X-original-commit: 97ebec0ebe46296576e17d7abcecc5de65795816

--- tests/plugins/history.test.ts
+++ tests/plugins/history.test.ts
@@ -150,6 +150,7 @@ describe("history", () => {
       A: {},
     };
     expect(() => {
+      // @ts-expect-error
       history.addChange(state, "A", "B", true, 5);
     }).toThrow();
   });
@@ -160,6 +161,7 @@ describe("history", () => {
       A: {},
     };
     expect(() => {
+      // @ts-expect-error
       history.addChange(state, "A", "B", true, "C", 5);
     }).toThrow();
   });
