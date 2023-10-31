a797c96682069546135f4710c52c6a18ed410dfc
Adrien Minne (adrm) <adrm@odoo.com>
2023-10-31 14:21:03 +0100

    [IMP] tests: pre-compile owl templates
    This commit adds a pre-compile step for owl templates to the tests.
    This speeds up the test a lot.
    
    Performances:
    Before: "npm run test": ~103s
    After: "npm run test": ~88s (-15%)
    
    (There seems to be more improvements on better CPUs)
    
    X-original-commit: 53e20ac4abc67b4dd0cb142ff7c2ea32ee9886dc

---
+++ tests/setup/jest_global_setup.ts
@@ -0,0 +1,5 @@
+import { writeTemplatesToFile } from "../../tools/owl_templates/compile_templates.cjs";
+
+module.exports = function () {
+  writeTemplatesToFile();
+};
---
+++ tests/setup/jest_global_teardown.ts
@@ -0,0 +1,5 @@
+import { deleteCompiledTemplatesFile } from "../../tools/owl_templates/compile_templates.cjs";
+
+module.exports = function () {
+  deleteCompiledTemplatesFile();
+};
