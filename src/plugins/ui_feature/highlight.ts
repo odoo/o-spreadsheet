8e158513643e1eb0cccd65f63072f89eb91120b3
Adrien Minne (adrm) <adrm@odoo.com>
2023-11-15 10:59:10 +0100

    [IMP] highlight: draw highlight over cell borders
    Before this, because of our computations and canvas' shenanigans, the
    highlights were not drawn exactly over the cell borders, but slightly
    beside it. This was particularly visible when drawing only the borders
    of the highlight without fill color, and wasn't very pretty.
    
    This commit made use of the same computations to draw the highlights
    than the computations of the cell borders, which makes the highlights
    be drawn exactly over the cell borders.
    
    Task: 3571796

--- src/plugins/ui_feature/highlight.ts
+++ src/plugins/ui_feature/highlight.ts
@@ -2,6 +2,7 @@ import { isEqual, zoneToDimension } from "../../helpers/index";
 import { highlightRegistry } from "../../registries/highlight_registry";
 import { GridRenderingContext, Highlight, LAYERS } from "../../types/index";
 import { UIPlugin } from "../ui_plugin";
+import { RendererPlugin } from "./renderer";
 
 /**
  * HighlightPlugin
@@ -55,10 +56,10 @@ export class HighlightPlugin extends UIPlugin {
 
   drawGrid(renderingContext: GridRenderingContext) {
     // rendering selection highlights
-    const { ctx, thinLineWidth } = renderingContext;
+    const { ctx } = renderingContext;
 
     const sheetId = this.getters.getActiveSheetId();
-    const lineWidth = 3 * thinLineWidth;
+    const lineWidth = 2;
     ctx.lineWidth = lineWidth;
     /**
      * We only need to draw the highlights of the current sheet.
@@ -77,11 +78,12 @@ export class HighlightPlugin extends UIPlugin {
       const { x, y, width, height } = this.getters.getVisibleRect(h.zone);
       if (width > 0 && height > 0) {
         ctx.strokeStyle = h.color!;
-        ctx.strokeRect(x + lineWidth / 2, y + lineWidth / 2, width - lineWidth, height - lineWidth);
+        /** + 0.5 offset to have sharp lines. See comment in {@link RendererPlugin#drawBorders} for more details */
+        ctx.strokeRect(x + 0.5, y + 0.5, width, height);
         ctx.globalCompositeOperation = "source-over";
         if (!h.noFill) {
           ctx.fillStyle = h.color! + "20";
-          ctx.fillRect(x + lineWidth, y + lineWidth, width - 2 * lineWidth, height - 2 * lineWidth);
+          ctx.fillRect(x, y, width, height);
         }
       }
     }
