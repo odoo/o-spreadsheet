6c9ce12f58aef7eb52d776a2519b89ff0fc8b9dc
Lucas Lefèvre (lul) <lul@odoo.com>
2023-11-17 11:58:17 +0100

    [FIX] evaluation: cache range references values
    When a function references a range `=SUM(A1:B10)`, a new 2d array is rebuilt
    with the cell values for every range in every functions.
    
    However, for LOOKUP functions, it's very likely to have many cells with a
    LOOKUP function looking for a match in the exact same range.
    
    In the customer spreadsheet, `range` self-time goes from ~15% to almost nothing
    (0.2%). There's another spreadsheet where many cells references the same range
    with 100k cells. Loading the spreadsheet was taking 26s before and 7s after.
    
    opw-3588401
    Task: 3588401

--- src/plugins/ui/evaluation.ts
+++ src/plugins/ui/evaluation.ts
@@ -228,6 +228,8 @@ export class EvaluationPlugin extends UIPlugin {
       return cell.evaluated;
     }
 
+    const rangeCache = {};
+
     /**
      * Return the values of the cell(s) used in reference, but always in the format of a range even
      * if a single cell is referenced. It is a list of col values. This is useful for the formulas that describe parameters as
@@ -252,6 +254,12 @@ export class EvaluationPlugin extends UIPlugin {
         return [[]];
       }
       const { top, left, bottom, right } = zone;
+
+      const cacheKey = `${sheetId}-${top}-${left}-${bottom}-${right}`;
+      if (cacheKey in rangeCache) {
+        return rangeCache[cacheKey];
+      }
+
       const result: MatrixArg = new Array(right - left + 1);
 
       // Performance issue: nested loop is faster than a map here
@@ -265,6 +273,7 @@ export class EvaluationPlugin extends UIPlugin {
         }
         result[col - left] = rowValues;
       }
+      rangeCache[cacheKey] = result;
       return result;
     }
 
