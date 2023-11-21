d69422ad8ab839d338453384b8a8c7a8fc13cd43
Lucas Lefèvre (lul) <lul@odoo.com>
2023-11-21 11:13:51 +0100

    [FIX] composer: loop fixed full references
    Given the formula `=SUM(1:5)`, press F4 with you cursor on the reference
    to fix the references.
    => the result is `$$1:$$5` instead of `$1:$5`
    
    There's a similar issue with full column references.
    
    Task: 3571633
    X-original-commit: d814cd8d8281ff2cb9c5e2134e49eeaf7b65f8e7
    Part-of: odoo/o-spreadsheet#3308

--- tests/helpers/reference_types.test.ts
+++ tests/helpers/reference_types.test.ts
@@ -34,6 +34,28 @@ describe("loopThroughReferenceType", () => {
     expect(loopThroughReferenceType(refToken("Sheet2!$A1"))).toEqual(refToken("Sheet2!A1"));
   });
 
+  test("on full rows", () => {
+    expect(loopThroughReferenceType(refToken("1:5"))).toEqual(refToken("$1:$5"));
+    expect(loopThroughReferenceType(refToken("$1:$5"))).toEqual(refToken("1:5"));
+    expect(loopThroughReferenceType(refToken("$1:5"))).toEqual(refToken("1:$5"));
+    expect(loopThroughReferenceType(refToken("1:$5"))).toEqual(refToken("$1:5"));
+    expect(loopThroughReferenceType(refToken("B1:3"))).toEqual(refToken("$B$1:$3"));
+    expect(loopThroughReferenceType(refToken("$B$1:$3"))).toEqual(refToken("B$1:3"));
+    expect(loopThroughReferenceType(refToken("B$1:3"))).toEqual(refToken("$B1:$3"));
+    expect(loopThroughReferenceType(refToken("$B1:$3"))).toEqual(refToken("B1:3"));
+  });
+
+  test("on full columns", () => {
+    expect(loopThroughReferenceType(refToken("A:B"))).toEqual(refToken("$A:$B"));
+    expect(loopThroughReferenceType(refToken("$A:$B"))).toEqual(refToken("A:B"));
+    expect(loopThroughReferenceType(refToken("$A:B"))).toEqual(refToken("A:$B"));
+    expect(loopThroughReferenceType(refToken("A:$B"))).toEqual(refToken("$A:B"));
+    expect(loopThroughReferenceType(refToken("B2:C"))).toEqual(refToken("$B$2:$C"));
+    expect(loopThroughReferenceType(refToken("$B$2:$C"))).toEqual(refToken("B$2:C"));
+    expect(loopThroughReferenceType(refToken("B$2:C"))).toEqual(refToken("$B2:$C"));
+    expect(loopThroughReferenceType(refToken("$B2:$C"))).toEqual(refToken("B2:C"));
+  });
+
   test("can have sheet reference on range", () => {
     expect(loopThroughReferenceType(refToken("Sheet2!A1:B1"))).toEqual(
       refToken("Sheet2!$A$1:$B$1")
