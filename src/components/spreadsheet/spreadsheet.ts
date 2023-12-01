bd9d6e9b7e3ce1f369d4ff78abf539fbd98f29cc
Rémi Rahir (rar) <rar@odoo.com>
2023-12-01 11:57:53 +0100

    [FIX] spreadsheet: Force left-to-right direction
    Right now, we do not support the right-to-left direction in our
    component. While some ui features adapt to it, most components are not
    designes for this.  This revision enforces the ltr behaviour upon the
    whole spreadsheet.
    
    X-original-commit: a554a8ba8891fb2493c17d78587f43f814e04291

--- src/components/spreadsheet/spreadsheet.ts
+++ src/components/spreadsheet/spreadsheet.ts
@@ -82,6 +82,8 @@ css/* scss */ `
     *:before,
     *:after {
       box-sizing: content-box;
+      /** rtl not supported ATM */
+      direction: ltr;
     }
     .o-separator {
       border-bottom: ${MENU_SEPARATOR_BORDER_WIDTH}px solid ${SEPARATOR_COLOR};
