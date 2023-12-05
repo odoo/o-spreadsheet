cfa75815820bf55dd788d5c1f3827aa7e7ed291b
Adrien Minne (adrm) <adrm@odoo.com>
2023-12-05 09:56:42 +0100

    [IMP] stores: add a way for store to draw on the canvas
    Introduce a new store, the `RenderersManagerStore` that other stores
    can register into to be drawn on the canvas. The `RenderersManagerStore`
    draw method is called at the same place as the `model.drawGrid`, in
    the `drawGridHook` helper.
    
    Task: 3571796

---
+++ src/stores/renderer_manager_store.ts
@@ -0,0 +1,36 @@
+import { ModelStore, ReactiveStore } from "../store_engine";
+import { GridRenderingContext, LAYERS } from "../types";
+import { RendererStore } from "./renderer_store";
+
+export class RenderersManagerStore extends ReactiveStore {
+  protected model = this.get(ModelStore);
+  private renderers: Partial<Record<LAYERS, RendererStore[]>> = {};
+
+  register(renderer: RendererStore) {
+    for (const layer of renderer.layers) {
+      if (!this.renderers[layer]) {
+        this.renderers[layer] = [];
+      }
+      this.renderers[layer]!.push(renderer);
+    }
+  }
+
+  unRegister(renderer: RendererStore) {
+    for (const layer of Object.keys(this.renderers)) {
+      this.renderers[layer] = this.renderers[layer].filter((r: RendererStore) => r !== renderer);
+    }
+  }
+
+  triggerRender() {
+    this.model.dispatch("RENDER_CANVAS", {});
+  }
+
+  drawLayer(ctx: GridRenderingContext, layer: LAYERS) {
+    const renderers = this.renderers[layer];
+    if (renderers) {
+      for (const renderer of renderers) {
+        renderer.draw(ctx, layer);
+      }
+    }
+  }
+}
