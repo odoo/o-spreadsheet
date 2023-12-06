3253c595327fae346c35c216a8223a9ffc4e0fe5
anhe-odoo <anhe@odoo.com>
2023-12-06 10:11:57 +0000

    [FIX] chart: fix incoherent side_pannel state
    Task Description
    
    This task aims to adress some issues in the side pannel of a chart:
    1. When having only one point in a dataseries and selecting "First
    row as header", the point dissapear from the chart (legit), but
    we don't have the possibility to uncheck this checkbox.
    2. When having a normal dataset and another dataset containing one
    point, checking the "... as header" checkbox works has expected, but
    unchecking it don't make the 'one-point' dataset come back
    
    Related Task:
    
    closes odoo/o-spreadsheet#3598
    
    Task: 3380568
    X-original-commit: 3413a17a160e861a7c4b7a13e11d5cde22373ec6
    Signed-off-by: Lucas Lefèvre (lul) <lul@odoo.com>
    Signed-off-by: Hendrickx Anthony (anhe) <anhe@odoo.com>

--- src/helpers/figures/charts/line_chart.ts
+++ src/helpers/figures/charts/line_chart.ts
@@ -18,6 +18,7 @@ import {
 } from "../../../types/chart/chart";
 import { LegendPosition, VerticalAxisPosition } from "../../../types/chart/common_chart";
 import { LineChartDefinition, LineChartRuntime } from "../../../types/chart/line_chart";
+import { CellErrorType } from "../../../types/errors";
 import { Validator } from "../../../types/validator";
 import { toXlsxHexColor } from "../../../xlsx/helpers/colors";
 import { createRange } from "../../range";
@@ -161,7 +162,7 @@ export class LineChart extends AbstractChart {
     if (this.aggregated) return undefined;
     const dataSets: ExcelChartDataset[] = this.dataSets
       .map((ds: DataSet) => toExcelDataset(this.getters, ds))
-      .filter((ds) => ds.range !== ""); // && range !== CellErrorType.InvalidReference ? show incorrect #ref ?
+      .filter((ds) => ds.range !== "" && ds.range !== CellErrorType.InvalidReference);
     const labelRange = toExcelLabelRange(
       this.getters,
       this.labelRange,
