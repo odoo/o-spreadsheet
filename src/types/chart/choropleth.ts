0eb51d5c348833b1116959c59a90a5a098f50a67
Pierre Rousseau <pro@odoo.com>
2023-12-11 09:53:30 +0100

    wip

---
+++ src/types/chart/choropleth.ts
@@ -0,0 +1,19 @@
+import type { ChartConfiguration } from "chart.js";
+import { Color } from "../misc";
+import { LegendPosition } from "./common_chart";
+
+export interface PieChartDefinition {
+  readonly type: "choropleth";
+  readonly dataSets: string[];
+  readonly dataSetsHaveTitle: boolean;
+  readonly labelRange?: string;
+  readonly title: string;
+  readonly background?: Color;
+  readonly legendPosition: LegendPosition;
+  readonly aggregated?: boolean;
+}
+
+export type PieChartRuntime = {
+  chartJsConfig: ChartConfiguration;
+  background: Color;
+};
