b968fdd9c7b9010d651d4f84c2d8af93e093c929
dhrp-odoo <dhrp@odoo.com>
2023-12-13 13:13:17 +0000

    [FIX] BottomBarSheet: renaming a sheet with styled content
    Modified the span element to be contenteditable in plain-text only,
    resolving the traceback issue during sheet renaming.
    
    Task:: 3621086
    
    X-original-commit: 2fc63133bfdd25b7e64b37cf5b3a20b1b99f295c

--- tests/bottom_bar/bottom_bar_component.test.ts
+++ tests/bottom_bar/bottom_bar_component.test.ts
@@ -252,7 +252,7 @@ describe("BottomBar component", () => {
       const sheetName = fixture.querySelector<HTMLElement>(".o-sheet-name")!;
       expect(sheetName.getAttribute("contenteditable")).toEqual("false");
       await doubleClick(sheetName);
-      expect(sheetName.getAttribute("contenteditable")).toEqual("true");
+      expect(sheetName.getAttribute("contenteditable")).toEqual("plaintext-only");
       expect(document.activeElement).toEqual(sheetName);
     });
 
@@ -261,7 +261,7 @@ describe("BottomBar component", () => {
       await nextTick();
       await click(fixture, ".o-menu-item[data-name='rename'");
       const sheetName = fixture.querySelector<HTMLElement>(".o-sheet-name")!;
-      expect(sheetName.getAttribute("contenteditable")).toEqual("true");
+      expect(sheetName.getAttribute("contenteditable")).toEqual("plaintext-only");
       expect(document.activeElement).toEqual(sheetName);
     });
 
