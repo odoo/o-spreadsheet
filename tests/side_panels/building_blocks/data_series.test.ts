a5d53c522f5145ac547e6ebdb9eced7695baddd1
Pierre Rousseau <pro@odoo.com>
2023-12-14 14:47:06 +0100

    [IMP] chart: introduce building blocks for chart side panel
    This commit introduces new building blocks for the chart side panel,
    aimed to be reused in the side panels of the charts.
    
    Task: 3635897

---
+++ tests/side_panels/building_blocks/data_series.test.ts
@@ -0,0 +1,52 @@
+import { Component, xml } from "@odoo/owl";
+import { ChartDataSeries } from "../../../src/components/side_panel/chart/building_blocks/data_series/data_series";
+import { SpreadsheetChildEnv } from "../../../src/types";
+import { mountComponent } from "../../test_helpers/helpers";
+
+let fixture: HTMLElement;
+
+type Props = ChartDataSeries["props"];
+
+class Container extends Component<Props, SpreadsheetChildEnv> {
+  static template = xml/* xml */ `
+    <div class="container">
+      <ChartDataSeries t-props="props"/>
+    </div>
+  `;
+  static components = { ChartDataSeries };
+}
+
+async function mountDataSeries(props: Props) {
+  ({ fixture } = await mountComponent(Container, { props }));
+}
+
+describe("Data Series", () => {
+  test("Can render a data series component", async () => {
+    await mountDataSeries({
+      ranges: () => ["A1:B1"],
+      onSelectionChanged: () => {},
+      onSelectionConfirmed: () => {},
+    });
+    expect(fixture).toMatchSnapshot();
+  });
+
+  test("Title is 'Data range' when hasSingleRange is true", async () => {
+    await mountDataSeries({
+      ranges: () => ["A1:B1"],
+      onSelectionChanged: () => {},
+      onSelectionConfirmed: () => {},
+      hasSingleRange: true,
+    });
+    expect(fixture.querySelector(".o-section-title")!.textContent).toEqual("Data range");
+  });
+
+  test("Title is 'Data series' when hasSingleRange is false", async () => {
+    await mountDataSeries({
+      ranges: () => ["A1:B1"],
+      onSelectionChanged: () => {},
+      onSelectionConfirmed: () => {},
+      hasSingleRange: false,
+    });
+    expect(fixture.querySelector(".o-section-title")!.textContent).toEqual("Data series");
+  });
+});
---
+++ tests/side_panels/building_blocks/label_range.test.ts
@@ -0,0 +1,62 @@
+import { Component, xml } from "@odoo/owl";
+import { ChartLabelRange } from "../../../src/components/side_panel/chart/building_blocks/label_range/label_range";
+import { SpreadsheetChildEnv } from "../../../src/types";
+import { mountComponent } from "../../test_helpers/helpers";
+
+let fixture: HTMLElement;
+
+type Props = ChartLabelRange["props"];
+
+class Container extends Component<Props, SpreadsheetChildEnv> {
+  static template = xml/* xml */ `
+    <div class="container">
+      <ChartLabelRange t-props="props"/>
+    </div>
+  `;
+  static components = { ChartLabelRange };
+}
+
+async function mountLabelRange(props: Props) {
+  ({ fixture } = await mountComponent(Container, { props }));
+}
+
+describe("Label range", () => {
+  test("Can render a label range component", async () => {
+    await mountLabelRange({
+      range: () => "A1:B1",
+      isInvalid: false,
+      onSelectionChanged: () => {},
+      onSelectionConfirmed: () => {},
+    });
+    expect(fixture).toMatchSnapshot();
+  });
+
+  test("Can add options to the label range component", async () => {
+    await mountLabelRange({
+      range: () => "A1:B1",
+      isInvalid: false,
+      onSelectionChanged: () => {},
+      onSelectionConfirmed: () => {},
+      options: [
+        {
+          name: "my_option",
+          label: "My option",
+          value: true,
+          update: () => {},
+        },
+      ],
+    });
+    expect(fixture).toMatchSnapshot();
+  });
+
+  test("Can make the label range required", async () => {
+    await mountLabelRange({
+      range: () => "A1:B1",
+      isInvalid: false,
+      onSelectionChanged: () => {},
+      onSelectionConfirmed: () => {},
+      required: true,
+    });
+    expect(fixture.querySelector(".o-selection-input input")?.classList).toContain("o-required");
+  });
+});
