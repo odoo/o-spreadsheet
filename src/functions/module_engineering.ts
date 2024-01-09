61780b4e4581db52b4408fd056854f9228d4fc79
Lucas Lefèvre (lul) <lul@odoo.com>
2024-01-09 07:49:07 +0000

    [FIX] functions: name MID args
    Some function arguments are missing their names.
    
    It makes the composer autocomplete crash if you write "=MID("
    because the t-foreach key is duplicated (an empty string)
    
    Mistake from a815a16
    
    closes odoo/o-spreadsheet#3403
    
    X-original-commit: 67225a301e5719de086ec9e452c779d48f781d1e
    Signed-off-by: Pierre Rousseau (pro) <pro@odoo.com>
    Signed-off-by: Lucas Lefèvre (lul) <lul@odoo.com>

--- src/functions/module_engineering.ts
+++ src/functions/module_engineering.ts
@@ -11,8 +11,8 @@ const DEFAULT_DELTA_ARG = 0;
 export const DELTA = {
   description: _t("Compare two numeric values, returning 1 if they're equal."),
   args: [
-    arg(" (number)", _t("The first number to compare.")),
-    arg(` (number, default=${DEFAULT_DELTA_ARG})`, _t("The second number to compare.")),
+    arg("number1 (number)", _t("The first number to compare.")),
+    arg(`number2 (number, default=${DEFAULT_DELTA_ARG})`, _t("The second number to compare.")),
   ],
   returns: ["NUMBER"],
   compute: function (
