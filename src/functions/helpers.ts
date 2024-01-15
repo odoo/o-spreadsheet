thodinh/spreadsheet

[IMP] evaluation: allow formulas to handle error
The purpose of this commit is to allow the formulas
to return errors as results.

Functionally we want formulas like TRANSPOSE to be
able to transpose an error without invalidating the
entire evaluation process.

We also want formulas like LOOKUP not to be interrupted
when the search range has one or more errors.

To make this change we accepted errors in input and
output on the formulas,

It nevertheless remains possible to throw errors in the
formulas:
it is no longer the evaluation process at the cell level
which is stopped but the evaluation process at the formula
level.

Technically there is no longer a "try catch" at the
evaluator level,
here each formula is encapsulated in a “try catch”
when it enters the formula register

Other point, the _lastFnCalled present in the evaluation
context and allowing to know which is the last formula to
have been called before the raise  of an error is no longer
used. The information is available during the encapsulation
of the formula in the "try catch".
So, _lastFnCalled has therefore been removed from the
evaluation context.

Task: 2969401

Co-authored-by: Lucas Lefèvre (lul) <lul@odoo.com>

src/functions/helpers.ts
@@ -9,7 +9,7 @@ const SORT_TYPES_ORDER = ["number", "string", "boolean", "undefined"];
 
 export function assert(condition: () => boolean, message: string): void {
   if (!condition()) {
-    throw new EvaluationError(CellErrorType.GenericError, message);
+    throw new EvaluationError(message, CellErrorType.GenericError);
   }
 }
 // -----------------------------------------------------------------------------
@@ -39,10 +39,7 @@ export const expectStringSetError = (stringSet: string[], value: string) => {
   );
 };
 
-export function toNumber(
-  value: string | number | boolean | null | undefined,
-  locale: Locale
-): number {
+export function toNumber(value: CellValue | undefined, locale: Locale): number {
   switch (typeof value) {
     case "number":
       return value;
@@ -58,6 +55,9 @@ export function toNumber(
       }
       throw new Error(expectNumberValueError(value));
     default:
+      if (value instanceof EvaluationError) {
+        throw value;
+      }
       return 0;
   }
 }
@@ -90,24 +90,18 @@ export function tryCastAsNumberMatrix(data: Matrix<any>, argName: string): Matri
   return data as Matrix<number>;
 }
 
-export function strictToNumber(
-  value: string | number | boolean | null | undefined,
-  locale: Locale
-): number {
+export function strictToNumber(value: CellValue | undefined, locale: Locale): number {
   if (value === "") {
     throw new Error(expectNumberValueError(value));
   }
   return toNumber(value, locale);
 }
 
-export function toInteger(value: string | number | boolean | null | undefined, locale: Locale) {
+export function toInteger(value: CellValue | undefined, locale: Locale) {
   return Math.trunc(toNumber(value, locale));
 }
 
-export function strictToInteger(
-  value: string | number | boolean | null | undefined,
-  locale: Locale
-) {
+export function strictToInteger(value: CellValue | undefined, locale: Locale) {
   return Math.trunc(strictToNumber(value, locale));
 }
 
@@ -121,7 +115,7 @@ export function assertNumberGreaterThanOrEqualToOne(value: number) {
   );
 }
 
-export function toString(value: string | number | boolean | null | undefined): string {
+export function toString(value: CellValue | undefined): string {
   switch (typeof value) {
     case "string":
       return value;
@@ -130,6 +124,9 @@ export function toString(value: string | number | boolean | null | undefined): s
     case "boolean":
       return value ? "TRUE" : "FALSE";
     default:
+      if (value instanceof EvaluationError) {
+        throw value;
+      }
       return "";
   }
 }
@@ -156,7 +153,7 @@ const expectBooleanValueError = (value: string) =>
     value
   );
 
-export function toBoolean(value: string | number | boolean | null | undefined): boolean {
+export function toBoolean(value: CellValue | undefined): boolean {
   switch (typeof value) {
     case "boolean":
       return value;
@@ -176,21 +173,21 @@ export function toBoolean(value: string | number | boolean | null | undefined): 
     case "number":
       return value ? true : false;
     default:
+      if (value instanceof EvaluationError) {
+        throw value;
+      }
       return false;
   }
 }
 
-function strictToBoolean(value: string | number | boolean | null | undefined): boolean {
+function strictToBoolean(value: CellValue | undefined): boolean {
   if (value === "") {
     throw new Error(expectBooleanValueError(value));
   }
   return toBoolean(value);
 }
 
-export function toJsDate(
-  value: string | number | boolean | null | undefined,
-  locale: Locale
-): Date {
+export function toJsDate(value: CellValue | undefined, locale: Locale): Date {
   return numberToJsDate(toNumber(value, locale));
 }
 
@@ -299,6 +296,8 @@ export function reduceNumbers(
     (acc, ArgValue) => {
       if (typeof ArgValue === "number") {
         return cb(acc, ArgValue);
+      } else if (ArgValue instanceof EvaluationError) {
+        throw ArgValue;
       }
       return acc;
     },

odoo/o-spreadsheet

src/functions/helpers.ts
@@ -10,7 +10,7 @@ const SORT_TYPES_ORDER = ["number", "string", "boolean", "undefined"];
 
 export function assert(condition: () => boolean, message: string): void {
   if (!condition()) {
-    throw new EvaluationError(CellErrorType.GenericError, message);
+    throw new EvaluationError(message, CellErrorType.GenericError);
   }
 }
 // -----------------------------------------------------------------------------
@@ -40,10 +40,7 @@ export const expectStringSetError = (stringSet: string[], value: string) => {
   );
 };
 
-export function toNumber(
-  value: string | number | boolean | null | undefined,
-  locale: Locale
-): number {
+export function toNumber(value: CellValue | undefined, locale: Locale): number {
   switch (typeof value) {
     case "number":
       return value;
@@ -59,6 +56,9 @@ export function toNumber(
       }
       throw new Error(expectNumberValueError(value));
     default:
+      if (value instanceof EvaluationError) {
+        throw value;
+      }
       return 0;
   }
 }
@@ -91,24 +91,18 @@ export function tryCastAsNumberMatrix(data: Matrix<any>, argName: string): Matri
   return data as Matrix<number>;
 }
 
-export function strictToNumber(
-  value: string | number | boolean | null | undefined,
-  locale: Locale
-): number {
+export function strictToNumber(value: CellValue | undefined, locale: Locale): number {
   if (value === "") {
     throw new Error(expectNumberValueError(value));
   }
   return toNumber(value, locale);
 }
 
-export function toInteger(value: string | number | boolean | null | undefined, locale: Locale) {
+export function toInteger(value: CellValue | undefined, locale: Locale) {
   return Math.trunc(toNumber(value, locale));
 }
 
-export function strictToInteger(
-  value: string | number | boolean | null | undefined,
-  locale: Locale
-) {
+export function strictToInteger(value: CellValue | undefined, locale: Locale) {
   return Math.trunc(strictToNumber(value, locale));
 }
 
@@ -122,7 +116,7 @@ export function assertNumberGreaterThanOrEqualToOne(value: number) {
   );
 }
 
-export function toString(value: string | number | boolean | null | undefined): string {
+export function toString(value: CellValue | undefined): string {
   switch (typeof value) {
     case "string":
       return value;
@@ -131,6 +125,9 @@ export function toString(value: string | number | boolean | null | undefined): s
     case "boolean":
       return value ? "TRUE" : "FALSE";
     default:
+      if (value instanceof EvaluationError) {
+        throw value;
+      }
       return "";
   }
 }
@@ -157,7 +154,7 @@ const expectBooleanValueError = (value: string) =>
     value
   );
 
-export function toBoolean(value: string | number | boolean | null | undefined): boolean {
+export function toBoolean(value: CellValue | undefined): boolean {
   switch (typeof value) {
     case "boolean":
       return value;
@@ -177,21 +174,21 @@ export function toBoolean(value: string | number | boolean | null | undefined): 
     case "number":
       return value ? true : false;
     default:
+      if (value instanceof EvaluationError) {
+        throw value;
+      }
       return false;
   }
 }
 
-function strictToBoolean(value: string | number | boolean | null | undefined): boolean {
+function strictToBoolean(value: CellValue | undefined): boolean {
   if (value === "") {
     throw new Error(expectBooleanValueError(value));
   }
   return toBoolean(value);
 }
 
-export function toJsDate(
-  value: string | number | boolean | null | undefined,
-  locale: Locale
-): DateTime {
+export function toJsDate(value: CellValue | undefined, locale: Locale): DateTime {
   return numberToJsDate(toNumber(value, locale));
 }
 
@@ -300,6 +297,8 @@ export function reduceNumbers(
     (acc, ArgValue) => {
       if (typeof ArgValue === "number") {
         return cb(acc, ArgValue);
+      } else if (ArgValue instanceof EvaluationError) {
+        throw ArgValue;
       }
       return acc;
     },