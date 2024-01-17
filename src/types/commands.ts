6882ade0e64edb870c7b4c5a3bededf74d56e97f
Rémi Rahir (rar) <rar@odoo.com>
2024-01-17 15:29:37 +0100

    [FIX] SelectionInput: Allow commands that target non focused input
    Currently, dispatching a selectionInput related command can be rejected
    by the focused input even if the said command does not concern it.
    
    Example:
    - Set up 2 selection inputs, one of them with a single range (like for a
    line chart)
    - Focus the single range input
    - click on the "Add range" of the multi-range input
    -> nothing happens
    
    The command are rejected as they pass through the currently focused
    input, which might not have the same properties (e.g. `hasSingleRange`)
    than the one targetted by the command.
    
    closes odoo/o-spreadsheet#3474
    
    Task: 3689504
    Signed-off-by: Lucas Lefèvre (lul) <lul@odoo.com>

--- src/types/commands.ts
+++ src/types/commands.ts
@@ -1056,6 +1056,7 @@ export const enum CommandResult {
   InvalidSheetId,
   InputAlreadyFocused,
   MaximumRangesReached,
+  InvalidInputId,
   InvalidChartDefinition,
   InvalidDataSet,
   InvalidLabelRange,
