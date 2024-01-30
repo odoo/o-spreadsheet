290d4802bb0a62eb4349c45fbe521d16172f0135
Lucas Lefèvre (lul) <lul@odoo.com>
2024-01-30 16:03:05 +0100

    [IMP] composer: auto complete sheet names
    The composer now proposes sheet names when typing in the composer.
    Function have a higher priority. Sheet names only appears if no function
    is matched.
    
    Feedback from RNG
    
    Task: 3662965

---
+++ src/registries/auto_completes/sheet_name_auto_complete.ts
@@ -0,0 +1,28 @@
+import { getCanonicalSheetName } from "../../helpers";
+import { autoCompleteProviders } from "./auto_complete_registry";
+
+autoCompleteProviders.add("sheet_names", {
+  sequence: 150,
+  autoSelectFirstProposal: true,
+  getProposals(tokenAtCursor) {
+    if (
+      tokenAtCursor.type === "SYMBOL" ||
+      (tokenAtCursor.type === "UNKNOWN" && tokenAtCursor.value.startsWith("'"))
+    ) {
+      return this.getters.getSheetIds().map((sheetId) => {
+        const sheetName = getCanonicalSheetName(this.getters.getSheetName(sheetId));
+        return {
+          text: sheetName,
+          fuzzySearchKey: sheetName.startsWith("'") ? sheetName : "'" + sheetName, // typing a single quote is a way to avoid matching function names
+        };
+      });
+    }
+    return [];
+  },
+  selectProposal(tokenAtCursor, value) {
+    const start = tokenAtCursor.start;
+    const end = tokenAtCursor.end;
+    this.composer.changeComposerCursorSelection(start, end);
+    this.composer.replaceComposerCursorSelection(value + "!");
+  },
+});
---
+++ tests/composer/auto_complete/sheet_name_auto_complete.test.ts
@@ -0,0 +1,70 @@
+import { ComposerStore } from "../../../src/components/composer/composer/composer_store";
+import { createSheet } from "../../test_helpers/commands_helpers";
+import { makeStore } from "../../test_helpers/stores";
+
+describe("Sheet name auto complete", () => {
+  test("auto complete a single sheet", () => {
+    const { store: composer, model } = makeStore(ComposerStore);
+    createSheet(model, { name: "MySheet" });
+    composer.startEdition("=MyS");
+    const autoComplete = composer.autocompleteProvider;
+    const proposals = autoComplete?.proposals;
+    expect(proposals).toEqual([
+      {
+        text: "MySheet",
+        fuzzySearchKey: "'MySheet",
+      },
+    ]);
+    autoComplete?.selectProposal(proposals![0].text);
+    expect(composer.currentContent).toEqual("=MySheet!");
+  });
+
+  test("auto complete a sheet with spaces", () => {
+    const { store: composer, model } = makeStore(ComposerStore);
+    createSheet(model, { name: "My awesome sheet" });
+    composer.startEdition("=aweso");
+    const autoComplete = composer.autocompleteProvider;
+    const proposals = autoComplete?.proposals;
+    expect(proposals).toEqual([
+      {
+        text: "'My awesome sheet'",
+        fuzzySearchKey: "'My awesome sheet'",
+      },
+    ]);
+    autoComplete?.selectProposal(proposals![0].text);
+    expect(composer.currentContent).toEqual("='My awesome sheet'!");
+  });
+
+  test("function auto complete has higher priority", () => {
+    const { store: composer, model } = makeStore(ComposerStore);
+    createSheet(model, { name: "SUM" });
+    composer.startEdition("=SU");
+    const autoComplete = composer.autocompleteProvider;
+    const proposals = autoComplete?.proposals;
+    expect(proposals![0].text).toEqual("SUM");
+    expect(proposals![0].description?.toString()).toEqual(
+      "Sum of a series of numbers and/or cells."
+    );
+  });
+
+  test("starting with single quote matches the sheet even if the quote is not required", () => {
+    const { store: composer, model } = makeStore(ComposerStore);
+    createSheet(model, { name: "Hello" });
+    composer.startEdition("='Hel");
+    const autoComplete = composer.autocompleteProvider;
+    const proposals = autoComplete?.proposals;
+    expect(proposals![0].text).toEqual("Hello");
+    autoComplete?.selectProposal(proposals![0].text);
+    expect(composer.currentContent).toEqual("=Hello!");
+  });
+
+  test("one single quote matches all sheets", () => {
+    const { store: composer, model } = makeStore(ComposerStore);
+    createSheet(model, { name: "Hello" });
+    composer.startEdition("='");
+    const autoComplete = composer.autocompleteProvider;
+    const proposals = autoComplete?.proposals;
+    expect(proposals![0].text).toEqual("Sheet1");
+    expect(proposals![1].text).toEqual("Hello");
+  });
+});
