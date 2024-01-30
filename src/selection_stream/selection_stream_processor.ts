1a67085b7ca19b254c14ec929f598e58a75debef
Rémi Rahir (rar) <rar@odoo.com>
2024-01-30 16:51:04 +0000

    [FIX] Selection: Loop selection based on content
    Similarly to the issue addressed in PR #3214, we want `loopSelection`
    to consider seemingly empty cells when considering the expansion zone.
    
    This revision splits the behaviour of the selection processor for
    `loopSelection` and the other navigation features (like cluster
    jumping) as we still want to ignore cells with an empty evaluation in
    those cases. The reason being that the user could be lost if, when
    navigating with their keyboards, they end up with their selection on
    empty cell.
    
    As for #3214, the fix will need to be adapted to account for the
    spreaded formulas.
    
    closes odoo/o-spreadsheet#3663
    
    Task: 3709340
    X-original-commit: 2a21fea2dbaf4f792ef19c797b1b66ebe024c684
    Signed-off-by: Lucas Lefèvre (lul) <lul@odoo.com>
    Signed-off-by: Rémi Rahir (rar) <rar@odoo.com>

--- src/selection_stream/selection_stream_processor.ts
+++ src/selection_stream/selection_stream_processor.ts
@@ -583,13 +583,12 @@ export class SelectionStreamProcessorImpl implements SelectionStreamProcessor {
    * next cluster if the given cell is outside a cluster or at the border of a cluster in the given direction.
    */
   private getEndOfCluster(startPosition: Position, dim: "cols" | "rows", dir: -1 | 1): HeaderIndex {
-    const sheet = this.getters.getActiveSheet();
     let currentPosition = startPosition;
 
     // If both the current cell and the next cell are not empty, we want to go to the end of the cluster
     const nextCellPosition = this.getNextCellPosition(startPosition, dim, dir);
     let mode: "endOfCluster" | "nextCluster" =
-      !this.isCellEmpty(currentPosition, sheet.id) && !this.isCellEmpty(nextCellPosition, sheet.id)
+      !this.isEvaluatedCellEmpty(currentPosition) && !this.isEvaluatedCellEmpty(nextCellPosition)
         ? "endOfCluster"
         : "nextCluster";
 
@@ -602,7 +601,7 @@ export class SelectionStreamProcessorImpl implements SelectionStreamProcessor {
       ) {
         break;
       }
-      const isNextCellEmpty = this.isCellEmpty(nextCellPosition, sheet.id);
+      const isNextCellEmpty = this.isEvaluatedCellEmpty(nextCellPosition);
       if (mode === "endOfCluster" && isNextCellEmpty) {
         break;
       } else if (mode === "nextCluster" && !isNextCellEmpty) {
@@ -616,13 +615,24 @@ export class SelectionStreamProcessorImpl implements SelectionStreamProcessor {
   }
 
   /**
-   * Check if a cell is empty or undefined in the model. If the cell is part of a merge,
-   * check if the merge containing the cell is empty.
+   * Check if a cell evaluated value is empty. If the cell is part of a merge,
+   * the check applies to the main cell of the merge.
    */
-  private isCellEmpty({ col, row }: Position, sheetId = this.getters.getActiveSheetId()): boolean {
+  private isEvaluatedCellEmpty({ col, row }: Position): boolean {
+    const sheetId = this.getters.getActiveSheetId();
     const position = this.getters.getMainCellPosition({ sheetId, col, row });
     const cell = this.getters.getEvaluatedCell(position);
     return cell.type === CellValueType.empty;
+  }
+
+  /**
+   * Checks if a cell is empty (i.e. does not have a content). If the cell is part of a merge,
+   * the check applies to the main cell of the merge.
+   */
+  private isCellEmpty({ col, row }: Position): boolean {
+    const sheetId = this.getters.getActiveSheetId();
+    const position = this.getters.getMainCellPosition({ sheetId, col, row });
+    return !this.getters.getCell(position)?.content;
   }
 
   /** Computes the next cell position in the given direction by crossing through merges and skipping hidden cells.
