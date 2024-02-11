244fef0a484d3449b43c3913b5d4c926cc6a0643
Lucas Lefèvre (lul) <lul@odoo.com>
2024-02-11 16:00:08 +0100

    [PERF] model: reuse success dispatch result
    Saves ~35MB when loading spreadsheet 142716 on odoo.com
    
    Task: 3741429
    X-original-commit: 22fea36f8f4d7aab20140c10a657d20b7c9e63b6
    Part-of: odoo/o-spreadsheet#3702

--- src/types/commands.ts
+++ src/types/commands.ts
@@ -995,7 +995,7 @@ export class DispatchResult {
    * Static helper which returns a successful DispatchResult
    */
   static get Success() {
-    return new DispatchResult();
+    return SUCCESS;
   }
 
   get isSuccessful(): boolean {
@@ -1010,6 +1010,8 @@ export class DispatchResult {
     return this.reasons.includes(reason);
   }
 }
+
+const SUCCESS = new DispatchResult();
 
 export type CancelledReason = Exclude<CommandResult, CommandResult.Success>;
 
