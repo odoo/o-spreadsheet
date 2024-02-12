d026f0cc3657c0697bf9bef56de3383eafa885c1
anhe-odoo <anhe@odoo.com>
2024-02-12 09:37:19 +0100

    [IMP] index: export ClipboardHandlers Classes
    With the new clipboard refactoring, we are now able to implement
    clipboard handlers to extend the o-spreadsheet clipboard behavior.
    Then, if one's wants to implement a new handler, it should be
    better to use the abstract classes as a base class for the handler,
    so this commit aims to export them.
    
    closes odoo/o-spreadsheet#3670
    
    Signed-off-by: Rémi Rahir (rar) <rar@odoo.com>

--- src/index.ts
+++ src/index.ts
@@ -339,3 +339,6 @@ export type {
   FPayload,
 } from "./types";
 export type { FunctionRegistry };
+
+export { AbstractCellClipboardHandler } from "./clipboard_handlers/abstract_cell_clipboard_handler";
+export { AbstractFigureClipboardHandler } from "./clipboard_handlers/abstract_figure_clipboard_handler";
