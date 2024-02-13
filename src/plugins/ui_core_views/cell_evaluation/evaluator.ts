5aaa4cabbef41f6d48d0485d2e856cfce022f572
Vincent Schippefilt <vsc@odoo.com>
2024-02-13 09:37:03 +0100

    [FIX] performance: JetSet
    Avoid spreading and collecting for nothing
    
    closes odoo/o-spreadsheet#3680
    
    X-original-commit: 9ff785f827c47c57a89ec534d9628d907c104ccb
    Signed-off-by: Rémi Rahir (rar) <rar@odoo.com>
    Signed-off-by: Vincent Schippefilt (vsc) <vsc@odoo.com>

--- src/plugins/ui_core_views/cell_evaluation/evaluator.ts
+++ src/plugins/ui_core_views/cell_evaluation/evaluator.ts
@@ -109,9 +109,9 @@ export class Evaluator {
     const cells: PositionId[] = positions.map((p) => this.encoder.encode(p));
     const cellsToCompute = new JetSet<PositionId>(cells);
     const arrayFormulasPositionIds = this.getArrayFormulasImpactedByChangesOf(cells);
-    cellsToCompute.add(...this.getCellsDependingOn(cells));
-    cellsToCompute.add(...arrayFormulasPositionIds);
-    cellsToCompute.add(...this.getCellsDependingOn(arrayFormulasPositionIds));
+    cellsToCompute.addMany(this.getCellsDependingOn(cells));
+    cellsToCompute.addMany(arrayFormulasPositionIds);
+    cellsToCompute.addMany(this.getCellsDependingOn(arrayFormulasPositionIds));
     this.evaluate(cellsToCompute);
   }
 
@@ -129,7 +129,7 @@ export class Evaluator {
       }
       if (!content) {
         // The previous content could have blocked some array formulas
-        impactedPositionIds.add(...this.getArrayFormulasBlockedByOrSpreadingOn(positionId));
+        impactedPositionIds.addMany(this.getArrayFormulasBlockedByOrSpreadingOn(positionId));
       }
     }
     return impactedPositionIds;
@@ -188,7 +188,7 @@ export class Evaluator {
     }
     const arrayFormulas = this.spreadingRelations.getFormulaPositionsSpreadingOn(positionId);
     const cells = new JetSet<PositionId>(arrayFormulas);
-    cells.add(...this.getCellsDependingOn(arrayFormulas));
+    cells.addMany(this.getCellsDependingOn(arrayFormulas));
     return cells;
   }
 
@@ -389,7 +389,7 @@ export class Evaluator {
 
       // check if formula dependencies present in the spread zone
       // if so, they need to be recomputed
-      this.nextPositionsToUpdate.add(...this.getCellsDependingOn([positionId]));
+      this.nextPositionsToUpdate.addMany(this.getCellsDependingOn([positionId]));
     };
   }
 
@@ -405,8 +405,8 @@ export class Evaluator {
         continue;
       }
       this.evaluatedCells.delete(child);
-      this.nextPositionsToUpdate.add(...this.getCellsDependingOn([child]));
-      this.nextPositionsToUpdate.add(...this.getArrayFormulasBlockedByOrSpreadingOn(child));
+      this.nextPositionsToUpdate.addMany(this.getCellsDependingOn([child]));
+      this.nextPositionsToUpdate.addMany(this.getArrayFormulasBlockedByOrSpreadingOn(child));
     }
     this.spreadingRelations.removeNode(positionId);
   }
