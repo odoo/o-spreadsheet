49a84cf7eb8feda1966ac2dfa4a157ec4792a2db
Alexis Lacroix <laa@odoo.com>
2024-02-19 15:45:41 +0100

    [REF] tofixup adapt autofillDV

--- src/plugins/ui_feature/autofill.ts
+++ src/plugins/ui_feature/autofill.ts
@@ -6,7 +6,6 @@ import {
   recomputeZones,
   toCartesian,
   toXC,
-  zoneToXc,
 } from "../../helpers/index";
 import { autofillModifiersRegistry, autofillRulesRegistry } from "../../registries/index";
 import {
@@ -473,12 +472,10 @@ export class AutofillPlugin extends UIPlugin {
     }
 
     const dvRangesZones = dvOrigin.ranges.map((range) => range.zone);
-    const newDvRanges = recomputeZones(dvRangesZones.concat(positionToZone({ col, row })), []).map(
-      zoneToXc
-    );
+    const newDvRanges = recomputeZones(dvRangesZones.concat(positionToZone({ col, row })), []);
     this.dispatch("ADD_DATA_VALIDATION_RULE", {
       rule: dvOrigin,
-      ranges: newDvRanges.map((xc) => this.getters.getRangeDataFromXc(sheetId, xc)),
+      ranges: newDvRanges.map((zone) => this.getters.getRangeDataFromZone(sheetId, zone)),
       sheetId,
     });
   }
