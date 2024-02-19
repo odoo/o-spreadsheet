96d4313fd00788f803e4d0d96fa454762bc95863
Lucas Lefèvre (lul) <lul@odoo.com>
2024-02-19 12:45:30 +0100

    [FIX] side_panel: safer dynamic props
    There are several problems in the current way side panels and their
    "props" are designed.
    
    If the side panel receives an id as a prop (e.g. `figureId`, `pivotId`),
    we have to check that the underlying figure exists when the side panel is
    open. We also have to check at every render that it has not been deleted.
    
    If the id is not given through a props but retrieved from the model with
    getters (`getters.getSelectedFigureId`), the issue is the same: nothing
    garantees it will always be defined.
    
    It's currently done with `onWillUpdateProps` which closes the side panel if
    the id is no longer valid.
    Closing the side panel at this point (while rendering) breaks the mental
    model of `UI = f(data)`. It's also error prone as the state is changed
    in the middle of a render. It means `sidePanel.isOpen` might be `true`
    at the begining of the template but the side panel might actually be closed
    later in the template.
    
    `onWillUpdateProps` should be avoided as much as possible.
    
    This commit adds the ability to declare a function in the side panel registry
    entry to:
    1) validate the props
    2) generate props based on the model (e.g. the selected figure/pivot/...)
    
    This callback function is called every time the side panel is rendered
    to check the props are still valid before rendering the side panel.
    
    Note that I had to move where all the entries were added to the side
    panel registry in a different file to avoid a circular dependency.
    
    closes odoo/o-spreadsheet#3725
    
    Signed-off-by: Rémi Rahir (rar) <rar@odoo.com>

--- src/registries/side_panel_registry.ts
+++ src/registries/side_panel_registry.ts
@@ -1,74 +1,20 @@
-import { ChartPanel } from "../components/side_panel/chart/main_chart_panel/main_chart_panel";
-import { ConditionalFormattingPanel } from "../components/side_panel/conditional_formatting/conditional_formatting";
-import { CustomCurrencyPanel } from "../components/side_panel/custom_currency/custom_currency";
-import { DataValidationEditor } from "../components/side_panel/data_validation/dv_editor/dv_editor";
-import { FindAndReplacePanel } from "../components/side_panel/find_and_replace/find_and_replace";
-import { MoreFormatsPanel } from "../components/side_panel/more_formats/more_formats";
-import { RemoveDuplicatesPanel } from "../components/side_panel/remove_duplicates/remove_duplicates";
-import { SettingsPanel } from "../components/side_panel/settings/settings_panel";
-import { SplitIntoColumnsPanel } from "../components/side_panel/split_to_columns_panel/split_to_columns_panel";
-import { _t } from "../translation";
-import { SpreadsheetChildEnv } from "../types";
-import { DataValidationPanel } from "./../components/side_panel/data_validation/data_validation_panel";
+import { SidePanelState } from "../components/side_panel/side_panel/side_panel_store";
+import { Getters, SpreadsheetChildEnv } from "../types";
 import { Registry } from "./registry";
 
 //------------------------------------------------------------------------------
 // Side Panel Registry
 //------------------------------------------------------------------------------
+
 export interface SidePanelContent {
   title: string | ((env: SpreadsheetChildEnv) => string);
   Body: any;
   Footer?: any;
+  /**
+   * A callback used to validate the props or generate new props
+   * based on the current state of the spreadsheet model, using the getters.
+   */
+  computeState?: (getters: Getters, initialProps: object) => SidePanelState;
 }
 
 export const sidePanelRegistry = new Registry<SidePanelContent>();
-
-sidePanelRegistry.add("ConditionalFormatting", {
-  title: _t("Conditional formatting"),
-  Body: ConditionalFormattingPanel,
-});
-
-sidePanelRegistry.add("ChartPanel", {
-  title: _t("Chart"),
-  Body: ChartPanel,
-});
-
-sidePanelRegistry.add("FindAndReplace", {
-  title: _t("Find and Replace"),
-  Body: FindAndReplacePanel,
-});
-
-sidePanelRegistry.add("CustomCurrency", {
-  title: _t("Custom currency format"),
-  Body: CustomCurrencyPanel,
-});
-
-sidePanelRegistry.add("SplitToColumns", {
-  title: _t("Split text into columns"),
-  Body: SplitIntoColumnsPanel,
-});
-
-sidePanelRegistry.add("Settings", {
-  title: _t("Spreadsheet settings"),
-  Body: SettingsPanel,
-});
-
-sidePanelRegistry.add("RemoveDuplicates", {
-  title: _t("Remove duplicates"),
-  Body: RemoveDuplicatesPanel,
-});
-
-sidePanelRegistry.add("DataValidation", {
-  title: _t("Data validation"),
-  Body: DataValidationPanel,
-});
-
-sidePanelRegistry.add("DataValidationEditor", {
-  title: _t("Data validation"),
-  Body: DataValidationEditor,
-});
-
-sidePanelRegistry.add("MoreFormats", {
-  title: _t("More date formats"),
-  Body: MoreFormatsPanel,
-});
---
+++ src/registries/side_panel_registry_entries.ts
@@ -0,0 +1,74 @@
+import { ChartPanel } from "../components/side_panel/chart/main_chart_panel/main_chart_panel";
+import { ConditionalFormattingPanel } from "../components/side_panel/conditional_formatting/conditional_formatting";
+import { CustomCurrencyPanel } from "../components/side_panel/custom_currency/custom_currency";
+import { DataValidationPanel } from "../components/side_panel/data_validation/data_validation_panel";
+import { DataValidationEditor } from "../components/side_panel/data_validation/dv_editor/dv_editor";
+import { FindAndReplacePanel } from "../components/side_panel/find_and_replace/find_and_replace";
+import { MoreFormatsPanel } from "../components/side_panel/more_formats/more_formats";
+import { RemoveDuplicatesPanel } from "../components/side_panel/remove_duplicates/remove_duplicates";
+import { SettingsPanel } from "../components/side_panel/settings/settings_panel";
+import { SplitIntoColumnsPanel } from "../components/side_panel/split_to_columns_panel/split_to_columns_panel";
+import { _t } from "../translation";
+import { Getters, UID } from "../types";
+import { sidePanelRegistry } from "./side_panel_registry";
+
+//------------------------------------------------------------------------------
+// Side Panel Registry
+//------------------------------------------------------------------------------
+
+sidePanelRegistry.add("ConditionalFormatting", {
+  title: _t("Conditional formatting"),
+  Body: ConditionalFormattingPanel,
+});
+
+sidePanelRegistry.add("ChartPanel", {
+  title: _t("Chart"),
+  Body: ChartPanel,
+  computeState: (getters: Getters, initialProps: { figureId: UID }) => {
+    const figureId = getters.getSelectedFigureId() ?? initialProps.figureId;
+    if (!getters.isChartDefined(figureId)) {
+      return { isOpen: false };
+    }
+    return { isOpen: true, props: { figureId } };
+  },
+});
+
+sidePanelRegistry.add("FindAndReplace", {
+  title: _t("Find and Replace"),
+  Body: FindAndReplacePanel,
+});
+
+sidePanelRegistry.add("CustomCurrency", {
+  title: _t("Custom currency format"),
+  Body: CustomCurrencyPanel,
+});
+
+sidePanelRegistry.add("SplitToColumns", {
+  title: _t("Split text into columns"),
+  Body: SplitIntoColumnsPanel,
+});
+
+sidePanelRegistry.add("Settings", {
+  title: _t("Spreadsheet settings"),
+  Body: SettingsPanel,
+});
+
+sidePanelRegistry.add("RemoveDuplicates", {
+  title: _t("Remove duplicates"),
+  Body: RemoveDuplicatesPanel,
+});
+
+sidePanelRegistry.add("DataValidation", {
+  title: _t("Data validation"),
+  Body: DataValidationPanel,
+});
+
+sidePanelRegistry.add("DataValidationEditor", {
+  title: _t("Data validation"),
+  Body: DataValidationEditor,
+});
+
+sidePanelRegistry.add("MoreFormats", {
+  title: _t("More date formats"),
+  Body: MoreFormatsPanel,
+});
