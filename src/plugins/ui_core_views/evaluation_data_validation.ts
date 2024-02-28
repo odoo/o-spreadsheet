cb53fc2499b834336b4b240e9bb27d4a1f900f66
Adrien Minne (adrm) <adrm@odoo.com>
2024-02-28 14:29:18 +0100

    [IMP] data validation: performance of icons
    A sheet with a lot of data validation icons was slow to render, because:
    
    - we loop on all the cells of the data validation, and not only the
    visible cells
    - we created components even for non-visible cells
    - we use `getColDimensionsInViewport` which is slow
    
    This commit fixes all of the above. We go from ~10fps to ~45fps when
    scrolling the sheet.
    
    We could be even faster by not using components and only t-call to
    templates, but the world isn't ready yet.
    
    Task: 3773923
    Part-of: odoo/o-spreadsheet#3770

--- src/plugins/ui_core_views/evaluation_data_validation.ts
+++ src/plugins/ui_core_views/evaluation_data_validation.ts
@@ -38,8 +38,6 @@ type SheetValidationResult = { [col: HeaderIndex]: Array<Lazy<ValidationResult>>
 export class EvaluationDataValidationPlugin extends UIPlugin {
   static getters = [
     "getDataValidationInvalidCriterionValueMessage",
-    "getDataValidationCheckBoxCellPositions",
-    "getDataValidationListCellsPositions",
     "getInvalidDataValidationMessage",
     "getValidationResultForCellValue",
     "isCellValidCheckbox",
@@ -120,26 +118,6 @@ export class EvaluationDataValidationPlugin extends UIPlugin {
     const error = this.getRuleErrorForCellValue(cellValue, cellPosition, rule);
 
     return error ? { error, rule, isValid: false } : VALID_RESULT;
-  }
-
-  getDataValidationCheckBoxCellPositions(): CellPosition[] {
-    const rules = this.getters
-      .getDataValidationRules(this.getters.getActiveSheetId())
-      .filter((rule) => rule.criterion.type === "isBoolean");
-    return getCellPositionsInRanges(rules.map((rule) => rule.ranges).flat()).filter((position) =>
-      this.isCellValidCheckbox(position)
-    );
-  }
-
-  getDataValidationListCellsPositions(): CellPosition[] {
-    const rules = this.getters
-      .getDataValidationRules(this.getters.getActiveSheetId())
-      .filter(
-        (rule) =>
-          (rule.criterion.type === "isValueInList" || rule.criterion.type === "isValueInRange") &&
-          rule.criterion.displayStyle === "arrow"
-      );
-    return getCellPositionsInRanges(rules.map((rule) => rule.ranges).flat());
   }
 
   private getValidationResultForCell(cellPosition: CellPosition): ValidationResult {
