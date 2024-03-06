b78361b053a5bd3014d365523d06aa445c8b20de
Adrien Minne (adrm) <adrm@odoo.com>
2024-03-06 08:47:46 +0000

    [FIX] formula assistant: localize argument separator
    The argument separator in the formula assistant was not localized, it
    was always a comma no matter the locale. This commit fixes it.
    
    Task: 3789860
    X-original-commit: 7e7b069229ec343ea1896d698718c9cda0641f8c

--- src/components/composer/formula_assistant/formula_assistant.ts
+++ src/components/composer/formula_assistant/formula_assistant.ts
@@ -1,6 +1,6 @@
 import { Component, onWillUnmount, useState } from "@odoo/owl";
 import { COMPOSER_ASSISTANT_COLOR } from "../../../constants";
-import { FunctionDescription } from "../../../types";
+import { FunctionDescription, SpreadsheetChildEnv } from "../../../types";
 import { css } from "../../helpers/css";
 
 // -----------------------------------------------------------------------------
@@ -46,7 +46,7 @@ interface AssistantState {
   allowCellSelectionBehind: boolean;
 }
 
-export class FunctionDescriptionProvider extends Component<Props> {
+export class FunctionDescriptionProvider extends Component<Props, SpreadsheetChildEnv> {
   static template = "o-spreadsheet-FunctionDescriptionProvider";
   static props = {
     functionName: String,
@@ -79,5 +79,9 @@ export class FunctionDescriptionProvider extends Component<Props> {
     this.timeOutId = setTimeout(() => {
       this.assistantState.allowCellSelectionBehind = false;
     }, 2000) as unknown as number;
+  }
+
+  get formulaArgSeparator() {
+    return this.env.model.getters.getLocale().formulaArgSeparator + " ";
   }
 }
