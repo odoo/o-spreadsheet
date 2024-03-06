ecee8ed153763db07e2a75812822667480551786
Adrien Minne (adrm) <adrm@odoo.com>
2024-03-06 08:47:46 +0000

    [FIX] formula assistant: localize argument separator
    The argument separator in the formula assistant was not localized, it
    was always a comma no matter the locale. This commit fixes it.
    
    closes odoo/o-spreadsheet#3882
    
    Task: 3789860
    X-original-commit: 7e7b069229ec343ea1896d698718c9cda0641f8c
    Signed-off-by: Lucas Lefèvre (lul) <lul@odoo.com>
    Signed-off-by: Adrien Minne (adrm) <adrm@odoo.com>

--- tests/composer/formula_assistant_component.test.ts
+++ tests/composer/formula_assistant_component.test.ts
@@ -2,7 +2,9 @@ import { setTranslationMethod } from "../../src";
 import { arg, functionRegistry } from "../../src/functions/index";
 import { Model } from "../../src/model";
 import { _t } from "../../src/translation";
+import { DEFAULT_LOCALE } from "../../src/types";
 import { registerCleanup } from "../setup/jest.setup";
+import { updateLocale } from "../test_helpers/commands_helpers";
 import { keyDown, keyUp } from "../test_helpers/dom_helper";
 import {
   ComposerWrapper,
@@ -251,6 +253,14 @@ describe("formula assistant", () => {
         await typeInComposer("=FUNC3(");
         expect(fixture.querySelectorAll(".o-formula-assistant-head")[0].textContent).toBe(
           "FUNC3 ( f3Arg1, [f3Arg2, ...] ) "
+        );
+      });
+
+      test("arguments separator is localized", async () => {
+        updateLocale(model, { ...DEFAULT_LOCALE, formulaArgSeparator: ";" });
+        await typeInComposer("=FUNC1(");
+        expect(fixture.querySelectorAll(".o-formula-assistant-head")[0].textContent).toBe(
+          "FUNC1 ( f1Arg1; f1Arg2 ) "
         );
       });
     });
