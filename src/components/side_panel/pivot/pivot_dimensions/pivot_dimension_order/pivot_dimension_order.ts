1bdb115744b2e3bacb1caea238cc12f703dba68d
Pierre Rousseau <pro@odoo.com>
2024-03-26 13:11:25 +0100

    dimension order

---
+++ src/components/side_panel/pivot/pivot_dimensions/pivot_dimension_order/pivot_dimension_order.ts
@@ -0,0 +1,16 @@
+import { Component } from "@odoo/owl";
+import { SpreadsheetChildEnv } from "../../../../..";
+import { PivotDimension } from "../../../../../types/pivot";
+
+interface Props {
+  dimension: PivotDimension;
+  onUpdated: (dimension: PivotDimension, ev: InputEvent) => void;
+}
+
+export class PivotDimensionOrder extends Component<Props, SpreadsheetChildEnv> {
+  static template = "o-spreadsheet-PivotDimensionOrder";
+  static props = {
+    dimension: Object,
+    onUpdated: Function,
+  };
+}
--- src/index.ts
+++ src/index.ts
@@ -39,6 +39,7 @@ import { Section } from "./components/side_panel/components/section/section";
 import { FindAndReplaceStore } from "./components/side_panel/find_and_replace/find_and_replace_store";
 import { AddDimensionButton } from "./components/side_panel/pivot/pivot_dimensions/add_dimension_button/add_dimension_button";
 import { PivotDimensionGranularity } from "./components/side_panel/pivot/pivot_dimensions/pivot_dimension_granularity/pivot_dimension_granularity";
+import { PivotDimensionOrder } from "./components/side_panel/pivot/pivot_dimensions/pivot_dimension_order/pivot_dimension_order";
 import { SidePanelStore } from "./components/side_panel/side_panel/side_panel_store";
 import { ValidationMessages } from "./components/validation_messages/validation_messages";
 import {
@@ -325,6 +326,7 @@ export const components = {
   ValidationMessages,
   AddDimensionButton,
   PivotDimensionGranularity,
+  PivotDimensionOrder,
 };
 
 export const hooks = {
