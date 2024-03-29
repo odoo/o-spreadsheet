0acf0a618b8ef78bb447f78bfd13618454bacd33
Lucas Lefèvre (lul) <lul@odoo.com>
2024-03-29 10:29:31 +0100

    [REF] ChartPanel: move business logic to store
    This commit prepares the next commit where the chart creation context
    will be preserved across multiple type changes.
    
    Task: 3833884

--- src/components/side_panel/chart/main_chart_panel/main_chart_panel_store.ts
+++ src/components/side_panel/chart/main_chart_panel/main_chart_panel_store.ts
@@ -1,9 +1,25 @@
+import { getChartDefinitionFromContextCreation } from "../../../../helpers/figures/charts";
 import { SpreadsheetStore } from "../../../../stores";
+import { ChartType, UID } from "../../../../types";
 
 export class MainChartPanelStore extends SpreadsheetStore {
   panel: "configuration" | "design" = "configuration";
 
   activatePanel(panel: "configuration" | "design") {
     this.panel = panel;
+  }
+
+  changeChartType(figureId: UID, type: ChartType) {
+    const context = this.getters.getContextCreationChart(figureId);
+    const sheetId = this.getters.getFigureSheetId(figureId);
+    if (!context || !sheetId) {
+      return;
+    }
+    const definition = getChartDefinitionFromContextCreation(context, type);
+    this.model.dispatch("UPDATE_CHART", {
+      definition,
+      id: figureId,
+      sheetId,
+    });
   }
 }
