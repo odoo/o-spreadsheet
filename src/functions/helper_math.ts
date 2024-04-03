e30d1cf10ab6f4d8e332448aa794a188a4c179b0
Pierre Rousseau <pro@odoo.com>
2024-04-03 10:06:55 +0200

    [REF] functions: create helper for COUNTUNIQUE
    This commit is a step to introduce the spreadsheet pivot. The function
    COUNTUNIQUE is used in the pivot table to count the unique values in a
    range. The helper function is created to avoid code duplication.
    
    Task: 3748717
    
    Co-authored-by: rrahir <rar@odoo.com>
    Co-authored-by: Pierre Rousseau <pro@odoo.com>

--- src/functions/helper_math.ts
+++ src/functions/helper_math.ts
@@ -1,6 +1,21 @@
-import { Arg, Locale } from "../types";
-import { reduceNumbers } from "./helpers";
+import { Arg, FPayload, Locale } from "../types";
+import { reduceAny, reduceNumbers } from "./helpers";
+
+export function isDefined(data: FPayload | undefined): boolean {
+  if (data === undefined) {
+    return false;
+  }
+  const { value } = data;
+  if (value === null || value === "") {
+    return false;
+  }
+  return true;
+}
 
 export function sum(values: Arg[], locale: Locale): number {
   return reduceNumbers(values, (acc, a) => acc + a, 0, locale);
+}
+
+export function countUnique(args: Arg[]): number {
+  return reduceAny(args, (acc, a) => (isDefined(a) ? acc.add(a?.value) : acc), new Set()).size;
 }
