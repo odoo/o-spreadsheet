fb640e4d675ecaa6d7ea13bebb3c45505859f70d
Rémi Rahir (rar) <rar@odoo.com>
2024-04-05 13:55:31 +0200

    [FIX] XLSX: simplify `pushElement` helper
    `pushElement` was returning an object but only one of its properties was
    used. After this revision, the helper only returns the id inside the
    list. It also alleviates the memory print.
    
    Task: 3733743
    Part-of: odoo/o-spreadsheet#4015

--- src/xlsx/helpers/content_helpers.ts
+++ src/xlsx/helpers/content_helpers.ts
@@ -34,11 +34,6 @@ import {
 } from "../constants";
 import { XLSX_FORMAT_MAP } from "../conversion/conversion_maps";
 
-type PropertyPosition<T> = {
-  id: number;
-  list: T[];
-};
-
 // -------------------------------------
 //            CF HELPERS
 // -------------------------------------
@@ -166,15 +161,12 @@ function extractFormat(cell: ExcelCellData, data: WorkbookData): Format | undefi
 }
 
 export function normalizeStyle(construct: XLSXStructure, styles: ExtractedStyle): number {
-  const { id: fontId } = pushElement(styles["font"], construct.fonts);
-  const { id: fillId } = pushElement(styles["fill"], construct.fills);
-  const { id: borderId } = pushElement(styles["border"], construct.borders);
   // Normalize this
   const numFmtId = convertFormat(styles["numFmt"], construct.numFmts);
   const style = {
-    fontId,
-    fillId,
-    borderId,
+    fontId: pushElement(styles["font"], construct.fonts),
+    fillId: pushElement(styles["fill"], construct.fills),
+    borderId: pushElement(styles["border"], construct.borders),
     numFmtId,
     alignment: {
       vertical: styles.alignment.vertical,
@@ -182,9 +174,7 @@ export function normalizeStyle(construct: XLSXStructure, styles: ExtractedStyle)
     },
   } as XLSXStyle;
 
-  const { id } = pushElement(style, construct.styles);
-
-  return id;
+  return pushElement(style, construct.styles);
 }
 
 function convertFormat(
@@ -196,8 +186,7 @@ function convertFormat(
   }
   let formatId: number | undefined = XLSX_FORMAT_MAP[format.format];
   if (!formatId) {
-    const { id } = pushElement(format, numFmtStructure);
-    formatId = id + FIRST_NUMFMT_ID;
+    formatId = pushElement(format, numFmtStructure) + FIRST_NUMFMT_ID;
   }
   return formatId;
 }
@@ -226,10 +215,10 @@ export function addRelsToFile(
   return id;
 }
 
-export function pushElement<T>(property: T, propertyList: T[]): PropertyPosition<T> {
+export function pushElement<T>(property: T, propertyList: T[]): number {
   for (let [key, value] of Object.entries(propertyList)) {
     if (JSON.stringify(value) === JSON.stringify(property)) {
-      return { id: parseInt(key, 10), list: propertyList };
+      return parseInt(key, 10);
     }
   }
   let elemId = propertyList.findIndex((elem) => JSON.stringify(elem) === JSON.stringify(property));
@@ -237,10 +226,7 @@ export function pushElement<T>(property: T, propertyList: T[]): PropertyPosition
     propertyList.push(property);
     elemId = propertyList.length - 1;
   }
-  return {
-    id: elemId,
-    list: propertyList,
-  };
+  return elemId;
 }
 
 const chartIds: UID[] = [];
