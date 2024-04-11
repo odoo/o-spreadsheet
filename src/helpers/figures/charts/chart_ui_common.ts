49a41ed684ea548b6d9b5ca9878749a593f163ac
Adrien Minne (adrm) <adrm@odoo.com>
2024-04-11 14:02:14 +0000

    [FIX] charts: time axis broken with long formatted date
    In the charts, we truncate the labels if they are too long
    (>20 characters)[1]. But in line chart with time axis, chartJS parses
    the labels to get a date. This parsing obviously fails if the label
    is truncated.
    
    This commit fixes the issue by not truncating the labels if the
    axis is a time axis.
    
    [1] e.g. a format like "12/04/2024 12:34:56 AM".
    
    closes odoo/o-spreadsheet#4145
    
    Task: 3857242
    X-original-commit: 1f97287d3c8c306abeb1daf98f6c7d406e38a623
    Signed-off-by: Rémi Rahir (rar) <rar@odoo.com>
    Signed-off-by: Adrien Minne (adrm) <adrm@odoo.com>

--- src/helpers/figures/charts/chart_ui_common.ts
+++ src/helpers/figures/charts/chart_ui_common.ts
@@ -100,7 +100,7 @@ export function getDefaultChartJsRuntime(
   chart: AbstractChart,
   labels: string[],
   fontColor: Color,
-  { format, locale }: LocaleFormat
+  { format, locale, truncateLabels }: LocaleFormat & { truncateLabels?: boolean }
 ): Required<ChartConfiguration> {
   const options: ChartOptions = {
     // https://www.chartjs.org/docs/latest/general/responsive.html
@@ -148,7 +148,7 @@ export function getDefaultChartJsRuntime(
     type: chart.type as ChartType,
     options,
     data: {
-      labels: labels.map(truncateLabel),
+      labels: truncateLabels ? labels.map(truncateLabel) : labels,
       datasets: [],
     },
     platform: undefined as unknown as typeof BasePlatform, // This key is optional and will be set by chart.js
