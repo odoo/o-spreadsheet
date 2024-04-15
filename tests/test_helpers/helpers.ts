4240b73ff22307a839e1a259a3430c315359abd2
Adrien Minne (adrm) <adrm@odoo.com>
2024-04-15 08:57:30 +0200

    [FIX] chart: avoid useless chart updates
    There was an `useEffect` in the `chartJS` component to update the
    chartJS object on runtime change. There was 2 problems:
    
    1) useEffect on object compare the references, so if the chart plugin
    was ever changed to return copy of runtime it would break
    2) the chart plugin re-build every chartRuntime on each command that
    could affect a chart. This created a chart update at each UPDATE_CELL
    even if the actual runtime didn't change.
    
    This commit replace the `useEffect` dependency with a `deepEquals`
    
    Task: 3697660

--- tests/test_helpers/helpers.ts
+++ tests/test_helpers/helpers.ts
@@ -621,7 +621,7 @@ export const mockChart = () => {
     }
     toBase64Image = () => "";
     destroy = () => {};
-    update = () => {};
+    update() {}
     options = mockChartData.options;
     config = mockChartData;
   }
