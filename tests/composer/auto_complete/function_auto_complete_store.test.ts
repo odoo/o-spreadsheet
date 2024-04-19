12851682f9c5c408c31957f9372e1bc41276782f
Lucas Lefèvre (lul) <lul@odoo.com>
2024-04-19 10:25:04 +0200

    [FIX] composer: show auto-complete when other proposals
    Steps to reproduce:
    
    - Type in a cell "=SUM" (capitalized, it's important)
    - click else where to close the composer (the cell is now a #BAD_EXPR)
    - double click on the cell to open the composer
    - click at the end of "=SUM" to put your cursor at the end to force the
      autocomplete to show up (*)
    - Observe that the autocomplete dropdown doesn't open, even if intending to
      type "SUMIF".
    
    (*)the auto-complete also suffers from this bug: https://www.odoo.com/web#id=3872312&cids=1&menu_id=4720&action=333&active_id=2328&model=project.task&view_type=form
    
    closes odoo/o-spreadsheet#4096
    
    Task: 3836124
    Signed-off-by: Pierre Rousseau (pro) <pro@odoo.com>

---
+++ tests/composer/auto_complete/function_auto_complete_store.test.ts
@@ -0,0 +1,18 @@
+import { ComposerStore } from "../../../src/components/composer/composer/composer_store";
+import { setCellContent } from "../../test_helpers/commands_helpers";
+import { makeStore } from "../../test_helpers/stores";
+
+describe("Function auto complete", () => {
+  test("start with exact match, but with other proposals", () => {
+    const { store: composer, model } = makeStore(ComposerStore);
+    setCellContent(model, "A1", "=SUM");
+    composer.startEdition();
+    const autoComplete = composer.autocompleteProvider;
+    const proposals = autoComplete?.proposals;
+    expect(proposals).toHaveLength(10);
+    expect(proposals?.[0].text).toEqual("SUM");
+    expect(proposals?.[1].text).toEqual("SUMIF");
+    autoComplete?.selectProposal(proposals![0].text);
+    expect(composer.currentContent).toEqual("=SUM(");
+  });
+});
