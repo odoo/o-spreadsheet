2dab793593b91c1fb871cf7fd83636fb59a75458
Adrien Minne (adrm) <adrm@odoo.com>
2024-04-24 09:45:03 +0200

    [IMP] clipboard: insert new rows when pasting charts
    Currently, when pasting a chart at the edges of a sheet, the pasted
    chart is shifted so that it doesn't go out of the sheet.
    
    This commit changes the behaviour so that now the chart is always
    pasted with its top-left corner at the top-left corner of the selection,
    and insert new rows/columns if needed so that the chart fits in the
    sheet.
    
    Also take the occasion to factorize the code in common between the
    chart clipboard and the image clipboard into the figure clipboard.
    
    Task: 3869372

--- src/clipboard_handlers/abstract_figure_clipboard_handler.ts
+++ src/clipboard_handlers/abstract_figure_clipboard_handler.ts
@@ -1,8 +1,116 @@
-import { ClipboardFigureData } from "../types";
+import { UuidGenerator } from "../helpers";
+import {
+  ClipboardFigureData,
+  ClipboardOptions,
+  ClipboardPasteTarget,
+  CommandResult,
+  DOMCoordinates,
+  Figure,
+  FigureSize,
+  UID,
+  Zone,
+} from "../types";
 import { ClipboardHandler } from "./abstract_clipboard_handler";
 
-export class AbstractFigureClipboardHandler<T> extends ClipboardHandler<T> {
+export type FigureClipboardContent = {
+  sheetId: UID;
+  figureId: UID;
+  copiedFigure?: Figure;
+};
+
+export class AbstractFigureClipboardHandler<
+  T extends FigureClipboardContent
+> extends ClipboardHandler<T> {
   copy(data: ClipboardFigureData): T | undefined {
     return;
+  }
+
+  getPasteTarget(
+    targetZones: Zone[],
+    content: FigureClipboardContent,
+    options?: ClipboardOptions
+  ): ClipboardPasteTarget {
+    if (!content?.copiedFigure) {
+      return { zones: [] };
+    }
+    const target = targetZones[0];
+    const sheetId = this.getters.getActiveSheetId();
+
+    const topInPixel = this.getters.getRowDimensions(sheetId, target.top).start;
+    let bottom = this.getters.getRowAtPosition(sheetId, topInPixel + content.copiedFigure.height);
+    if (!bottom) {
+      const missingRows = this.getters.getHeadersToAddForPositionToBeInSheet(
+        sheetId,
+        "ROW",
+        topInPixel + content.copiedFigure.height
+      );
+      bottom = this.getters.getNumberRows(sheetId) - 1 + missingRows;
+    }
+
+    const leftInPixel = this.getters.getColDimensions(sheetId, target.left).start;
+    let right = this.getters.getColAtPosition(sheetId, leftInPixel + content.copiedFigure.width);
+    if (!right) {
+      const missingCols = this.getters.getHeadersToAddForPositionToBeInSheet(
+        sheetId,
+        "COL",
+        leftInPixel + content.copiedFigure.width
+      );
+      right = this.getters.getNumberCols(sheetId) - 1 + missingCols;
+    }
+
+    return {
+      zones: [{ left: target.left, top: target.top, right, bottom }],
+      figureId: new UuidGenerator().uuidv4(),
+    };
+  }
+
+  paste(
+    target: ClipboardPasteTarget,
+    clippedContent: FigureClipboardContent,
+    options: ClipboardOptions
+  ) {
+    if (!clippedContent?.copiedFigure || !target.figureId) {
+      return;
+    }
+    const { zones, figureId } = target;
+    const { width, height } = clippedContent.copiedFigure;
+    const sheetId = this.getters.getActiveSheetId();
+    const targetX = this.getters.getColDimensions(sheetId, zones[0].left).start;
+    const targetY = this.getters.getRowDimensions(sheetId, zones[0].top).start;
+    const position = { x: targetX, y: targetY };
+
+    this.pasteFigure(sheetId, clippedContent, figureId, position, { height, width });
+
+    if (options?.isCutOperation) {
+      this.dispatch("DELETE_FIGURE", {
+        sheetId: clippedContent.sheetId,
+        id: clippedContent.copiedFigure.id,
+      });
+    }
+  }
+
+  isPasteAllowed(
+    sheetId: UID,
+    target: Zone[],
+    content: FigureClipboardContent,
+    option?: ClipboardOptions
+  ) {
+    if (target.length === 0) {
+      return CommandResult.EmptyTarget;
+    }
+    if (option?.pasteOption !== undefined) {
+      return CommandResult.WrongFigurePasteOption;
+    }
+    return CommandResult.Success;
+  }
+
+  pasteFigure(
+    sheetId: UID,
+    clippedContent: FigureClipboardContent,
+    figureId: UID,
+    position: DOMCoordinates,
+    size: FigureSize
+  ): void {
+    throw new Error("The method should be implemented by its sub-class.");
   }
 }
