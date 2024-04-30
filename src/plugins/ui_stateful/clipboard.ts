a22fede718a781562267a822b7732d088fa5d5ed
Adrien Minne (adrm) <adrm@odoo.com>
2024-04-30 14:58:17 +0200

    [FIX] clipboard: wrong clipboard invalidation
    The CUT clipboard should be invalidated when inserting rows inside
    of the CUT zone.
    
    But we didn't check the sheet where the rows are
    inserted, so the clipboard was invalidating when inserting rows inside
    another sheet.
    
    closes odoo/o-spreadsheet#4234
    
    Task: 3901961
    X-original-commit: ae31d6534828f94eac2a7a179376e8a281242125
    Signed-off-by: Lucas Lefèvre (lul) <lul@odoo.com>
    Signed-off-by: Adrien Minne (adrm) <adrm@odoo.com>

--- src/plugins/ui_stateful/clipboard.ts
+++ src/plugins/ui_stateful/clipboard.ts
@@ -185,7 +185,7 @@ export class ClipboardPlugin extends UIPlugin {
         this.status = "invisible";
 
         // If we add a col/row inside or before the cut area, we invalidate the clipboard
-        if (this.state?.operation !== "CUT") {
+        if (this.state?.operation !== "CUT" || cmd.sheetId !== this.state?.sheetId) {
           return;
         }
         const isClipboardDirty = this.state.isColRowDirtyingClipboard(
@@ -201,7 +201,7 @@ export class ClipboardPlugin extends UIPlugin {
         this.status = "invisible";
 
         // If we remove a col/row inside or before the cut area, we invalidate the clipboard
-        if (this.state?.operation !== "CUT") {
+        if (this.state?.operation !== "CUT" || cmd.sheetId !== this.state?.sheetId) {
           return;
         }
         for (let el of cmd.elements) {
