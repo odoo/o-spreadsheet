704d6f5fe8a2f0fa08f162e9beab05c92fe3b024
Rémi Rahir (rar) <rar@odoo.com>
2024-05-13 07:30:11 +0000

    [FIX] Composer: F4 handler should not bubble out of the composer
    Since pull request 2126[^1], the shortcut F4 is handled by the grid
    component. Unfortunately, the same shortcut is handled in the composer
    as well and its propabation was not stopped. This means that a user
    wanting to loop the references inside their formula could see some
    unexpected side effects due to the grid replaying some commands.
    
    [^1]: https://github.com/odoo/o-spreadsheet/issues/2126
    
    Task: 3916488
    X-original-commit: be5500d1cdd30691bd2c358d6b3d792f8e8abdd1

--- tests/composer/composer_integration_component.test.ts
+++ tests/composer/composer_integration_component.test.ts
@@ -10,7 +10,9 @@ import { colors, toHex, toZone } from "../../src/helpers";
 import { ContentEditableHelper } from "../__mocks__/content_editable_helper";
 import {
   activateSheet,
+  copy,
   createSheet,
+  paste,
   renameSheet,
   resizeColumns,
   resizeRows,
@@ -32,6 +34,7 @@ import {
 import {
   getActivePosition,
   getActiveSheetFullScrollInfo,
+  getCellContent,
   getCellText,
   getSelectionAnchorCellXc,
 } from "../test_helpers/getters_helpers";
@@ -486,6 +489,19 @@ describe("Grid composer", () => {
     await simulateClick(fixture.querySelectorAll(".o-sheet")[1]);
     expect(model.getters.getActiveSheetId()).toEqual("42");
     expect(document.activeElement).toBe(fixture.querySelector(".o-grid div.o-composer")!);
+  });
+
+  test("pressing F4 loops the references without impacting the 'redo' feature of the grid", async () => {
+    setCellContent(model, "A1", "coucou");
+    setCellContent(model, "A2", "coucou2");
+    copy(model, "A1:A2");
+    paste(model, "A3:A4");
+    selectCell(model, "B1");
+    await startComposition("=C4");
+    model.dispatch("CHANGE_COMPOSER_CURSOR_SELECTION", { start: 1, end: 1 });
+    await nextTick();
+    await keyDown({ key: "F4" });
+    expect(getCellContent(model, "B2")).toBe("");
   });
 
   describe("grid composer basic style", () => {
