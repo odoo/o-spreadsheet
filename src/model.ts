31b25ab85e26e472c66f24f73839b51e4ce62602
Vincent Schippefilt <vsc@odoo.com>
2024-05-22 10:51:04 +0200

    Revert "because of previous commit not adding (for no reason) a history change for basically each command,"
    This reverts commit 62177fa54038f48d6de5d3bed9bda3a26466c2a3.

--- src/model.ts
+++ src/model.ts
@@ -553,7 +553,6 @@ export class Model extends EventBus<any> implements CommandDispatcher {
             console.info(type, time, "ms");
           }
         });
-        console.log(`for command ${type}`, "changes:", changes, commands);
         this.session.save(command, commands, changes);
         this.status = Status.Ready;
         this.trigger("update");
