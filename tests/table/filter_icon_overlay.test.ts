e03aee2872db8482abef414b785ed502bb934d64
Rémi Rahir (rar) <rar@odoo.com>
2024-05-24 15:11:14 +0200

    [FIX] gridOverlay: self-handle grid clicks
    How to reproduce:
    
    - Add a filter on a spreadsheet,
    - MouseDown on the filter icon (in the grid),
    - drag the mouse -> KABOOM
    
    This issue is present since this PR[^1]. Using the behaviour of a parent
    component just for the sake of sparing some code/ behaviour redundancy
    seems risky and the bug introduced with [^1] is an example. This
    revision considers another approach where `FilterIconOverlay` is not
    aware of its parent behaviour. We simply let events bubble and both
    `GridOverlay` and `Grid` take their own responsibilities.
    
    [^1]: https://github.com/odoo/o-spreadsheet/pull/3726
    
    Task: 3868838

--- tests/table/filter_icon_overlay.test.ts
+++ tests/table/filter_icon_overlay.test.ts
@@ -1,8 +1,9 @@
 import { Model } from "../../src";
 import { DEFAULT_CELL_HEIGHT, DEFAULT_CELL_WIDTH } from "../../src/constants";
+import { toZone } from "../../src/helpers";
 import { createTable } from "../test_helpers/commands_helpers";
-import { simulateClick } from "../test_helpers/dom_helper";
-import { mountSpreadsheet } from "../test_helpers/helpers";
+import { edgeScrollDelay, simulateClick, triggerMouseEvent } from "../test_helpers/dom_helper";
+import { mountSpreadsheet, nextTick } from "../test_helpers/helpers";
 
 describe("Filter Icon Overlay component", () => {
   test("Overlapping filters are overwritten by the latest inserted", async () => {
@@ -20,14 +21,29 @@ describe("Filter Icon Overlay component", () => {
     expect(fixture.querySelectorAll(".o-filter-icon").length).toBe(3);
   });
 
-  test("Click on filter icon bubble and select the underlying cell", async () => {
+  test("MouseEvent on filter icon bubbles and selects the underlying cell", async () => {
+    jest.useFakeTimers();
     const model = new Model();
     createTable(model, "B2:B3");
     const sheetId = model.getters.getActiveSheetId();
     const {} = await mountSpreadsheet({ model });
     expect(model.getters.getActivePosition()).toEqual({ sheetId, col: 0, row: 0 });
+    const { width } = model.getters.getSheetViewDimension();
 
     await simulateClick(".o-filter-icon", DEFAULT_CELL_WIDTH, DEFAULT_CELL_HEIGHT);
+
+    triggerMouseEvent(".o-filter-icon", "pointerdown", DEFAULT_CELL_WIDTH, DEFAULT_CELL_HEIGHT);
+    await nextTick();
     expect(model.getters.getActivePosition()).toEqual({ sheetId, col: 1, row: 1 });
+    triggerMouseEvent(
+      ".o-filter-icon",
+      "pointermove",
+      2.5 * DEFAULT_CELL_WIDTH,
+      DEFAULT_CELL_HEIGHT
+    );
+    const advanceTimer = edgeScrollDelay(1.5 * width, DEFAULT_CELL_HEIGHT);
+
+    jest.advanceTimersByTime(advanceTimer);
+    expect(model.getters.getSelectedZone()).toEqual(toZone("B2:C2"));
   });
 });
