a666fd48713c1b115dbc52669ca9abbca86cdfcd
Alexis Lacroix <laa@odoo.com>
2024-05-24 13:34:46 +0200

    [FIX] functions: throw errors with formula that return range
    Formulas typed as "returning only ranges" can actually
    return simple values instead of a matrix. This phenomenon
    occurs precisely when an error is thrown: it is always
    returned as a simple value.
    
    Normally, this type of error is handled during
    compilation. But with what is said above this is not
    the case...
    
    Two ways to solve the problem have been proposed here:
    
    The first solution could have been to encapsulate
    the throing error in an matrix when it is a function
    returning a range. By doing this, we ensure that the
    function typing is correct and we keep the handling
    of this type of error during compilation.
    
    The second solution would be to say that we cannot
    determine in advance what the result of a sub-function
    will be (simple error vs array). Thus, the argument
    check would be done at runtime.
    
    The first solution is good, but its implementation might
    cause comprehension issues for the user. Indeed, it
    seems strange that for functions rejecting only ranges
    as input, depending on the return type of the sub-formula
    (simple vs range), the formula may crash or not for
    a same error.
    
    Let's take the following example to illustrate this
    problem: Suppose the case of 'SUMIF' which only accepts
    ranges as parameters. Consider the functions 'ABS', which
    naturally returns a simple value, and 'TRANSPOSE', which
    naturally returns a range.
    
    - SUMIF(ABS(), 42)
    - SUMIF(TRANSPOSE(), 42)
    
    In these two examples, no arguments were given to 'ABS'
    and 'TRANSPOSE'. Both sub-functions will respectively
    return an error. With the first solution, only the first
    case will throw, whereas in the second case, both will
    throw.
    
    We therefore choose to go with the second solution,
    despite its implications, to ensure better consistency
    and understanding for the user.
    
    closes odoo/o-spreadsheet#4186
    
    Task: 3918882
    Signed-off-by: Rémi Rahir (rar) <rar@odoo.com>

--- src/types/functions.ts
+++ src/types/functions.ts
@@ -19,6 +19,7 @@ export type ArgType =
 
 export interface ArgDefinition {
   acceptMatrix?: boolean;
+  acceptMatrixOnly?: boolean;
   repeating?: boolean;
   optional?: boolean;
   description: string;
