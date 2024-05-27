03da6b422e6bb86d62238d6292ab4f960ae12a97
dhrp-odoo <dhrp@odoo.com>
2024-05-27 12:25:43 +0000

    [FIX] FigureComponent: Stop Ctrl+A propagation
    Previously, when a figure was selected, And pressing CTRL + A would
    select all rows and columns in the sheet, and pressing the Delete key
    would delete the selected figure. This occurred because all key events
    were bubbling up to the grid component.
    
    This PR stops the propagation for the Ctrl+A key event in the figure
    component.
    
    closes odoo/o-spreadsheet#4283
    
    Task: 3863300
    Signed-off-by: Rémi Rahir (rar) <rar@odoo.com>

--- src/components/helpers/dom_helpers.ts
+++ src/components/helpers/dom_helpers.ts
@@ -2,6 +2,8 @@ import { Rect } from "./../../types/rendering";
 
 const macRegex = /Mac/i;
 
+const MODIFIER_KEYS = ["Shift", "Control", "Alt", "Meta"];
+
 /**
  * Return true if the event was triggered from
  * a child element.
@@ -52,6 +54,33 @@ export function setElementScrollTop(el: HTMLElement | null, scroll: number) {
 
 export function getOpenedMenus(): HTMLElement[] {
   return Array.from(document.querySelectorAll<HTMLElement>(".o-spreadsheet .o-menu"));
+}
+
+const letterRegex = /^[a-zA-Z]$/;
+
+/**
+ * Transform a keyboard event into a shortcut string that represent this event. The letters keys will be uppercased.
+ *
+ * @argument ev - The keyboard event to transform
+ * @argument mode - Use either ev.key of ev.code to get the string shortcut
+ *
+ * @example
+ * event : { ctrlKey: true, key: "a" } => "Ctrl+A"
+ * event : { shift: true, alt: true, key: "Home" } => "Alt+Shift+Home"
+ */
+export function keyboardEventToShortcutString(
+  ev: KeyboardEvent,
+  mode: "key" | "code" = "key"
+): string {
+  let keyDownString = "";
+  if (!MODIFIER_KEYS.includes(ev.key)) {
+    if (isCtrlKey(ev)) keyDownString += "Ctrl+";
+    if (ev.altKey) keyDownString += "Alt+";
+    if (ev.shiftKey) keyDownString += "Shift+";
+  }
+  const key = mode === "key" ? ev.key : ev.code;
+  keyDownString += letterRegex.test(key) ? key.toUpperCase() : key;
+  return keyDownString;
 }
 
 export function isMacOS(): boolean {
