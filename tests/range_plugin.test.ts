c3469638e0b2e19fb454354b31497b18452b64c8
Adrien Minne (adrm) <adrm@odoo.com>
2024-05-27 08:40:33 +0000

    [FIX] ranges: getRangeFromSheetXC with invalid sheetId
    When `getRangeFromSheetXC` is called, it either crashes or returns an
    corrupted range. The getter should handle this case, and return an
    invalid range.
    
    Task: 3578461
    X-original-commit: d6bc2b79c064ddc2f3e6ba0502bb74ad6eade513

--- tests/range_plugin.test.ts
+++ tests/range_plugin.test.ts
@@ -574,6 +574,28 @@ describe("range plugin", () => {
     deleteSheet(m, "s1");
     expect(m.getters.getRangeString(range)).toBe(CellErrorType.InvalidReference);
   });
+
+  test("getRangeFromSheetXC on invalid sheet", () => {
+    let getRange = () => m.getters.getRangeFromSheetXC("NotASheet", "A1");
+    expect(getRange).not.toThrow();
+    expect(getRange().invalidXc).toBe("A1");
+
+    getRange = () => m.getters.getRangeFromSheetXC("NotASheet", "A:A");
+    expect(getRange).not.toThrow();
+    expect(getRange().invalidXc).toBe("A:A");
+
+    getRange = () => m.getters.getRangeFromSheetXC("NotASheet", "Sheet1!A1");
+    expect(getRange).not.toThrow();
+    expect(getRange().invalidXc).toBe("Sheet1!A1");
+
+    getRange = () => m.getters.getRangeFromSheetXC("", "Sheet1!A:A");
+    expect(getRange).not.toThrow();
+    expect(getRange().invalidXc).toBe("Sheet1!A:A");
+
+    getRange = () => m.getters.getRangeFromSheetXC("", "A1:A2");
+    expect(getRange).not.toThrow();
+    expect(getRange().invalidXc).toBe("A1:A2");
+  });
 });
 
 describe("Helpers", () => {
