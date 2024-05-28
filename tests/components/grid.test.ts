c26e39bc518baaeb7885630c48ef5f70b0cd4345
Rémi Rahir (rar) <rar@odoo.com>
2024-05-28 16:08:47 +0000

    [FIX] composer: Prevent default paste in composer
    Steps to reproduce:
    - click on the column A
    - copy/paste the column
    -> the layout is broken, some divs were shifted up and are hidden by the
    topbar. Namely, The column header is not visible anymore.
    
    This issue is related to the one addressed in this pr[^1]. While we
    would prevent the handling of `insertFromPaste` inputs, the default
    browser behaviour would still occur and modify the
    `contentEditableHelper`. This would trigger a replacement of some
    components DOM elements, notably the header overlay. Since, from a
    position POV, the canvas is positioned just below the said overlay, it
    was also shifted.
    
    This revision is taking a more aggressive approach than the one in [^1],
    we stop the paste event totally (not just the following input) in the
    composer when we are not editing.
    
    [^1]: https://github.com/odoo/o-spreadsheet/pull/2254
    
    Task: 3864000
    X-original-commit: efd4115afdc7e2cd7d32499f0c4fcb0ea5c9d72f

--- tests/components/grid.test.ts
+++ tests/components/grid.test.ts
@@ -993,20 +993,6 @@ describe("Grid component", () => {
     await nextTick();
     expect(fixture.querySelector(".o-menu")).toBeTruthy();
   });
-
-  test("input event triggered from a paste should not open composer", async () => {
-    const input = fixture.querySelector(".o-grid div.o-composer");
-    input?.dispatchEvent(
-      new InputEvent("input", {
-        data: "d",
-        bubbles: true,
-        isComposing: false,
-        inputType: "insertFromPaste",
-      })
-    );
-    await nextTick();
-    expect(model.getters.getEditionMode()).toBe("inactive");
-  });
 });
 
 describe("Multi User selection", () => {
