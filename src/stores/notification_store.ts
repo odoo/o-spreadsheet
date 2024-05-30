705744ee59a03cf4d49033f299a190dfcee98434
Rémi Rahir (rar) <rar@odoo.com>
2024-05-30 14:24:10 +0200

    [IMP] Notificiation: set a default notification store
    Following the introduction of stores, the minimal setup has evolved as
    it requires to instanciate a some stores before the creation of a
    spreadsheet app. The documentation was not updated accordingly.
    
    More specifically, the only store that we required to instantiate
    manually was the `NotificationStore` and we had to add it to the App env
    as well. These last step are a pain for developpers that only want to
    set a quick and minimal integration.
    
    This commit sets default notification methods (raiseError, notifyUser,
    askConfirmation) which can either be passed as props to a `Spreadsheet`
    component and/or be set by invoking the `NotificationStore`.
    
    closes odoo/o-spreadsheet#4188
    
    Task: 3919166
    Signed-off-by: Lucas Lefèvre (lul) <lul@odoo.com>

--- src/stores/notification_store.ts
+++ src/stores/notification_store.ts
@@ -1,10 +1,34 @@
-import { createAbstractStore } from "../store_engine";
 import { InformationNotification } from "../types";
 
-export interface NotificationStore {
-  mutators: readonly ["notifyUser", "raiseError", "askConfirmation"];
-  notifyUser: (notification: InformationNotification) => any;
-  raiseError: (text: string, callback?: () => void) => any;
-  askConfirmation: (content: string, confirm: () => any, cancel?: () => any) => any;
+export interface NotificationStoreMethods {
+  notifyUser: (notification: InformationNotification) => void;
+  raiseError: (text: string, callback?: () => void) => void;
+  askConfirmation: (content: string, confirm: () => void, cancel?: () => void) => void;
 }
-export const NotificationStore = createAbstractStore<NotificationStore>("Notifications");
+
+export class NotificationStore {
+  mutators = [
+    "notifyUser",
+    "raiseError",
+    "askConfirmation",
+    "updateNotificationCallbacks",
+  ] as const;
+  notifyUser: NotificationStoreMethods["notifyUser"] = (notification) => window.alert(notification);
+  askConfirmation: NotificationStoreMethods["askConfirmation"] = (content, confirm, cancel) => {
+    if (window.confirm(content)) {
+      confirm();
+    } else {
+      cancel?.();
+    }
+  };
+  raiseError: NotificationStoreMethods["raiseError"] = (text, callback) => {
+    window.alert(text);
+    callback?.();
+  };
+
+  updateNotificationCallbacks(methods: Partial<NotificationStoreMethods>) {
+    this.notifyUser = methods.notifyUser || this.notifyUser;
+    this.raiseError = methods.raiseError || this.raiseError;
+    this.askConfirmation = methods.askConfirmation || this.askConfirmation;
+  }
+}
