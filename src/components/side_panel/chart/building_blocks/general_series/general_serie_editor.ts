85aee6c617530cb933233347dd2d22b87a39a263
Anthony Hendrickx <anhe@odoo.com>
2024-06-13 11:32:15 +0200

    [IMP] charts: add radar chart

---
+++ src/components/side_panel/chart/building_blocks/general_series/general_serie_editor.ts
@@ -0,0 +1,102 @@
+import { Component, useState } from "@odoo/owl";
+import { getNthColor, toHex } from "../../../../../helpers";
+import {
+  ChartWithAxisDefinition,
+  DispatchResult,
+  SpreadsheetChildEnv,
+  UID,
+} from "../../../../../types";
+import { SidePanelCollapsible } from "../../../components/collapsible/side_panel_collapsible";
+import { RoundColorPicker } from "../../../components/round_color_picker/round_color_picker";
+import { Section } from "../../../components/section/section";
+
+interface Props {
+  figureId: UID;
+  definition: ChartWithAxisDefinition;
+  updateChart: (figureId: UID, definition: Partial<ChartWithAxisDefinition>) => DispatchResult;
+  canChangeVerticalAxis?: boolean;
+}
+
+export class GeneralSeriesEditor extends Component<Props, SpreadsheetChildEnv> {
+  static template = "o-spreadsheet-GeneralSeriesEditor";
+  static components = {
+    SidePanelCollapsible,
+    Section,
+    RoundColorPicker,
+  };
+  static props = {
+    figureId: String,
+    definition: Object,
+    updateChart: Function,
+    canChangeVerticalAxis: { type: Boolean, optional: true },
+    slots: { type: Object, optional: true },
+  };
+
+  private state = useState({ index: 0 });
+
+  getDataSeries() {
+    const runtime = this.env.model.getters.getChartRuntime(this.props.figureId);
+    if (!runtime || !("chartJsConfig" in runtime)) {
+      return [];
+    }
+    return runtime.chartJsConfig.data.datasets.map((d) => d.label);
+  }
+
+  updateSerieEditor(ev) {
+    this.state.index = ev.target.selectedIndex;
+  }
+
+  updateDataSeriesColor(color: string) {
+    const dataSets = this.props.definition.dataSets;
+    if (!dataSets?.[this.state.index]) return;
+    dataSets[this.state.index] = {
+      ...dataSets[this.state.index],
+      backgroundColor: color,
+    };
+    this.props.updateChart(this.props.figureId, { dataSets });
+  }
+
+  getDataSerieColor() {
+    const dataSets = this.props.definition.dataSets;
+    if (!dataSets?.[this.state.index]) return "";
+    const color = dataSets[this.state.index].backgroundColor;
+    return color ? toHex(color) : getNthColor(this.state.index);
+  }
+
+  updateDataSeriesAxis(ev) {
+    const axis = ev.target.value;
+    const dataSets = this.props.definition.dataSets;
+    if (!dataSets?.[this.state.index]) return;
+    dataSets[this.state.index] = {
+      ...dataSets[this.state.index],
+      yAxisId: axis === "left" ? "y" : "y1",
+    };
+    this.props.updateChart(this.props.figureId, { dataSets });
+  }
+
+  getDataSerieAxis() {
+    const dataSets = this.props.definition.dataSets;
+    if (!dataSets?.[this.state.index]) return "left";
+    return dataSets[this.state.index].yAxisId === "y1" ? "right" : "left";
+  }
+
+  updateDataSeriesLabel(ev) {
+    const label = ev.target.value;
+    const dataSets = this.props.definition.dataSets;
+    if (!dataSets?.[this.state.index]) return;
+    dataSets[this.state.index] = {
+      ...dataSets[this.state.index],
+      label,
+    };
+    this.props.updateChart(this.props.figureId, { dataSets });
+  }
+
+  getDataSerieLabel() {
+    const dataSets = this.props.definition.dataSets;
+    return dataSets[this.state.index]?.label || this.getDataSeries()[this.state.index];
+  }
+
+  get canHaveTwoVerticalAxis() {
+    return "horizontal" in this.props.definition ? !this.props.definition.horizontal : true;
+  }
+}
