f4cbfde8e21ef862c86d05acbf85e3f5746dbfbd
Rémi Rahir (rar) <rar@odoo.com>
2024-06-26 16:34:52 +0200

    [FIX] BottomBarSheet: Prevent drag of sheet while editing its name
    While editing the name of a spreadsheet in the bottom bar, it was
    impossible to simply select a part of the text because by clicking down,
    you would trigger the drag-and-drop of the sheet position.
    
    Task: 4010842
    X-original-commit: 9ba34eb20c68464195db2eb8729de04606a950b6

--- src/components/bottom_bar_sheet/bottom_bar_sheet.ts
+++ src/components/bottom_bar_sheet/bottom_bar_sheet.ts
@@ -153,7 +153,7 @@ export class BottomBarSheet extends Component<Props, SpreadsheetChildEnv> {
     }
   }
 
-  onClickSheetName(ev: MouseEvent) {
+  onMouseEventSheetName(ev: MouseEvent) {
     if (this.state.isEditing) ev.stopPropagation();
   }
 
