1b181bac60690af5bb6ec541eb2a84af521af95e
Adrien Minne (adrm) <adrm@odoo.com>
2024-07-02 13:29:42 +0000

    [FIX] chart: cumulative not working for linear charts
    The `cumulative` chart option was not working for line charts with
    a linear/time axis.
    
    This was because the data in the linear charts
    runtime was an array of objects `{x, y}` rather than an array of
    numbers, and this wasn't properly handled in the cumulative
    computations.
    
    closes odoo/o-spreadsheet#4585
    
    Task: 4028957
    X-original-commit: 7e988e2b5c678871fa4566a61e1de4f513513d48
    Signed-off-by: Pierre Rousseau (pro) <pro@odoo.com>
    Signed-off-by: Adrien Minne (adrm) <adrm@odoo.com>

--- src/helpers/figures/charts/chart_common_line_scatter.ts
+++ src/helpers/figures/charts/chart_common_line_scatter.ts
@@ -260,10 +260,6 @@ export function createLineOrScatterChartRuntime(
   const colors = new ColorGenerator();
   const definition = chart.getDefinition();
   for (let [index, { label, data }] of dataSetsValues.entries()) {
-    if (["linear", "time"].includes(axisType)) {
-      // Replace empty string labels by undefined to make sure chartJS doesn't decide that "" is the same as 0
-      data = data.map((y, index) => ({ x: labels[index] || undefined, y }));
-    }
     const color = colors.next();
     let backgroundRGBA = colorToRGBA(color);
     if (areaChart) {
@@ -278,6 +274,10 @@ export function createLineOrScatterChartRuntime(
         }
         return value;
       });
+    }
+    if (["linear", "time"].includes(axisType)) {
+      // Replace empty string labels by undefined to make sure chartJS doesn't decide that "" is the same as 0
+      data = data.map((y, index) => ({ x: labels[index] || undefined, y }));
     }
 
     const backgroundColor = rgbaToHex(backgroundRGBA);
