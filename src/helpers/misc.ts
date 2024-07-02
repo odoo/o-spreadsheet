dffe0d1a7b7d5f5386d97ca1d5a0a71267582538
Adrien Minne (adrm) <adrm@odoo.com>
2024-07-02 14:16:54 +0000

    [REV] chart: avoid useless chart updates
    This reverts commit 6eb43533d.
    
    It turns out that checking the deep equality of runtime while ignoring
    functions is not a good fix to avoid useless chart updates. The problem
    is that some runtime changes apply only to the callbacks (eg. changing
    the dataset format only change the ticks callback).
    
    The only real alternative would be to add the variables that are used
    in the callbacks to the runtime, so the deepEquals would work. But this
    is very error prone: we'll 100% forget to add a variable at some point.
    
    So we will accept the useless updates for now, until we see a real
    performance issue.
    
    Task: 4029016
    X-original-commit: ef4c70d651189f8d6009c60396fd2e848cec1d4b

--- src/helpers/misc.ts
+++ src/helpers/misc.ts
@@ -355,7 +355,7 @@ export function getAddHeaderStartIndex(position: "before" | "after", base: numbe
 /**
  * Compares two objects.
  */
-export function deepEquals(o1: any, o2: any, ignoreFunctions?: "ignoreFunctions"): boolean {
+export function deepEquals(o1: any, o2: any): boolean {
   if (o1 === o2) return true;
   if ((o1 && !o2) || (o2 && !o1)) return false;
   if (typeof o1 !== typeof o2) return false;
@@ -369,14 +369,10 @@ export function deepEquals(o1: any, o2: any, ignoreFunctions?: "ignoreFunctions"
   }
 
   for (const key in o1) {
-    const typeOfO1Key = typeof o1[key];
-    if (typeOfO1Key !== typeof o2[key]) return false;
-    if (typeOfO1Key === "object") {
-      if (!deepEquals(o1[key], o2[key], ignoreFunctions)) return false;
+    if (typeof o1[key] !== typeof o2[key]) return false;
+    if (typeof o1[key] === "object") {
+      if (!deepEquals(o1[key], o2[key])) return false;
     } else {
-      if (ignoreFunctions && typeOfO1Key === "function") {
-        continue;
-      }
       if (o1[key] !== o2[key]) return false;
     }
   }
