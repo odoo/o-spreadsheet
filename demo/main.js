c2bd0e08fb42edf7ad8683d3f18212a2cf603002
Alexis Lacroix <laa@odoo.com>
2024-07-02 11:22:15 +0200

    [IMP] demo: add large demo data set for array formulas
    Task: 4036899
    Part-of: odoo/o-spreadsheet#4599
    Signed-off-by: Vincent Schippefilt (vsc) <vsc@odoo.com>
    Signed-off-by: Lucas Lefèvre (lul) <lul@odoo.com>

--- demo/main.js
+++ demo/main.js
@@ -240,6 +240,8 @@ class Demo extends Component {
     }
     this.createModel(data || demoData);
     // this.createModel(makeLargeDataset(26, 10_000, ["numbers"]));
+    // this.createModel(makeLargeDataset(26, 10_000, ["formulas"]));
+    // this.createModel(makeLargeDataset(26, 10_000, ["arrayFormulas"]));
     // this.createModel({});
   }
 
