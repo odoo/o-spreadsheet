ad7d564b0b794080988c8c2936a591ebe0f431cb
Rémi Rahir (rar) <rar@odoo.com>
2024-07-05 14:55:03 +0200

    [IMP] tests: drop ts-jest in favor of swc/jest
    SWC (speedy web compiler) is an rust-based faster alternative to the
    traditional typescript transpiler `tsc`. SInce it does not support
    typechecking, we cannot only rely on it in order to generate our
    declaration files our build our library. We can however leverage the
    already existing integration with jest, expecially the package
    `@swc/jest` that works as an alternative to  `js-test`.
    
    In order to preserve the typechecking of the test files, we alter the
    `test` command to first run a simple typecheck with `tsc` and then run
    the test suite with `@swc/jest` as a TS transpiler.
    
    Some pros:
    - Better execution time for the whole test suite
    - running a standalone test way faster
    - a pre-step of typecheck takes less time than currently beacause jest
      will keep running every test file and will not stop its execution
      because of a single file in error
    
    Benchmark
    ---------
    
    Running command `npm run test -- -w 50%` -  11178 tests:
    
    Before:  131 secondes
    After:   82 secondes (17 of typecheck + 68 of actual tests transpiled
    and run
    
    On Runbot
    
    closes odoo/o-spreadsheet#4649
    
    Before: 250 secondes
    After:  122 secondes
    Task: 4049142
    Signed-off-by: Lucas Lefèvre (lul) <lul@odoo.com>

--- tests/__mocks__/mock_misc_helpers.ts
+++ tests/__mocks__/mock_misc_helpers.ts
@@ -3,7 +3,7 @@ import { DebouncedFunction } from "../../src/types";
 /** Mocked debounce that doesn't actually do any debouncing, but just calls the function directly */
 export function debounce<T extends (...args: any) => void>(func: T): DebouncedFunction<T> {
   const debounced = function (this: any): void {
-    func.apply(this, arguments);
+    func.apply(this, Array.from(arguments));
   };
   debounced.isDebouncePending = () => false;
   debounced.stopDebounce = () => {};
