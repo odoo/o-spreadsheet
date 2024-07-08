2abe37090e86f76befdd226c8bce5774a21d9a46
Vincent Schippefilt <vsc@odoo.com>
2024-07-08 15:16:51 +0200

    [IMP] uuid: reduce size of unique identifyer
    Before this commit, we used a UUID-V4 unique identifyer.
    It is a string like xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx (36 chars).
    Those ids are used all over the spreadsheet internal data structure and also when sending
    commands over the network.
    
    Knowing that we need some kind of unique identifier to differenciate similar commands, done at
    the same time (relatively to network speed), on the same spreadsheet, we don't need that level of entropy.
    
    With this commit, I reduce the size of the identifiers to xxxxxxxxxxx (10 chars), which
    is more than enough for our needs.

--- src/helpers/uuid.ts
+++ src/helpers/uuid.ts
@@ -17,13 +17,12 @@ export class UuidGenerator {
       return String(this.fastIdStart);
       //@ts-ignore
     } else if (window.crypto && window.crypto.getRandomValues) {
-      //@ts-ignore
-      return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
+      return "100000000".replace(/[018]/g, (c: any) =>
         (c ^ (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))).toString(16)
       );
     } else {
       // mainly for jest and other browsers that do not have the crypto functionality
-      return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+      return "xxxxxxxxxxx".replace(/[xy]/g, function (c) {
         var r = (Math.random() * 16) | 0,
           v = c === "x" ? r : (r & 0x3) | 0x8;
         return v.toString(16);
