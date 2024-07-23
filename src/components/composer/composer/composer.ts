af96c11cfda2d7f8a14c789127d0cef16c88eb6e
dhrp-odoo <dhrp@odoo.com>
2024-07-23 10:30:14 +0000

    [FIX] composer: layout break when composer assistant is opened
    When typing =S in the composer, the composer assistant will open.
    If there wasn't enough space on right (default 300px width), this would
    break the layout. This PR addresses the issue by adding a width check
    for the composer assistant. If the width condition is met, the style
    
    closes odoo/o-spreadsheet#4719
    
    Right: 0px is applied to prevent the layout from breaking.
    Task: 4037213
    X-original-commit: 52d2cd36012c581b4f3d0ca325286fb4eb61d67c
    Signed-off-by: Dhrutik Patel (dhrp) <dhrp@odoo.com>
    Signed-off-by: Adrien Minne (adrm) <adrm@odoo.com>

--- src/components/composer/composer/composer.ts
+++ src/components/composer/composer/composer.ts
@@ -18,6 +18,7 @@ import {
 } from "../../../types/index";
 import { css, cssPropertiesToCss } from "../../helpers/css";
 import { keyboardEventToShortcutString } from "../../helpers/dom_helpers";
+import { useSpreadsheetRect } from "../../helpers/position_hook";
 import { updateSelectionWithArrowKeys } from "../../helpers/selection_helpers";
 import { TextValueProvider } from "../autocomplete_dropdown/autocomplete_dropdown";
 import { AutoCompleteStore } from "../autocomplete_dropdown/autocomplete_dropdown_store";
@@ -163,8 +164,10 @@ export class Composer extends Component<ComposerProps, SpreadsheetChildEnv> {
     argToFocus: 0,
   });
   private compositionActive: boolean = false;
+  private spreadsheetRect = useSpreadsheetRect();
 
   get assistantStyle(): string {
+    const composerRect = this.composerRef.el!.getBoundingClientRect();
     const assistantStyle: CSSProperties = {};
 
     assistantStyle["min-width"] = `${this.props.rect?.width || ASSISTANT_WIDTH}px`;
@@ -192,6 +195,9 @@ export class Composer extends Component<ComposerProps, SpreadsheetChildEnv> {
       }
     } else if (this.props.delimitation) {
       assistantStyle["max-height"] = `${this.props.delimitation.height}px`;
+      if (composerRect.left + ASSISTANT_WIDTH > this.spreadsheetRect.width) {
+        assistantStyle.right = `0px`;
+      }
     }
     return cssPropertiesToCss(assistantStyle);
   }
