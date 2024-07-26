odoo/o-spreadsheet

commit bde73c8f277a96c420fbdf33d8b119d6f2742da0

[FIX] functions: COUNTIF does not handle dates
Dates predicates (eg. "<01/01/2024") were not handled in the COUNTIF
function and its variants.

Related ticket: 4042962

closes odoo/o-spreadsheet#4642

Task: 4045506
Signed-off-by: Lucas Lefèvre (lul) <lul@odoo.com>

src/functions/helpers.ts
@@ -1,5 +1,5 @@
 // HELPERS
-import { memoize } from "../helpers";
+import { isDateTime, memoize } from "../helpers";
 import { DateTime, numberToJsDate, parseDateTime } from "../helpers/dates";
 import { isNumber, parseNumber } from "../helpers/numbers";
 import { _lt } from "../translation";
@@ -335,7 +335,7 @@ function getPredicate(descr: string, isQuery: boolean): Predicate {
     }
   }
 
-  if (isNumber(operand)) {
+  if (isNumber(operand) || isDateTime(operand)) {
     operand = toNumber(operand);
   } else if (operand === "TRUE" || operand === "FALSE") {
     operand = toBoolean(operand);

tests/functions/module_math.test.ts
@@ -815,6 +815,21 @@ describe("COUNTIF formula", () => {
     expect(gridResult.A2).toBe(0);
     expect(gridResult.A3).toBe(0);
   });
+
+  test("COUNTIF with date predicate", () => {
+    const grid = {
+      A1: "01/01/2024",
+      A2: "01/02/2024",
+      B1: '=COUNTIF(A1, "<01/02/2024")',
+      B2: '=COUNTIF(A2, "<01/02/2024")',
+      B3: '=COUNTIF(A2, "<=01/02/2024")',
+    };
+    expect(evaluateGrid(grid)).toMatchObject({
+      B1: 1,
+      B2: 0,
+      B3: 1,
+    });
+  });
 });
 
 describe("COUNTIFS formula", () => {

commit 318be4ffb54117e71f3d131d6c44e02594b7e79c

[FIX] functions: COUNTIF count string dates
Steps to reproduce:
in A1: ="01/01/2024"
in B1: =COUNTIF(A1, "01/01/2024")

The result of B1 is 0, but it should be 1

Bug introduced with bde73c8f277a96c420fbdf33d8b119d6f2742da0

Task: 4077445

src/functions/helpers.ts
@@ -385,9 +385,11 @@ function evaluatePredicate(value: CellValue | undefined = "", criterion: Predica
   if (operand === undefined) {
     return false;
   }
-
   if (typeof operand === "number" && operator === "=") {
-    return toString(value) === toString(operand);
+    if (typeof value === "string" && (isNumber(value) || isDateTime(value))) {
+      return toNumber(value) === operand;
+    }
+    return value === operand;
   }
 
   if (operator === "<>" || operator === "=") {

tests/functions/module_math.test.ts
@@ -820,14 +820,43 @@ describe("COUNTIF formula", () => {
     const grid = {
       A1: "01/01/2024",
       A2: "01/02/2024",
+      A3: '="01/01/2024"',
       B1: '=COUNTIF(A1, "<01/02/2024")',
       B2: '=COUNTIF(A2, "<01/02/2024")',
       B3: '=COUNTIF(A2, "<=01/02/2024")',
+      B4: '=COUNTIF(A3, "01/01/2024")',
+      B5: '=COUNTIF(A3, "<=01/01/2024")',
+      B6: '=COUNTIF(A3, "01/2024")',
     };
     expect(evaluateGrid(grid)).toMatchObject({
       B1: 1,
       B2: 0,
       B3: 1,
+      B4: 1,
+      B5: 0,
+      B6: 0, // @compatibility: on google sheets, return 1
+    });
+  });
+
+  test("COUNTIF with string against a date predicate", () => {
+    const grid = {
+      A1: "hello",
+      B1: '=COUNTIF(A1, "01/02/2024")',
+    };
+    expect(evaluateGrid(grid)).toMatchObject({
+      B1: 0,
+    });
+  });
+
+  test("COUNTIF with number against a date predicate", () => {
+    const grid = {
+      A1: "0",
+      B1: '=COUNTIF(A1, "12/30/1899")',
+      B2: '=COUNTIF(A1, "<=12/30/1899")',
+    };
+    expect(evaluateGrid(grid)).toMatchObject({
+      B1: 1,
+      B2: 1,
     });
   });
 });