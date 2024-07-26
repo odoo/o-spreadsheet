109470c372db5fc71199ca61df5b850c2ce034b0
Lucas Lefèvre (lul) <lul@odoo.com>
2024-07-26 12:11:22 +0000

    [FIX] functions: COUNTIF count string dates
    Steps to reproduce:
    in A1: ="01/01/2024"
    in B1: =COUNTIF(A1, "01/01/2024")
    
    The result of B1 is 0, but it should be 1
    
    Bug introduced with bde73c8f277a96c420fbdf33d8b119d6f2742da0
    
    Task: 4077445
    X-original-commit: 40813a19a5e89d62542beee7ec65056706a6936e

--- src/functions/helpers.ts
+++ src/functions/helpers.ts
@@ -554,15 +554,21 @@ const wildcardToRegExp = memoize(function wildcardToRegExp(operand: string): Reg
   return new RegExp("^" + exp + "$", "i");
 });
 
-function evaluatePredicate(value: CellValue | undefined = "", criterion: Predicate): boolean {
+function evaluatePredicate(
+  value: CellValue | undefined = "",
+  criterion: Predicate,
+  locale: Locale
+): boolean {
   const { operator, operand } = criterion;
 
   if (operand === undefined || value === null || operand === null) {
     return false;
   }
-
   if (typeof operand === "number" && operator === "=") {
-    return value.toString() === operand.toString();
+    if (typeof value === "string" && (isNumber(value, locale) || isDateTime(value, locale))) {
+      return toNumber(value, locale) === operand;
+    }
+    return value === operand;
   }
 
   if (operator === "<>" || operator === "=") {
@@ -661,7 +667,7 @@ export function visitMatchingRanges(
       for (let k = 0; k < countArg - 1; k += 2) {
         const criteriaValue = toMatrix(args[k])[i][j].value;
         const criterion = predicates[k / 2];
-        validatedPredicates = evaluatePredicate(criteriaValue ?? undefined, criterion);
+        validatedPredicates = evaluatePredicate(criteriaValue ?? undefined, criterion, locale);
         if (!validatedPredicates) {
           break;
         }
