0e3337f464eaf4adba0968d774a06af03346c080
dhrp-odoo <dhrp@odoo.com>
2024-07-31 11:01:13 +0530

    [IMP] EditableName: various enhancements
    This commit makes several enhancements to the `editable_name` component:
    - The input field now automatically selects all text when focused.
    - Users can now press Enter to save changes and Escape to cancel them.
    - A "Cancel" button has been added for users to discard changes.
    
    Task: 4080323

--- src/components/side_panel/pivot/editable_name/editable_name.ts
+++ src/components/side_panel/pivot/editable_name/editable_name.ts
@@ -1,12 +1,17 @@
 /** @odoo-module */
 
-import { Component, useState } from "@odoo/owl";
+import { Component, onPatched, useRef, useState } from "@odoo/owl";
 import { SpreadsheetChildEnv } from "../../../../types";
 
 interface Props {
   name: string;
   displayName: string;
   onChanged: (name: string) => void;
+}
+
+interface State {
+  isEditing: boolean;
+  name: string;
 }
 
 export class EditableName extends Component<Props, SpreadsheetChildEnv> {
@@ -16,22 +21,55 @@ export class EditableName extends Component<Props, SpreadsheetChildEnv> {
     displayName: String,
     onChanged: Function,
   };
-  private state!: { isEditing: boolean; name: string };
+
+  private state: State = useState({ isEditing: false, name: "" });
+  private inputRef = useRef<HTMLInputElement>("inputRef");
+  private editionState: "initializing" | "editing" = "initializing";
 
   setup() {
-    this.state = useState({
-      isEditing: false,
-      name: "",
+    onPatched(() => {
+      if (this.state.isEditing && this.editionState === "initializing") {
+        this.editionState = "editing";
+        this.focusInputAndSelectContent();
+      }
     });
   }
 
   rename() {
     this.state.isEditing = true;
     this.state.name = this.props.name;
+    this.editionState = "initializing";
   }
 
   save() {
     this.props.onChanged(this.state.name.trim());
     this.state.isEditing = false;
+    this.editionState = "initializing";
+  }
+
+  cancel() {
+    this.state.isEditing = false;
+    this.editionState = "initializing";
+  }
+
+  handleKeyDown(ev: KeyboardEvent) {
+    if (!this.state.isEditing) return;
+
+    switch (ev.key) {
+      case "Enter":
+        this.save();
+        break;
+      case "Escape":
+        this.cancel();
+        break;
+    }
+  }
+
+  private focusInputAndSelectContent() {
+    if (this.state.isEditing && this.inputRef.el) {
+      const input = this.inputRef.el;
+      input.focus();
+      input.select();
+    }
   }
 }
