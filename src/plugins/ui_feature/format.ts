odoo/o-spreadsheet

commit 1b667256cc386e4b8e8ff2e216e66bd9b9a03d62

[IMP] format: set decimal change on each cell of zone
Earlier,only the first cell of a zone was inspected for its format, and
subsequently, the decimal places of all other cells in the zone were
updated based on the format of the first cell.

Now improvised feature will check the format of each individual cell within
each zone and adjust them accordingly.

Odoo task ID : 2900469

closes odoo/o-spreadsheet#2018

Signed-off-by: Rémi Rahir (rar) <rar@odoo.com>

src/plugins/ui_feature/format.ts
@@ -1,6 +1,14 @@
 import { DATETIME_FORMAT } from "../../constants";
-import { changeDecimalPlaces, createDefaultFormat } from "../../helpers";
-import { CellValueType, Command, Format, SetDecimalStep, UID, Zone } from "../../types/index";
+import { changeDecimalPlaces, createDefaultFormat, positions, positionToZone } from "../../helpers";
+import {
+  CellPosition,
+  CellValueType,
+  Command,
+  Format,
+  SetDecimalStep,
+  UID,
+  Zone,
+} from "../../types/index";
 import { UIPlugin } from "../ui_plugin";
 
 export class FormatPlugin extends UIPlugin {
@@ -24,22 +32,26 @@ export class FormatPlugin extends UIPlugin {
    * The change of the decimal quantity is done one by one, the sign of the step
    * variable indicates whether we are increasing or decreasing.
    *
-   * If several cells are in the zone, the format resulting from the change of the
-   * first cell (with number type) will be applied to the whole zone.
+   * If several cells are in the zone, each cell's format will be individually
+   * evaluated and updated with the number type.
    */
   private setDecimal(sheetId: UID, zones: Zone[], step: SetDecimalStep) {
-    // Find the first cell with a number value and get the format
-    const numberFormat = this.searchNumberFormat(sheetId, zones);
-    if (numberFormat !== undefined) {
-      // Depending on the step sign, increase or decrease the decimal representation
-      // of the format
-      const newFormat = changeDecimalPlaces(numberFormat, step);
-      // Apply the new format on the whole zone
-      this.dispatch("SET_FORMATTING", {
-        sheetId,
-        target: zones,
-        format: newFormat,
-      });
+    // Find the each cell with a number value and get the format
+    for (const zone of zones) {
+      for (const position of positions(zone)) {
+        const numberFormat = this.getCellNumberFormat({ sheetId, ...position });
+        if (numberFormat !== undefined) {
+          // Depending on the step sign, increase or decrease the decimal representation
+          // of the format
+          const newFormat = changeDecimalPlaces(numberFormat, step);
+          // Apply the new format on the whole zone
+          this.dispatch("SET_FORMATTING", {
+            sheetId,
+            target: [positionToZone(position)],
+            format: newFormat,
+          });
+        }
+      }
     }
   }
 
@@ -48,18 +60,14 @@ export class FormatPlugin extends UIPlugin {
    * number value. Returns a default format if the cell hasn't format. Returns
    * undefined if no number value in the range.
    */
-  private searchNumberFormat(sheetId: UID, zones: Zone[]): Format | undefined {
-    for (let zone of zones) {
-      for (let row = zone.top; row <= zone.bottom; row++) {
-        for (let col = zone.left; col <= zone.right; col++) {
-          const cell = this.getters.getEvaluatedCell({ sheetId, col, row });
-          if (
-            cell.type === CellValueType.number &&
-            !cell.format?.match(DATETIME_FORMAT) // reject dates
-          ) {
-            return cell.format || createDefaultFormat(cell.value);
-          }
-        }
+  private getCellNumberFormat(position: CellPosition): Format | undefined {
+    for (const pos of [position]) {
+      const cell = this.getters.getEvaluatedCell(pos);
+      if (
+        cell.type === CellValueType.number &&
+        !cell.format?.match(DATETIME_FORMAT) // reject dates
+      ) {
+        return cell.format || createDefaultFormat(cell.value);
       }
     }
     return undefined;

tests/plugins/formatting.test.ts
@@ -32,6 +32,7 @@ import {
   setAnchorCorner,
   setCellContent,
   setFormat,
+  setSelection,
   setStyle,
 } from "../test_helpers/commands_helpers";
 import { getCell, getCellContent, getEvaluatedCell } from "../test_helpers/getters_helpers";
@@ -360,51 +361,6 @@ describe("formatting values (when change decimal)", () => {
     expect(getCell(model, "C1")!.format).toBe(twentyZerosC1);
   });
 
-  test("Change decimal format on a range gives the same format for all cells", () => {
-    const model = new Model();
-
-    // give values ​​with different formats
-
-    setCellContent(model, "A2", "9");
-    selectCell(model, "A2");
-    setFormat(model, "0.00%");
-    expect(getCellContent(model, "A2")).toBe("900.00%");
-
-    setCellContent(model, "A3", "4567.8");
-    selectCell(model, "A3");
-    setFormat(model, "#,##0");
-    expect(getCellContent(model, "A3")).toBe("4,568");
-
-    setCellContent(model, "C1", "42.42");
-    selectCell(model, "C1");
-    setFormat(model, "0.000");
-    expect(getCellContent(model, "C1")).toBe("42.420");
-
-    // select A1, then expand selection to A1:C3
-
-    selectCell(model, "A1");
-
-    setAnchorCorner(model, "C3");
-
-    // increase decimalFormat on the selection
-
-    setDecimal(model, 1);
-
-    expect(getCellContent(model, "A2")).toBe("9.0000");
-    expect(getCellContent(model, "A3")).toBe("4567.8000");
-    expect(getCellContent(model, "C1")).toBe("42.4200");
-
-    expect(getCell(model, "A1")!.format).toBe("0.0000");
-    expect(getCell(model, "A2")!.format).toBe("0.0000");
-    expect(getCell(model, "A3")!.format).toBe("0.0000");
-    expect(getCell(model, "B1")!.format).toBe("0.0000");
-    expect(getCell(model, "B2")!.format).toBe("0.0000");
-    expect(getCell(model, "B3")!.format).toBe("0.0000");
-    expect(getCell(model, "C1")!.format).toBe("0.0000");
-    expect(getCell(model, "C2")!.format).toBe("0.0000");
-    expect(getCell(model, "C3")!.format).toBe("0.0000");
-  });
-
   test("Change decimal format on a range does nothing if there isn't 'number' type", () => {
     const model = new Model();
 
@@ -438,6 +394,36 @@ describe("formatting values (when change decimal)", () => {
     expect(getCell(model, "A2")!.format).toBe("0.00%");
     expect(getCell(model, "A3")!.format).toBe("m/d/yyyy");
     expect(getCell(model, "C1")!.format).toBe(undefined);
+  });
+
+  test("Check multiple format in selected zone", async () => {
+    let model = new Model();
+    setCellContent(model, "A1", "100%");
+    setCellContent(model, "B1", "$190.12");
+    setCellContent(model, "C1", "$21");
+    setSelection(model, ["A1:C1"]);
+    setDecimal(model, 1);
+    expect(getCellContent(model, "A1")).toEqual("100.0%");
+    expect(getCellContent(model, "B1")).toEqual("$190.120");
+    expect(getCellContent(model, "C1")).toEqual("$21.0");
+  });
+
+  test("Check multiple format in multiple zone", async () => {
+    let model = new Model();
+    setCellContent(model, "A1", "100%");
+    setCellContent(model, "B1", "$190.12");
+    setCellContent(model, "C1", "$21");
+    setCellContent(model, "A3", "100%");
+    setCellContent(model, "B3", "$190.12");
+    setCellContent(model, "C3", "$21");
+    setSelection(model, ["A1:C1", "A3:C3"]);
+    setDecimal(model, 1);
+    expect(getCellContent(model, "A1")).toEqual("100.0%");
+    expect(getCellContent(model, "B1")).toEqual("$190.120");
+    expect(getCellContent(model, "C1")).toEqual("$21.0");
+    expect(getCellContent(model, "A3")).toEqual("100.0%");
+    expect(getCellContent(model, "B3")).toEqual("$190.120");
+    expect(getCellContent(model, "C3")).toEqual("$21.0");
   });
 });
 

commit 26a379ad4daf66f2dc70be2e374ba4cc7dc77b85

[FIX] format: dispatch only once
Steps to reproduce:
- select a huge zone with numbers
- click on the "Increase decimal places" tool in the top bar

=> there's one dipatched command for each position in the zone, which
can huge.

We want:
- one dispatched command per format
- positions grouped to a single zone if there are contiguous (a single big
  zone instead of many zones, each being actually a single position)

Issue introduced by 1b667256cc386e4b8e8ff2e216e66bd9b9a03d62

Task: 4091502

src/plugins/ui_feature/format.ts
@@ -2,14 +2,16 @@ import {
   changeDecimalPlaces,
   createDefaultFormat,
   isDateTimeFormat,
-  positions,
   positionToZone,
+  positions,
 } from "../../helpers";
+import { futureRecomputeZones } from "../../helpers/recompute_zones";
 import {
   CellPosition,
   CellValueType,
   Command,
   Format,
+  Position,
   SetDecimalStep,
   UID,
   Zone,
@@ -41,6 +43,7 @@ export class FormatPlugin extends UIPlugin {
    * evaluated and updated with the number type.
    */
   private setDecimal(sheetId: UID, zones: Zone[], step: SetDecimalStep) {
+    const positionsByFormat: Record<Format, Position[]> = {};
     // Find the each cell with a number value and get the format
     for (const zone of zones) {
       for (const position of positions(zone)) {
@@ -50,14 +53,21 @@ export class FormatPlugin extends UIPlugin {
           // of the format
           const locale = this.getters.getLocale();
           const newFormat = changeDecimalPlaces(numberFormat, step, locale);
-          // Apply the new format on the whole zone
-          this.dispatch("SET_FORMATTING", {
-            sheetId,
-            target: [positionToZone(position)],
-            format: newFormat,
-          });
+          positionsByFormat[newFormat] = positionsByFormat[newFormat] || [];
+          positionsByFormat[newFormat].push(position);
         }
       }
+    }
+    // consolidate all positions with the same format in bigger zones
+    for (const newFormat in positionsByFormat) {
+      const zones = futureRecomputeZones(
+        positionsByFormat[newFormat].map((position) => positionToZone(position))
+      );
+      this.dispatch("SET_FORMATTING", {
+        sheetId,
+        format: newFormat,
+        target: zones,
+      });
     }
   }
 