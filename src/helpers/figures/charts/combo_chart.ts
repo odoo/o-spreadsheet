777e0b13bd1b860fa92d914624dfc4efcd3bf3cc
Rémi Rahir (rar) <rar@odoo.com>
2024-08-09 10:04:37 +0200

    [FIX] Charts: Remove trendline tooltip title
    The title of the tooltip for the trendlines don't make much sense at the
    moment, we either output the index of the data point or half-handled
    labels.
    
    This revision hides the tooltip title of the trendlines until we
    find an more elegant solution, like syncing the titles with the original
    dataset values, or even interpolating in the case of numerical
    data in the x axis.
    
    Task: 4105007

--- src/helpers/figures/charts/combo_chart.ts
+++ src/helpers/figures/charts/combo_chart.ts
@@ -356,6 +356,15 @@ export function createComboChartRuntime(chart: ComboChart, getters: Getters): Co
      * distinguish the originals and trendLine datasets after
      */
     trendDatasets.forEach((x) => config.data.datasets!.push(x));
+
+    const originalTooltipTitle = config.options.plugins!.tooltip!.callbacks!.title;
+    config.options.plugins!.tooltip!.callbacks!.title = function (tooltipItem) {
+      if (tooltipItem.some((item) => item.dataset.xAxisID !== TREND_LINE_XAXIS_ID)) {
+        // @ts-expect-error
+        return originalTooltipTitle?.(tooltipItem);
+      }
+      return "";
+    };
   }
 
   return { chartJsConfig: config, background: chart.background || BACKGROUND_CHART_COLOR };
