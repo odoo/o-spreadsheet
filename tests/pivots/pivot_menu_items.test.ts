2b80293ae1647fa90769478ff3965a3021558a35
Pierre Rousseau <pro@odoo.com>
2024-08-20 05:56:18 +0000

    [FIX] pivot: insert pivot with id not in quotes
    Now, the formula that is inserted in the pivot is without quotes for the
    id.
    
    Before: `=PIVOT("1")`
    After: `=PIVOT(1)`
    Task: 4066384
    X-original-commit: c54298ae89603aa27afa42f44c642f55b937582a

--- tests/pivots/pivot_menu_items.test.ts
+++ tests/pivots/pivot_menu_items.test.ts
@@ -275,7 +275,7 @@ describe("Pivot reinsertion menu item", () => {
     const env = makeTestEnv({ model });
     selectCell(model, "B8");
     doAction(reinsertPivotPath, env, topbarMenuRegistry);
-    expect(getCellText(model, "B8")).toEqual(`=PIVOT("1")`);
+    expect(getCellText(model, "B8")).toEqual(`=PIVOT(1)`);
     expect(
       model.getters.getCoreTable({
         sheetId: model.getters.getActiveSheetId(),
@@ -303,7 +303,7 @@ describe("Pivot reinsertion menu item", () => {
     createSheet(model, { sheetId: "smallSheet", rows: 1, cols: 1, activate: true });
     const env = makeTestEnv({ model });
     doAction(reinsertPivotPath, env, topbarMenuRegistry);
-    expect(getCellText(model, "A1")).toEqual(`=PIVOT("1")`);
+    expect(getCellText(model, "A1")).toEqual(`=PIVOT(1)`);
     expect(model.getters.getPivot(model.getters.getPivotId("1")!).isValid()).toBeTruthy();
     expect(model.getters.getNumberCols("smallSheet")).toEqual(2);
     expect(model.getters.getNumberRows("smallSheet")).toEqual(4); // title, col group, row header, total
@@ -324,13 +324,13 @@ describe("Pivot reinsertion menu item", () => {
     const env = makeTestEnv({ model });
     selectCell(model, "B8");
     doAction(reinsertPivotPath, env, topbarMenuRegistry);
-    expect(getCellText(model, "B8")).toEqual(`=PIVOT("1")`);
+    expect(getCellText(model, "B8")).toEqual(`=PIVOT(1)`);
     expect(getTable(model, "B8")).toBeDefined();
     undo(model);
     expect(getCell(model, "B8")).toBeUndefined();
     expect(getTable(model, "B8")).toBeUndefined();
     redo(model);
-    expect(getCellText(model, "B8")).toEqual(`=PIVOT("1")`);
+    expect(getCellText(model, "B8")).toEqual(`=PIVOT(1)`);
     expect(getTable(model, "B8")).toBeDefined();
   });
 });
