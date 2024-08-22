86340e63424368b805bbacd7ef36bec07732386b
Adrien Minne (adrm) <adrm@odoo.com>
2024-08-22 08:58:01 +0200

    [IMP] clipboard: copy whole pivot as dynamic pivot
    When we copy part of a dynamic pivot, we don't copy the values but
    pivot formulas instead.
    
    But when copying (or cutting) the whole pivot, we should copy the
    dynamic pivot and not the pivot formulas. The intent of the user in
    this case is most likely to copy the pivot as a whole and not to
    replace it with pivot formulas (particularly when cutting).
    
    Task: 4123272

--- src/clipboard_handlers/cell_clipboard.ts
+++ src/clipboard_handlers/cell_clipboard.ts
@@ -1,5 +1,5 @@
 import { canonicalizeNumberValue } from "../formulas/formula_locale";
-import { deepEquals, formatValue } from "../helpers";
+import { deepEquals, formatValue, isZoneInside } from "../helpers";
 import { getPasteZones } from "../helpers/clipboard/clipboard_helpers";
 import { createPivotFormula } from "../helpers/pivot/pivot_helpers";
 import {
@@ -46,8 +46,13 @@ export class CellClipboardHandler extends AbstractCellClipboardHandler<
         const evaluatedCell = this.getters.getEvaluatedCell(position);
         const pivotId = this.getters.getPivotIdFromPosition(position);
         const spreader = this.getters.getArrayFormulaSpreadingOn(position);
-        if (pivotId) {
-          if (spreader && (!deepEquals(spreader, position) || !isCopyingOneCell)) {
+        if (pivotId && spreader) {
+          const pivotZone = this.getters.getSpreadZone(spreader);
+          if (
+            (!deepEquals(spreader, position) || !isCopyingOneCell) &&
+            pivotZone &&
+            !data.zones.some((z) => isZoneInside(pivotZone, z))
+          ) {
             const pivotCell = this.getters.getPivotCellFromPosition(position);
             const formulaPivotId = this.getters.getPivotFormulaId(pivotId);
             const pivotFormula = createPivotFormula(formulaPivotId, pivotCell);
