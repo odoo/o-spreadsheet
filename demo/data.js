610930ba95a5a7a28190f46371430643f2bd3126
Adrien Minne (adrm) <adrm@odoo.com>
2024-09-13 13:56:57 +0200

    [FIX] demo: typo in demo data table style
    Task: 0

--- demo/data.js
+++ demo/data.js
@@ -2421,7 +2421,7 @@ export const demoData = {
             numberOfHeaders: 1,
             bandedRows: false,
             bandedColumns: false,
-            styleId: "TableStyleLigh12",
+            styleId: "TableStyleLight12",
           },
         },
         {
