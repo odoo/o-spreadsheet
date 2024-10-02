ba4e3378572b90cb02b9f5806417516ac87b7eb2
Lucas Lefèvre (lul) <lul@odoo.com>
2024-10-02 14:38:33 +0200

    [REF] side panel: use title props
    Currently, the title of a `Section` component is a title slot. We are using
    a slot to translate the title: a props like `title="'My title'"` wouldn't work
    since this props is javascript expression, while text content inside xml
    elements are extracted and translated.
    
    But since the introduction of the `.translate` modifier, we can use a props!
    
    It's much more natural (props vs slot) and explicit (`.translate`)
    
    Task: 4226704

--- tests/side_panels/components/section.test.ts
+++ tests/side_panels/components/section.test.ts
@@ -25,12 +25,47 @@ describe("Section", () => {
     expect(fixture).toMatchSnapshot();
   });
 
-  test("Can render a section with a title", async () => {
+  test("Can render a section with a title slot", async () => {
     class SectionContainer extends Component<Props, SpreadsheetChildEnv> {
       static template = xml/* xml */ `
     <div class="container">
       <Section t-props="props">
         <t t-set-slot="title">My title</t>
+        <div class="content">My content</div>
+      </Section>
+    </div>
+  `;
+      static components = { Section };
+      static props = { class: String };
+    }
+    const props = { class: "my-class" };
+    ({ fixture } = await mountComponent(SectionContainer, { props }));
+    expect(fixture).toMatchSnapshot();
+  });
+
+  test("Can render a section with a title props", async () => {
+    class SectionContainer extends Component<Props, SpreadsheetChildEnv> {
+      static template = xml/* xml */ `
+    <div class="container">
+      <Section t-props="props" title.translate="My title">
+        <div class="content">My content</div>
+      </Section>
+    </div>
+  `;
+      static components = { Section };
+      static props = { class: String };
+    }
+    const props = { class: "my-class" };
+    ({ fixture } = await mountComponent(SectionContainer, { props }));
+    expect(fixture).toMatchSnapshot();
+  });
+
+  test("Can render a section with both title props and slot", async () => {
+    class SectionContainer extends Component<Props, SpreadsheetChildEnv> {
+      static template = xml/* xml */ `
+    <div class="container">
+      <Section t-props="props" title.translate="My title from props">
+      <t t-set-slot="title">My title from slot</t>
         <div class="content">My content</div>
       </Section>
     </div>
