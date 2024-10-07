00b937905f6ae9aff70f96e7ec3f3265be8785dd
Lucas Lefèvre (lul) <lul@odoo.com>
2024-10-07 08:08:50 +0000

    [FIX] data validation: unify checkbox style
    Data validation check-boxes don't have the same style as any other
    check-boxes in the UI.
    
    I also added a missing test checking and unchecking the checkbox.
    
    Task: 4235283
    X-original-commit: 246d233be97d41e884db6724c1470f393700d03a

--- src/components/data_validation_overlay/dv_checkbox/dv_checkbox.ts
+++ src/components/data_validation_overlay/dv_checkbox/dv_checkbox.ts
@@ -2,15 +2,13 @@ import { Component } from "@odoo/owl";
 import { GRID_ICON_EDGE_LENGTH } from "../../../constants";
 import { CellPosition, SpreadsheetChildEnv } from "../../../types";
 import { css } from "../../helpers";
+import { CHECKBOX_WIDTH, Checkbox } from "../../side_panel/components/checkbox/checkbox";
 
-const CHECKBOX_WIDTH = 15;
 const MARGIN = (GRID_ICON_EDGE_LENGTH - CHECKBOX_WIDTH) / 2;
 
 css/* scss */ `
   .o-dv-checkbox {
     box-sizing: border-box !important;
-    width: ${CHECKBOX_WIDTH}px;
-    height: ${CHECKBOX_WIDTH}px;
     accent-color: #808080;
     margin: ${MARGIN}px;
     /** required to prevent the checkbox position to be sensible to the font-size (affects Firefox) */
@@ -24,14 +22,16 @@ interface Props {
 
 export class DataValidationCheckbox extends Component<Props, SpreadsheetChildEnv> {
   static template = "o-spreadsheet-DataValidationCheckbox";
+  static components = {
+    Checkbox,
+  };
   static props = {
     cellPosition: Object,
   };
 
-  onCheckboxChange(ev: Event) {
-    const newValue = (ev.target as HTMLInputElement).checked;
+  onCheckboxChange(value: boolean) {
     const { sheetId, col, row } = this.props.cellPosition;
-    const cellContent = newValue ? "TRUE" : "FALSE";
+    const cellContent = value ? "TRUE" : "FALSE";
     this.env.model.dispatch("UPDATE_CELL", { sheetId, col, row, content: cellContent });
   }
 
