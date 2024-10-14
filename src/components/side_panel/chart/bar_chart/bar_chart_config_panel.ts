10e21b41706efe90387ef3a9cba1977fe4c047c0
dhrp-odoo <dhrp@odoo.com>
2024-10-14 14:57:30 +0530

    [FIX] charts: correct stacked checkbox label
    This commit addresses an issue where the stacked checkbox label in the
    chart config panel was not updating when the chart type was changed.
    
    Task: 4251670

--- src/components/side_panel/chart/bar_chart/bar_chart_config_panel.ts
+++ src/components/side_panel/chart/bar_chart/bar_chart_config_panel.ts
@@ -1,7 +1,15 @@
+import { BarChartDefinition } from "../../../../types/chart";
 import { GenericChartConfigPanel } from "../building_blocks/generic_side_panel/config_panel";
 
 export class BarConfigPanel extends GenericChartConfigPanel {
   static template = "o-spreadsheet-BarConfigPanel";
+
+  get stackedLabel(): string {
+    const definition = this.props.definition as BarChartDefinition;
+    return definition.horizontal
+      ? this.chartTerms.StackedBarChart
+      : this.chartTerms.StackedColumnChart;
+  }
 
   onUpdateStacked(stacked: boolean) {
     this.props.updateChart(this.props.figureId, {
