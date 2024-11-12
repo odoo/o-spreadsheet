19b2faa929625d7bc98417ca993cecf32d4b422f
Lucas Lefèvre (lul) <lul@odoo.com>
2024-11-12 16:11:20 +0100

    [REF] export: remove useless property
    This optional property is never set and never used.
    
    Task: 4295344
    Part-of: odoo/o-spreadsheet#5156
    Signed-off-by: Pierre Rousseau (pro) <pro@odoo.com>

--- src/types/workbook_data.ts
+++ src/types/workbook_data.ts
@@ -81,7 +81,6 @@ export interface ExcelWorkbookData extends WorkbookData {
 export interface ExcelCellData extends CellData {
   value: CellValue;
   isFormula: Boolean;
-  computedFormat?: Format;
 }
 export interface ExcelSheetData extends Omit<SheetData, "figureTables" | "cols" | "rows"> {
   cells: { [key: string]: ExcelCellData | undefined };
