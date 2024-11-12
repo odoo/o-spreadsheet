064a7cf84a7ffe61677c5b64ae61e380e05e56d8
dhrp-odoo <dhrp@odoo.com>
2024-11-12 05:58:37 +0000

    [FIX] color_picker: conditionally hide reset button
    This FW PR includes two changes:
    
    1. Fixes a traceback error in the Conditional Formatting Color Scale
    editor by hiding the "No Color" button when not applicable.
    
    2. Prevents errors by validating color input earlier to avoid empty
    string parsing issues.
    
    closes odoo/o-spreadsheet#5187
    
    X-original-commit: 47659fd175c3a21c00cb196d0d054e7dcff55f57
    Task: 4102704
    Signed-off-by: Rémi Rahir (rar) <rar@odoo.com>
    Signed-off-by: Dhrutik Patel (dhrp) <dhrp@odoo.com>

--- tests/conditional_formatting/conditional_formatting_panel_component.test.ts
+++ tests/conditional_formatting/conditional_formatting_panel_component.test.ts
@@ -19,6 +19,7 @@ import {
   getTarget,
   keyDown,
   setInputValueAndTrigger,
+  simulateClick,
   triggerMouseEvent,
   triggerWheelEvent,
 } from "../test_helpers/dom_helper";
@@ -1052,6 +1053,14 @@ describe("UI of conditional formats", () => {
     await click(fixture, selectors.buttonSave);
     expect(model.getters.getConditionalFormats(model.getters.getActiveSheetId())).toHaveLength(0);
     expect(errorMessages()).toEqual(["Invalid Maxpoint formula"]);
+  });
+
+  test("Hides the 'No Color' button when the color picker is opened for the color scale", async () => {
+    await simulateClick(selectors.buttonAdd);
+    await simulateClick(document.querySelectorAll(selectors.cfTabSelector)[1]);
+    await simulateClick(selectors.colorScaleEditor.minColor);
+
+    expect(fixture.querySelector(".o-buttons .o-cancel")).toBeNull();
   });
 
   test("If there is no midpoint in a color scale, the color picker is invisible", async () => {
