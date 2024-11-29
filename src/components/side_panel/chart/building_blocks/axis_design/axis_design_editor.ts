708085ba92476a4061a59c14caf71f37b38a2c9a
Adrien Minne (adrm) <adrm@odoo.com>
2024-11-29 10:14:33 +0100

    [REF] chart: simplify `TextStyler` component
    This commit simplifies the `TextStyler` by
    
    - delete all the props `updateColor`, `toggleBold`, ... in favor of a
    single callback `updateStyle`
    - rename `name` props to `label` to be more explicit
    - use `ActionButton` components instead of custom HTML that looks like
    an action button
    
    Task: 4364295

--- src/components/side_panel/chart/building_blocks/axis_design/axis_design_editor.ts
+++ src/components/side_panel/chart/building_blocks/axis_design/axis_design_editor.ts
@@ -1,8 +1,8 @@
 import { Component, useState } from "@odoo/owl";
 import { CHART_AXIS_TITLE_FONT_SIZE } from "../../../../../constants";
+import { deepCopy } from "../../../../../helpers";
 import {
   ChartWithDataSetDefinition,
-  Color,
   DispatchResult,
   SpreadsheetChildEnv,
   TitleDesign,
@@ -35,81 +35,14 @@ export class AxisDesignEditor extends Component<Props, SpreadsheetChildEnv> {
 
   state = useState({ currentAxis: "x" });
 
+  defaultFontSize = CHART_AXIS_TITLE_FONT_SIZE;
+
   get axisTitleStyle(): TitleDesign {
-    const axisDesign = this.props.definition.axesDesign?.[this.state.currentAxis] ?? {};
-    return {
-      color: "",
-      align: "center",
-      fontSize: CHART_AXIS_TITLE_FONT_SIZE,
-      ...axisDesign.title,
-    };
+    return this.props.definition.axesDesign?.[this.state.currentAxis]?.title ?? {};
   }
 
   get badgeAxes() {
     return this.props.axesList.map((axis) => ({ value: axis.id, label: axis.name }));
-  }
-
-  updateAxisTitleColor(color: Color) {
-    const axesDesign = this.props.definition.axesDesign ?? {};
-    axesDesign[this.state.currentAxis] = {
-      ...axesDesign[this.state.currentAxis],
-      title: {
-        ...(axesDesign[this.state.currentAxis]?.title ?? {}),
-        color,
-      },
-    };
-    this.props.updateChart(this.props.figureId, { axesDesign });
-  }
-
-  updateAxisTitleFontSize(fontSize: number) {
-    const axesDesign = this.props.definition.axesDesign ?? {};
-    axesDesign[this.state.currentAxis] = {
-      ...axesDesign[this.state.currentAxis],
-      title: {
-        ...(axesDesign[this.state.currentAxis]?.title ?? {}),
-        fontSize,
-      },
-    };
-    this.props.updateChart(this.props.figureId, { axesDesign });
-  }
-
-  toggleBoldAxisTitle() {
-    const axesDesign = this.props.definition.axesDesign ?? {};
-    const title = axesDesign[this.state.currentAxis]?.title ?? {};
-    axesDesign[this.state.currentAxis] = {
-      ...axesDesign[this.state.currentAxis],
-      title: {
-        ...title,
-        bold: !title?.bold,
-      },
-    };
-    this.props.updateChart(this.props.figureId, { axesDesign });
-  }
-
-  toggleItalicAxisTitle() {
-    const axesDesign = this.props.definition.axesDesign ?? {};
-    const title = axesDesign[this.state.currentAxis]?.title ?? {};
-    axesDesign[this.state.currentAxis] = {
-      ...axesDesign[this.state.currentAxis],
-      title: {
-        ...title,
-        italic: !title?.italic,
-      },
-    };
-    this.props.updateChart(this.props.figureId, { axesDesign });
-  }
-
-  updateAxisTitleAlignment(align: "left" | "center" | "right") {
-    const axesDesign = this.props.definition.axesDesign ?? {};
-    const title = axesDesign[this.state.currentAxis]?.title ?? {};
-    axesDesign[this.state.currentAxis] = {
-      ...axesDesign[this.state.currentAxis],
-      title: {
-        ...title,
-        align,
-      },
-    };
-    this.props.updateChart(this.props.figureId, { axesDesign });
   }
 
   updateAxisEditor(ev) {
@@ -122,13 +55,22 @@ export class AxisDesignEditor extends Component<Props, SpreadsheetChildEnv> {
   }
 
   updateAxisTitle(text: string) {
-    const axesDesign = this.props.definition.axesDesign ?? {};
+    const axesDesign = deepCopy(this.props.definition.axesDesign) ?? {};
     axesDesign[this.state.currentAxis] = {
       ...axesDesign[this.state.currentAxis],
       title: {
         ...axesDesign?.[this.state.currentAxis]?.title,
         text,
       },
+    };
+    this.props.updateChart(this.props.figureId, { axesDesign });
+  }
+
+  updateAxisTitleStyle(style: TitleDesign) {
+    const axesDesign = deepCopy(this.props.definition.axesDesign) ?? {};
+    axesDesign[this.state.currentAxis] = {
+      ...axesDesign[this.state.currentAxis],
+      title: style,
     };
     this.props.updateChart(this.props.figureId, { axesDesign });
   }
