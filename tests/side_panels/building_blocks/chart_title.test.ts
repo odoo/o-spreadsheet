aacdb53cbd5cbe2b9d704b376c47e299df70bd06
Adrien Minne (adrm) <adrm@odoo.com>
2024-11-29 09:46:37 +0100

    [IMP] chart: extract `ChartTitle` tools into `TextStyler`
    This commit extract the style editing tools of the `ChartTitle`
    component into a new component called `TextStyler`. This will be used
    in the following commits to have a more generic component that can be
    used to style any text in the chart.
    
    It also adds add the possibility ot edit a background color or a
    vertical alignment in this `TextStyler` component.
    
    The commit also simplifies the TextStyler compared to the previous
    behaviour of `ChartTitle` by:
    
    - deleting all the props updateColor, toggleBold, ... in favor of a
    single callback updateStyle
    - using ActionButton components instead of custom HTML that looks like
    an action button
    
    Task: 4575651
    Part-of: odoo/o-spreadsheet#5756
    Signed-off-by: Vincent Schippefilt (vsc) <vsc@odoo.com>

--- tests/side_panels/building_blocks/chart_title.test.ts
+++ tests/side_panels/building_blocks/chart_title.test.ts
@@ -1,29 +1,43 @@
 import { ChartTitle } from "../../../src/components/side_panel/chart/building_blocks/chart_title/chart_title";
+import { TextStyler } from "../../../src/components/side_panel/chart/building_blocks/text_styler/text_styler";
 import { click, setInputValueAndTrigger } from "../../test_helpers/dom_helper";
 import { mountComponentWithPortalTarget } from "../../test_helpers/helpers";
 
 let fixture: HTMLElement;
 
-async function mountChartTitle(props: ChartTitle["props"]) {
-  ({ fixture } = await mountComponentWithPortalTarget(ChartTitle, { props }));
+async function mountChartTitle(props: Partial<ChartTitle["props"]>) {
+  const defaultProps: ChartTitle["props"] = {
+    title: "My title",
+    updateTitle: () => {},
+    updateStyle: () => {},
+    style: {},
+    defaultStyle: { fontSize: 10 },
+  };
+  ({ fixture } = await mountComponentWithPortalTarget(ChartTitle, {
+    props: { ...defaultProps, ...props },
+  }));
+}
+
+async function mountTextStyler(props: Partial<TextStyler["props"]>) {
+  const defaultProps: TextStyler["props"] = {
+    updateStyle: () => {},
+    style: {},
+    defaultStyle: { fontSize: 10 },
+  };
+  ({ fixture } = await mountComponentWithPortalTarget(TextStyler, {
+    props: { ...defaultProps, ...props },
+  }));
 }
 
 describe("Chart title", () => {
   test("Can render a chart title component", async () => {
-    await mountChartTitle({
-      title: "My title",
-      updateTitle: () => {},
-      style: { fontSize: 22 },
-      onFontSizeChanged: () => {},
-    });
+    await mountChartTitle({});
     expect(fixture).toMatchSnapshot();
   });
 
   test("Can render a chart title component with default title prop if not provided", async () => {
     await mountChartTitle({
-      updateTitle: () => {},
-      style: { fontSize: 22 },
-      onFontSizeChanged: () => {},
+      title: undefined,
     });
 
     const input = fixture.querySelector("input")!;
@@ -32,12 +46,7 @@ describe("Chart title", () => {
 
   test("Update is called when title is changed, not on input", async () => {
     const updateTitle = jest.fn();
-    await mountChartTitle({
-      title: "My title",
-      updateTitle,
-      style: { fontSize: 22 },
-      onFontSizeChanged: () => {},
-    });
+    await mountChartTitle({ updateTitle });
     const input = fixture.querySelector("input")!;
     expect(input.value).toBe("My title");
     await setInputValueAndTrigger(input, "My new title", "onlyInput");
@@ -46,80 +55,85 @@ describe("Chart title", () => {
     expect(updateTitle).toHaveBeenCalledTimes(1);
   });
 
-  test("UpdateColor is called when title color is changed", async () => {
-    const updateColor = jest.fn();
-    await mountChartTitle({
-      title: "My title",
-      updateTitle: () => {},
-      style: { fontSize: 22 },
-      updateColor,
-      onFontSizeChanged: () => {},
-    });
-    expect(updateColor).toHaveBeenCalledTimes(0);
-    await click(fixture, ".o-color-picker-button");
+  test("Can change text color", async () => {
+    const updateStyle = jest.fn();
+    await mountChartTitle({ updateStyle });
+    expect(updateStyle).toHaveBeenCalledTimes(0);
+    await click(fixture, ".o-color-picker-button[title='Text color']");
     await click(fixture, ".o-color-picker-line-item[data-color='#EFEFEF'");
-    expect(updateColor).toHaveBeenCalledWith("#EFEFEF");
+    expect(updateStyle).toHaveBeenCalledWith({ color: "#EFEFEF" });
   });
 
   test.each(["Left", "Center", "Right"])(
-    "UpdateAlignment is called when alignment is changed",
+    "Can change alignment to %s",
     async (alignment: string) => {
-      const updateAlignment = jest.fn();
-      await mountChartTitle({
-        title: "My title",
-        updateTitle: () => {},
-        style: { fontSize: 22 },
-        updateAlignment,
-        onFontSizeChanged: () => {},
-      });
-      expect(updateAlignment).toHaveBeenCalledTimes(0);
+      const updateStyle = jest.fn();
+      await mountChartTitle({ updateStyle });
+      expect(updateStyle).toHaveBeenCalledTimes(0);
       await click(fixture, ".o-menu-item-button[title='Horizontal alignment']");
       await click(fixture, `.o-menu-item-button[title='${alignment}']`);
-      expect(updateAlignment).toHaveBeenCalledWith(alignment.toLowerCase());
+      expect(updateStyle).toHaveBeenCalledWith({ align: alignment.toLowerCase() });
     }
   );
 
-  test("ToggleBold is called when clicking on bold button", async () => {
-    const toggleBold = jest.fn();
-    await mountChartTitle({
-      title: "My title",
-      updateTitle: () => {},
-      style: { fontSize: 22 },
-      toggleBold,
-      onFontSizeChanged: () => {},
-    });
-    expect(toggleBold).toHaveBeenCalledTimes(0);
+  test("Can make text bold", async () => {
+    const updateStyle = jest.fn();
+    await mountChartTitle({ updateStyle });
+    expect(updateStyle).toHaveBeenCalledTimes(0);
     await click(fixture, ".o-menu-item-button[title='Bold']");
-    expect(toggleBold).toHaveBeenCalledTimes(1);
+    expect(updateStyle).toHaveBeenCalledWith({ bold: true });
   });
 
-  test("ToggleItalic is called when clicking on italic button", async () => {
-    const toggleItalic = jest.fn();
-    await mountChartTitle({
-      title: "My title",
-      updateTitle: () => {},
-      style: { fontSize: 22 },
-      toggleItalic,
-      onFontSizeChanged: () => {},
-    });
-    expect(toggleItalic).toHaveBeenCalledTimes(0);
+  test("Can make text italic", async () => {
+    const updateStyle = jest.fn();
+    await mountChartTitle({ updateStyle });
+    expect(updateStyle).toHaveBeenCalledTimes(0);
     await click(fixture, ".o-menu-item-button[title='Italic']");
-    expect(toggleItalic).toHaveBeenCalledTimes(1);
+    expect(updateStyle).toHaveBeenCalledWith({ italic: true });
   });
 
-  test("OnFontSizeChanged is called when font size is changed", async () => {
-    const onFontSizeChanged = jest.fn();
-    await mountChartTitle({
-      title: "My title",
-      updateTitle: () => {},
-      style: { fontSize: 14 },
-      onFontSizeChanged,
-    });
+  test("Can change font size", async () => {
+    const updateStyle = jest.fn();
+    await mountChartTitle({ updateStyle });
     const fontSizeEditor = fixture.querySelector(".o-font-size-editor")!;
     expect(fontSizeEditor).not.toBeNull();
-    expect(onFontSizeChanged).toHaveBeenCalledTimes(0);
+    expect(updateStyle).toHaveBeenCalledTimes(0);
     const input = fontSizeEditor.querySelector("input")!;
     await setInputValueAndTrigger(input, "20");
-    expect(onFontSizeChanged).toHaveBeenCalledWith(20);
+    expect(updateStyle).toHaveBeenCalledWith({ fontSize: 20 });
+  });
+});
+
+describe("TextStyler", () => {
+  test("Alignments buttons are not visible by default", async () => {
+    await mountTextStyler({});
+
+    expect(".o-menu-item-button[title='Horizontal alignment']").toHaveCount(0);
+    expect(".o-menu-item-button[title='Vertical alignment']").toHaveCount(0);
+  });
+
+  test("Can show horizontal alignment buttons", async () => {
+    await mountTextStyler({ hasHorizontalAlign: true, hasVerticalAlign: true });
+
+    expect(".o-menu-item-button[title='Horizontal alignment']").toHaveCount(1);
+    expect(".o-menu-item-button[title='Vertical alignment']").toHaveCount(1);
+  });
+
+  test("Can change fill color", async () => {
+    const updateStyle = jest.fn();
+    await mountTextStyler({ updateStyle, hasBackgroundColor: true });
+    expect(updateStyle).toHaveBeenCalledTimes(0);
+    await click(fixture, ".o-color-picker-button[title='Fill color']");
+    await click(fixture, ".o-color-picker-line-item[data-color='#EFEFEF'");
+    expect(updateStyle).toHaveBeenCalledWith({ fillColor: "#EFEFEF" });
+  });
+
+  test("Can change vertical alignment", async () => {
+    const updateStyle = jest.fn();
+    await mountTextStyler({ updateStyle, hasVerticalAlign: true });
+    expect(updateStyle).toHaveBeenCalledTimes(0);
+    await click(fixture, ".o-menu-item-button[title='Vertical alignment']");
+    await click(fixture, `.o-menu-item-button[title='Middle']`);
+    expect(updateStyle).toHaveBeenCalledWith({ verticalAlign: "middle" });
   });
 });
