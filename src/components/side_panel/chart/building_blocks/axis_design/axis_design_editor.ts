0c59bd318377741139881eb6cee2e52612d7add7
Adrien Minne (adrm) <adrm@odoo.com>
2024-11-29 09:46:37 +0100

    [MOV] chart: rename `ChartTitle` to `TextStyler`
    Rename the `ChartTitle` component to `TextStyler` to make it sounds
    more generic and to prepare for the next commits.
    
    Task: 4575651

--- src/components/side_panel/chart/building_blocks/axis_design/axis_design_editor.ts
+++ src/components/side_panel/chart/building_blocks/axis_design/axis_design_editor.ts
@@ -11,7 +11,7 @@ import {
 import { WaterfallChartDefinition } from "../../../../../types/chart/waterfall_chart";
 import { BadgeSelection } from "../../../components/badge_selection/badge_selection";
 import { Section } from "../../../components/section/section";
-import { ChartTitle } from "../title/title";
+import { TextStyler } from "../text_styler/text_styler";
 
 export interface AxisDefinition {
   id: string;
@@ -30,7 +30,7 @@ interface Props {
 
 export class AxisDesignEditor extends Component<Props, SpreadsheetChildEnv> {
   static template = "o-spreadsheet-AxisDesignEditor";
-  static components = { Section, ChartTitle, BadgeSelection };
+  static components = { Section, TextStyler, BadgeSelection };
   static props = { figureId: String, definition: Object, updateChart: Function, axesList: Array };
 
   state = useState({ currentAxis: "x" });
