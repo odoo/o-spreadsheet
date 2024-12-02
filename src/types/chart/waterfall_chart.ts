4fba909da941800c0a2fce65d28907aab0ffd055
Anthony Hendrickx <anhe@odoo.com>
2024-12-02 11:35:49 +0100

    [IMP] charts: allow to invert columns/rows
    Task Description
    
    This task add the possibility for the user to invert the columns
     and rows of a chart dataset.
    
    Related Task
    
    Task: 3693182

--- src/types/chart/waterfall_chart.ts
+++ src/types/chart/waterfall_chart.ts
@@ -1,17 +1,10 @@
 import type { ChartConfiguration } from "chart.js";
 import { Color } from "../misc";
-import { AxesDesign, CustomizedDataSet, TitleDesign } from "./chart";
-import { LegendPosition, VerticalAxisPosition } from "./common_chart";
+import { CommonChartDefinition, VerticalAxisPosition } from "./common_chart";
 
-export interface WaterfallChartDefinition {
+export interface WaterfallChartDefinition extends CommonChartDefinition {
   readonly type: "waterfall";
-  readonly dataSets: CustomizedDataSet[];
-  readonly dataSetsHaveTitle: boolean;
-  readonly labelRange?: string;
-  readonly title: TitleDesign;
-  readonly background?: Color;
   readonly verticalAxisPosition: VerticalAxisPosition;
-  readonly legendPosition: LegendPosition;
   readonly aggregated?: boolean;
   readonly showSubTotals: boolean;
   readonly showConnectorLines: boolean;
@@ -19,8 +12,6 @@ export interface WaterfallChartDefinition {
   readonly positiveValuesColor?: Color;
   readonly negativeValuesColor?: Color;
   readonly subTotalValuesColor?: Color;
-  readonly axesDesign?: AxesDesign;
-  readonly showValues?: boolean;
 }
 
 export type WaterfallChartRuntime = {
