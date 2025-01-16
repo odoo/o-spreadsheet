41f5559f3121369dcaeedca9c0a548fb7cfe497e
Florian Damhaut <flda@odoo.com>
2025-01-16 14:24:04 +0100

    [IMP] scorecard: add styling to Key & baseline
    Add styling to key and baseline value.
    
    Task: 4353740

--- src/components/side_panel/chart/scorecard_chart_panel/scorecard_chart_design_panel.ts
+++ src/components/side_panel/chart/scorecard_chart_panel/scorecard_chart_design_panel.ts
@@ -1,12 +1,23 @@
 import { Component } from "@odoo/owl";
-import { SCORECARD_CHART_TITLE_FONT_SIZE } from "../../../../constants";
+import {
+  DEFAULT_SCORECARD_BASELINE_FONT_SIZE,
+  DEFAULT_SCORECARD_KEY_VALUE_FONT_SIZE,
+  SCORECARD_CHART_TITLE_FONT_SIZE,
+} from "../../../../constants";
 import { _t } from "../../../../translation";
 import { ScorecardChartDefinition } from "../../../../types/chart/scorecard_chart";
-import { Color, DispatchResult, SpreadsheetChildEnv, UID } from "../../../../types/index";
+import {
+  Color,
+  DispatchResult,
+  SpreadsheetChildEnv,
+  TitleDesign,
+  UID,
+} from "../../../../types/index";
 import { Checkbox } from "../../components/checkbox/checkbox";
 import { SidePanelCollapsible } from "../../components/collapsible/side_panel_collapsible";
 import { RoundColorPicker } from "../../components/round_color_picker/round_color_picker";
 import { Section } from "../../components/section/section";
+import { ChartTitle } from "../building_blocks/chart_title/chart_title";
 import { GeneralDesignEditor } from "../building_blocks/general_design/general_design_editor";
 
 type ColorPickerId = undefined | "backgroundColor" | "baselineColorUp" | "baselineColorDown";
@@ -26,6 +37,7 @@ export class ScorecardChartDesignPanel extends Component<Props, SpreadsheetChild
     SidePanelCollapsible,
     Section,
     Checkbox,
+    ChartTitle,
   };
   static props = {
     figureId: String,
@@ -56,10 +68,6 @@ export class ScorecardChartDesignPanel extends Component<Props, SpreadsheetChild
     return _t(term);
   }
 
-  updateBaselineDescr(ev) {
-    this.props.updateChart(this.props.figureId, { baselineDescr: ev.target.value });
-  }
-
   setColor(color: Color, colorPickerId: ColorPickerId) {
     switch (colorPickerId) {
       case "backgroundColor":
@@ -72,5 +80,43 @@ export class ScorecardChartDesignPanel extends Component<Props, SpreadsheetChild
         this.props.updateChart(this.props.figureId, { baselineColorUp: color });
         break;
     }
+  }
+
+  get keyStyle(): TitleDesign {
+    return {
+      align: "center",
+      fontSize: DEFAULT_SCORECARD_KEY_VALUE_FONT_SIZE,
+      ...this.props.definition.keyDescr,
+    };
+  }
+
+  get baselineStyle(): TitleDesign {
+    return {
+      align: "center",
+      fontSize: DEFAULT_SCORECARD_BASELINE_FONT_SIZE,
+      ...this.props.definition.baselineDescr,
+    };
+  }
+
+  setKeyText(text: string) {
+    this.props.updateChart(this.props.figureId, {
+      keyDescr: { ...this.props.definition.keyDescr, text },
+    });
+  }
+
+  updateKeyStyle(style: TitleDesign) {
+    const keyDescr = { ...this.keyStyle, ...style };
+    this.props.updateChart(this.props.figureId, { keyDescr });
+  }
+
+  setBaselineText(text: string) {
+    this.props.updateChart(this.props.figureId, {
+      baselineDescr: { ...this.props.definition.baselineDescr, text },
+    });
+  }
+
+  updateBaselineStyle(style: TitleDesign) {
+    const baselineDescr = { ...this.baselineStyle, ...style };
+    this.props.updateChart(this.props.figureId, { baselineDescr });
   }
 }
