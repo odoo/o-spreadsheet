9a193d147021d5524367bab4edc50660ec82d86d
Mehdi Rachico (mera) <mera@odoo.com>
2025-01-20 16:55:35 +0000

    [IMP] xlsx: support exporting array formulas
    This commit improves the export of vectorized & array formulas,
    including those that spill across cells, ensuring better compatibility
    with other spreadsheet applications. The adopted approach is to treat
    all formulas as array formulas & to add an additional xml file that
    contains metadata about array formula properties. As a result, we improve
    the integrity & usability of our generated XLSX files.
    
    Task: 4134347
    X-original-commit: c0c3046e1cd650c07beb271e04544b269487c390

--- src/xlsx/xlsx_writer.ts
+++ src/xlsx/xlsx_writer.ts
@@ -10,7 +10,13 @@ import {
   XMLString,
 } from "../types/xlsx";
 import { XLSXExportXMLFile } from "./../types/xlsx";
-import { CONTENT_TYPES, NAMESPACE, RELATIONSHIP_NSR, XLSX_RELATION_TYPE } from "./constants";
+import {
+  ARRAY_FORMULA_URI,
+  CONTENT_TYPES,
+  NAMESPACE,
+  RELATIONSHIP_NSR,
+  XLSX_RELATION_TYPE,
+} from "./constants";
 import { IMAGE_MIMETYPE_TO_EXTENSION_MAPPING } from "./conversion";
 import { createChart } from "./functions/charts";
 import { addConditionalFormatting } from "./functions/conditional_formatting";
@@ -203,6 +209,30 @@ function createWorksheets(data: ExcelWorkbookData, construct: XLSXStructure): XL
     `;
     files.push(createXMLFile(parseXML(sheetXml), `xl/worksheets/sheet${sheetIndex}.xml`, "sheet"));
   }
+  const sheetMetadataXml = escapeXml/*xml*/ `
+    <metadata xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main" xmlns:xda="http://schemas.microsoft.com/office/spreadsheetml/2017/dynamicarray">
+        <metadataTypes count="1">
+            <metadataType name="XLDAPR" minSupportedVersion="120000" copy="1" pasteAll="1"
+                pasteValues="1" merge="1" splitFirst="1" rowColShift="1" clearFormats="1"
+                clearComments="1" assign="1" coerce="1" cellMeta="1" />
+        </metadataTypes>
+        <futureMetadata name="XLDAPR" count="1">
+            <bk>
+                <extLst>
+                    <ext uri="{${ARRAY_FORMULA_URI}}">
+                        <xda:dynamicArrayProperties fDynamic="1" fCollapsed="0" />
+                    </ext>
+                </extLst>
+            </bk>
+        </futureMetadata>
+        <cellMetadata count="1">
+            <bk>
+                <rc t="1" v="0" />
+            </bk>
+        </cellMetadata>
+    </metadata>
+  `;
+  files.push(createXMLFile(parseXML(sheetMetadataXml), "xl/metadata.xml", "metadata"));
   addRelsToFile(construct.relsFiles, "xl/_rels/workbook.xml.rels", {
     type: XLSX_RELATION_TYPE.sharedStrings,
     target: "sharedStrings.xml",
@@ -210,6 +240,10 @@ function createWorksheets(data: ExcelWorkbookData, construct: XLSXStructure): XL
   addRelsToFile(construct.relsFiles, "xl/_rels/workbook.xml.rels", {
     type: XLSX_RELATION_TYPE.styles,
     target: "styles.xml",
+  });
+  addRelsToFile(construct.relsFiles, "xl/_rels/workbook.xml.rels", {
+    type: XLSX_RELATION_TYPE.metadata,
+    target: "metadata.xml",
   });
   return files;
 }
