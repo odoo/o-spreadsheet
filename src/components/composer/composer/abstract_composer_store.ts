8057e52ba0e3691ece32cb19279558780d9bfe5f
Lucas Lefèvre (lul) <lul@odoo.com>
2025-01-27 12:04:22 +0000

    [FIX] data_validation: don't prefilter on initial exact match
    Steps to reproduce:
    - Add dropdown list data validation with those options:
      XS, S, M, L
    - select S
    - double click the cell again to change the value
    
    => only S and XS are in the auto-complete dropdown
    
    closes odoo/o-spreadsheet#5870
    
    Opw: 4499009
    Task: 4518009
    X-original-commit: 398e85b82b086c38f0eb61744c4a71b25596996d
    Signed-off-by: Rémi Rahir (rar) <rar@odoo.com>
    Signed-off-by: Lucas Lefèvre (lul) <lul@odoo.com>

--- src/components/composer/composer/abstract_composer_store.ts
+++ src/components/composer/composer/abstract_composer_store.ts
@@ -559,6 +559,17 @@ export abstract class AbstractComposerStore extends SpreadsheetStore {
       const exactMatch = proposals?.find((p) => p.text === tokenAtCursor.value);
       // remove tokens that are likely to be other parts of the formula that slipped in the token if it's a string
       const searchTerm = tokenAtCursor.value.replace(/[ ,\(\)]/g, "");
+      if (
+        this._currentContent === this.initialContent &&
+        provider.displayAllOnInitialContent &&
+        proposals?.length
+      ) {
+        return {
+          proposals,
+          selectProposal: provider.selectProposal,
+          autoSelectFirstProposal: provider.autoSelectFirstProposal ?? false,
+        };
+      }
       if (exactMatch && this._currentContent !== this.initialContent) {
         // this means the user has chosen a proposal
         return;
