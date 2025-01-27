64a7f23afb5382ca32f89b6761dfaca832dc6862
Adrien Minne (adrm) <adrm@odoo.com>
2025-01-27 09:27:37 +0100

    [FIX] chart: pyramid chart datasets are wrong in the panel
    Bug:
    - Create a pyramid chart
    - Set it's ranges to A1: C3
    - In the side panel the ranges are [A1:A3, B1:B3, C1:C3]
    - But only the first two ranges are actually in the chart definition,
    C1:C3 is dropped
    - Closing and re-opening the panel display the correct ranges
    
    Task: 4517138

--- tests/test_helpers/chart_helpers.ts
+++ tests/test_helpers/chart_helpers.ts
@@ -1,4 +1,5 @@
-import { Model, UID } from "../../src";
+import { Model, SpreadsheetChildEnv, UID } from "../../src";
+import { nextTick } from "./helpers";
 
 export function isChartAxisStacked(model: Model, chartId: UID, axis: "x" | "y"): boolean {
   return getChartConfiguration(model, chartId).options?.scales?.[axis]?.stacked;
@@ -7,4 +8,10 @@ export function isChartAxisStacked(model: Model, chartId: UID, axis: "x" | "y"):
 export function getChartConfiguration(model: Model, chartId: UID) {
   const runtime = model.getters.getChartRuntime(chartId) as any;
   return runtime.chartJsConfig;
+}
+
+export async function openChartConfigSidePanel(model: Model, env: SpreadsheetChildEnv, id: UID) {
+  model.dispatch("SELECT_FIGURE", { id });
+  env.openSidePanel("ChartPanel");
+  await nextTick();
 }
