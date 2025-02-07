3bdeea3c3c1cbd42860152c7377853d3ba7cdfb7
Lucas Lefèvre (lul) <lul@odoo.com>
2025-02-07 12:44:56 +0100

    reduce

--- tests/collaborative/collaborative_monkey_party.test.ts
+++ tests/collaborative/collaborative_monkey_party.test.ts
@@ -1,6 +1,5 @@
-import seedrandom from "seedrandom";
 import { Model } from "../../src";
-import { range } from "../../src/helpers";
+import { deepEquals, range } from "../../src/helpers";
 import { Command } from "../../src/types";
 // import { redo, undo } from "../test_helpers/commands_helpers";
 import { FunctionCodeBuilder } from "../../src/formulas/code_builder";
@@ -46,22 +45,120 @@ function actionsToTestCode(testTitle, actions: UserAction[][]) {
   const code = new FunctionCodeBuilder();
   code.append(`test("${testTitle}", () => {`);
   for (const commandGroup of actions) {
-    code.append("network.concurrent(() => {");
-    for (const { command, user } of commandGroup) {
-      const userName = user.getters.getClient().name.toLowerCase();
-      if (command.type === "REQUEST_UNDO") {
-        code.append(`undo(${userName});`);
-      } else if (command.type === "REQUEST_REDO") {
-        code.append(`redo(${userName});`);
-      } else {
-        const cmdPayload = JSON.stringify({ ...command, type: undefined });
-        code.append(`${userName}.dispatch("${command.type}", ${cmdPayload});`);
+    if (commandGroup.length === 1) {
+      appendCommand(code, commandGroup[0]);
+    } else {
+      code.append("network.concurrent(() => {");
+      for (const { command, user } of commandGroup) {
+        appendCommand(code, { user, command });
       }
+      code.append("});");
     }
-    code.append("});");
   }
   code.append("expect([alice, bob, charlie]).toHaveSynchronizedExportedData();", "});");
   return code.toString();
+}
+
+function appendCommand(code: FunctionCodeBuilder, { user, command }: UserAction) {
+  const userName = user.getters.getClient().name.toLowerCase();
+  if (command.type === "REQUEST_UNDO") {
+    code.append(`undo(${userName});`);
+  } else if (command.type === "REQUEST_REDO") {
+    code.append(`redo(${userName});`);
+  } else {
+    const cmdPayload = JSON.stringify({ ...command, type: undefined });
+    code.append(`${userName}.dispatch("${command.type}", ${cmdPayload});`);
+  }
+}
+
+function rerunTest(actions: UserAction[][]) {
+  const { network, alice, bob, charlie } = setupCollaborativeEnv();
+  const newUsers = { alice, bob, charlie };
+  for (const concurrentChunk of actions) {
+    for (const action of concurrentChunk) {
+      action.user = newUsers[action.user.getters.getClient().name.toLowerCase()];
+    }
+  }
+  return run(network, [alice, bob, charlie], actions);
+}
+
+function minimizeFailingCommands(actions: UserAction[][]) {
+  // try removing concurrent actions chunk by chunk
+  let reduced = true;
+  while (reduced) {
+    reduced = false;
+    for (let chunkIndex = 0; chunkIndex < actions.length; chunkIndex++) {
+      const testCase = actions.slice(0, chunkIndex).concat(actions.slice(chunkIndex + 1));
+
+      const { fail } = rerunTest(testCase);
+      if (fail) {
+        actions = testCase;
+        reduced = true;
+      } else {
+        // reduce the chunck command-by-command
+        let chunkReduced = true;
+        while (chunkReduced) {
+          chunkReduced = false;
+          const chunk = actions[chunkIndex];
+          for (let cmdIndex = 0; cmdIndex < chunk.length; cmdIndex++) {
+            const testChunkCase = [...actions];
+            testChunkCase[chunkIndex] = chunk.slice(0, cmdIndex).concat(chunk.slice(cmdIndex + 1));
+            const { fail } = rerunTest(testChunkCase);
+            if (fail) {
+              actions = testChunkCase;
+              chunkReduced = true;
+            }
+          }
+        }
+      }
+    }
+  }
+  return actions;
+}
+
+function areSynced(users: Model[]): boolean {
+  // FIXME we are checking the same combination twice
+  for (let a of users) {
+    for (let b of users) {
+      if (a === b) {
+        continue;
+      }
+      const exportA = a.exportData();
+      const exportB = b.exportData();
+      if (!deepEquals(exportA, exportB)) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+function run(network: MockTransportService, users: Model[], actions: UserAction[][]) {
+  const executed: UserAction[][] = [];
+  for (const commandGroup of actions) {
+    const concurrentlyExecuted: UserAction[] = [];
+    executed.push(concurrentlyExecuted);
+    try {
+      network.concurrent(() => {
+        for (const { command, user } of commandGroup) {
+          const result = user.dispatch(command.type, command);
+          if (result.isSuccessful) {
+            concurrentlyExecuted.push({ command, user });
+          }
+        }
+      });
+    } catch (e) {
+      concurrentlyExecuted.push(...commandGroup);
+      return {
+        fail: true,
+        executedActions: executed,
+      };
+    }
+  }
+  return {
+    fail: !areSynced(users),
+    executedActions: executed,
+  };
 }
 
 describe("monkey party", () => {
@@ -83,8 +180,8 @@ describe("monkey party", () => {
     ({ network, alice, bob, charlie } = setupCollaborativeEnv());
   });
 
-  // test.each(["1738918805500"])("monkey party with seed %s", (seed) => {
-  test.each(seeds)("monkey party with seed %s", (seed) => {
+  test.each(["1738925119284"])("monkey party with seed %s", (seed) => {
+    // test.each(seeds)("monkey party with seed %s", (seed) => {
     seedrandom(seed, { global: true });
 
     // add some undo/redo
@@ -98,26 +195,36 @@ describe("monkey party", () => {
     const actions = assignUser(shuffle(commandsWithUndoRedo), [alice, bob, charlie]);
     const concurrencyGroups = randomConcurrencyGroup(actions);
 
-    const executed: UserAction[][] = [];
-    for (const commandGroup of concurrencyGroups) {
-      const concurrentlyExecuted: UserAction[] = [];
-      executed.push(concurrentlyExecuted);
-      network.concurrent(() => {
-        for (const { command, user } of commandGroup) {
-          try {
-            const result = user.dispatch(command.type, command);
-            if (result.isSuccessful) {
-              concurrentlyExecuted.push({ command, user });
-            }
-          } catch (e) {
-            concurrentlyExecuted.push({ command, user });
-            throw new Error(`Error while executing ${command.type}`, { cause: e });
-          }
-        }
-      });
-      // console.log(actionsToTestCode(seed, executed));
-      // expect([alice, bob, charlie]).toHaveSynchronizedExportedData();
+    const { fail, executedActions } = run(network, [alice, bob, charlie], concurrencyGroups);
+    if (fail) {
+      // console.log(actionsToTestCode(seed, executedActions));
+      // console.log(minimizeFailingCommands(executedActions).length);
+      console.log(actionsToTestCode(seed, minimizeFailingCommands(executedActions)));
+      // console.log(executedActions.length);
     }
+    expect([alice, bob, charlie]).toHaveSynchronizedExportedData();
+  });
+
+  test("1738925119284", () => {
+    bob.dispatch("CREATE_SHEET", {
+      sheetId: "sheet2",
+      position: 1,
+      name: "Sheet2",
+    });
+    charlie.dispatch("DUPLICATE_SHEET", {
+      sheetId: "sheet2",
+      sheetIdTo: "sheet3",
+    });
+    alice.dispatch("HIDE_SHEET", { sheetId: "sheet2" });
+    network.concurrent(() => {
+      bob.dispatch("DELETE_SHEET", { sheetId: "sheet3" });
+      alice.dispatch("CREATE_SHEET", {
+        sheetId: "sheet4",
+        name: "New pivot (copy) (Pivot #2)",
+        position: 2,
+      });
+      charlie.dispatch("DELETE_SHEET", { sheetId: "Sheet1" });
+    });
     expect([alice, bob, charlie]).toHaveSynchronizedExportedData();
   });
 
