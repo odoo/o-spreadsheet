f9e8f1fac802c41e2a58463bd1d8e6cad97390b5
Lucas Lefèvre (lul) <lul@odoo.com>
2025-02-11 08:14:46 +0000

    [FIX] cell: delete cells on sheet deletion
    Steps to reproduce:
    - create a new sheet
    - update a cell on that sheet
    - delete the sheet
    - re-create a new sheet with the same sheet id
    
    => when deleting the sheet, the cell plugin keeps its data-structure.
    It causes a crash when exporting the data.
    
    Task: 4567308
    X-original-commit: be0a36dad4ecb170ecc07db3b33a2b2e37893a5e
    Part-of: odoo/o-spreadsheet#5744
    Signed-off-by: Rémi Rahir (rar) <rar@odoo.com>
    Signed-off-by: Lucas Lefèvre (lul) <lul@odoo.com>

--- tests/sheet/sheets_plugin.test.ts
+++ tests/sheet/sheets_plugin.test.ts
@@ -17,6 +17,7 @@ import {
   createSheetWithName,
   deleteColumns,
   deleteRows,
+  deleteSheet,
   freezeColumns,
   freezeRows,
   hideColumns,
@@ -265,6 +266,17 @@ describe("sheets", () => {
     const model = new Model();
     setCellContent(model, "A1", "hello");
     expect(getCell(model, "A1", "invalidSheetId")!).toBe(undefined);
+  });
+
+  test("delete then create a sheet with the same id", () => {
+    const model = new Model();
+    const newSheetId = "42";
+    createSheet(model, { sheetId: newSheetId });
+    setCellContent(model, "A1", "hello", newSheetId);
+    deleteSheet(model, newSheetId);
+    createSheet(model, { sheetId: newSheetId });
+    expect(getCell(model, "A1", newSheetId)).toBeUndefined();
+    expect(model.exportData().sheets[1].cells).toEqual({});
   });
 
   test("cannot activate an invalid sheet", () => {
