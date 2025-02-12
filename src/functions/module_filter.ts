83c44ed850f1227cd2d2770b3d681ac969438ef9
Anthony Hendrickx <anhe@odoo.com>
2025-02-12 07:04:38 +0000

    [FIX] formulas: SORTN is not exported anymore
    Task Description
    
    As SORTN is a formula that only exists in Google Sheet, and not in
    MS Excel, this shouldn't be exported as it won't be recognized by
    Excel.
    
    Related Task
    
    Task: 4564445
    X-original-commit: 318aa173fc817469b970477551de4146884e00f5

--- src/functions/module_filter.ts
+++ src/functions/module_filter.ts
@@ -284,7 +284,7 @@ export const SORTN: AddFunctionDescription = {
       }
     }
   },
-  isExported: true,
+  isExported: false,
 };
 
 // -----------------------------------------------------------------------------
