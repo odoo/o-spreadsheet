131611aa014292b3d7b712218005a9dc05e053af
dhrp-odoo <dhrp@odoo.com>
2025-02-12 09:03:51 +0000

    [FIX] pie_chart: use chart background for border color
    Previously, the border color was always white
    (or `BACKGROUND_CHART_COLOR` in newer versions).
    
    Now, it defaults to the chart background if available;
    otherwise, it falls back to white.
    
    closes odoo/o-spreadsheet#5732
    
    Task: 4570603
    X-original-commit: 34b39f169c2ddfa227b060de5344673f57b5efb9
    Signed-off-by: Rémi Rahir (rar) <rar@odoo.com>
    Signed-off-by: Dhrutik Patel (dhrp) <dhrp@odoo.com>

--- tests/figures/chart/charts_component.test.ts
+++ tests/figures/chart/charts_component.test.ts
@@ -5,6 +5,7 @@ import { BACKGROUND_CHART_COLOR } from "../../../src/constants";
 import { toHex, toZone } from "../../../src/helpers";
 import { ScorecardChart } from "../../../src/helpers/figures/charts";
 import { CHART_TYPES, ChartDefinition, ChartType, SpreadsheetChildEnv } from "../../../src/types";
+import { PieChartRuntime } from "../../../src/types/chart";
 import { BarChartDefinition } from "../../../src/types/chart/bar_chart";
 import { LineChartDefinition } from "../../../src/types/chart/line_chart";
 import { getChartConfiguration } from "../../test_helpers/chart_helpers";
@@ -1620,6 +1621,13 @@ describe("charts", () => {
 
     await keyDown({ key: "Z", ctrlKey: true });
     expect(getCellContent(model, "D6")).toEqual("");
+  });
+
+  test("Pie chart border color matches the background color", async () => {
+    createTestChart("basicChart");
+    updateChart(model, chartId, { type: "pie", background: "#FF0000" });
+    const runtime = model.getters.getChartRuntime(chartId) as PieChartRuntime;
+    expect(runtime.chartJsConfig.data?.datasets?.[0].borderColor).toBe("#FF0000");
   });
 
   test("Chart is re-rendered if its label format change", async () => {
