691ee761dbe8354edd88472e62a75d1ce54419ba
Adrien Minne (adrm) <adrm@odoo.com>
2025-02-13 07:23:00 +0000

    [FIX] uuid: remove black magic to generate uuid
    The code to generate UUIDs was copied straight from the internet
    and contained some nonsensical nonsense.
    
    `([1e7] + -1e3 + -4e3 + -8e3 + -1e11)` looks like some cryptographic
    magic, but all it does is generate a STATIC STRING. This is gibberish.
    The @ts-ignore are also not needed with proper typing and conversion.
    
    This commit also adds some tests for the generation of UUIDs, ensuring
    we don't burn everything, which can happens as proven by ec362e2bff.
    
    closes odoo/o-spreadsheet#5757
    
    Task: 4574102
    X-original-commit: 2702575f09db2731e05f31c2e3ee70eaf5613531
    Signed-off-by: Lucas Lefèvre (lul) <lul@odoo.com>
    Signed-off-by: Adrien Minne (adrm) <adrm@odoo.com>

--- src/helpers/uuid.ts
+++ src/helpers/uuid.ts
@@ -24,12 +24,11 @@ export class UuidGenerator {
     if (this.isFastIdStrategy) {
       this.fastIdStart++;
       return String(this.fastIdStart);
-      //@ts-ignore
-    } else if (window.crypto && window.crypto.getRandomValues) {
-      //@ts-ignore
-      return ([1e7] + -1e3).replace(/[018]/g, (c) =>
-        (c ^ (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))).toString(16)
-      );
+    } else if (window.crypto) {
+      return "10000000-1000".replace(/[01]/g, (c) => {
+        const n = Number(c);
+        return (n ^ (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (n / 4)))).toString(16);
+      });
     } else {
       // mainly for jest and other browsers that do not have the crypto functionality
       return "xxxxxxxx-xxxx".replace(/[xy]/g, function (c) {
@@ -48,12 +47,11 @@ export class UuidGenerator {
     if (this.isFastIdStrategy) {
       this.fastIdStart++;
       return String(this.fastIdStart);
-      //@ts-ignore
-    } else if (window.crypto && window.crypto.getRandomValues) {
-      //@ts-ignore
-      return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
-        (c ^ (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))).toString(16)
-      );
+    } else if (window.crypto) {
+      return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, (c) => {
+        const n = Number(c);
+        return (n ^ (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (n / 4)))).toString(16);
+      });
     } else {
       // mainly for jest and other browsers that do not have the crypto functionality
       return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
