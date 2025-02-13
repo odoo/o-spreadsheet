5ab9d280a77eeae39cda95fa2518a13aa7206cd4
Lucas Lefèvre (lul) <lul@odoo.com>
2025-02-13 11:25:36 +0100

    [FIX] charts: duplicate chart in deterministic order
    The order given by `Object.values()` depends on the order of insertion in
    the object.
    But in a collaborative context, users executes command in different orders.
    Because of that, when duplicating a sheet, charts are duplicated in a
    different order.
    
    Task: 4567308

--- tests/collaborative/collaborative.test.ts
+++ tests/collaborative/collaborative.test.ts
@@ -20,6 +20,7 @@ import {
   createTableStyle,
   deleteRows,
   deleteSheet,
+  duplicateSheet,
   groupHeaders,
   hideRows,
   hideSheet,
@@ -483,6 +484,19 @@ describe("Multi users synchronisation", () => {
     alice.dispatch("DUPLICATE_SHEET", {
       sheetId: alice.getters.getActiveSheetId(),
       sheetIdTo: "Sheet2",
+    });
+    expect([alice, bob, charlie]).toHaveSynchronizedExportedData();
+  });
+
+  test("duplicate charts in deterministic order", () => {
+    const { network, alice, bob, charlie } = setupCollaborativeEnv();
+    createChart(bob, { type: "bar" }, "figureId");
+    redo(bob);
+    setCellContent(alice, "A1", "hello");
+    duplicateSheet(charlie, "Sheet1", "duplicateSheetId");
+    network.concurrent(() => {
+      undo(alice);
+      charlie.dispatch("DELETE_FIGURE", { id: "figureId", sheetId: "Sheet1" });
     });
     expect([alice, bob, charlie]).toHaveSynchronizedExportedData();
   });
