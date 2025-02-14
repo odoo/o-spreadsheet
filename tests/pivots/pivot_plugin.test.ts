f334d35123113d4532ed8cc0e378f0216f2db34e
Lucas Lefèvre (lul) <lul@odoo.com>
2025-02-14 08:56:32 +0000

    [FIX] pivot: reject command with invalid pivot id
    Task: 4567308
    X-original-commit: 35511979567ab56d947a9d1698428b9b3ee0dd0c

--- tests/pivots/pivot_plugin.test.ts
+++ tests/pivots/pivot_plugin.test.ts
@@ -185,6 +185,38 @@ describe("Pivot plugin", () => {
     );
   });
 
+  test("cannot update a pivot with a wrong id", () => {
+    const model = new Model();
+    const updateResult = model.dispatch("UPDATE_PIVOT", {
+      pivotId: "9999",
+      pivot: {
+        columns: [],
+        rows: [],
+        measures: [],
+        name: "Pivot",
+        type: "SPREADSHEET",
+      },
+    });
+    expect(updateResult).toBeCancelledBecause(CommandResult.PivotIdNotFound);
+  });
+
+  test("cannot duplicate a pivot with a wrong id", () => {
+    const model = new Model();
+    const updateResult = model.dispatch("DUPLICATE_PIVOT", {
+      pivotId: "9999",
+      newPivotId: "1",
+    });
+    expect(updateResult).toBeCancelledBecause(CommandResult.PivotIdNotFound);
+  });
+
+  test("cannot remove a pivot with a wrong id", () => {
+    const model = new Model();
+    const updateResult = model.dispatch("REMOVE_PIVOT", {
+      pivotId: "9999",
+    });
+    expect(updateResult).toBeCancelledBecause(CommandResult.PivotIdNotFound);
+  });
+
   test("forbidden characters are removed from new sheet name when duplicating a pivot", () => {
     const grid = {
       A1: "Customer",
