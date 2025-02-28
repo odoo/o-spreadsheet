095142dd8ae1c5f7f98d39ce678275b72792d506
Rémi Rahir (rar) <rar@odoo.com>
2025-02-28 12:47:35 +0000

    [FIX] HeaderOverlay: Fix unhide button position
    The visibility of the button to unhide headers never properly accounted
    for the presence of the frozen pane. Furthermore, recent fixes in the
    viewport made the situation worse because the offset of the last header
    was badly computed when it was hidden.
    
    closes odoo/o-spreadsheet#5888
    
    Task: 4548264
    X-original-commit: e9e6333d55d3bbdac400f0f2a07f2e4871104d53
    Signed-off-by: Adrien Minne (adrm) <adrm@odoo.com>
    Signed-off-by: Rémi Rahir (rar) <rar@odoo.com>

---
+++ src/components/headers_overlay/unhide_headers.ts
@@ -0,0 +1,87 @@
+import { Component } from "@odoo/owl";
+import { HEADER_HEIGHT, HEADER_WIDTH } from "../../constants";
+import { positionToZone } from "../../helpers";
+import { ConsecutiveIndexes, HeaderIndex, SpreadsheetChildEnv } from "../../types";
+import { cssPropertiesToCss } from "../helpers";
+
+interface Props {
+  headersGroups: ConsecutiveIndexes[];
+  offset: number;
+  headerRange: { start: HeaderIndex; end: HeaderIndex };
+}
+
+export class UnhideRowHeaders extends Component<Props, SpreadsheetChildEnv> {
+  static template = "o-spreadsheet-UnhideRowHeaders";
+  static props = {
+    headersGroups: Array,
+    headerRange: Object,
+    offset: { type: Number, optional: true },
+  };
+  static defaultProps = { offset: 0 };
+
+  get sheetId() {
+    return this.env.model.getters.getActiveSheetId();
+  }
+
+  getUnhidePreviousButtonStyle(hiddenIndex: HeaderIndex): string {
+    const rect = this.env.model.getters.getRect(positionToZone({ col: 0, row: hiddenIndex }));
+    const y = rect.y + rect.height - HEADER_HEIGHT;
+    return cssPropertiesToCss({ top: y - this.props.offset + "px", "margin-right": "1px" });
+  }
+
+  getUnhideNextButtonStyle(hiddenIndex: HeaderIndex): string {
+    const rect = this.env.model.getters.getRect(positionToZone({ col: 0, row: hiddenIndex }));
+    const y = rect.y - HEADER_HEIGHT;
+    return cssPropertiesToCss({ top: y - this.props.offset + "px", "margin-right": "1px" });
+  }
+
+  unhide(hiddenElements: HeaderIndex[]) {
+    this.env.model.dispatch("UNHIDE_COLUMNS_ROWS", {
+      sheetId: this.sheetId,
+      dimension: "ROW",
+      elements: hiddenElements,
+    });
+  }
+
+  isVisible(header: HeaderIndex) {
+    return header >= this.props.headerRange.start && header <= this.props.headerRange.end;
+  }
+}
+
+export class UnhideColumnHeaders extends Component<Props, SpreadsheetChildEnv> {
+  static template = "o-spreadsheet-UnhideColumnHeaders";
+  static props = {
+    headersGroups: Array,
+    headerRange: Object,
+    offset: { type: Number, optional: true },
+  };
+  static defaultProps = { offset: 0 };
+
+  get sheetId() {
+    return this.env.model.getters.getActiveSheetId();
+  }
+
+  getUnhidePreviousButtonStyle(hiddenIndex: HeaderIndex): string {
+    const rect = this.env.model.getters.getRect(positionToZone({ col: hiddenIndex, row: 0 }));
+    const x = rect.x + rect.width - HEADER_WIDTH;
+    return cssPropertiesToCss({ left: x - this.props.offset + "px" });
+  }
+
+  getUnhideNextButtonStyle(hiddenIndex: HeaderIndex): string {
+    const rect = this.env.model.getters.getRect(positionToZone({ col: hiddenIndex, row: 0 }));
+    const x = rect.x - HEADER_WIDTH;
+    return cssPropertiesToCss({ left: x - this.props.offset + "px" });
+  }
+
+  unhide(hiddenElements: HeaderIndex[]) {
+    this.env.model.dispatch("UNHIDE_COLUMNS_ROWS", {
+      sheetId: this.sheetId,
+      dimension: "COL",
+      elements: hiddenElements,
+    });
+  }
+
+  isVisible(header: HeaderIndex) {
+    return header >= this.props.headerRange.start && header <= this.props.headerRange.end;
+  }
+}
