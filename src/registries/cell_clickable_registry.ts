002b68545ba8c924f71bde383d83f2f27f8f9b63
Lucas Lefèvre (lul) <lul@odoo.com>
2025-03-05 16:44:32 +0100

    [IMP] dashboard: cells are alive!
    Currently, dashboards feel like static HTML pages from the early 2000s.
    Make dashboard feeling alive by adding hover effects.
    
    Task: 4630549

--- src/registries/cell_clickable_registry.ts
+++ src/registries/cell_clickable_registry.ts
@@ -1,12 +1,18 @@
 import { openLink } from "../helpers/links";
-import { CellPosition, Getters, SpreadsheetChildEnv } from "../types";
+import { CellPosition, Getters, SpreadsheetChildEnv, Style, Zone } from "../types";
 import { Registry } from "./registry";
 
 export interface CellClickableItem {
   condition: (position: CellPosition, getters: Getters) => boolean;
   execute: (position: CellPosition, env: SpreadsheetChildEnv, isMiddleClick?: boolean) => void;
+  hoverStyle?: (position: CellPosition, getters: Getters) => ClickableCellHoverStyle[];
   title?: string;
   sequence: number;
+}
+
+interface ClickableCellHoverStyle {
+  zone: Zone;
+  style: Style;
 }
 
 export const clickableCellRegistry = new Registry<CellClickableItem>();
