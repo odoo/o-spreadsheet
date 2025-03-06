a9bd9745ab11507e1b462c1a9e96e6406071d483
Adrien Minne (adrm) <adrm@odoo.com>
2025-03-06 10:37:30 +0100

    [FIX] chart: improve chartjs extension robustness
    This commit makes it so our ChartJS extensions work even if the chartJS
    library was reloaded between the moment where we loaded the
    `o_spreadsheet` library and the moment where we actually use the
    extensions.
    
    closes odoo/o-spreadsheet#5839
    
    Task: 4627232
    Signed-off-by: Lucas Lefèvre (lul) <lul@odoo.com>
    Signed-off-by: Adrien Minne (adrm) <adrm@odoo.com>

--- src/helpers/figures/charts/chart_ui_common.ts
+++ src/helpers/figures/charts/chart_ui_common.ts
@@ -1,6 +1,8 @@
 import type { BasePlatform, ChartConfiguration, ChartOptions, ChartType } from "chart.js";
 import { LegendOptions } from "chart.js";
 import { DeepPartial } from "chart.js/dist/types/utils";
+import { chartShowValuesPlugin } from "../../../components/figures/chart/chartJs/chartjs_show_values_plugin";
+import { waterfallLinesPlugin } from "../../../components/figures/chart/chartJs/chartjs_waterfall_plugin";
 import { ChartTerms } from "../../../components/translations_terms";
 import { DEFAULT_CHART_FONT_SIZE, DEFAULT_CHART_PADDING, MAX_CHAR_LABEL } from "../../../constants";
 import { isEvaluationError } from "../../../functions/helpers";
@@ -340,7 +342,8 @@ export function chartToImage(
   if ("chartJsConfig" in runtime) {
     const config = deepCopy(runtime.chartJsConfig);
     config.plugins = [backgroundColorChartJSPlugin];
-    const chart = new window.Chart(canvas, config);
+    const Chart = getChartJSConstructor();
+    const chart = new Chart(canvas, config);
     const imgContent = chart.toBase64Image() as string;
     chart.destroy();
     div.remove();
@@ -392,4 +395,13 @@ export function getChartJsLegend(
       ...legend.labels,
     },
   };
+}
+
+/** Return window.Chart, making sure all our extensions are loaded in ChartJS */
+export function getChartJSConstructor() {
+  if (window.Chart && !window.Chart?.registry.plugins.get("chartShowValuesPlugin")) {
+    window.Chart.register(chartShowValuesPlugin);
+    window.Chart.register(waterfallLinesPlugin);
+  }
+  return window.Chart;
 }
