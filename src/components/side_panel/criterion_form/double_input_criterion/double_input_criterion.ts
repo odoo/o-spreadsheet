72e6aac58a6b3daa1733e59f0a8507692cc6432a
Adrien Minne (adrm) <adrm@odoo.com>
2025-03-12 09:33:04 +0100

    [REF] data validation: make criterion component names more generic
    This commit rename all the `data_validation_criterion` components to
    `criterion` components, since they will be used for more than just data
    validation.
    
    Task: 4592118

--- src/components/side_panel/criterion_form/double_input_criterion/double_input_criterion.ts
+++ src/components/side_panel/criterion_form/double_input_criterion/double_input_criterion.ts
@@ -1,9 +1,9 @@
-import { DataValidationCriterionForm } from "../criterion_form";
-import { DataValidationInput } from "../criterion_input/criterion_input";
+import { CriterionForm } from "../criterion_form";
+import { CriterionInput } from "../criterion_input/criterion_input";
 
-export class DataValidationDoubleInputCriterionForm extends DataValidationCriterionForm {
-  static template = "o-spreadsheet-DataValidationDoubleInput";
-  static components = { DataValidationInput };
+export class DoubleInputCriterionForm extends CriterionForm {
+  static template = "o-spreadsheet-DoubleInputCriterionForm";
+  static components = { CriterionInput };
 
   onFirstValueChanged(value: string) {
     const values = this.props.criterion.values;
