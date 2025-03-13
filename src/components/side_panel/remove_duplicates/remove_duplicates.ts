3094a79cd081b7952169787ae763f8c646371ba3
Rémi Rahir (rar) <rar@odoo.com>
2025-03-13 11:42:46 +0100

    [FIX] RemoveDuplicate: fix component initialization
    The `RemoveDuplicate` component was never correctly initialized but it
    was hidden at runtime because opening the sidepanel triggers a full
    rendering of `Spreadsheet` which in turn fixed the internal state of
    `RemoveDuplicate`.
    
    Task: 4646342

--- src/components/side_panel/remove_duplicates/remove_duplicates.ts
+++ src/components/side_panel/remove_duplicates/remove_duplicates.ts
@@ -1,4 +1,4 @@
-import { Component, onMounted, onWillUpdateProps, useState } from "@odoo/owl";
+import { Component, onWillUpdateProps, useState } from "@odoo/owl";
 import { numberToLetters, zoneToDimension } from "../../../helpers";
 import { _t } from "../../../translation";
 import { HeaderIndex, SpreadsheetChildEnv } from "../../../types/index";
@@ -33,7 +33,7 @@ export class RemoveDuplicatesPanel extends Component<Props, SpreadsheetChildEnv>
   });
 
   setup() {
-    onMounted(() => this.updateColumns());
+    this.updateColumns();
     onWillUpdateProps(() => this.updateColumns());
   }
 
