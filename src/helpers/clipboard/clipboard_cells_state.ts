854cdfa6a7acf7cb0e1830130ce3eba0bff3d1cb
Adrien Minne (adrm) <adrm@odoo.com>
2025-03-14 09:10:21 +0000

    [FIX] clipboard: cross-sheet cut/paste is broken for merges
    Preface: the handling of `MOVE_RANGES` is broken in multiple plugins.
    When calling `adaptRanges`, we don't check that the resulting range
    is in the same sheet as the original range.
    
    Fixing that in stable is probably not a good idea. This would mean that
    suddenly cfs/merges could appear where they were previously
    not, and could break existing sheets.
    
    The handling of copy/paste of merges is done either in `pasteMergeIfExist`
    (for copy/paste) or by `MOVE_RANGES` (for cut/paste). The latter
    is broken.
    
    This commit fixes that by handling all the copy/cut cases of the merges
    in `pasteMergeIfExist` rather than relying on `MOVE_RANGES`. The
    `MOVE_RANGES` command is still dispatched, but it now do not affect
    merges since they are already deleted from their original location.
    
    Task: 3905618
    X-original-commit: eed70cb8914ae14a8e437c454f4eed05d47ee618

--- src/helpers/clipboard/clipboard_cells_state.ts
+++ src/helpers/clipboard/clipboard_cells_state.ts
@@ -349,12 +349,7 @@ export class ClipboardCellsState extends ClipboardCellsAbstractState {
           continue;
         }
         const position = { col: col + c, row: row + r, sheetId: sheetId };
-        // TODO: refactor this part. the "Paste merge" action is also executed with
-        // MOVE_RANGES in pasteFromCut. Adding a condition on the operation type here
-        // is not appropriate
-        if (this.operation !== "CUT") {
-          this.pasteMergeIfExist(origin.position, position);
-        }
+        this.pasteMergeIfExist(origin.position, position, this.operation);
         this.pasteCell(origin, position, this.operation, clipboardOptions);
         if (shouldPasteCF) {
           this.pasteCf(origin.position, position);
@@ -427,7 +422,11 @@ export class ClipboardCellsState extends ClipboardCellsAbstractState {
    * If the origin position given is the top left of a merge, merge the target
    * position.
    */
-  private pasteMergeIfExist(origin: CellPosition, target: CellPosition) {
+  private pasteMergeIfExist(
+    origin: CellPosition,
+    target: CellPosition,
+    operation: ClipboardOperation
+  ) {
     let { sheetId, col, row } = origin;
 
     const { col: mainCellColOrigin, row: mainCellRowOrigin } =
@@ -436,6 +435,9 @@ export class ClipboardCellsState extends ClipboardCellsAbstractState {
       const merge = this.getters.getMerge(origin);
       if (!merge) {
         return;
+      }
+      if (operation === "CUT") {
+        this.dispatch("REMOVE_MERGE", { sheetId, target: [merge] });
       }
       ({ sheetId, col, row } = target);
       this.dispatch("ADD_MERGE", {
