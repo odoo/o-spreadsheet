odoo/o-spreadsheet

commit 48d7a15dae5ede5c9e662d7099e248177e05404d

[IMP] BottomBar: Add responsiveness
Add specific design/behaviour for the bottom bar on small screens.

closes odoo/o-spreadsheet#5717

Task: 4562085
Signed-off-by: Lucas Lefèvre (lul) <lul@odoo.com>

src/components/top_bar/top_bar.ts
@@ -63,10 +63,12 @@ css/* scss */ `
     font-size: 13px;
     font-weight: 500;
     background-color: #fff;
+    width: inherit;
 
     .o-topbar-top {
       border-bottom: 1px solid ${SEPARATOR_COLOR};
       padding: 2px 10px;
+      overflow: hidden;
 
       /* Menus */
       .o-topbar-topleft {

src/components/top_bar/top_bar.xml
@@ -1,11 +1,9 @@
 <templates>
   <t t-name="o-spreadsheet-TopBar">
-    <t t-set="text_color">Text Color</t>
-    <t t-set="fill_color">Fill Color</t>
     <div
-      class="o-spreadsheet-topbar o-two-columns d-flex flex-column user-select-none"
+      class="o-spreadsheet-topbar o-two-columns d-flex flex-column user-select-none overflow-hidden"
       t-on-click="props.onClick">
-      <div class="o-topbar-top d-flex justify-content-between  flex-wrap">
+      <div class="o-topbar-top d-flex justify-content-between">
         <!-- Menus -->
         <div class="o-topbar-topleft d-flex">
           <t t-foreach="menus" t-as="menu" t-key="menu_index">

tests/__snapshots__/top_bar_component.test.ts.snap
@@ -6,10 +6,10 @@ exports[`TopBar component can set cell format 1`] = `
     class="o-spreadsheet"
   >
     <div
-      class="o-spreadsheet-topbar o-two-columns d-flex flex-column user-select-none"
+      class="o-spreadsheet-topbar o-two-columns d-flex flex-column user-select-none overflow-hidden"
     >
       <div
-        class="o-topbar-top d-flex justify-content-between  flex-wrap"
+        class="o-topbar-top d-flex justify-content-between"
       >
         <!-- Menus -->
         <div
@@ -1115,10 +1115,10 @@ exports[`TopBar component can set cell format 1`] = `
 
 exports[`TopBar component simple rendering 1`] = `
 <div
-  class="o-spreadsheet-topbar o-two-columns d-flex flex-column user-select-none"
+  class="o-spreadsheet-topbar o-two-columns d-flex flex-column user-select-none overflow-hidden"
 >
   <div
-    class="o-topbar-top d-flex justify-content-between  flex-wrap"
+    class="o-topbar-top d-flex justify-content-between"
   >
     <!-- Menus -->
     <div

demo/main.js
@@ -320,7 +320,7 @@ Demo.template = xml/* xml */ `
       <Spreadsheet model="model" notifyUser="notifyUser" t-key="state.key"/>
     </div>
   </div>
-  <div t-else="" class="vh-100">
+  <div t-else="" class="vh-100 vw-100">
     <Spreadsheet model="model" t-key="state.key" notifyUser="notifyUser"/>
   </div>
 `;

src/components/animation/ripple.ts
@@ -34,7 +34,7 @@ interface RippleProps {
   allowOverflow?: boolean;
   enabled: boolean;
   onAnimationEnd: () => void;
-  class: string;
+  class?: string;
 }
 
 interface RippleEffectProps
@@ -74,6 +74,7 @@ class RippleEffect extends Component<RippleEffectProps, SpreadsheetChildEnv> {
     onAnimationEnd: Function,
     style: String,
   };
+
   private rippleRef = useRef("ripple");
 
   setup() {

src/components/bottom_bar/bottom_bar.ts
@@ -38,6 +38,7 @@ css/* scss */ `
       }
 
       .o-bottom-bar-fade-in {
+        z-index: 1;
         background-image: linear-gradient(90deg, #cfcfcf, transparent 1%);
       }
 
@@ -68,6 +69,22 @@ css/* scss */ `
           font-size: 18px;
         }
       }
+    }
+  }
+
+  .mobile.o-spreadsheet-bottom-bar {
+    padding-left: 1rem;
+
+    .add-sheet-container {
+      order: 2;
+    }
+
+    .o-add-sheet {
+      margin-left: 0.5rem;
+    }
+
+    .o-all-sheets {
+      max-width: none;
     }
   }
 `;

src/components/bottom_bar/bottom_bar.xml
@@ -2,10 +2,11 @@
   <t t-name="o-spreadsheet-BottomBar">
     <div
       class="o-spreadsheet-bottom-bar o-two-columns d-flex align-items-center overflow-hidden"
+      t-att-class="{'mobile': env.isSmall}"
       t-on-click="props.onClick"
       t-ref="bottomBar"
       t-on-contextmenu.prevent="">
-      <Ripple>
+      <Ripple class="'add-sheet-container'">
         <div
           class="o-sheet-item o-add-sheet me-2 p-1"
           t-if="!env.model.getters.isReadonly()"
@@ -18,7 +19,7 @@
           <t t-call="o-spreadsheet-Icon.LIST"/>
         </div>
       </Ripple>
-      <div class="o-all-sheets position-relative flex-shrink-0 d-flex h-100 me-3">
+      <div class="o-all-sheets position-relative d-flex h-100 flex-fill overflow-hidden">
         <div
           class="o-bottom-bar-fade-in position-absolute h-100 w-100 pe-none"
           t-if="state.isSheetListScrollableLeft"
@@ -42,43 +43,44 @@
           t-if="state.isSheetListScrollableRight"
         />
       </div>
-      <div
-        class="o-bottom-bar-arrows d-flex h-100 me-5 align-items-center"
-        t-if="state.isSheetListScrollableLeft || state.isSheetListScrollableRight">
-        <Ripple
-          ignoreClickPosition="true"
-          width="20"
-          height="20"
-          offsetX="1"
-          allowOverflow="true"
-          enabled="state.isSheetListScrollableLeft">
-          <div
-            class="o-bottom-bar-arrow o-bottom-bar-arrow-left d-flex align-items-center me-2"
-            t-att-class="{'o-disabled': !state.isSheetListScrollableLeft}"
-            t-on-click="onArrowLeft">
-            <t t-call="o-spreadsheet-Icon.CARET_LEFT"/>
-          </div>
-        </Ripple>
-        <Ripple
-          ignoreClickPosition="true"
-          width="20"
-          height="20"
-          offsetX="-1"
-          allowOverflow="true"
-          enabled="state.isSheetListScrollableRight">
-          <div
-            class="o-bottom-bar-arrow o-bottom-bar-arrow-right d-flex align-items-center me-4"
-            t-att-class="{'o-disabled': !state.isSheetListScrollableRight}"
-            t-on-click="onArrowRight">
-            <t t-call="o-spreadsheet-Icon.CARET_RIGHT"/>
-          </div>
-        </Ripple>
-      </div>
-
-      <BottomBarStatistic
-        openContextMenu="(x, y, registry) => this.openContextMenu(x, y, 'listSelectionStatistics', registry)"
-        closeContextMenu="() => this.closeContextMenuWithId('listSelectionStatistics')"
-      />
+      <t t-if="!env.isSmall">
+        <div
+          class="o-bottom-bar-arrows d-flex h-100 me-4 align-items-center ms-3"
+          t-if="state.isSheetListScrollableLeft || state.isSheetListScrollableRight">
+          <Ripple
+            ignoreClickPosition="true"
+            width="20"
+            height="20"
+            offsetX="1"
+            allowOverflow="true"
+            enabled="state.isSheetListScrollableLeft">
+            <div
+              class="o-bottom-bar-arrow o-bottom-bar-arrow-left d-flex align-items-center"
+              t-att-class="{'o-disabled': !state.isSheetListScrollableLeft}"
+              t-on-click="onArrowLeft">
+              <t t-call="o-spreadsheet-Icon.CARET_LEFT"/>
+            </div>
+          </Ripple>
+          <Ripple
+            ignoreClickPosition="true"
+            width="20"
+            height="20"
+            offsetX="-1"
+            allowOverflow="true"
+            enabled="state.isSheetListScrollableRight">
+            <div
+              class="o-bottom-bar-arrow o-bottom-bar-arrow-right d-flex align-items-center"
+              t-att-class="{'o-disabled': !state.isSheetListScrollableRight}"
+              t-on-click="onArrowRight">
+              <t t-call="o-spreadsheet-Icon.CARET_RIGHT"/>
+            </div>
+          </Ripple>
+        </div>
+        <BottomBarStatistic
+          openContextMenu="(x, y, registry) => this.openContextMenu(x, y, 'listSelectionStatistics', registry)"
+          closeContextMenu="() => this.closeContextMenuWithId('listSelectionStatistics')"
+        />
+      </t>
 
       <Menu
         t-if="menuState.isOpen"

commit c0756e3aa6fdcc57fe95c67ce506de0d3967ec6b

[REV] topbar: remove overflow hidden
Commit 48d7a15dae mistakenly added `overflow: hidden` to the topbar.
That breaks the formula assistant. This commit removes it.

Task: 0

src/components/top_bar/top_bar.ts
@@ -63,12 +63,10 @@ css/* scss */ `
     font-size: 13px;
     font-weight: 500;
     background-color: #fff;
-    width: inherit;
 
     .o-topbar-top {
       border-bottom: 1px solid ${SEPARATOR_COLOR};
       padding: 2px 10px;
-      overflow: hidden;
 
       /* Menus */
       .o-topbar-topleft {

src/components/top_bar/top_bar.xml
@@ -1,7 +1,7 @@
 <templates>
   <t t-name="o-spreadsheet-TopBar">
     <div
-      class="o-spreadsheet-topbar o-two-columns d-flex flex-column user-select-none overflow-hidden"
+      class="o-spreadsheet-topbar o-two-columns d-flex flex-column user-select-none"
       t-on-click="props.onClick">
       <div class="o-topbar-top d-flex justify-content-between">
         <!-- Menus -->

tests/__snapshots__/top_bar_component.test.ts.snap
@@ -6,7 +6,7 @@ exports[`TopBar component can set cell format 1`] = `
     class="o-spreadsheet"
   >
     <div
-      class="o-spreadsheet-topbar o-two-columns d-flex flex-column user-select-none overflow-hidden"
+      class="o-spreadsheet-topbar o-two-columns d-flex flex-column user-select-none"
     >
       <div
         class="o-topbar-top d-flex justify-content-between"
@@ -1115,7 +1115,7 @@ exports[`TopBar component can set cell format 1`] = `
 
 exports[`TopBar component simple rendering 1`] = `
 <div
-  class="o-spreadsheet-topbar o-two-columns d-flex flex-column user-select-none overflow-hidden"
+  class="o-spreadsheet-topbar o-two-columns d-flex flex-column user-select-none"
 >
   <div
     class="o-topbar-top d-flex justify-content-between"

tests/spreadsheet/__snapshots__/spreadsheet_component.test.ts.snap
@@ -7,7 +7,7 @@ exports[`Simple Spreadsheet Component simple rendering snapshot 1`] = `
 >
   
   <div
-    class="o-spreadsheet-topbar o-two-columns d-flex flex-column user-select-none overflow-hidden"
+    class="o-spreadsheet-topbar o-two-columns d-flex flex-column user-select-none"
   >
     <div
       class="o-topbar-top d-flex justify-content-between"
@@ -1011,7 +1011,7 @@ exports[`components take the small screen into account 1`] = `
 >
   
   <div
-    class="o-spreadsheet-topbar o-two-columns d-flex flex-column user-select-none overflow-hidden"
+    class="o-spreadsheet-topbar o-two-columns d-flex flex-column user-select-none"
   >
     <div
       class="o-topbar-top d-flex justify-content-between"