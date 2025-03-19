f028f90cad406d6fbd763d72413f81818fb41aaf
Mehdi Rachico (mera) <mera@odoo.com>
2025-03-19 13:38:25 +0000

    [FIX] xlsx: prevent unwanted legend when importing chart from excel
    When creating an XLSX file with a chart that has no legend, the legend
    unexpectedly appears after importing the file. This issue occurs due to
    the default behavior of the import process, which adds a bottom legend
    even if it was not initially set.
    
    Task: 4632987
    X-original-commit: e987da3aa6036d29995d9ec4e5dabba2062a31d6

--- tests/xlsx/xlsx_import.test.ts
+++ tests/xlsx/xlsx_import.test.ts
@@ -816,6 +816,20 @@ describe("Import xlsx data", () => {
     expect(figure.width).toEqual(figure.height);
   });
 
+  test.each([
+    ["line chart", "top"],
+    ["bar chart", "right"],
+    ["scatter chart", "bottom"],
+    ["scatter chart with textual labels", "left"],
+    ["combo chart", "none"],
+  ])("Can import %s charts with correct legend position", (chartTitle, chartLegendPosition) => {
+    const testSheet = getWorkbookSheet("jestCharts", convertedData)!;
+    const figure = testSheet.figures.find((figure) => figure.data.title.text === chartTitle)!;
+    const chartData = figure.data as BarChartDefinition | ComboChartDefinition;
+    expect(chartData.title.text).toEqual(chartTitle);
+    expect(chartData.legendPosition).toEqual(chartLegendPosition);
+  });
+
   describe("Misc tests", () => {
     test("Newlines characters in strings are preserved", () => {
       const testSheet = getWorkbookSheet("jestMiscTest", convertedData)!;
