e987da3aa6036d29995d9ec4e5dabba2062a31d6
Mehdi Rachico (mera) <mera@odoo.com>
2025-03-19 14:38:25 +0100

    [FIX] xlsx: prevent unwanted legend when importing chart from excel
    When creating an XLSX file with a chart that has no legend, the legend
    unexpectedly appears after importing the file. This issue occurs due to
    the default behavior of the import process, which adds a bottom legend
    even if it was not initially set.
    
    closes odoo/o-spreadsheet#5972
    
    Task: 4632987
    Signed-off-by: Adrien Minne (adrm) <adrm@odoo.com>

--- tests/xlsx_import.test.ts
+++ tests/xlsx_import.test.ts
@@ -756,6 +756,19 @@ describe("Import xlsx data", () => {
     }
   });
 
+  test.each([
+    ["line chart", "top"],
+    ["bar chart", "right"],
+    ["doughnut chart", "bottom"],
+    ["pie chart", "none"],
+  ])("Can import %s charts with correct legend position", (chartTitle, chartLegendPosition) => {
+    const testSheet = getWorkbookSheet("jestCharts", convertedData)!;
+    const figure = testSheet.figures.find((figure) => figure.data.title === chartTitle)!;
+    const chartData = figure.data as BarChartDefinition;
+    expect(chartData.title).toEqual(chartTitle);
+    expect(chartData.legendPosition).toEqual(chartLegendPosition);
+  });
+
   describe("Misc tests", () => {
     test("Newlines characters in strings are removed", () => {
       const testSheet = getWorkbookSheet("jestMiscTest", convertedData)!;
