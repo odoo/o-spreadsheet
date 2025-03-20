b68da99ad1423c802f7bfc404eac21c94d924095
Mehdi Rachico (mera) <mera@odoo.com>
2025-03-20 13:55:13 +0000

    [FIX] composer: prevent autocomplete on unknown characters in composer
    Before this commit, the composer incorrectly triggered autocomplete suggestions
    even when unknown or special characters (e.g., "é") were typed. This led to irrelevant
    suggestions and formula errors when selecting an option.
    
    This commit ensures that unknown characters are properly considered, preventing autocomplete
    from being triggered in such cases, improving the reliability of the function autocomplete.
    
    closes odoo/o-spreadsheet#6149
    
    Task: 4652661
    X-original-commit: c8e7bf5552b57e1c2a0e7e861b94768845bf9a21
    Signed-off-by: Lucas Lefèvre (lul) <lul@odoo.com>
    Signed-off-by: Mehdi Rachico (mera) <mera@odoo.com>

--- src/formulas/tokenizer.ts
+++ src/formulas/tokenizer.ts
@@ -154,6 +154,12 @@ function tokenizeString(chars: TokenizingChars): Token | null {
   return null;
 }
 
+/**
+  - \p{L} is for any letter (from any language)
+  - \p{N} is for any number
+  - the u flag at the end is for unicode, which enables the `\p{...}` syntax
+ */
+const unicodeSymbolCharRegexp = /\p{L}|\p{N}|_|\.|!|\$/u;
 const SYMBOL_CHARS = new Set("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_.!$");
 
 /**
@@ -195,7 +201,10 @@ function tokenizeSymbol(chars: TokenizingChars): Token | null {
       };
     }
   }
-  while (chars.current && SYMBOL_CHARS.has(chars.current)) {
+  while (
+    chars.current &&
+    (SYMBOL_CHARS.has(chars.current) || chars.current.match(unicodeSymbolCharRegexp))
+  ) {
     result += chars.shift();
   }
   if (result.length) {
