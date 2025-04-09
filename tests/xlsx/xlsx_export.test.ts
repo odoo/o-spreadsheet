617d7c657b85aec9218e5b016794da79f3b92a9c
Adrien Minne (adrm) <adrm@odoo.com>
2025-04-09 07:09:05 +0000

    [FIX] xlsx: correctly export aggregated charts
    The feature to aggregate the chart data based on the labels does not
    exist in xlsx files. But rather than exporting the chart as normal,
    ignoring the aggregation, we exported the chart as an image.
    
    closes odoo/o-spreadsheet#6242
    
    Task: 4714410
    X-original-commit: 2ed572676d003be9b3b13a672948656deb5483d3
    Signed-off-by: Rémi Rahir (rar) <rar@odoo.com>
    Signed-off-by: Adrien Minne (adrm) <adrm@odoo.com>

--- tests/xlsx/xlsx_export.test.ts
+++ tests/xlsx/xlsx_export.test.ts
@@ -4,7 +4,7 @@ import { RAND, RANDARRAY, RANDBETWEEN } from "../../src/functions/module_math";
 import { buildSheetLink, toXC } from "../../src/helpers";
 import { DEFAULT_TABLE_CONFIG } from "../../src/helpers/table_presets";
 import { Model } from "../../src/model";
-import { CustomizedDataSet, Dimension, ExcelChartType } from "../../src/types";
+import { CustomizedDataSet, Dimension } from "../../src/types";
 import { XLSXExportXMLFile, XMLString } from "../../src/types/xlsx";
 import { hexaToInt } from "../../src/xlsx/conversion";
 import { adaptFormulaToExcel } from "../../src/xlsx/functions/cells";
@@ -1285,43 +1285,14 @@ describe("Test XLSX export", () => {
       expect(await exportPrettifiedXlsx(model)).toMatchSnapshot();
     });
 
-    test.each(["bar", "line", "pie", "scatter", "radar"] as const)(
-      "%s chart that aggregate labels is exported as image",
-      async (type: ExcelChartType) => {
-        const model = new Model({
-          sheets: [
-            {
-              ...chartData.sheets,
-              cells: {
-                ...chartData.sheets[0].cells,
-                A6: "P1",
-                A7: "P2",
-                A8: "P3",
-                A9: "P4",
-                B6: "17",
-                B7: "26",
-                B8: "13",
-                B9: "31",
-                C6: "31",
-                C7: "18",
-                C8: "9",
-                C9: "27",
-              },
-            },
-          ],
-        });
-        createChart(
-          model,
-          {
-            dataSets: [{ dataRange: "Sheet1!B1:B9" }],
-            labelRange: "Sheet1!A2:A9",
-            aggregated: true,
-            type,
-          },
-          "1"
-        );
-        expect(getExportedExcelData(model).sheets[0].charts.length).toBe(0);
-        expect(getExportedExcelData(model).sheets[0].images.length).toBe(1);
+    test.each(["bar", "line", "pie"] as const)(
+      "%s chart that aggregate labels is exported as normal chart, ignoring the aggregation",
+      async (type: "bar" | "line" | "pie") => {
+        const model = new Model();
+        createChart(model, { aggregated: true, type }, "1");
+        const exportedData = getExportedExcelData(model);
+        expect(exportedData.sheets[0].charts.length).toBe(1);
+        expect(exportedData.sheets[0].images.length).toBe(0);
       }
     );
 
