72012b9e3197323c95133f49a8cba7c804d2eb59
Florian Damhaut <flda@odoo.com>
2025-04-10 08:46:56 +0000

    [FIX] sheet: make sheetName comparison case insensitive
    As sheetName are case insensitive, the comparison should
    too.
    
    Task: 4707398
    X-original-commit: 7f60d915dd498d160a3c2e2e06c38bf66082c389

--- src/components/composer/composer/abstract_composer_store.ts
+++ src/components/composer/composer/abstract_composer_store.ts
@@ -8,6 +8,7 @@ import {
   getZoneArea,
   isEqual,
   isNumber,
+  isSheetNameEqual,
   positionToZone,
   splitReference,
   zoneToDimension,
@@ -459,7 +460,7 @@ export abstract class AbstractComposerStore extends SpreadsheetStore {
         const { xc, sheetName: sheet } = splitReference(token.value);
         const sheetName = sheet || this.getters.getSheetName(this.sheetId);
 
-        if (this.getters.getSheetName(activeSheetId) !== sheetName) {
+        if (!isSheetNameEqual(this.getters.getSheetName(activeSheetId), sheetName)) {
           return false;
         }
         const refRange = this.getters.getRangeFromSheetXC(activeSheetId, xc);
--- src/plugins/core/range.ts
+++ src/plugins/core/range.ts
@@ -16,6 +16,7 @@ import {
   toUnboundedZone,
   unionUnboundedZones,
 } from "../../helpers/index";
+import { isSheetNameEqual } from "../../helpers/sheet";
 import { CellErrorType } from "../../types/errors";
 import {
   ApplyRangeChange,
@@ -185,7 +186,7 @@ export class RangeAdapter implements CommandHandler<CoreCommand> {
           if (range.sheetId === cmd.sheetId) {
             return { changeType: "CHANGE", range };
           }
-          if (cmd.name && range.invalidSheetName === cmd.name) {
+          if (isSheetNameEqual(range.invalidSheetName, cmd.name)) {
             const invalidSheetName = undefined;
             const sheetId = cmd.sheetId;
             const newRange = range.clone({ sheetId, invalidSheetName });
