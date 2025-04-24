4cf7f95d55a7f051f6117de4c0940e339db26412
Alexis Lacroix <laa@odoo.com>
2025-04-24 09:34:32 +0000

    [PERF] formula functions: avoid to use errors derived from native error.
    Errors are now generated solely based on the value stored in the object,
    without relying on an inherited error type.
    
    On an error-heavy sheet, this reduces computation time by 80% compared
    to evaluateAllCells.
    
    Task: 4671645
    X-original-commit: 984a70cce4f15a8f4ff8e283f677c6163cd9b34d
    Part-of: odoo/o-spreadsheet#6184
    Signed-off-by: Lucas Lefèvre (lul) <lul@odoo.com>

--- src/types/errors.ts
+++ src/types/errors.ts
@@ -14,9 +14,12 @@ export const CellErrorType = {
 
 export const errorTypes: Set<string> = new Set(Object.values(CellErrorType));
 
-export class EvaluationError extends Error {
-  constructor(message = _t("Error"), readonly value: string = CellErrorType.GenericError) {
-    super(message);
+export class EvaluationError {
+  constructor(
+    readonly message: string = _t("Error"),
+    readonly value: string = CellErrorType.GenericError
+  ) {
+    this.message = message.toString();
   }
 }
 
