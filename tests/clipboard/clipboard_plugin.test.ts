odoo/o-spreadsheet

commit 816977d7b8a5bc4ba048aa46b844f5e00636a58e

[IMP] composer: preview formula expressions result
This commit adds a preview of the formula expression result in the
composer when hovering over a formula token. We can hover any part of
the formula (a range, a primitive, a sub-formula, etc.) and see the
result of the expression in a speech bubble. This allows for easy
debugging of complex formulas.

One issue faced is that is that if we try to change the composer <span>
content/classes while selecting text with the mouse, the selection
will look very buggy. It also happens if we keep the same spans and
only change their classes. To avoid this issue, we'll simply disable
the feature when the DOM selection start is not the same as the end.

closes odoo/o-spreadsheet#5575

Task: 4471274
Signed-off-by: Lucas Lefèvre (lul) <lul@odoo.com>

src/formulas/parser.ts
@@ -12,11 +12,40 @@ const UNARY_OPERATORS_POSTFIX = ["%"];
 
 const ASSOCIATIVE_OPERATORS = ["*", "+", "&"];
 
+interface RichToken extends Token {
+  tokenIndex: number;
+}
+
+export class TokenList {
+  private tokens: RichToken[];
+  currentIndex: number = 0;
+  current: RichToken | undefined;
+  length: number;
+
+  constructor(tokens: RichToken[]) {
+    this.tokens = tokens;
+    this.current = tokens[0];
+    this.length = tokens.length;
+  }
+
+  shift() {
+    const current = this.tokens[this.currentIndex];
+    this.current = this.tokens[++this.currentIndex];
+    return current;
+  }
+
+  get next(): RichToken | undefined {
+    return this.tokens[this.currentIndex + 1];
+  }
+}
+
 // -----------------------------------------------------------------------------
 // PARSER
 // -----------------------------------------------------------------------------
 interface ASTBase {
   debug?: boolean;
+  tokenStartIndex: number;
+  tokenEndIndex: number;
 }
 
 interface ASTNumber extends ASTBase {
@@ -103,7 +132,7 @@ const OP_PRIORITY = {
  *  for (1+2)*3, the next operand is (1+2)
  *  for SUM(1,2)+3, the next operand is SUM(1,2)
  */
-function parseOperand(tokens: Token[]): AST {
+function parseOperand(tokens: TokenList): AST {
   const current = tokens.shift();
   if (!current) {
     throw new BadExpressionError();
@@ -114,55 +143,94 @@ function parseOperand(tokens: Token[]): AST {
       next.debug = true;
       return next;
     case "NUMBER":
-      return { type: "NUMBER", value: parseNumber(current.value, DEFAULT_LOCALE) };
+      return {
+        type: "NUMBER",
+        value: parseNumber(current.value, DEFAULT_LOCALE),
+        tokenStartIndex: current.tokenIndex,
+        tokenEndIndex: current.tokenIndex,
+      };
     case "STRING":
-      return { type: "STRING", value: removeStringQuotes(current.value) };
+      return {
+        type: "STRING",
+        value: removeStringQuotes(current.value),
+        tokenStartIndex: current.tokenIndex,
+        tokenEndIndex: current.tokenIndex,
+      };
     case "INVALID_REFERENCE":
       return {
         type: "REFERENCE",
         value: CellErrorType.InvalidReference,
+        tokenStartIndex: current.tokenIndex,
+        tokenEndIndex: current.tokenIndex,
       };
 
     case "REFERENCE":
-      if (tokens[0]?.value === ":" && tokens[1]?.type === "REFERENCE") {
+      if (tokens.current?.value === ":" && tokens.next?.type === "REFERENCE") {
         tokens.shift();
         const rightReference = tokens.shift();
         return {
           type: "REFERENCE",
           value: `${current.value}:${rightReference?.value}`,
+          tokenStartIndex: current.tokenIndex,
+          tokenEndIndex: rightReference.tokenIndex,
         };
       }
       return {
         type: "REFERENCE",
         value: current.value,
+        tokenStartIndex: current.tokenIndex,
+        tokenEndIndex: current.tokenIndex,
       };
     case "SYMBOL":
       const value = current.value;
-      const nextToken = tokens[0];
+      const nextToken = tokens.current;
       if (
         nextToken?.type === "LEFT_PAREN" &&
         functionRegex.test(current.value) &&
         value === unquote(value, "'")
       ) {
-        const args = parseFunctionArgs(tokens);
-        return { type: "FUNCALL", value: value, args };
+        const { args, rightParen } = parseFunctionArgs(tokens);
+        return {
+          type: "FUNCALL",
+          value: value,
+          args,
+          tokenStartIndex: current.tokenIndex,
+          tokenEndIndex: rightParen.tokenIndex,
+        };
       }
       const upperCaseValue = value.toUpperCase();
       if (upperCaseValue === "TRUE" || upperCaseValue === "FALSE") {
-        return { type: "BOOLEAN", value: upperCaseValue === "TRUE" };
+        return {
+          type: "BOOLEAN",
+          value: upperCaseValue === "TRUE",
+          tokenStartIndex: current.tokenIndex,
+          tokenEndIndex: current.tokenIndex,
+        };
       }
-      return { type: "SYMBOL", value: unquote(current.value, "'") };
+      return {
+        type: "SYMBOL",
+        value: unquote(current.value, "'"),
+        tokenStartIndex: current.tokenIndex,
+        tokenEndIndex: current.tokenIndex,
+      };
     case "LEFT_PAREN":
       const result = parseExpression(tokens);
-      consumeOrThrow(tokens, "RIGHT_PAREN", _t("Missing closing parenthesis"));
-      return result;
+      const rightParen = consumeOrThrow(tokens, "RIGHT_PAREN", _t("Missing closing parenthesis"));
+      return {
+        ...result,
+        tokenStartIndex: current.tokenIndex,
+        tokenEndIndex: rightParen.tokenIndex,
+      };
     case "OPERATOR":
       const operator = current.value;
       if (UNARY_OPERATORS_PREFIX.includes(operator)) {
+        const operand = parseExpression(tokens, OP_PRIORITY[operator]);
         return {
           type: "UNARY_OPERATION",
           value: operator,
-          operand: parseExpression(tokens, OP_PRIORITY[operator]),
+          operand,
+          tokenStartIndex: current.tokenIndex,
+          tokenEndIndex: operand.tokenEndIndex,
         };
       }
       throw new BadExpressionError(_t("Unexpected token: %s", current.value));
@@ -171,54 +239,66 @@ function parseOperand(tokens: Token[]): AST {
   }
 }
 
-function parseFunctionArgs(tokens: Token[]): AST[] {
+function parseFunctionArgs(tokens: TokenList) {
   consumeOrThrow(tokens, "LEFT_PAREN", _t("Missing opening parenthesis"));
-  const nextToken = tokens[0];
+  const nextToken = tokens.current;
   if (nextToken?.type === "RIGHT_PAREN") {
-    consumeOrThrow(tokens, "RIGHT_PAREN");
-    return [];
+    const rightParen = consumeOrThrow(tokens, "RIGHT_PAREN");
+    return { args: [], rightParen };
   }
   const args: AST[] = [];
   args.push(parseOneFunctionArg(tokens));
-  while (tokens[0]?.type !== "RIGHT_PAREN") {
+  while (tokens.current?.type !== "RIGHT_PAREN") {
     consumeOrThrow(tokens, "ARG_SEPARATOR", _t("Wrong function call"));
     args.push(parseOneFunctionArg(tokens));
   }
-  consumeOrThrow(tokens, "RIGHT_PAREN");
-  return args;
+  const rightParen = consumeOrThrow(tokens, "RIGHT_PAREN");
+  return { args, rightParen };
 }
 
-function parseOneFunctionArg(tokens: Token[]): AST {
-  const nextToken = tokens[0];
+function parseOneFunctionArg(tokens: TokenList): AST {
+  const nextToken = tokens.current;
   if (nextToken?.type === "ARG_SEPARATOR" || nextToken?.type === "RIGHT_PAREN") {
     // arg is empty: "sum(1,,2)" "sum(,1)" "sum(1,)"
-    return { type: "EMPTY", value: "" };
+    return {
+      type: "EMPTY",
+      value: "",
+      tokenStartIndex: nextToken.tokenIndex,
+      tokenEndIndex: nextToken.tokenIndex,
+    };
   }
   return parseExpression(tokens);
 }
 
-function consumeOrThrow(tokens, type, message?) {
+function consumeOrThrow(tokens: TokenList, type, message?: string) {
   const token = tokens.shift();
   if (!token || token.type !== type) {
     throw new BadExpressionError(message);
   }
+  return token;
 }
 
-function parseExpression(tokens: Token[], parent_priority: number = 0): AST {
+function parseExpression(tokens: TokenList, parent_priority: number = 0): AST {
   if (tokens.length === 0) {
     throw new BadExpressionError();
   }
   let left = parseOperand(tokens);
   // as long as we have operators with higher priority than the parent one,
   // continue parsing the expression because it is a child sub-expression
-  while (tokens[0]?.type === "OPERATOR" && OP_PRIORITY[tokens[0].value] > parent_priority) {
-    const operator = tokens.shift()!.value;
+  while (
+    tokens.current?.type === "OPERATOR" &&
+    OP_PRIORITY[tokens.current.value] > parent_priority
+  ) {
+    const operatorToken = tokens.shift();
+    const operator = operatorToken.value;
     if (UNARY_OPERATORS_POSTFIX.includes(operator)) {
       left = {
         type: "UNARY_OPERATION",
         value: operator,
         operand: left,
         postfix: true,
+        tokenStartIndex: left.tokenStartIndex,
+        tokenEndIndex: operatorToken.tokenIndex,
       };
     } else {
       const right = parseExpression(tokens, OP_PRIORITY[operator]);
@@ -227,6 +307,8 @@ function parseExpression(tokens: Token[], parent_priority: number = 0): AST {
         value: operator,
         left,
         right,
+        tokenStartIndex: left.tokenStartIndex,
+        tokenEndIndex: right.tokenEndIndex,
       };
     }
   }
@@ -241,12 +323,14 @@ export function parse(str: string): AST {
 }
 
 export function parseTokens(tokens: Token[]): AST {
-  tokens = tokens.filter((x) => x.type !== "SPACE");
-  if (tokens[0]?.value === "=") {
-    tokens.splice(0, 1);
+  const richTokens = tokens.map((token, index) => ({ ...token, tokenIndex: index }));
+  const tokensToParse = richTokens.filter((x) => x.type !== "SPACE");
+  const tokenList = new TokenList(tokensToParse);
+  if (tokenList.current?.value === "=") {
+    tokenList.shift();
   }
-  const result = parseExpression(tokens);
-  if (tokens.length) {
+  const result = parseExpression(tokenList);
+  if (tokenList.current) {
     throw new BadExpressionError();
   }
   return result;

src/components/composer/autocomplete_dropdown/autocomplete_dropdown.xml
@@ -16,7 +16,7 @@
               t-foreach="htmlContent"
               t-as="content"
               t-key="content_index"
-              t-att-class="content.class"
+              t-att-class="content.classes?.join(' ')"
               t-attf-style="color: {{content.color || 'inherit'}};"
               t-esc="content.value"
             />

src/components/composer/composer/abstract_composer_store.ts
@@ -1,9 +1,13 @@
+import { AST } from "prettier";
 import { composerTokenize, EnrichedToken } from "../../../formulas/composer_tokenizer";
+import { iterateAstNodes, parseTokens } from "../../../formulas/parser";
 import { POSTFIX_UNARY_OPERATORS } from "../../../formulas/tokenizer";
 import { functionRegistry } from "../../../functions";
+import { isEvaluationError, transposeMatrix } from "../../../functions/helpers";
 import {
   colors,
   concat,
+  formatValue,
   fuzzyLookup,
   getZoneArea,
   isEqual,
@@ -31,14 +35,18 @@ import {
   Command,
   Direction,
   EditionMode,
+  FunctionResultObject,
   HeaderIndex,
   Highlight,
+  isMatrix,
+  Matrix,
   Range,
   RangePart,
   UID,
   UnboundedZone,
   Zone,
 } from "../../../types";
+import { EvaluationError } from "../../../types/errors";
 import { SelectionEvent } from "../../../types/event_stream";
 
 export const DEFAULT_TOKEN_COLOR: Color = "#000000";
@@ -73,6 +81,7 @@ export abstract class AbstractComposerStore extends SpreadsheetStore {
     "toggleEditionMode",
     "changeComposerCursorSelection",
     "replaceComposerCursorSelection",
+    "hoverToken",
   ] as const;
   protected col: HeaderIndex = 0;
   protected row: HeaderIndex = 0;
@@ -84,6 +93,9 @@ export abstract class AbstractComposerStore extends SpreadsheetStore {
   protected selectionEnd: number = 0;
   protected initialContent: string | undefined = "";
   private colorIndexByRange: { [xc: string]: number } = {};
+
+  hoveredTokens: EnrichedToken[] = [];
+  hoveredContentEvaluation: string = "";
 
   protected notificationStore = this.get(NotificationStore);
   private highlightStore = this.get(HighlightStore);
@@ -278,6 +290,97 @@ export abstract class AbstractComposerStore extends SpreadsheetStore {
     }
   }
 
+  hoverToken(tokenIndex: number | undefined) {
+    this.currentTokens.forEach((t) => (t.isInHoverContext = undefined));
+    const tokens = [...this.currentTokens];
+    if (tokenIndex === undefined || ["ARG_SEPARATOR", "SPACE"].includes(tokens[tokenIndex].type)) {
+      this.hoveredContentEvaluation = "";
+      this.hoveredTokens = [];
+      return;
+    }
+
+    const missingParenthesis = this.getNumberOfMissingParenthesis(tokens);
+    if (missingParenthesis > 0) {
+      tokens.push(...Array(missingParenthesis).fill({ value: ")", type: "RIGHT_PAREN" }));
+    }
+
+    let hoveredContextTokens = tokens;
+    if (tokenIndex !== 0) {
+      const relatedTokens = this.getRelatedTokens(tokens, tokenIndex);
+      const notHoveredTokens = tokens.filter(
+        (t) => !relatedTokens.includes(t) && t.type !== "SPACE"
+      );
+      // Includes starting "=" if all the other tokens are hovered
+      hoveredContextTokens =
+        notHoveredTokens.length === 1 && notHoveredTokens[0] === tokens[0] ? tokens : relatedTokens;
+    }
+
+    hoveredContextTokens.forEach((t) => (t.isInHoverContext = true));
+
+    let hoveredFormula = hoveredContextTokens.map((t) => t.value).join("");
+    if (!isFormula(hoveredFormula)) {
+      hoveredFormula = `=${hoveredFormula}`;
+    }
+    const canonicalFormula = canonicalizeNumberContent(hoveredFormula, this.getters.getLocale());
+    const result = this.getters.evaluateFormulaResult(this.sheetId, canonicalFormula);
+    this.hoveredTokens = hoveredContextTokens;
+    this.hoveredContentEvaluation = this.evaluationResultToDisplayString(result);
+  }
+
+  private getRelatedTokens(tokens: EnrichedToken[], tokenIndex: number): EnrichedToken[] {
+    try {
+      const ast = parseTokens(tokens);
+      let match: AST | undefined = undefined;
+      for (const node of iterateAstNodes(ast)) {
+        if (tokenIndex >= node.tokenStartIndex && tokenIndex <= node.tokenEndIndex) {
+          match = node;
+        } else if (tokenIndex < node.tokenStartIndex) {
+          break;
+        }
+      }
+      if (!match) {
+        return tokens; // Happens if we're hovering spaces at the start/end of the formula
+      }
+      return tokens.slice(match.tokenStartIndex, match.tokenEndIndex + 1);
+    } catch (e) {
+      if (e instanceof EvaluationError) {
+        return tokens;
+      }
+      throw e;
+    }
+  }
+
+  private evaluationResultToDisplayString(
+    result: Matrix<FunctionResultObject> | FunctionResultObject
+  ): string {
+    const locale = this.getters.getLocale();
+    if (isMatrix(result)) {
+      const rowSeparator = locale.decimalSeparator === "," ? "/" : ",";
+      const arrayStr = transposeMatrix(result)
+        .map((row) => row.map((val) => this.cellValueToDisplayString(val)).join(rowSeparator))
+        .join(";");
+      return `{${arrayStr}}`;
+    }
+
+    return this.cellValueToDisplayString(result);
+  }
+
+  private cellValueToDisplayString(result: FunctionResultObject): string {
+    const value = result.value;
+    switch (typeof value) {
+      case "number":
+        return formatValue(value, { locale: this.getters.getLocale(), format: result.format });
+      case "string":
+        if (isEvaluationError(value)) {
+          return value;
+        }
+        return `"${value}"`;
+      case "boolean":
+        return value ? "TRUE" : "FALSE";
+    }
+    return "0";
+  }
+
   private captureSelection(zone: Zone, col?: HeaderIndex, row?: HeaderIndex) {
     this.model.selection.capture(
       this,
@@ -347,9 +450,7 @@ export abstract class AbstractComposerStore extends SpreadsheetStore {
       }
       if (content) {
         if (isFormula(content)) {
-          const left = this.currentTokens.filter((t) => t.type === "LEFT_PAREN").length;
-          const right = this.currentTokens.filter((t) => t.type === "RIGHT_PAREN").length;
-          const missing = left - right;
+          const missing = this.getNumberOfMissingParenthesis(this.currentTokens);
           if (missing > 0) {
             content += concat(new Array(missing).fill(")"));
           }
@@ -384,6 +485,8 @@ export abstract class AbstractComposerStore extends SpreadsheetStore {
     this.editionMode = "inactive";
     this.model.selection.release(this);
     this.colorIndexByRange = {};
+    this.hoveredTokens = [];
+    this.hoveredContentEvaluation = "";
   }
 
   /**
@@ -830,5 +933,11 @@ export abstract class AbstractComposerStore extends SpreadsheetStore {
       return true;
     }
     return false;
+  }
+
+  private getNumberOfMissingParenthesis(tokens: EnrichedToken[]): number {
+    const left = tokens.filter((t) => t.type === "LEFT_PAREN").length;
+    const right = tokens.filter((t) => t.type === "RIGHT_PAREN").length;
+    return left - right;
   }
 }

src/components/composer/composer/composer.ts
@@ -1,7 +1,7 @@
 import { Component, onMounted, onWillUnmount, useEffect, useRef, useState } from "@odoo/owl";
 import { NEWLINE, PRIMARY_BUTTON_BG, SCROLLBAR_WIDTH } from "../../../constants";
 import { functionRegistry } from "../../../functions/index";
-import { clip, isFormula, setColorAlpha } from "../../../helpers/index";
+import { clip, debounce, deepEquals, isFormula, setColorAlpha } from "../../../helpers/index";
 
 import { EnrichedToken } from "../../../formulas/composer_tokenizer";
 import { argTargeting } from "../../../functions/arguments";
@@ -25,6 +25,7 @@ import { TextValueProvider } from "../autocomplete_dropdown/autocomplete_dropdow
 import { AutoCompleteStore } from "../autocomplete_dropdown/autocomplete_dropdown_store";
 import { ContentEditableHelper } from "../content_editable_helper";
 import { FunctionDescriptionProvider } from "../formula_assistant/formula_assistant";
+import { SpeechBubble } from "../speech_bubble/speech_bubble";
 import { DEFAULT_TOKEN_COLOR } from "./abstract_composer_store";
 import { CellComposerStore } from "./cell_composer_store";
 
@@ -34,14 +35,17 @@ const ASSISTANT_WIDTH = 300;
 const CLOSE_ICON_RADIUS = 9;
 
 export const selectionIndicatorClass = "selector-flag";
-const backgroundClass = "background-flag";
+export const highlightParenthesisClass = "highlight-parenthesis-flag";
+const highlightClass = "highlight-flag";
 const selectionIndicatorColor = "#a9a9a9";
 const selectionIndicator = "␣";
 
 export type HtmlContent = {
   value: string;
+  onHover?: (rect: Rect) => void;
+  onStopHover?: () => void;
   color?: Color;
-  class?: string;
+  classes?: string[];
 };
 
 css/* scss */ `
@@ -62,16 +66,23 @@ css/* scss */ `
 
         span {
           white-space: pre-wrap;
+          /* On some browsers (chromium ?) it is somehow possible to hover two of the composer's spans at the same time, leading to
+           * flickering with a succession of onmouseenter/onmouseleave events. A small invisible padding seems to prevent the issue.*/
+          padding-left: 0.01px;
 
           &.${selectionIndicatorClass}:after {
             content: "${selectionIndicator}";
             color: ${selectionIndicatorColor};
           }
 
-          &.${backgroundClass} {
+          &.${highlightParenthesisClass}:not(.${highlightClass}) {
             border-radius: 5px;
             background-color: lightgray;
-            padding: 0px 1.5px 1.5px 1.5px;
+            padding: 1.5px 0px 1.5px 0px;
+          }
+
+          &.${highlightClass} {
+            background-color: #e6edf3;
           }
         }
       }
@@ -129,6 +140,7 @@ export interface CellComposerProps {
 interface ComposerState {
   positionStart: number;
   positionEnd: number;
+  hoveredRect: Rect | undefined;
 }
 
 interface FunctionDescriptionState {
@@ -153,7 +165,7 @@ export class Composer extends Component<CellComposerProps, SpreadsheetChildEnv> 
     composerStore: Object,
     placeholder: { type: String, optional: true },
   };
-  static components = { TextValueProvider, FunctionDescriptionProvider };
+  static components = { TextValueProvider, FunctionDescriptionProvider, SpeechBubble };
   static defaultProps = {
     inputStyle: "",
     isDefaultFocus: false,
@@ -168,6 +180,7 @@ export class Composer extends Component<CellComposerProps, SpreadsheetChildEnv> 
   composerState: ComposerState = useState({
     positionStart: 0,
     positionEnd: 0,
+    hoveredRect: undefined,
   });
 
   autoCompleteState!: Store<AutoCompleteStore>;
@@ -182,6 +195,19 @@ export class Composer extends Component<CellComposerProps, SpreadsheetChildEnv> 
   });
   private compositionActive: boolean = false;
   private spreadsheetRect = useSpreadsheetRect();
+  private lastHoveredTokenIndex: number | undefined = undefined;
+
+  private debouncedHover = debounce((tokenIndex: number | undefined, hoveredRect?: Rect) => {
+    const selection = this.contentHelper.getCurrentSelection();
+    if (selection.start !== selection.end) {
+      return;
+    }
+    const currentHoveredContext = this.props.composerStore.hoveredTokens;
+    this.props.composerStore.hoverToken(tokenIndex);
+    if (!deepEquals(currentHoveredContext, this.props.composerStore.hoveredTokens)) {
+      this.composerState.hoveredRect = hoveredRect;
+    }
+  }, 120);
 
   get assistantStyleProperties(): CSSProperties {
     const composerRect = this.composerRef.el!.getBoundingClientRect();
@@ -276,6 +302,7 @@ export class Composer extends Component<CellComposerProps, SpreadsheetChildEnv> 
     });
     onWillUnmount(() => {
       this.env.model.selection.detachObserver(this);
+      this.debouncedHover.stopDebounce();
     });
     useEffect(() => {
       this.processContent();
@@ -554,6 +581,13 @@ export class Composer extends Component<CellComposerProps, SpreadsheetChildEnv> 
     this.contentHelper.removeSelection();
   }
 
+  onMouseup() {
+    const selection = this.contentHelper.getCurrentSelection();
+    if (selection.start !== selection.end) {
+      this.props.composerStore.hoverToken(undefined);
+    }
+  }
+
   onClick() {
     if (this.env.model.getters.isReadonly()) {
       return;
@@ -651,36 +685,59 @@ export class Composer extends Component<CellComposerProps, SpreadsheetChildEnv> 
     } else if (isValidFormula && this.props.focus !== "inactive") {
       return this.splitHtmlContentIntoLines(this.getHtmlContentFromTokens());
     }
-    return this.splitHtmlContentIntoLines([{ value }]);
+    return this.splitHtmlContentIntoLines([{ value, classes: [] }]);
   }
 
   private getHtmlContentFromTokens(): HtmlContent[] {
     const tokens = this.props.composerStore.currentTokens;
     const result: HtmlContent[] = [];
     const { end, start } = this.props.composerStore.composerSelection;
-    for (const token of tokens) {
+    for (let index = 0; index < tokens.length; index++) {
+      const token = tokens[index];
       let color = token.color || DEFAULT_TOKEN_COLOR;
       if (token.isBlurred) {
         color = setColorAlpha(color, 0.5);
       }
-      result.push({ value: token.value, color });
+      const classes: string[] = [];
       if (
         token.type === "REFERENCE" &&
         this.props.composerStore.tokenAtCursor === token &&
         this.props.composerStore.editionMode === "selecting"
       ) {
-        result[result.length - 1].class = "text-decoration-underline";
+        classes.push("text-decoration-underline");
       }
 
       if (end === start && token.isParenthesisLinkedToCursor) {
-        result[result.length - 1].class = backgroundClass;
+        classes.push(highlightParenthesisClass);
+      }
+
+      if (token.isInHoverContext) {
+        classes.push(highlightClass);
       }
 
       if (this.props.composerStore.showSelectionIndicator && end === start && end === token.end) {
-        result[result.length - 1].class = selectionIndicatorClass;
+        classes.push(selectionIndicatorClass);
       }
+
+      result.push({
+        value: token.value,
+        color,
+        classes,
+        onHover: (rect) => this.onTokenHover(index, rect),
+        onStopHover: () => this.onTokenHover(undefined),
+      });
     }
+
     return result;
+  }
+
+  private onTokenHover(tokenIndex: number | undefined, hoveredRect?: Rect) {
+    // We want to debounce the hover event to avoid flickering, but we also don't want to keep delaying the debounce timer
+    // if the user keeps moving its mouse over the same token.
+    if (this.lastHoveredTokenIndex !== tokenIndex) {
+      this.lastHoveredTokenIndex = tokenIndex;
+      this.debouncedHover(tokenIndex, hoveredRect);
+    }
   }
 
   /**
@@ -723,7 +780,7 @@ export class Composer extends Component<CellComposerProps, SpreadsheetChildEnv> 
   }
 
   private isContentEmpty(content: HtmlContent): boolean {
-    return !(content.value || content.class);
+    return !(content.value || content.classes?.length);
   }
 
   /**
@@ -826,5 +883,13 @@ export class Composer extends Component<CellComposerProps, SpreadsheetChildEnv> 
     }
     this.autoCompleteState.provider?.selectProposal(value);
     this.processTokenAtCursor();
+  }
+
+  get displaySpeechBubble(): boolean {
+    return !!(
+      this.props.focus !== "inactive" &&
+      this.composerState.hoveredRect &&
+      this.props.composerStore.hoveredContentEvaluation
+    );
   }
 }

src/components/composer/composer/composer.xml
@@ -31,6 +31,7 @@
           t-on-mousewheel.stop=""
           t-on-input="onInput"
           t-on-pointerdown="onMousedown"
+          t-on-pointerup="onMouseup"
           t-on-click="onClick"
           t-on-keyup="onKeyup"
           t-on-paste="onPaste"
@@ -77,6 +78,11 @@
           />
         </div>
       </div>
+      <SpeechBubble
+        t-if="displaySpeechBubble"
+        content="props.composerStore.hoveredContentEvaluation"
+        anchorRect="composerState.hoveredRect"
+      />
     </div>
   </t>
 </templates>

src/components/composer/content_editable_helper.ts
@@ -1,5 +1,9 @@
 import { deepEquals, toHex } from "../../helpers";
-import { getCurrentSelection, iterateChildren } from "../helpers/dom_helpers";
+import {
+  getBoundingRectAsPOJO,
+  getCurrentSelection,
+  iterateChildren,
+} from "../helpers/dom_helpers";
 import { NEWLINE } from "./../../constants";
 import { HtmlContent } from "./composer/composer";
 
@@ -152,16 +156,21 @@ export class ContentEditableHelper {
           continue;
         }
         // this is an empty line in the content
-        if (!content.value && !content.class) {
+        if (!content.value && !content.classes?.length) {
           if (child) p.removeChild(child);
           continue;
         }
         const span = document.createElement("span");
         span.innerText = content.value;
         span.style.color = content.color || "";
-        if (content.class) {
-          span.classList.add(content.class);
-        }
+        span.addEventListener("mousemove", () => {
+          content.onHover?.(getBoundingRectAsPOJO(span));
+        });
+        span.addEventListener("mouseleave", () => {
+          content.onStopHover?.();
+        });
+        span.classList.add(...(content.classes || []));
+
         if (child) {
           p.replaceChild(span, child);
         } else {
@@ -259,7 +268,7 @@ function compareContentToSpanElement(content: HtmlContent, node: HTMLElement): b
   const nodeColor = node.style?.color ? toHex(node.style.color) : "";
 
   const sameColor = contentColor === nodeColor;
-  const sameClass = deepEquals([content.class], [...node.classList]);
+  const sameClass = deepEquals(content.classes, [...node.classList]);
   const sameContent = node.innerText === content.value;
   return sameColor && sameClass && sameContent;
 }

commit 572d5591bd734c2c2aaeae2e13bffc08b3b88b9e

[FIX] pivot: copy from referenced id
Steps to reproduce

- insert a pivot into a spreadsheet
- type the formula =PIVOT(A1) where A1 contains the pivot id
- copy any value from the pivot
- paste it somewhere else
=> the "fixed" formula should be pasted, but only the value is pasted

The bug was introduced by 816977d7b8a5

`...token` doesn't work as expected. For reference tokens coming stored on the
cells, the objects are not POJO, but instances of `RangeReferenceToken`.
The `value` property is a getter `get value()` which is ignored when
spreading the object (=> `.value` is undefined)

Task: 4756878

src/formulas/parser.ts
@@ -323,7 +323,11 @@ export function parse(str: string): AST {
 }
 
 export function parseTokens(tokens: Token[]): AST {
-  const richTokens = tokens.map((token, index) => ({ ...token, tokenIndex: index }));
+  const richTokens = tokens.map((token, index) => ({
+    type: token.type,
+    value: token.value,
+    tokenIndex: index,
+  }));
   const tokensToParse = richTokens.filter((x) => x.type !== "SPACE");
   const tokenList = new TokenList(tokensToParse);
   if (tokenList.current?.value === "=") {

tests/clipboard/clipboard_plugin.test.ts
@@ -2228,6 +2228,25 @@ describe("clipboard", () => {
     expect(getEvaluatedCell(model, "G5").format).toBe("#,##0.0");
   });
 
+  test("copy spread pivot from a referenced id", () => {
+    // prettier-ignore
+    const grid = {
+      A1: "Customer", B1: "Price", C1: "1",
+      A2: "Alice",    B2: "10",    C2: "=PIVOT(C1)",
+      A3: "Bob",      B3: "30"
+    };
+    const model = createModelFromGrid(grid);
+    addPivot(model, "A1:B3", {
+      columns: [],
+      rows: [{ fieldName: "Customer" }],
+      measures: [{ id: "Price:sum", fieldName: "Price", aggregator: "sum" }],
+    });
+
+    copy(model, "D4");
+    paste(model, "G4");
+    expect(getCell(model, "G4")?.content).toBe('=PIVOT.VALUE(1,"Price:sum","Customer","Alice")');
+  });
+
   test("copying a spread pivot cell with (Undefined)", () => {
     // prettier-ignore
     const grid = {