4ad16d794034d3c3c698d618a0453196ca40b5b7
Rémi Rahir (rar) <rar@odoo.com>
2025-04-28 10:39:00 +0000

    [FIX] GridComposer: limit the composer size
    Currently, opening the gridcomposer with a content with too many
    lines will completely break the layout as the composer is not bounded.
    
    This revision forces a limit size to it, similarly to the strategy done
    in the TopbarComposer. It also allows users to scroll inside the said
    composer when possible.
    
    Task: 4686816
    X-original-commit: db76a73e9e69594ed409f645dc93990dbff5c9f7

--- src/components/composer/grid_composer/grid_composer.ts
+++ src/components/composer/grid_composer/grid_composer.ts
@@ -123,6 +123,8 @@ export class GridComposer extends Component<Props, SpreadsheetChildEnv> {
 
   get composerProps(): CellComposerProps {
     const { width, height } = this.env.model.getters.getSheetViewDimensionWithHeaders();
+    // Remove the wrapper border width
+    const maxHeight = this.props.gridDims.height - this.rect.y - 2 * COMPOSER_BORDER_WIDTH;
     return {
       rect: { ...this.rect },
       delimitation: {
@@ -142,6 +144,7 @@ export class GridComposer extends Component<Props, SpreadsheetChildEnv> {
         }),
       onInputContextMenu: this.props.onInputContextMenu,
       composerStore: this.composerStore,
+      inputStyle: `max-height: ${maxHeight}px;`,
     };
   }
 
