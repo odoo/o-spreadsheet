e9dade74f4db6ac91a27e20dd63d8facddf3fb70
Rémi Rahir (rar) <rar@odoo.com>
2025-05-08 09:12:24 +0000

    [FIX] Stores: skip unnecessary renderings
    Currently, calling the mutator of a Store (through `useStore`) will
    automatically trigger a full-on rendering of the spreadsheet.
    The rationale being that the sotre state **might** have been updated
    but this also mean that we will trigger a full render even if the call
    to the mutatore had 0 effect.
    
    This revision adds the possibility for a store to specifcally notify
    that its state was not altered and that the render can therefore be
    skipped.
    
    This leaves more room for granularity than a reactivity observer would
    and we avoid its overhead as well.
    
    Task: 4781429
    X-original-commit: 43578783610e67f28b17cc37587299d6edab7896

--- src/components/composer/composer/composer.ts
+++ src/components/composer/composer/composer.ts
@@ -285,7 +285,7 @@ export class Composer extends Component<CellComposerProps, SpreadsheetChildEnv> 
         this.props.composerStore.editionMode === "inactive" &&
         !this.props.isDefaultFocus
       ) {
-        this.DOMFocusableElementStore.focusableElement?.focus();
+        this.DOMFocusableElementStore.focus();
       }
     });
 
