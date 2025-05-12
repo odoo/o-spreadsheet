421542ba1bb5929a63c9b0cc9c7739df50951474
hokolomopo <adrm@odoo.com>
2025-05-12 11:30:32 +0200

    [MOV] charts: extract chart animations from Odoo
    Extract the code handling chart animation in dashboard from odoo to
    o-spreadsheet.
    
    Task: 4787283

---
+++ tests/figures/chart/chart_animations.test.ts
@@ -0,0 +1,75 @@
+import { Chart } from "chart.js";
+import { Model, readonlyAllowedCommands } from "../../../src";
+import { createChart, setCellContent, updateChart } from "../../test_helpers/commands_helpers";
+import { mockChart, mountSpreadsheet, nextTick } from "../../test_helpers/helpers";
+
+mockChart();
+
+let mockedChart: any;
+beforeEach(() => {
+  jest
+    .spyOn((window as any).Chart.prototype, "constructorMock")
+    .mockImplementation(function (this: Chart) {
+      mockedChart = this;
+    });
+});
+
+describe("Chart animations in dashboard", () => {
+  test("Charts are animated only at first render", async () => {
+    const model = new Model();
+    createChart(model, { type: "bar" });
+    model.updateMode("dashboard");
+
+    await mountSpreadsheet({ model });
+    expect(".o-figure").toHaveCount(1);
+    expect(mockedChart.config.options.animation.animateRotate).toBe(true);
+
+    // Scroll the figure out of the viewport and back in
+    model.dispatch("SET_VIEWPORT_OFFSET", { offsetX: 0, offsetY: 500 });
+    await nextTick();
+    expect(".o-figure").toHaveCount(0);
+
+    model.dispatch("SET_VIEWPORT_OFFSET", { offsetX: 0, offsetY: 0 });
+    await nextTick();
+    expect(".o-figure").toHaveCount(1);
+    expect(mockedChart.config.options.animation).toBe(false);
+  });
+
+  test("Animations are replayed only when chart data changes", async () => {
+    readonlyAllowedCommands.add("UPDATE_CELL");
+
+    const model = new Model();
+    createChart(model, { type: "bar", dataSets: [{ dataRange: "A1:A6" }] });
+    model.updateMode("dashboard");
+    await mountSpreadsheet({ model });
+
+    expect(mockedChart.config.options.animation).toEqual({ animateRotate: true });
+
+    // Dispatch a command that doesn't change the chart data
+    setCellContent(model, "A50", "6");
+    await nextTick();
+    expect(mockedChart.config.options.animation).toBe(false);
+
+    // Change the chart data
+    setCellContent(model, "A2", "6");
+    await nextTick();
+    expect(mockedChart.config.options.animation).toEqual({ animateRotate: true });
+
+    readonlyAllowedCommands.delete("UPDATE_CELL");
+  });
+
+  test("Charts are animated when chart type changes", async () => {
+    const model = new Model();
+    createChart(model, { type: "bar", dataSets: [{ dataRange: "A1:A6" }] }, "chartId");
+    model.updateMode("dashboard");
+    await mountSpreadsheet({ model });
+
+    model.dispatch("EVALUATE_CELLS");
+    await nextTick();
+    expect(mockedChart.config.options.animation).toBe(false);
+
+    updateChart(model, "chartId", { type: "pie" });
+    await nextTick();
+    expect(mockedChart.config.options.animation.animateRotate).toBe(true);
+  });
+});
