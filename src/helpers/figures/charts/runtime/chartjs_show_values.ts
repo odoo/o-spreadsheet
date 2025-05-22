e379ca879f993e9812fa439417d23717eb5ac99a
hokolomopo <adrm@odoo.com>
2025-05-22 13:28:38 +0000

    [FIX] waterfall: wrong show value for waterfall chart
    The shot value chartJs plugin would show the same value for a waterfall
    chart than a bar chart (the value of the top of the bar), instead of the
    correct value for a waterfall chart (the difference between the top and
    the bottom of the bar).
    
    closes odoo/o-spreadsheet#6538
    
    Task: 4812692
    X-original-commit: 51771fc546935b2b331f28b1d964e18a9f0cef9c
    Signed-off-by: Rémi Rahir (rar) <rar@odoo.com>
    Signed-off-by: Adrien Minne (adrm) <adrm@odoo.com>

--- src/helpers/figures/charts/runtime/chartjs_show_values.ts
+++ src/helpers/figures/charts/runtime/chartjs_show_values.ts
@@ -1,5 +1,10 @@
+import { ChartMeta } from "chart.js";
 import { ChartShowValuesPluginOptions } from "../../../../components/figures/chart/chartJs/chartjs_show_values_plugin";
-import { ChartRuntimeGenerationArgs, ChartWithDataSetDefinition } from "../../../../types/chart";
+import {
+  ChartRuntimeGenerationArgs,
+  ChartWithDataSetDefinition,
+  WaterfallChartDefinition,
+} from "../../../../types/chart";
 import { formatChartDatasetValue } from "../chart_common";
 
 export function getChartShowValues(
@@ -11,6 +16,60 @@ export function getChartShowValues(
     horizontal: "horizontal" in definition && definition.horizontal,
     showValues: "showValues" in definition ? !!definition.showValues : false,
     background: definition.background,
-    callback: formatChartDatasetValue(axisFormats, locale),
+    callback: (value: number | string, dataset: ChartMeta) => {
+      const axisId = getDatasetAxisId(definition, dataset);
+      return formatChartDatasetValue(axisFormats, locale)(value, axisId);
+    },
   };
+}
+
+export function getPyramidChartShowValues(
+  definition: ChartWithDataSetDefinition,
+  args: ChartRuntimeGenerationArgs
+): ChartShowValuesPluginOptions {
+  const { axisFormats, locale } = args;
+  return {
+    horizontal: true,
+    showValues: "showValues" in definition ? !!definition.showValues : false,
+    background: definition.background,
+    callback: (value: number | string, dataset: ChartMeta) => {
+      value = Math.abs(Number(value));
+      return formatChartDatasetValue(axisFormats, locale)(value, dataset.xAxisID || "x");
+    },
+  };
+}
+
+export function getWaterfallChartShowValues(
+  definition: WaterfallChartDefinition,
+  args: ChartRuntimeGenerationArgs
+): ChartShowValuesPluginOptions {
+  const { axisFormats, locale, dataSetsValues } = args;
+
+  const subtotalIndexes = dataSetsValues.reduce((subtotalIndexes, ds) => {
+    subtotalIndexes.push((subtotalIndexes.at(-1) || -1) + ds.data.length + 1);
+    return subtotalIndexes;
+  }, [] as number[]);
+
+  return {
+    showValues: "showValues" in definition ? !!definition.showValues : false,
+    background: definition.background,
+    callback: (value: number | string, dataset: any, index: number) => {
+      const raw = dataset._dataset.data[index];
+      const delta = raw[1] - raw[0];
+      let sign = delta >= 0 ? "+" : "";
+      if (definition.showSubTotals && subtotalIndexes.includes(index) && sign === "+") {
+        sign = "";
+      }
+      return `${sign}${formatChartDatasetValue(axisFormats, locale)(delta, dataset.yAxisID)}`;
+    },
+  };
+}
+
+function getDatasetAxisId(definition: ChartWithDataSetDefinition, dataset: ChartMeta): string {
+  if (dataset.rAxisID) {
+    return dataset.rAxisID;
+  }
+  const axisId =
+    "horizontal" in definition && definition.horizontal ? dataset.xAxisID : dataset.yAxisID;
+  return axisId || "y";
 }
