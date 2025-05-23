aed69ce7bc8db3aa780aa970b151006d102b32a2
Lucas Lefèvre (lul) <lul@odoo.com>
2025-05-23 17:19:57 +0200

    [IMP] chips: add hover effect on arrow

--- src/components/data_validation_overlay/dv_list_icon/dv_list_icon.ts
+++ src/components/data_validation_overlay/dv_list_icon/dv_list_icon.ts
@@ -1,6 +1,9 @@
 import { Component } from "@odoo/owl";
 import { GRID_ICON_EDGE_LENGTH, TEXT_BODY_MUTED } from "../../../constants";
+import { darkenColor } from "../../../helpers";
+import { Store, useStore } from "../../../store_engine";
 import { CellPosition, SpreadsheetChildEnv } from "../../../types";
+import { HoveredCellStore } from "../../grid/immediate_hovered_cell_store";
 import { css, cssPropertiesToCss } from "../../helpers";
 
 const ICON_WIDTH = 13;
@@ -32,6 +35,11 @@ export class DataValidationListIcon extends Component<Props, SpreadsheetChildEnv
   static props = {
     cellPosition: Object,
   };
+  hoveredCellStore!: Store<HoveredCellStore>;
+
+  setup() {
+    this.hoveredCellStore = useStore(HoveredCellStore);
+  }
 
   onClick() {
     const { col, row } = this.props.cellPosition;
@@ -41,8 +49,13 @@ export class DataValidationListIcon extends Component<Props, SpreadsheetChildEnv
 
   get chipStyle() {
     const style = this.env.model.getters.getDataValidationCellStyle(this.props.cellPosition);
+    const isHovered =
+      this.hoveredCellStore.col === this.props.cellPosition.col &&
+      this.hoveredCellStore.row === this.props.cellPosition.row;
+    const shadowColor = darkenColor(style?.textColor || TEXT_BODY_MUTED, 0.2);
     return cssPropertiesToCss({
       color: style?.textColor,
+      filter: isHovered ? `drop-shadow(0 0 2px ${shadowColor})` : "",
     });
   }
 }
