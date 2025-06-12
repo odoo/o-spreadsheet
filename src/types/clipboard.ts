de144a5f09b388651d5132fbe3cb9046914ada03
dhrp-odoo <dhrp@odoo.com>
2025-06-12 16:26:27 +0530

    [REF] clipboard: extract paste helpers to reduce duplication
    The same paste logic is required in both clipboard.ts and
    paint_format_store.ts.
    
    To improve reusability and readability, the following helpers
    were extracted:
    
    - getPasteTargetFromHandlers: computes the paste target and
      affected zones.
    
    - applyClipboardHandlersPaste: applies each clipboard handler to
      paste the data.
    
    - selectPastedZone: updates the selection after paste.
    
    Task: 4807659

--- src/types/clipboard.ts
+++ src/types/clipboard.ts
@@ -1,5 +1,5 @@
 import { SpreadsheetClipboardData } from "../plugins/ui_stateful";
-import { HeaderIndex, UID, Zone } from "./misc";
+import { ClipboardCell, HeaderIndex, UID, Zone } from "./misc";
 
 export enum ClipboardMIMEType {
   PlainText = "text/plain",
@@ -40,4 +40,12 @@ export type ClipboardPasteTarget = {
   sheetId: UID;
   zones: Zone[];
   figureId?: UID;
+};
+
+export type MinimalClipboardData = {
+  sheetId?: UID;
+  cells?: ClipboardCell[][];
+  zones?: Zone[];
+  figureId?: UID;
+  [key: string]: unknown;
 };
