31fa459b9c17ba040f636561e70b048334072c92
rmbh-odoo <rmbh@odoo.com>
2025-07-03 05:16:54 +0000

    [FIX] auto_complete: remove toggle button for data validation
    Before this commit:
    - The autocomplete assistant in data validation
    included an open/close toggle button.
    
    After this commit:
    - The open/close button is removed in the data validation context
    to simplify the UI and prevent accidental closure.
    
    closes odoo/o-spreadsheet#6833
    
    Task: 4854464
    X-original-commit: ec8a90d51f52a0d8e998c8b0038b851a7ae34bbd
    Signed-off-by: Adrien Minne (adrm) <adrm@odoo.com>
    Signed-off-by: Ronakkumar Mukeshbhai Bharadiya (rmbh) <rmbh@odoo.com>

--- src/components/composer/composer/composer.ts
+++ src/components/composer/composer/composer.ts
@@ -608,10 +608,12 @@ export class Composer extends Component<CellComposerProps, SpreadsheetChildEnv> 
   }
 
   closeAssistant() {
+    if (!this.canBeToggled) return;
     this.assistant.forcedClosed = true;
   }
 
   openAssistant() {
+    if (!this.canBeToggled) return;
     this.assistant.forcedClosed = false;
   }
 
@@ -623,6 +625,10 @@ export class Composer extends Component<CellComposerProps, SpreadsheetChildEnv> 
     ) {
       event.stopPropagation();
     }
+  }
+
+  get canBeToggled() {
+    return this.autoCompleteState.provider?.canBeToggled ?? true;
   }
 
   // ---------------------------------------------------------------------------
@@ -778,7 +784,7 @@ export class Composer extends Component<CellComposerProps, SpreadsheetChildEnv> 
   }
 
   private autoComplete(value: string) {
-    if (!value || this.assistant.forcedClosed) {
+    if (!value || (this.assistant.forcedClosed && this.canBeToggled)) {
       return;
     }
     this.autoCompleteState.provider?.selectProposal(value);
