e7a5b57131ca31c32cec4d2181d3f0cff1402a9b
Pierre Rousseau <pro@odoo.com>
2025-07-08 07:45:38 +0000

    [FIX] error_tooltip: Do not show cause position in dashboard
    closes odoo/o-spreadsheet#6769
    
    Task: 4926129
    X-original-commit: 1b3219a0a3473816acd9d6694ee607eb00d8171f
    Signed-off-by: Lucas Lefèvre (lul) <lul@odoo.com>
    Signed-off-by: Pierre Rousseau (pro) <pro@odoo.com>

--- src/components/error_tooltip/error_tooltip.ts
+++ src/components/error_tooltip/error_tooltip.ts
@@ -48,6 +48,9 @@ export class ErrorToolTip extends Component<ErrorToolTipProps, SpreadsheetChildE
   }
 
   get errorOriginPositionString() {
+    if (this.env.model.getters.isDashboard()) {
+      return "";
+    }
     const evaluationError = this.evaluationError;
     const position = evaluationError?.errorOriginPosition;
     if (!position || deepEquals(position, this.props.cellPosition)) {
