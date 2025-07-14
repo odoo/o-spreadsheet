3c8a667dbaa67a96874974fffb3c9ed6bf12c922
Adrien Minne (adrm) <adrm@odoo.com>
2025-07-14 10:00:36 +0200

    [FIX] data validation: handle cell format in auto-complete
    The auto complete of data validation showed the raw cell value instead
    of the formatted value. We should display the formatted value in the
    auto-complete, but still use the raw value when selecting a proposal.
    
    Task: 4865661

--- src/registries/auto_completes/data_validation_auto_complete.ts
+++ src/registries/auto_completes/data_validation_auto_complete.ts
@@ -1,4 +1,5 @@
-import { isNotNull } from "../../helpers";
+import { CellPosition, CellValue, Getters } from "../..";
+import { positions } from "../../helpers";
 import { autoCompleteProviders } from "./auto_complete_registry";
 
 autoCompleteProviders.add("dataValidation", {
@@ -10,34 +11,45 @@ autoCompleteProviders.add("dataValidation", {
     if (!this.composer.currentEditedCell) {
       return [];
     }
-    const position = this.composer.currentEditedCell;
-    const rule = this.getters.getValidationRuleForCell(position);
-    if (
-      !rule ||
-      (rule.criterion.type !== "isValueInList" && rule.criterion.type !== "isValueInRange")
-    ) {
-      return [];
-    }
 
-    let values: string[];
-    if (rule.criterion.type === "isValueInList") {
-      values = rule.criterion.values;
-    } else {
-      const range = this.getters.getRangeFromSheetXC(position.sheetId, rule.criterion.values[0]);
-      values = Array.from(
-        new Set(
-          this.getters
-            .getRangeValues(range)
-            .filter(isNotNull)
-            .map((value) => value.toString())
-            .filter((val) => val !== "")
-        )
-      );
-    }
-    return values.map((value) => ({ text: value }));
+    return getProposedValues(this.getters, this.composer.currentEditedCell).map((value) => ({
+      text: value.value?.toString() || "",
+      htmlContent: [{ value: value.label }],
+      fuzzySearchKey: value.label,
+    }));
   },
   selectProposal(tokenAtCursor, value) {
     this.composer.setCurrentContent(value);
     this.composer.stopEdition();
   },
 });
+
+function getProposedValues(
+  getters: Getters,
+  position: CellPosition
+): { label: string; value: CellValue }[] {
+  const rule = getters.getValidationRuleForCell(position);
+  if (
+    !rule ||
+    (rule.criterion.type !== "isValueInList" && rule.criterion.type !== "isValueInRange")
+  ) {
+    return [];
+  }
+
+  let values: { label: string; value: CellValue }[] = [];
+  if (rule.criterion.type === "isValueInList") {
+    values = rule.criterion.values.map((value) => ({ label: value, value }));
+  } else {
+    const labelsSet = new Set<string>();
+    const range = getters.getRangeFromSheetXC(position.sheetId, rule.criterion.values[0]);
+    for (const p of positions(range.zone)) {
+      const cell = getters.getEvaluatedCell({ ...p, sheetId: range.sheetId });
+      if (cell.formattedValue && !labelsSet.has(cell.formattedValue)) {
+        labelsSet.add(cell.formattedValue);
+        values.push({ label: cell.formattedValue, value: cell.value });
+      }
+    }
+  }
+
+  return values;
+}
