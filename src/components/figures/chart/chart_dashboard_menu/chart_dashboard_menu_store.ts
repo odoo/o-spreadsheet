50b881510643afa63b13bf69779c7a54dee0d999
Lucas Lefèvre (lul) <lul@odoo.com>
2025-07-17 09:41:26 +0200

    [REF] chart: move dashboard menu state to a store
    This commit moves the state of ChartDashboardMenu component to a store.
    The goal is to support chart updates in dashboard mode.
    It means this component would need to react to UPDATE_CHART commands
    to preserve the `originalChartDefinition`.
    
    Only stores can react to commands, hence we are moving the state to a store.
    
    Task: 4942915

---
+++ src/components/figures/chart/chart_dashboard_menu/chart_dashboard_menu_store.ts
@@ -0,0 +1,77 @@
+import { ChartDefinition, ChartType, UID } from "../../../..";
+import { chartRegistry, chartSubtypeRegistry } from "../../../../registries/chart_types";
+import { Get } from "../../../../store_engine";
+import { SpreadsheetStore } from "../../../../stores";
+
+export class ChartDashboardMenuStore extends SpreadsheetStore {
+  mutators = ["reset"] as const;
+  private originalChartDefinition: ChartDefinition;
+  constructor(get: Get, private chartId: UID) {
+    super(get);
+    this.originalChartDefinition = this.getters.getChartDefinition(this.chartId);
+  }
+
+  get changeChartTypeMenuItems() {
+    const definition = this.getters.getChartDefinition(this.chartId);
+    if (!["line", "bar", "pie"].includes(definition.type)) {
+      return [];
+    }
+
+    return ["column", "line", "pie"].map((type) => {
+      const item = chartSubtypeRegistry.get(type);
+      return {
+        id: item.chartType,
+        label: item.displayName,
+        onClick: () => this.updateType(item.chartType),
+        isSelected: item.chartType === this.getters.getChartDefinition(this.chartId).type,
+        iconClass: this.getIconClasses(item.chartType),
+      };
+    });
+  }
+
+  reset(chartId: UID) {
+    this.chartId = chartId;
+    this.originalChartDefinition = this.getters.getChartDefinition(chartId);
+  }
+
+  private updateType(type: ChartType) {
+    const figureId = this.chartId;
+    const currentDefinition = this.getters.getChartDefinition(figureId);
+    if (currentDefinition.type === type) {
+      return;
+    }
+
+    let definition: ChartDefinition;
+    if (this.originalChartDefinition.type === type) {
+      definition = this.originalChartDefinition;
+    } else {
+      const newChartInfo = chartSubtypeRegistry.get(type);
+      const ChartClass = chartRegistry.get(newChartInfo.chartType);
+      const chartCreationContext = this.getters.getContextCreationChart(figureId);
+      if (!chartCreationContext) return;
+      definition = {
+        ...ChartClass.getChartDefinitionFromContextCreation(chartCreationContext),
+        ...newChartInfo.subtypeDefinition,
+      } as ChartDefinition;
+    }
+
+    this.model.dispatch("UPDATE_CHART", {
+      definition,
+      figureId,
+      sheetId: this.getters.getActiveSheetId(),
+    });
+  }
+
+  private getIconClasses(type: ChartType) {
+    if (type.includes("bar")) {
+      return "fa fa-bar-chart";
+    }
+    if (type.includes("line")) {
+      return "fa fa-line-chart";
+    }
+    if (type.includes("pie")) {
+      return "fa fa-pie-chart";
+    }
+    return "";
+  }
+}
