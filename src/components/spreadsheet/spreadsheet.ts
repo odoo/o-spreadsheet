85a45231217c4dfadf454131b83fd8783e88bbc0
Adrien Minne (adrm) <adrm@odoo.com>
2025-07-18 09:48:10 +0000

    [FIX] chart: chartJS extensions loaded too late
    The `onMounted` hook in child components runs before the one in their
    parent. So loading the ChartJS extensions in the Spreadsheet's `onMounted`
    doesn't work, since the `ChartJsComponent` needs them in its own
    `onMounted`, which runs first.
    
    It actually worked by pure chance before. At the first render of the
    model, the SheetView size isn't set yet, so `getters.getVisibleFigures()`
    doesn't return anything. And only at the second render the charts are
    displayed. However if we tried to load a `Spreadsheet` with a model
    that has an initialized SheetView size, the charts would not be
    loaded correctly.
    
    Task: 4954034
    X-original-commit: 2b8083a582b2f035d5591b74769fc7f94523f8c1

--- src/components/spreadsheet/spreadsheet.ts
+++ src/components/spreadsheet/spreadsheet.ts
@@ -53,10 +53,7 @@ import {
 import { BottomBar } from "../bottom_bar/bottom_bar";
 import { ComposerFocusStore } from "../composer/composer_focus_store";
 import { SpreadsheetDashboard } from "../dashboard/dashboard";
-import {
-  registerChartJSExtensions,
-  unregisterChartJsExtensions,
-} from "../figures/chart/chartJs/chart_js_extension";
+import { unregisterChartJsExtensions } from "../figures/chart/chartJs/chart_js_extension";
 import { Grid } from "../grid/grid";
 import { HeaderGroupContainer } from "../header_group/header_group_container";
 import { css, cssPropertiesToCss } from "../helpers/css";
@@ -441,7 +438,6 @@ export class Spreadsheet extends Component<SpreadsheetProps, SpreadsheetChildEnv
       this.checkViewportSize();
       stores.on("store-updated", this, render);
       resizeObserver.observe(this.spreadsheetRef.el!);
-      registerChartJSExtensions();
     });
     onWillUnmount(() => {
       this.unbindModelEvents();
