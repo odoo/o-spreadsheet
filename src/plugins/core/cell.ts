d13adfd41d0e3afd550f5f41c494dfe0957679d4
Rémi Rahir (rar) <rar@odoo.com>
2025-08-19 14:01:56 +0200

    [FIX] borders: Recompute borders on `SET_BORDERS_ON_TARGET`
    The new command, notably used in the clipboard paste and autofill, was
    not added to the subset of commands that invalidate the computed border
    style.
    
    Task: 5024825

--- src/plugins/core/cell.ts
+++ src/plugins/core/cell.ts
@@ -205,7 +205,6 @@ export class CellPlugin extends CorePlugin<CoreState> implements CoreState {
     for (let zone of recomputeZones(zones)) {
       for (let col = zone.left; col <= zone.right; col++) {
         for (let row = zone.top; row <= zone.bottom; row++) {
-          // commandHelpers.updateCell(sheetId, col, row, { style: undefined});
           this.dispatch("UPDATE_CELL", {
             sheetId,
             col,
