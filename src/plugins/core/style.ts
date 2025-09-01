6143f9a391207a813fb32ecb9919d47da34dcd14
Florian Damhaut <flda@odoo.com>
2025-09-01 16:07:17 +0200

    [REF] Style: The Style Strikes Back

--- src/plugins/core/style.ts
+++ src/plugins/core/style.ts
@@ -42,7 +42,12 @@ interface StylePluginState {
 }
 
 export class StylePlugin extends CorePlugin<StylePluginState> implements StylePluginState {
-  static getters = ["getCellStyle", "getStyleCustomColor"] as const;
+  static getters = [
+    "getCellStyle",
+    "getCellStyleInZone",
+    "getZoneStyles",
+    "getStyleCustomColor",
+  ] as const;
 
   readonly styles: Record<UID, ZoneStyle[] | undefined> = {};
 
@@ -72,9 +77,6 @@ export class StylePlugin extends CorePlugin<StylePluginState> implements StylePl
         this.clearStyle(cmd.sheetId, [positionToZone(cmd)]);
         break;
       case "CLEAR_CELLS":
-        this.clearStyle(cmd.sheetId, cmd.target);
-        break;
-      case "DELETE_CONTENT":
         this.clearStyle(cmd.sheetId, cmd.target);
         break;
       case "DELETE_SHEET":
@@ -203,6 +205,15 @@ export class StylePlugin extends CorePlugin<StylePluginState> implements StylePl
           styles.set({ sheetId, col, row }, style);
         }
       }
+    }
+    return styles;
+  }
+
+  getZoneStyles(sheetId: UID, zone: Zone): ZoneStyle[] {
+    const styles: ZoneStyle[] = [];
+    for (const style of this.styles[sheetId] ?? []) {
+      const inter = intersection(style.zone, zone);
+      if (inter) styles.push({ zone: inter, style: style.style });
     }
     return styles;
   }
