467929535fa1d196407485c6a34ecb0e05d0b39e
Adrien Minne (adrm) <adrm@odoo.com>
2025-09-11 08:08:34 +0000

    [FIX] functions: fix LINEST error massage
    The functions using the helper `tryCastAsNumberMatrix` had a wrong
    error message:
    
    - the two arguments in the translated string were inverted
    - an argument of `_t` was `typeof cell`, which isn't translated
    - the other argument of the `_t` was something like
        `the first argument (data_y)`, which wasn't translated either
    
    Task: 5059375
    X-original-commit: b796c32c0975bf632c6a948a9fc23ebaddd80039

--- src/functions/module_statistical.ts
+++ src/functions/module_statistical.ts
@@ -573,9 +573,9 @@ export const GROWTH: AddFunctionDescription = {
     assertNonEmptyMatrix(knownDataY, "known_data_y");
     return expM(
       predictLinearValues(
-        logM(toNumberMatrix(knownDataY, "the first argument (known_data_y)")),
-        toNumberMatrix(knownDataX, "the second argument (known_data_x)"),
-        toNumberMatrix(newDataX, "the third argument (new_data_y)"),
+        logM(toNumberMatrix(knownDataY, "known_data_y")),
+        toNumberMatrix(knownDataX, "known_data_x"),
+        toNumberMatrix(newDataX, "new_data_y"),
         toBoolean(b)
       )
     );
@@ -687,8 +687,8 @@ export const LINEST: AddFunctionDescription = {
   ): (number | string)[][] {
     assertNonEmptyMatrix(dataY, "data_y");
     return fullLinearRegression(
-      toNumberMatrix(dataX, "the first argument (data_y)"),
-      toNumberMatrix(dataY, "the second argument (data_x)"),
+      toNumberMatrix(dataX, "data_x"),
+      toNumberMatrix(dataY, "data_y"),
       toBoolean(calculateB),
       toBoolean(verbose)
     );
@@ -731,8 +731,8 @@ export const LOGEST: AddFunctionDescription = {
   ): (number | string)[][] {
     assertNonEmptyMatrix(dataY, "data_y");
     const coeffs = fullLinearRegression(
-      toNumberMatrix(dataX, "the second argument (data_x)"),
-      logM(toNumberMatrix(dataY, "the first argument (data_y)")),
+      toNumberMatrix(dataX, "data_x"),
+      logM(toNumberMatrix(dataY, "data_y")),
       toBoolean(calculateB),
       toBoolean(verbose)
     );
@@ -1596,9 +1596,9 @@ export const TREND: AddFunctionDescription = {
   ): Matrix<number> {
     assertNonEmptyMatrix(knownDataY, "known_data_y");
     return predictLinearValues(
-      toNumberMatrix(knownDataY, "the first argument (known_data_y)"),
-      toNumberMatrix(knownDataX, "the second argument (known_data_x)"),
-      toNumberMatrix(newDataX, "the third argument (new_data_y)"),
+      toNumberMatrix(knownDataY, "known_data_y"),
+      toNumberMatrix(knownDataX, "known_data_x"),
+      toNumberMatrix(newDataX, "new_data_y"),
       toBoolean(b)
     );
   },
