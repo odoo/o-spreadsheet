2959941984c872c6fe74131f6d03017abdff3bbb
Rémi Rahir (rar) <rar@odoo.com>
2025-09-15 17:40:38 +0200

    [REF] TextInput: Extract logic in generic component
    Task: 4756754

---
+++ src/components/generic_input/generic_input.ts
@@ -0,0 +1,89 @@
+import { Component, useExternalListener, useRef } from "@odoo/owl";
+import { Ref } from "../../types";
+import { useAutofocus } from "../helpers/autofocus_hook";
+import { SpreadsheetChildEnv } from "@odoo/o-spreadsheet-engine/types/spreadsheet_env";
+
+export interface GenericInputProps {
+  value: string;
+  onChange: (value: string) => void;
+  class?: string;
+  id?: string;
+  placeholder?: string;
+  autofocus?: boolean;
+}
+
+export class GenericInput<T extends GenericInputProps> extends Component<T, SpreadsheetChildEnv> {
+  static props = {
+    value: String,
+    onChange: Function,
+    class: { type: String, optional: true },
+    id: { type: String, optional: true },
+    placeholder: { type: String, optional: true },
+    autofocus: { type: Boolean, optional: true },
+    alwaysShowBorder: { type: Boolean, optional: true },
+  };
+
+  protected refName = "input";
+  private inputRef!: Ref<HTMLInputElement>;
+
+  setup() {
+    this.inputRef = useRef(this.refName);
+    useExternalListener(
+      window,
+      "click",
+      (ev) => {
+        if (ev.target !== this.inputRef.el && this.inputRef.el?.value !== this.props.value) {
+          this.save();
+        }
+      },
+      { capture: true }
+    );
+    if (this.props.autofocus) {
+      useAutofocus({ refName: this.refName });
+    }
+  }
+
+  onKeyDown(ev: KeyboardEvent) {
+    switch (ev.key) {
+      case "Enter":
+        this.save();
+        ev.preventDefault();
+        ev.stopPropagation();
+        break;
+      case "Escape":
+        if (this.inputRef.el) {
+          this.inputRef.el.value = this.props.value;
+          this.inputRef.el.blur();
+        }
+        ev.preventDefault();
+        ev.stopPropagation();
+        break;
+    }
+  }
+
+  save() {
+    const currentValue = (this.inputRef.el?.value || "").trim();
+    if (currentValue !== this.props.value) {
+      this.props.onChange(currentValue);
+    }
+    this.inputRef.el?.blur();
+  }
+
+  onMouseDown(ev: MouseEvent) {
+    // Stop the event if the input is not focused, we handle everything in onMouseUp
+    if (ev.target !== document.activeElement) {
+      ev.preventDefault();
+      ev.stopPropagation();
+    }
+  }
+
+  onMouseUp(ev: MouseEvent) {
+    const target = ev.target as HTMLInputElement;
+    if (target !== document.activeElement) {
+      target.focus();
+      target.select();
+      ev.preventDefault();
+      ev.stopPropagation();
+    }
+  }
+}
