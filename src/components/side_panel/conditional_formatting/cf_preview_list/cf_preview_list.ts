6431aee78422f4dbd9895dda4e9e7762576a6bc7
rmbh-odoo <rmbh@odoo.com>
2025-09-16 18:51:07 +0530

    [REF] side_panel: split CF into main and editor panels, use store
    Before this commit:
    - The cf side panel handled both list and editor in a single panel.
    - The logic of the main panel and the editor was mixed,
      preventing direct access to the editor.
    - Business and UI logic for the editor were coupled.
    
    After this commit:
    - Use a store to manage the state of the editor panel.
    - Make the main panel and editor panel independent.
    - Change undo behavior in the editor to redirect back to the main panel.
    - Refactor related test cases.
    
    Task: 4982596

--- src/components/side_panel/conditional_formatting/cf_preview_list/cf_preview_list.ts
+++ src/components/side_panel/conditional_formatting/cf_preview_list/cf_preview_list.ts
@@ -1,38 +1,34 @@
-import { ICONS } from "@odoo/o-spreadsheet-engine/components/icons/icons";
+import { localizeCFRule } from "@odoo/o-spreadsheet-engine/helpers/locale";
 import { SpreadsheetChildEnv } from "@odoo/o-spreadsheet-engine/types/spreadsheet_env";
-import { Component, onWillUpdateProps, useRef } from "@odoo/owl";
-import { deepEquals } from "../../../../helpers";
+import { Component, useRef } from "@odoo/owl";
+import { zoneToXc } from "../../../../helpers";
 import { ConditionalFormat, UID } from "../../../../types";
 import { getBoundingRectAsPOJO } from "../../../helpers/dom_helpers";
 import { useDragAndDropListItems } from "../../../helpers/drag_and_drop_dom_items_hook";
 import { ConditionalFormatPreview } from "../cf_preview/cf_preview";
 
 interface Props {
-  conditionalFormats: ConditionalFormat[];
-  onPreviewClick: (cf: ConditionalFormat) => void;
-  onAddConditionalFormat: () => void;
+  onCloseSidePanel: () => void;
 }
 
 export class ConditionalFormatPreviewList extends Component<Props, SpreadsheetChildEnv> {
   static template = "o-spreadsheet-ConditionalFormatPreviewList";
   static props = {
-    conditionalFormats: Array,
-    onPreviewClick: Function,
-    onAddConditionalFormat: Function,
+    onCloseSidePanel: Function,
   };
   static components = { ConditionalFormatPreview };
-
-  icons = ICONS;
 
   private dragAndDrop = useDragAndDropListItems();
   private cfListRef = useRef("cfList");
 
-  setup() {
-    onWillUpdateProps((nextProps: Props) => {
-      if (!deepEquals(this.props.conditionalFormats, nextProps.conditionalFormats)) {
-        this.dragAndDrop.cancel();
-      }
-    });
+  get conditionalFormats(): ConditionalFormat[] {
+    const cfs = this.env.model.getters.getConditionalFormats(
+      this.env.model.getters.getActiveSheetId()
+    );
+    return cfs.map((cf) => ({
+      ...cf,
+      rule: localizeCFRule(cf.rule, this.env.model.getters.getLocale()),
+    }));
   }
 
   getPreviewDivStyle(cf: ConditionalFormat): string {
@@ -44,7 +40,7 @@ export class ConditionalFormatPreviewList extends Component<Props, SpreadsheetCh
     const previewRects = Array.from(this.cfListRef.el!.children).map((previewEl) =>
       getBoundingRectAsPOJO(previewEl)
     );
-    const items = this.props.conditionalFormats.map((cf, index) => ({
+    const items = this.conditionalFormats.map((cf, index) => ({
       id: cf.id,
       size: previewRects[index].height,
       position: previewRects[index].y,
@@ -58,8 +54,36 @@ export class ConditionalFormatPreviewList extends Component<Props, SpreadsheetCh
     });
   }
 
+  onAddConditionalFormat() {
+    const sheetId = this.env.model.getters.getActiveSheetId();
+    const zones = this.env.model.getters.getSelectedZones();
+    const cf: Omit<ConditionalFormat, "ranges"> = {
+      id: this.env.model.uuidGenerator.smallUuid(),
+      rule: {
+        type: "CellIsRule",
+        operator: "isNotEmpty",
+        style: { fillColor: "#b6d7a8" },
+        values: [],
+      },
+    };
+    this.env.model.dispatch("ADD_CONDITIONAL_FORMAT", {
+      cf,
+      ranges: zones.map((zone) => this.env.model.getters.getRangeDataFromZone(sheetId, zone)),
+      sheetId,
+    });
+    return this.env.replaceSidePanel("ConditionalFormattingEditor", "ConditionalFormatting", {
+      cf: {
+        ...cf,
+        ranges: zones.map((zone) =>
+          zoneToXc(this.env.model.getters.getUnboundedZone(sheetId, zone))
+        ),
+      },
+      isNewCf: true,
+    });
+  }
+
   private onDragEnd(cfId: UID, finalIndex: number) {
-    const originalIndex = this.props.conditionalFormats.findIndex((sheet) => sheet.id === cfId);
+    const originalIndex = this.conditionalFormats.findIndex((sheet) => sheet.id === cfId);
     const delta = originalIndex - finalIndex;
     if (delta !== 0) {
       this.env.model.dispatch("CHANGE_CONDITIONAL_FORMAT_PRIORITY", {
