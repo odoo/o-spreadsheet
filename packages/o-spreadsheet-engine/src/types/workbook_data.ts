75dfd6f086ef11a6c3f32d1487eaa84e573e6757
Rémi Rahir (rar) <rar@odoo.com>
2025-09-25 14:14:47 +0200

    [IMP] SheetProtection: introducing the sheet protection
    This revisions adds a (naive) protection for the full sheet.
    The ability to block specific modifications (style, grid structure, etc)
    is not strictly compatible with our collaborative approach as the server
    is never aware of the current state of the spreadsheet.
    
    Task: 5103728

--- packages/o-spreadsheet-engine/src/types/workbook_data.ts
+++ packages/o-spreadsheet-engine/src/types/workbook_data.ts
@@ -63,6 +63,7 @@ export interface SheetData {
   panes?: PaneDivision;
   headerGroups?: Record<Dimension, HeaderGroup[]>;
   color?: Color;
+  isLocked?: boolean;
 }
 
 interface WorkbookSettings {
--- src/components/grid/grid.ts
+++ src/components/grid/grid.ts
@@ -440,6 +440,9 @@ export class Grid extends Component<Props, SpreadsheetChildEnv> {
   }
 
   get isAutofillVisible(): boolean {
+    if (this.env.model.getters.isSheetLocked(this.env.model.getters.getActiveSheetId())) {
+      return false;
+    }
     const zone = this.env.model.getters.getSelectedZone();
     const rect = this.env.model.getters.getVisibleRect({
       left: zone.right,
