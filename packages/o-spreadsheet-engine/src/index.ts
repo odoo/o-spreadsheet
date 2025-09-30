c0683de99550e0fc7ae2644161484b05a47d5813
Pierre Rousseau <pro@odoo.com>
2025-09-30 08:21:18 +0200

    second codex test

--- packages/o-spreadsheet-engine/src/index.ts
+++ packages/o-spreadsheet-engine/src/index.ts
@@ -1,3 +1,20 @@
+export {
+  OP_PRIORITY,
+  convertAstNodes,
+  iterateAstNodes,
+  mapAst,
+  parse,
+  parseTokens,
+} from "./formulas/parser";
+export type {
+  AST,
+  ASTFuncall,
+  ASTOperation,
+  ASTString,
+  ASTSymbol,
+  ASTUnaryOperation,
+} from "./formulas/parser";
+export { rangeTokenize } from "./formulas/range_tokenizer";
 export { POSTFIX_UNARY_OPERATORS, tokenize } from "./formulas/tokenizer";
 export type { Token } from "./formulas/tokenizer";
 export * from "./helpers";
