d1b802bee21ee0b579613d9871b7fc19ec2884bf
Adrien Minne (adrm) <adrm@odoo.com>
2025-10-09 15:02:07 +0200

    [FIX] charts: wrong padding for humanize number section
    closes odoo/o-spreadsheet#7368
    
    Task: 5155591
    Signed-off-by: Rémi Rahir (rar) <rar@odoo.com>
    Signed-off-by: Adrien Minne (adrm) <adrm@odoo.com>

--- src/components/side_panel/chart/building_blocks/humanize_numbers/humanize_numbers.ts
+++ src/components/side_panel/chart/building_blocks/humanize_numbers/humanize_numbers.ts
@@ -1,3 +1,5 @@
+import { _t } from "@odoo/o-spreadsheet-engine";
+import { formatLargeNumber, formatValue } from "@odoo/o-spreadsheet-engine/helpers/format/format";
 import { SpreadsheetChildEnv } from "@odoo/o-spreadsheet-engine/types/spreadsheet_env";
 import { Component } from "@odoo/owl";
 import { ChartWithDataSetDefinition, DispatchResult, UID } from "../../../../../types";
@@ -21,4 +23,11 @@ export class ChartHumanizeNumbers extends Component<Props, SpreadsheetChildEnv> 
     updateChart: Function,
     canUpdateChart: Function,
   };
+
+  get title() {
+    const locale = this.env.model.getters.getLocale();
+    const format = formatLargeNumber({ value: 1234567 }, undefined, locale);
+    const value = formatValue(1234567, { format, locale });
+    return _t("E.g. 1234567 -> %(value)s", { value });
+  }
 }
