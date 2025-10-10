a93564cd381ff17171ebe8381bf9c9de5afbc793
rmbh-odoo <rmbh@odoo.com>
2025-10-10 11:33:50 +0530

    [FIX] composer: set editionMode to inactive when composer is unmounted
    Steps to reproduce:
    - Open the CF side panel and edit a StandaloneComposer inside a color scale.
    - Switch the sheet. The panel closes, so the composer is unmounted.
    - Previously, the composer remained in editing mode after unmount.
    
    Before this commit:
    - Unmounting the composer did not set editionMode to inactive.
    - This caused focus issues in other parts of the UI.
    
    After this commit:
    - Unmounting the composer sets editionMode to inactive.
    - Focus and selection are now properly released.
    
    Task: 5149215

--- src/components/composer/composer/abstract_composer_store.ts
+++ src/components/composer/composer/abstract_composer_store.ts
@@ -93,6 +93,7 @@ export abstract class AbstractComposerStore extends SpreadsheetStore {
     this.highlightStore.register(this);
     this.onDispose(() => {
       this.highlightStore.unRegister(this);
+      this._cancelEdition();
     });
   }
   protected abstract confirmEdition(content: string): void;
