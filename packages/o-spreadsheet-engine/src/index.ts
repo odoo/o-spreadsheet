5c365aab0c47e08a2beaed0d326528c0ea744ef1
Lucas Lefèvre (lul) <lul@odoo.com>
2025-11-04 12:46:44 +0100

    m

--- packages/o-spreadsheet-engine/src/index.ts
+++ packages/o-spreadsheet-engine/src/index.ts
@@ -1,3 +1,93 @@
+import { transformRangeData } from "./collaborative/ot/ot_helpers";
+import { ChartTerms } from "./components/translations_terms";
+import {
+  FIGURE_ID_SPLITTER,
+  GRID_ICON_EDGE_LENGTH,
+  GRID_ICON_MARGIN,
+  HIGHLIGHT_COLOR,
+  PIVOT_TABLE_CONFIG,
+} from "./constants";
+import { getFunctionsFromTokens } from "./formulas/helpers";
+import { arg } from "./functions/arguments";
+import { functionRegistry } from "./functions/function_registry";
+import { isEvaluationError, toBoolean, toJsDate, toNumber } from "./functions/helpers";
+import {
+  UuidGenerator,
+  deepCopy,
+  deepEquals,
+  getUniqueText,
+  isDefined,
+  isNumber,
+  lazy,
+  sanitizeSheetName,
+  splitReference,
+  unquote,
+} from "./helpers";
+import { ColorGenerator, colorToRGBA, rgbaToHex } from "./helpers/color";
+import { lettersToNumber, numberToLetters, toCartesian, toXC } from "./helpers/coordinates";
+import { isDateTime } from "./helpers/dates";
+import { createCurrencyFormat, formatValue } from "./helpers/format/format";
+import { urlRegistry } from "./helpers/links";
+import {
+  getFirstPivotFunction,
+  getNumberOfPivotFunctions,
+} from "./helpers/pivot/pivot_composer_helpers";
+import {
+  areDomainArgsFieldsValid,
+  createCustomFields,
+  createPivotFormula,
+  getMaxObjectId,
+  isDateOrDatetimeField,
+  parseDimension,
+  pivotNormalizationValueRegistry,
+  pivotToFunctionValueRegistry,
+  toFunctionPivotValue,
+  toNormalizedPivotValue,
+} from "./helpers/pivot/pivot_helpers";
+import { pivotRegistry } from "./helpers/pivot/pivot_registry";
+import { pivotTimeAdapter, pivotTimeAdapterRegistry } from "./helpers/pivot/pivot_time_adapter";
+import { computeTextWidth } from "./helpers/text_helper";
+import {
+  expandZoneOnInsertion,
+  isInside,
+  mergeContiguousZones,
+  overlap,
+  positionToZone,
+  reduceZoneOnDeletion,
+  toUnboundedZone,
+  toZone,
+  union,
+} from "./helpers/zones";
+import {
+  createEmptyExcelSheet,
+  createEmptySheet,
+  createEmptyWorkbookData,
+} from "./migrations/data";
+import { migrationStepRegistry } from "./migrations/migration_steps";
+import {
+  corePluginRegistry,
+  coreViewsPluginRegistry,
+  featurePluginRegistry,
+  statefulUIPluginRegistry,
+} from "./plugins";
+import { UNDO_REDO_PIVOT_COMMANDS } from "./plugins/ui_core_views/pivot_ui";
+import { autofillModifiersRegistry } from "./registries/autofill_modifiers";
+import { autofillRulesRegistry } from "./registries/autofill_rules";
+import { chartRegistry } from "./registries/chart_registry";
+import { chartSubtypeRegistry } from "./registries/chart_subtype_registry";
+import { clipboardHandlersRegistries } from "./registries/clipboardHandlersRegistries";
+import { iconsOnCellRegistry } from "./registries/icons_on_cell_registry";
+import { inverseCommandRegistry } from "./registries/inverse_command_registry";
+import { otRegistry } from "./registries/ot_registry";
+import {
+  repeatCommandTransformRegistry,
+  repeatLocalCommandTransformRegistry,
+} from "./registries/repeat_transform_registry";
+import { errorTypes } from "./types/errors";
+import { DEFAULT_LOCALE } from "./types/locale";
+import { isMatrix } from "./types/misc";
+export { EvaluationError } from "./types/errors";
+
 export { FunctionCodeBuilder, Scope, type FunctionCode } from "./formulas/code_builder";
 export {
   OPERATOR_MAP,
@@ -29,6 +119,9 @@ export { FunctionRegistry, categories } from "./functions/function_registry";
 export * from "./helpers";
 export { Model } from "./model";
 export { BasePlugin } from "./plugins/base_plugin";
+export { CorePlugin } from "./plugins/core_plugin";
+export { CoreViewPlugin } from "./plugins/core_view_plugin";
+export { UIPlugin } from "./plugins/ui_plugin";
 export { Registry } from "./registry";
 export { StateObserver } from "./state_observer";
 export * from "./translation";
@@ -42,4 +135,102 @@ export * from "./types/locale";
 export * from "./types/misc";
 export * from "./types/rendering";
 export * from "./types/validator";
+
+export const registries = {
+  autofillModifiersRegistry,
+  autofillRulesRegistry,
+  errorTypes,
+  functionRegistry,
+  featurePluginRegistry,
+  iconsOnCellRegistry,
+  statefulUIPluginRegistry,
+  coreViewsPluginRegistry,
+  corePluginRegistry,
+
+  chartRegistry,
+  chartSubtypeRegistry,
+
+  otRegistry,
+  inverseCommandRegistry,
+  urlRegistry,
+
+  repeatLocalCommandTransformRegistry,
+  repeatCommandTransformRegistry,
+  clipboardHandlersRegistries,
+  pivotRegistry,
+  pivotTimeAdapterRegistry,
+
+  pivotNormalizationValueRegistry,
+
+  pivotToFunctionValueRegistry,
+  migrationStepRegistry,
+};
+
+export const constants = {
+  DEFAULT_LOCALE,
+  HIGHLIGHT_COLOR,
+  PIVOT_TABLE_CONFIG,
+  ChartTerms,
+  FIGURE_ID_SPLITTER,
+  GRID_ICON_EDGE_LENGTH,
+  GRID_ICON_MARGIN,
+};
+
+console.log("coucou2");
+export const helpers = {
+  arg,
+  isEvaluationError,
+  toBoolean,
+  toJsDate,
+  toNumber,
+  toString,
+  toNormalizedPivotValue,
+  toFunctionPivotValue,
+  toXC,
+  toZone,
+  toUnboundedZone,
+  toCartesian,
+  numberToLetters,
+  lettersToNumber,
+  UuidGenerator,
+  formatValue,
+  createCurrencyFormat,
+  ColorGenerator,
+  computeTextWidth,
+  createEmptyWorkbookData,
+  createEmptySheet,
+  createEmptyExcelSheet,
+  rgbaToHex,
+  colorToRGBA,
+  positionToZone,
+  isDefined,
+  isMatrix,
+  lazy,
+  transformRangeData,
+  deepEquals,
+  overlap,
+  union,
+  isInside,
+  deepCopy,
+  expandZoneOnInsertion,
+  reduceZoneOnDeletion,
+  unquote,
+  getMaxObjectId,
+  getFunctionsFromTokens,
+  getFirstPivotFunction,
+  getNumberOfPivotFunctions,
+  parseDimension,
+  isDateOrDatetimeField,
+  mergeContiguousZones,
+  pivotTimeAdapter,
+  UNDO_REDO_PIVOT_COMMANDS,
+  createPivotFormula,
+  areDomainArgsFieldsValid,
+  splitReference,
+  sanitizeSheetName,
+  getUniqueText,
+  isNumber,
+  isDateTime,
+  createCustomFields,
+};
 export const __info__ = {};
