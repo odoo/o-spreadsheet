65714a5907fe451f2c87a4d2834b9b5e3862cb33
Adrien Minne (adrm) <adrm@odoo.com>
2025-11-04 09:57:37 +0100

    [IMP] text_input: allow to display input in error state
    This commit adds a new props `errorMessage` to the `TextInput` component,
    that displays the input in error with an icon and a tooltip with
    the error message.
    
    Task: 3149088

--- src/components/text_input/text_input.ts
+++ src/components/text_input/text_input.ts
@@ -4,18 +4,23 @@ import { GenericInput, GenericInputProps } from "../generic_input/generic_input"
 interface Props extends GenericInputProps {
   alwaysShowBorder?: boolean;
   value: string;
+  errorMessage?: string;
 }
 
 export class TextInput extends GenericInput<Props> {
   static template = "o-spreadsheet-TextInput";
   static components = {};
-  static props = GenericInput.props;
+  static props = {
+    ...GenericInput.props,
+    errorMessage: { type: String, optional: true },
+  };
 
   get inputClass(): string {
     return [
       this.props.class,
       "w-100 os-input",
       this.props.alwaysShowBorder ? "o-input-border" : undefined,
+      this.props.errorMessage ? "o-invalid" : undefined,
     ]
       .filter(isDefined)
       .join(" ");
