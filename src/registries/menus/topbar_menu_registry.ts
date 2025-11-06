703b01fccabdcf929fa910ea6fb477df3b309b29
Rémi Rahir (rar) <rar@odoo.com>
2025-11-06 10:14:25 +0000

    [FIX] TopbarMenu: specify `isReadonlyAllowed`
    The logic that displays the top-level items of the topbarmenu is
    isolated from the `Menu` component logic. More specifically, it never
    checks if the item can be used in readonly and as such, we never took
    the time to define the key `isReadonlyAllowed` on those menu items.
    
    However `RibbonMenu` relies on the logic of `Menu` so in order to
    display the topbar menu items properly in mobile mode, we need the key
    `isReadonlyAllowed` to be defined.
    
    Task: 5245432
    X-original-commit: e053137c8a865b52d267850759a9536313271aa7

--- src/registries/menus/topbar_menu_registry.ts
+++ src/registries/menus/topbar_menu_registry.ts
@@ -22,6 +22,7 @@ topbarMenuRegistry
   .add("file", {
     name: _t("File"),
     sequence: 10,
+    isReadonlyAllowed: true,
   })
   .addChild("settings", ["file"], {
     name: _t("Settings"),
@@ -38,6 +39,7 @@ topbarMenuRegistry
   .add("edit", {
     name: _t("Edit"),
     sequence: 20,
+    isReadonlyAllowed: true,
   })
   .addChild("undo", ["edit"], {
     ...ACTION_EDIT.undo,
@@ -124,6 +126,7 @@ topbarMenuRegistry
   .add("view", {
     name: _t("View"),
     sequence: 30,
+    isReadonlyAllowed: true,
   })
   .addChild("unfreeze_panes", ["view"], {
     ...ACTION_VIEW.unFreezePane,
@@ -222,6 +225,7 @@ topbarMenuRegistry
   .add("insert", {
     name: _t("Insert"),
     sequence: 40,
+    isReadonlyAllowed: true,
   })
   .addChild("insert_row", ["insert"], {
     ...ACTION_INSERT.insertRow,
@@ -339,7 +343,11 @@ topbarMenuRegistry
   // FORMAT MENU ITEMS
   // ---------------------------------------------------------------------
 
-  .add("format", { name: _t("Format"), sequence: 50 })
+  .add("format", {
+    name: _t("Format"),
+    sequence: 50,
+    isReadonlyAllowed: true,
+  })
   .addChild("format_number", ["format"], {
     ...formatNumberMenuItemSpec,
     name: _t("Number"),
@@ -433,6 +441,7 @@ topbarMenuRegistry
   .add("data", {
     name: _t("Data"),
     sequence: 60,
+    isReadonlyAllowed: true,
   })
   .addChild("sort_range", ["data"], {
     ...ACTION_DATA.sortRange,
