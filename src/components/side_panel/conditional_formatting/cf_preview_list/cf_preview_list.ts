bb16a2458ade6582f51e991e65dc60642c721329
Adrien Minne (adrm) <adrm@odoo.com>
2025-11-19 13:52:13 +0000

    [FIX] cf: conditional formatting preview is truncated for nothing
    In the side panel, the preview of a conditional formatting rule is
    truncated at 142px, even if there is enough space to display it fully.
    
    Task: 5344000
    X-original-commit: 5d0df51fb1e1c45fcb05abbfc654afe39ef95137

--- src/components/side_panel/conditional_formatting/cf_preview_list/cf_preview_list.ts
+++ src/components/side_panel/conditional_formatting/cf_preview_list/cf_preview_list.ts
@@ -6,6 +6,124 @@ import { useDragAndDropListItems } from "../../../helpers/drag_and_drop_hook";
 import { ICONS } from "../../../icons/icons";
 import { ConditionalFormatPreview } from "../cf_preview/cf_preview";
 
+<<<<<<< 7ed20c4cf11c264476cf597a313c82d45ef3e64a
+||||||| 60a66b6807132b8b1498059652eb156ce9d66854
+css/* scss */ `
+  .o-cf-preview-list {
+    .o-cf-preview {
+      &.o-cf-cursor-ptr {
+        cursor: pointer;
+      }
+
+      border-bottom: 1px solid #ccc;
+      height: 60px;
+      padding: 10px;
+      position: relative;
+      cursor: pointer;
+      &:hover,
+      &.o-cf-dragging {
+        background-color: #ebebeb;
+      }
+
+      &:not(:hover) .o-cf-delete-button {
+        display: none;
+      }
+      .o-cf-preview-icon {
+        border: 1px solid lightgrey;
+        position: absolute;
+        height: 50px;
+        width: 50px;
+      }
+      .o-cf-preview-description {
+        left: 65px;
+        margin-bottom: auto;
+        margin-right: 8px;
+        margin-top: auto;
+        position: relative;
+        width: 142px;
+        .o-cf-preview-description-rule {
+          margin-bottom: 4px;
+          font-weight: 600;
+          color: #303030;
+          max-height: 2.8em;
+          line-height: 1.4em;
+        }
+        .o-cf-preview-range {
+          font-size: 12px;
+        }
+      }
+      .o-cf-delete {
+        left: 90%;
+        top: 39%;
+        position: absolute;
+      }
+      &:not(:hover):not(.o-cf-dragging) .o-cf-drag-handle {
+        display: none !important;
+      }
+      .o-cf-drag-handle {
+        left: -8px;
+        cursor: move;
+        .o-icon {
+          width: 6px;
+          height: 30px;
+        }
+      }
+    }
+  }
+`;
+=======
+css/* scss */ `
+  .o-cf-preview-list {
+    .o-cf-preview {
+      &.o-cf-cursor-ptr {
+        cursor: pointer;
+      }
+
+      border-bottom: 1px solid #ccc;
+      height: 60px;
+      padding: 10px;
+      position: relative;
+      cursor: pointer;
+      &:hover,
+      &.o-cf-dragging {
+        background-color: #ebebeb;
+      }
+
+      &:not(:hover) .o-cf-delete-button {
+        display: none;
+      }
+      .o-cf-preview-icon {
+        border: 1px solid lightgrey;
+        height: 50px;
+        width: 50px;
+      }
+      .o-cf-preview-description {
+        .o-cf-preview-description-rule {
+          margin-bottom: 4px;
+          font-weight: 600;
+          color: #303030;
+          max-height: 2.8em;
+          line-height: 1.4em;
+        }
+        .o-cf-preview-range {
+          font-size: 12px;
+        }
+      }
+      &:not(:hover):not(.o-cf-dragging) .o-cf-drag-handle {
+        display: none !important;
+      }
+      .o-cf-drag-handle {
+        left: 2px;
+        cursor: move;
+        .o-icon {
+          width: 6px;
+          height: 30px;
+        }
+      }
+    }
+  }
+`;
+>>>>>>> 8879ca80b4a87feee971293b478ea51fed01fa35
 interface Props {
   conditionalFormats: ConditionalFormat[];
   onPreviewClick: (cf: ConditionalFormat) => void;
