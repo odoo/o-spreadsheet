418f89ced6756762f6625ccc73711c4e48fa5892
Rémi Rahir (rar) <rar@odoo.com>
2025-11-19 11:38:09 +0100

    [IMP] Menu: Delegate enabled to children items
    Currently, if a menu is evaluated as disabled, we disable it and prevent
    the possibility to see its children by hover. However, there are some
    issues with this:
    - the user is mislead because we keep showing the arrow that suggest the
      presence of children items
    - the user can simply not navigate through the submenus to see what kind
      of feature could be available
    - if a menu has several children some of which are readonlyAllowed, then
      the readonlyAllowed condition has to be set on the parent as well; the
      'enabling' condition therefore becomes a combination set on both the
      parent and children
    - if a parent menu is computed as 'enabled' it will not be greyed out
      (suggests that it has useful children) even if the said children are
      not enabled.
    
    This revision changes the computation of 'enabled' state of item by
    looking at the full tree of children and applies the following logic;
    A parent menu is disabled if every leaf of the tree is disabled. A
    single 'enabled' leaf (that is a child without children of its own) will
    make all its parent enabled as well, up to the root. We also allow all
    submenus to be opened regardless of the state of the parent.
    
    The user will therefore have a visual hint that there are active
    submenus or not (if all children are disabled, so will be the parent)
    and they will also have the possibility to have a look at all children
    to explore the possibilities.
    
    Task: 5331717

--- tests/menus/menu_component.test.ts
+++ tests/menus/menu_component.test.ts
@@ -1,3 +1,4 @@
+import { Model } from "@odoo/o-spreadsheet-engine";
 import { createActions } from "../../src/actions/action";
 import { Menu } from "../../src/components/menu/menu";
 import { simulateClick } from "../test_helpers/dom_helper";
@@ -18,6 +19,30 @@ describe("Menu component", () => {
     const selector = ".o-menu div[data-name='test_menu']";
     const { fixture } = await mountComponent(Menu, {
       props: { menuItems, onClose: () => {}, onClickMenu: () => callback() },
+    });
+
+    expect(fixture.querySelector(selector)!.classList).toContain("disabled");
+    await simulateClick(selector);
+    expect(callback).not.toHaveBeenCalled();
+  });
+
+  test("Execute is not called when menu item is ont readonly allowed", async () => {
+    const callback = jest.fn();
+    const menuItems = createActions([
+      {
+        id: "test_menu",
+        name: "Test Menu",
+        isEnabled: () => true,
+        execute: () => {},
+      },
+    ]);
+
+    const selector = ".o-menu div[data-name='test_menu']";
+    const model = new Model();
+    model.updateMode("readonly");
+    const { fixture } = await mountComponent(Menu, {
+      props: { menuItems, onClose: () => {}, onClickMenu: () => callback() },
+      env: { model },
     });
 
     expect(fixture.querySelector(selector)!.classList).toContain("disabled");
