ab325f29c5a58af29da91d86da03f1bc3fb65f7c
Adrien Minne (adrm) <adrm@odoo.com>
2025-11-21 14:01:08 +0100

    [FIX] header_size: wrong row size from wrapped number
    If a literal cell containing a number was wrapped (because the number
    was large, or because it had a long format), the row height was wrong.
    
    closes odoo/o-spreadsheet#7527
    
    Task: 4878338
    Signed-off-by: Rémi Rahir (rar) <rar@odoo.com>

--- src/plugins/ui_core_views/header_sizes_ui.ts
+++ src/plugins/ui_core_views/header_sizes_ui.ts
@@ -53,6 +53,7 @@ export class HeaderSizeUIPlugin extends UIPlugin<HeaderSizeState> implements Hea
   handle(cmd: Command) {
     switch (cmd.type) {
       case "START":
+      case "UPDATE_LOCALE":
         for (const sheetId of this.getters.getSheetIds()) {
           this.initializeSheet(sheetId);
         }
@@ -173,7 +174,7 @@ export class HeaderSizeUIPlugin extends UIPlugin<HeaderSizeState> implements Hea
     const cell = this.getters.getCell(position);
 
     const colSize = this.getters.getColSize(position.sheetId, position.col);
-    return getDefaultCellHeight(this.ctx, cell, colSize);
+    return getDefaultCellHeight(this.ctx, cell, this.getters.getLocale(), colSize);
   }
 
   private isInMultiRowMerge(position: CellPosition): boolean {
