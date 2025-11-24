24b71c2ce7a5ec369d7dbd31b06e16624336bab2
Lucas Lefèvre (lul) <lul@odoo.com>
2025-11-24 17:00:30 +0100

    [REF] charts: remove useless getter
    This getter is only used once and the formatted values can be taken from
    the evaluated cells without the need to iterate over the entire range again.

--- packages/o-spreadsheet-engine/src/plugins/ui_core_views/cell_evaluation/evaluation_plugin.ts
+++ packages/o-spreadsheet-engine/src/plugins/ui_core_views/cell_evaluation/evaluation_plugin.ts
@@ -10,7 +10,7 @@ import {
   invalidateDependenciesCommands,
   invalidateEvaluationCommands,
 } from "../../../types/commands";
-import { Format, FormattedValue } from "../../../types/format";
+import { Format } from "../../../types/format";
 import {
   CellPosition,
   FunctionResultObject,
@@ -148,7 +148,6 @@ export class EvaluationPlugin extends CoreViewPlugin {
     "evaluateFormulaResult",
     "evaluateCompiledFormula",
     "getCorrespondingFormulaCell",
-    "getRangeFormattedValues",
     "getRangeValues",
     "getEvaluatedCell",
     "getEvaluatedCells",
@@ -248,17 +247,6 @@ export class EvaluationPlugin extends CoreViewPlugin {
   }
 
   /**
-   * Return the value of each cell in the range as they are displayed in the grid.
-   */
-  getRangeFormattedValues(range: Range): FormattedValue[] {
-    const sheet = this.getters.tryGetSheet(range.sheetId);
-    if (sheet === undefined) {
-      return [];
-    }
-    return this.mapVisiblePositions(range, (p) => this.getters.getEvaluatedCell(p).formattedValue);
-  }
-
-  /**
    * Return the value of each cell in the range.
    */
   getRangeValues(range: Range): EvaluatedCell[] {
@@ -269,20 +257,6 @@ export class EvaluationPlugin extends CoreViewPlugin {
     return this.mapVisiblePositions(range, (p) => this.getters.getEvaluatedCell(p));
   }
 
-<<<<<<< HEAD
-  /**
-   * Return the format of each cell in the range.
-   */
-  getRangeFormats(range: Range): (Format | undefined)[] {
-    const sheet = this.getters.tryGetSheet(range.sheetId);
-    if (sheet === undefined) {
-      return [];
-    }
-    return this.getters.getEvaluatedCellsInZone(sheet.id, range.zone).map((cell) => cell.format);
-  }
-
-=======
->>>>>>> 36957c47b ([REF] charts: format doesn't depend on ranges)
   getEvaluatedCell(position: CellPosition): EvaluatedCell {
     return this.evaluator.getEvaluatedCell(position);
   }
