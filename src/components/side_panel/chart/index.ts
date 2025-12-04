322b6601fd4f62a8a25c73030f85a68e96123bea
Anthony Hendrickx <anhe@odoo.com>
2025-12-04 16:00:47 +0100

    [FIX] charts: remove zoom slicer for scatter plot
    Task Description
    
    This PR aims to remove the zoomable feature for the scatter plot, as
    it's kind of non-sense to be able to zoom on an axis and not on the
    other for this type of chart. Moreover, we will soon be able to
    manually set the min/max of each axis manually (in master).
    
    Related Task
    
    Task: 5388389

--- src/components/side_panel/chart/index.ts
+++ src/components/side_panel/chart/index.ts
@@ -22,7 +22,6 @@ import { ScorecardChartDesignPanel } from "./scorecard_chart_panel/scorecard_cha
 import { SunburstChartDesignPanel } from "./sunburst_chart/sunburst_chart_design_panel";
 import { TreeMapChartDesignPanel } from "./treemap_chart/treemap_chart_design_panel";
 import { WaterfallChartDesignPanel } from "./waterfall_chart/waterfall_chart_design_panel";
-import { GenericZoomableChartDesignPanel } from "./zoomable_chart/design_panel";
 
 export { BarConfigPanel } from "./bar_chart/bar_chart_config_panel";
 export { GenericChartConfigPanel } from "./building_blocks/generic_side_panel/config_panel";
@@ -47,7 +46,7 @@ chartSidePanelComponentRegistry
   })
   .add("scatter", {
     configuration: ScatterConfigPanel,
-    design: GenericZoomableChartDesignPanel,
+    design: ChartWithAxisDesignPanel,
   })
   .add("bar", {
     configuration: BarConfigPanel,
--- src/types/chart/scatter_chart.ts
+++ src/types/chart/scatter_chart.ts
@@ -1,7 +1,7 @@
 import { LineChartDefinition, LineChartRuntime } from "./line_chart";
 
 export interface ScatterChartDefinition
-  extends Omit<LineChartDefinition, "type" | "stacked" | "cumulative"> {
+  extends Omit<LineChartDefinition, "type" | "stacked" | "cumulative" | "zoomable"> {
   readonly type: "scatter";
 }
 
