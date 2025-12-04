d74fc9aebab59470989d9e9d6dad68662b0c194e
Anthony Hendrickx <anhe@odoo.com>
2025-12-04 15:00:47 +0000

    [FIX] charts: remove zoom slicer for scatter plot
    Task Description
    
    This PR aims to remove the zoomable feature for the scatter plot, as
    it's kind of non-sense to be able to zoom on an axis and not on the
    other for this type of chart. Moreover, we will soon be able to
    manually set the min/max of each axis manually (in master).
    
    Related Task
    
    closes odoo/o-spreadsheet#8059
    
    Task: 5388389
    X-original-commit: 61f0761fd5630e0fbc7358d24c4b4e0d95eb584b
    Signed-off-by: Hendrickx Anthony (anhe) <anhe@odoo.com>
    Signed-off-by: Rémi Rahir (rar) <rar@odoo.com>

--- tests/test_helpers/chart_helpers.ts
+++ tests/test_helpers/chart_helpers.ts
@@ -134,7 +134,7 @@ export const GENERAL_CHART_CREATION_CONTEXT: Required<ChartCreationContext> = {
   headerDesign: { bold: false },
   treemapColoringOptions: { type: "categoryColor", colors: [], useValueBasedGradient: true },
   showHeaders: true,
-  zoomable: false,
+  zoomable: true,
   humanize: false,
   slicesColors: [],
 };
