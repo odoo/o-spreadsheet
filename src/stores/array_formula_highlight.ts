22244de19425c3cb119ca67ec8b1a22982e49733
dhrp-odoo <dhrp@odoo.com>
2025-12-10 11:21:56 +0530

    [FIX] array_formula_highlight: avoid false spill outline
    How to reproduce:
    - Type =MUNIT(2) in A1, and 5 in A2 so that A1 has a #SPILL error
    - In A4 type =A1:B2
    - When the selection is in A4, the highlight of the array formula
    becomes a SPILL highlight (dotted line). But it shouldn't, the
    formula in A4 is spilled correctly
    
    This commit: track true spill-block status instead of relying on the
    evaluated value, expose a spill-block getter, and use it in the
    highlight store so spilled formulas that return #SPILL! as data
    stay solid-highlighted.
    
    closes odoo/o-spreadsheet#7623
    
    Task: 5403906
    Signed-off-by: Lucas Lefèvre (lul) <lul@odoo.com>

--- src/stores/array_formula_highlight.ts
+++ src/stores/array_formula_highlight.ts
@@ -1,6 +1,5 @@
 import { Get } from "../store_engine";
-import { Highlight, Zone } from "../types";
-import { CellErrorType } from "../types/errors";
+import { Highlight } from "../types";
 import { HighlightStore } from "./highlight_store";
 import { SpreadsheetStore } from "./spreadsheet_store";
 
@@ -13,21 +12,22 @@ export class ArrayFormulaHighlight extends SpreadsheetStore {
   }
 
   get highlights(): Highlight[] {
-    let zone: Zone | undefined;
     const position = this.model.getters.getActivePosition();
-    const cell = this.getters.getEvaluatedCell(position);
     const spreader = this.model.getters.getArrayFormulaSpreadingOn(position);
-    zone = spreader
+    const zone = spreader
       ? this.model.getters.getSpreadZone(spreader, { ignoreSpillError: true })
       : this.model.getters.getSpreadZone(position, { ignoreSpillError: true });
     if (!zone) {
       return [];
     }
+    const isArrayFormulaBlocked = this.model.getters.isArrayFormulaSpillBlocked(
+      spreader ?? position
+    );
     return [
       {
         sheetId: position.sheetId,
         zone,
-        dashed: cell.value === CellErrorType.SpilledBlocked,
+        dashed: isArrayFormulaBlocked,
         color: "#17A2B8",
         noFill: true,
         thinLine: true,
