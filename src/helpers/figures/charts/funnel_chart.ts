ebdf01967f101f553caa8b5b1aa3f7b9789a7946
Lucas Lefèvre (lul) <lul@odoo.com>
2025-12-17 12:00:58 +0100

    [REF] chart: factorize copyInSheetId

--- src/helpers/figures/charts/funnel_chart.ts
+++ src/helpers/figures/charts/funnel_chart.ts
@@ -4,6 +4,7 @@ import { AbstractChart } from "@odoo/o-spreadsheet-engine/helpers/figures/charts
 import {
   checkDataset,
   checkLabelRange,
+  copyChartDataSourceInSheetId,
   createDataSets,
   duplicateDataSourceInDuplicatedSheet,
   duplicateLabelRangeInDuplicatedSheet,
@@ -118,15 +119,12 @@ export class FunnelChart extends AbstractChart {
   }
 
   copyInSheetId(sheetId: UID): FunnelChart {
-    const dataSource = {
-      dataSets: this.definition.dataSource.dataSets.map((dataSet) => {
-        const range = this.getters.getRangeFromSheetXC(this.sheetId, dataSet.dataRange);
-        return {
-          ...dataSet,
-          dataRange: this.getters.getRangeString(range, sheetId),
-        };
-      }),
-    };
+    const dataSource = copyChartDataSourceInSheetId(
+      this.getters,
+      this.sheetId,
+      sheetId,
+      this.definition.dataSource
+    );
     const definition = this.getDefinitionWithSpecificDataSets(dataSource, this.labelRange, sheetId);
     return new FunnelChart(definition, sheetId, this.getters);
   }
