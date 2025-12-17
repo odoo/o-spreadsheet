1017bde9ec6e4439479621ff54794d9d08d0aa4b
Lucas Lefèvre (lul) <lul@odoo.com>
2025-12-17 12:00:58 +0100

    [REF] chart: factorize copyInSheetId

--- src/helpers/figures/charts/waterfall_chart.ts
+++ src/helpers/figures/charts/waterfall_chart.ts
@@ -3,6 +3,7 @@ import { AbstractChart } from "@odoo/o-spreadsheet-engine/helpers/figures/charts
 import {
   checkDataset,
   checkLabelRange,
+  copyChartDataSourceInSheetId,
   createDataSets,
   duplicateDataSourceInDuplicatedSheet,
   duplicateLabelRangeInDuplicatedSheet,
@@ -129,15 +130,12 @@ export class WaterfallChart extends AbstractChart {
   }
 
   copyInSheetId(sheetId: UID): WaterfallChart {
-    const dataSource = {
-      dataSets: this.definition.dataSource.dataSets.map((dataSet) => {
-        const range = this.getters.getRangeFromSheetXC(this.sheetId, dataSet.dataRange);
-        return {
-          ...dataSet,
-          dataRange: this.getters.getRangeString(range, sheetId),
-        };
-      }),
-    };
+    const dataSource = copyChartDataSourceInSheetId(
+      this.getters,
+      this.sheetId,
+      sheetId,
+      this.definition.dataSource
+    );
     const definition = this.getDefinitionWithSpecificDataSets(dataSource, this.labelRange, sheetId);
     return new WaterfallChart(definition, sheetId, this.getters);
   }
