7c8456d9357d87bfb9985f76acce58177567ac1c
Rémi Rahir (rar) <rar@odoo.com>
2025-12-31 10:14:19 +0100

    [FIX] Actions: ensure the sequence is applied on action children
    Currently, the helper 'createActions' pre-sorts a list of menuRegistry
    entries but this sort is not applied to the children (which can be
    generated dynamically). It never showed up as we rarely add children
    entries from different sources and their sequence often matches the
    order of insertion in the registry.
    
    closes odoo/o-spreadsheet#7708
    
    Task: 5452669
    Signed-off-by: Lucas Lefèvre (lul) <lul@odoo.com>

--- src/actions/action.ts
+++ src/actions/action.ts
@@ -101,7 +101,8 @@ export function createAction(item: ActionSpec): Action {
           return children
             .map((child) => (typeof child === "function" ? child(env) : child))
             .flat()
-            .map(createAction);
+            .map(createAction)
+            .sort((a, b) => a.sequence - b.sequence);
         }
       : () => [],
     isReadonlyAllowed: item.isReadonlyAllowed || false,
