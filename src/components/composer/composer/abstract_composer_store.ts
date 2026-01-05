6924fc029df3bb3dcb52f214d9fd97429b0e8bf7
Rémi Rahir (rar) <rar@odoo.com>
2026-01-05 14:04:54 +0000

    [FIX] Composer: Capture the correct selection on `F2`
    How to reproduce:
    - Write =F9 in a composer
    - close and reopen it
    - press `F2` to edit the range
    - press `arrowDown`
    
    -> the range should be edited to F10 and is set to F7 instead.
    
    Task: 5462713
    X-original-commit: ef7b40f5c3b328acde98aaa20294c1cde5f891c4

--- src/components/composer/composer/abstract_composer_store.ts
+++ src/components/composer/composer/abstract_composer_store.ts
@@ -416,7 +416,7 @@ export abstract class AbstractComposerStore extends SpreadsheetStore {
     this.model.selection.capture(
       this,
       {
-        cell: { col: col ?? zone.left, row: row ?? zone.right },
+        cell: { col: col ?? zone.left, row: row ?? zone.top },
         zone,
       },
       {
