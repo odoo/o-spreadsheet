dcd419a83e852d06f7d22a3b82a75d4568418d01
Lucas Lefèvre (lul) <lul@odoo.com>
2026-01-05 09:26:12 +0100

    [FIX] range: add range to NONE
    Task: 5095364
    Part-of: odoo/o-spreadsheet#7831
    Signed-off-by: Pierre Rousseau (pro) <pro@odoo.com>
    Signed-off-by: Rémi Rahir (rar) <rar@odoo.com>

--- src/helpers/pivot/pivot_registry.ts
+++ src/helpers/pivot/pivot_registry.ts
@@ -90,8 +90,6 @@ function adaptPivotRange(
   }
   const change = applyChange(range);
   switch (change.changeType) {
-    case "NONE":
-      return range;
     case "REMOVE":
       return undefined;
     default:
