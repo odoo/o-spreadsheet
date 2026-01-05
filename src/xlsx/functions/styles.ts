b80bf3286de940890012504cfa3a4eabdaeb34ab
Florian Damhaut <flda@odoo.com>
2026-01-05 10:36:05 +0000

    Cherry pick of f4213f76fc52b6fdc1dde0eabaa7e021f1c961be failed
    stdout:
    Auto-merging src/constants.ts
    Auto-merging src/plugins/core/cell.ts
    CONFLICT (content): Merge conflict in src/plugins/core/cell.ts
    Auto-merging tests/cells/style_plugin.test.ts
    
    stderr:

--- src/xlsx/functions/styles.ts
+++ src/xlsx/functions/styles.ts
@@ -154,6 +154,9 @@ export function addStyles(styles: XLSXStyle[]): XMLString {
     if (style.alignment && style.alignment.wrapText) {
       alignAttrs.push(["wrapText", "1"]);
     }
+    if (style.alignment && style.alignment.shrinkToFit) {
+      alignAttrs.push(["shrinkToFit", "1"]);
+    }
 
     if (alignAttrs.length > 0) {
       attributes.push(["applyAlignment", "1"]); // for Libre Office
