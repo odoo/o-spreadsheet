d3f6219fe8dc7bbe39500e03f3b20239ce26a6a1
Lucas Lefèvre (lul) <lul@odoo.com>
2026-01-06 13:37:54 +0100

    [REF] charts: factorize definition transformation

--- src/helpers/figures/charts/combo_chart.ts
+++ src/helpers/figures/charts/combo_chart.ts
@@ -9,7 +9,6 @@ import {
   createDataSets,
   duplicateDataSourceInDuplicatedSheet,
   getDefinedAxis,
-  transformChartDefinitionWithDataSetsWithZone,
   updateChartRangesWithDataSets,
 } from "@odoo/o-spreadsheet-engine/helpers/figures/charts/chart_common";
 import { CHART_COMMON_OPTIONS } from "@odoo/o-spreadsheet-engine/helpers/figures/charts/chart_ui_common";
@@ -30,7 +29,6 @@ import {
   ExcelChartDefinition,
   Getters,
   Range,
-  RangeAdapter,
   UID,
 } from "../../../types";
 import {
@@ -65,14 +63,6 @@ export class ComboChart extends AbstractChart {
     super(definition, sheetId, getters);
     this.dataSets = createDataSets(getters, sheetId, definition.dataSource);
     this.labelRange = createValidRange(getters, sheetId, definition.dataSource.labelRange);
-  }
-
-  static transformDefinition(
-    chartSheetId: UID,
-    definition: ComboChartDefinition,
-    applyChange: RangeAdapter
-  ): ComboChartDefinition {
-    return transformChartDefinitionWithDataSetsWithZone(chartSheetId, definition, applyChange);
   }
 
   static validateChartDefinition(
