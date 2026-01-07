f671efaefe0e5cf24239750f447d0107c9168872
Lucas Lefèvre (lul) <lul@odoo.com>
2026-01-07 09:19:18 +0100

    [REF] charts: factorize copy on another sheet

--- src/helpers/figures/charts/calendar_chart.ts
+++ src/helpers/figures/charts/calendar_chart.ts
@@ -3,7 +3,6 @@ import { AbstractChart } from "@odoo/o-spreadsheet-engine/helpers/figures/charts
 import {
   checkDataset,
   checkLabelRange,
-  copyChartDataSourceInSheetId,
   createDataSets,
   updateChartRangesWithDataSets,
 } from "@odoo/o-spreadsheet-engine/helpers/figures/charts/chart_common";
@@ -115,17 +114,6 @@ export class CalendarChart extends AbstractChart {
       },
       auxiliaryRange: definition.dataSource.labelRange,
     };
-  }
-
-  copyInSheetId(sheetId: UID): CalendarChart {
-    const dataSource = copyChartDataSourceInSheetId(
-      this.getters,
-      this.sheetId,
-      sheetId,
-      this.definition.dataSource
-    );
-    const definition = this.getDefinitionWithSpecificDataSets(dataSource);
-    return new CalendarChart(definition, sheetId, this.getters);
   }
 
   getDefinition(): CalendarChartDefinition {
