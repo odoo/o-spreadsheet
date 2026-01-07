1902da4ae1d24f2bb92145a8691c8833815bf0d6
Lucas Lefèvre (lul) <lul@odoo.com>
2026-01-07 09:19:18 +0100

    [REF] charts: factorize copy on another sheet

--- src/helpers/figures/charts/geo_chart.ts
+++ src/helpers/figures/charts/geo_chart.ts
@@ -4,7 +4,6 @@ import { AbstractChart } from "@odoo/o-spreadsheet-engine/helpers/figures/charts
 import {
   checkDataset,
   checkLabelRange,
-  copyChartDataSourceInSheetId,
   createDataSets,
   updateChartRangesWithDataSets,
 } from "@odoo/o-spreadsheet-engine/helpers/figures/charts/chart_common";
@@ -84,17 +83,6 @@ export class GeoChart extends AbstractChart {
       ...definition,
       auxiliaryRange: definition.dataSource.labelRange,
     };
-  }
-
-  copyInSheetId(sheetId: UID): GeoChart {
-    const dataSource = copyChartDataSourceInSheetId(
-      this.getters,
-      this.sheetId,
-      sheetId,
-      this.definition.dataSource
-    );
-    const definition = this.getDefinitionWithSpecificDataSets(dataSource);
-    return new GeoChart(definition, sheetId, this.getters);
   }
 
   getDefinition(): GeoChartDefinition {
