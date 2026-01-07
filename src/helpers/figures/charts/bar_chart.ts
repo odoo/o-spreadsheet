ef2514316412b21a4456d5c2d9afb1dcffb17722
Lucas Lefèvre (lul) <lul@odoo.com>
2026-01-07 12:46:14 +0100

    [REF] charts: post process data source

--- src/helpers/figures/charts/bar_chart.ts
+++ src/helpers/figures/charts/bar_chart.ts
@@ -102,14 +102,7 @@ export class BarChart extends AbstractChart {
   }
 
   getDefinition(): BarChartDefinition {
-    return this.getDefinitionWithSpecificDataSets({
-      ...this.definition.dataSource,
-      dataSets: this.dataSets.map(({ dataSetId, dataRange }) => ({
-        dataSetId,
-        dataRange: this.getters.getRangeString(dataRange, this.sheetId),
-      })),
-      labelRange: this.labelRange && this.getters.getRangeString(this.labelRange, this.sheetId),
-    });
+    return this.getDefinitionWithSpecificDataSets(this.definition.dataSource);
   }
 
   private getDefinitionWithSpecificDataSets(dataSource: ChartRangeDataSource): BarChartDefinition {
