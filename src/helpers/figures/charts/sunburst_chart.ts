0b74b6e964e03b51a085c38c71e7206d7aefdfa7
Lucas Lefèvre (lul) <lul@odoo.com>
2026-01-07 12:46:14 +0100

    [REF] charts: post process data source

--- src/helpers/figures/charts/sunburst_chart.ts
+++ src/helpers/figures/charts/sunburst_chart.ts
@@ -99,14 +99,7 @@ export class SunburstChart extends AbstractChart {
   }
 
   getDefinition(): SunburstChartDefinition {
-    return this.getDefinitionWithSpecificDataSets({
-      ...this.definition.dataSource,
-      dataSets: this.dataSets.map(({ dataSetId, dataRange }) => ({
-        dataSetId,
-        dataRange: this.getters.getRangeString(dataRange, this.sheetId),
-      })),
-      labelRange: this.labelRange && this.getters.getRangeString(this.labelRange, this.sheetId),
-    });
+    return this.getDefinitionWithSpecificDataSets(this.definition.dataSource);
   }
 
   getContextCreation(): ChartCreationContext {
