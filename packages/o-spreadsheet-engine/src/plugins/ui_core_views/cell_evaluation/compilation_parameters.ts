6472e217a9669f99e57aaa6a6edec84248917d7b
Alexis Lacroix <laa@odoo.com>
2026-01-16 12:08:37 +0100

    [REF] compilation_parameters: store the matrix dependencies once accessed
    Task: 5905722

--- packages/o-spreadsheet-engine/src/plugins/ui_core_views/cell_evaluation/compilation_parameters.ts
+++ packages/o-spreadsheet-engine/src/plugins/ui_core_views/cell_evaluation/compilation_parameters.ts
@@ -40,8 +40,6 @@ export function buildCompilationParameters(
 class CompilationParametersBuilder {
   evalContext: EvalContext;
 
-  private rangeCache: Record<string, Matrix<FunctionResultObject>> = {};
-
   constructor(
     context: ModelConfig["custom"],
     private getters: Getters,
@@ -109,16 +107,6 @@ class CompilationParametersBuilder {
       return [[]];
     }
 
-    if (this.evalContext.__originCellPosition) {
-      this.evalContext.currentFormulaDependencies?.push(range);
-    }
-
-    const { top, left, bottom, right } = zone;
-    const cacheKey = `${sheetId}-${top}-${left}-${bottom}-${right}`;
-    if (cacheKey in this.rangeCache) {
-      return this.rangeCache[cacheKey];
-    }
-
     const height = _zone.bottom - _zone.top + 1;
     const width = _zone.right - _zone.left + 1;
     const matrix: Matrix<FunctionResultObject> = new Array(width);
@@ -130,11 +118,10 @@ class CompilationParametersBuilder {
       for (let row = _zone.top; row <= _zone.bottom; row++) {
         const rowIndex = row - _zone.top;
         const position = { sheetId, col, row };
-        matrix[colIndex][rowIndex] = this.computeCell(position);
+        matrix[colIndex][rowIndex] = this.getRef(position);
       }
     }
 
-    this.rangeCache[cacheKey] = matrix;
     return matrix;
   }
 
