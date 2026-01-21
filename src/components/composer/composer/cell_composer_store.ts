861e175ac423c4949344138f539e56b4313b92bb
Rémi Rahir (rar) <rar@odoo.com>
2026-01-21 16:09:19 +0100

    [FIX] Evaluation: Provide cell position for isolated formula evaluation
    Some formulas, like ROW & COLUMN need to know the coordinates of the
    cell on which they are applied to be computed and we did not provide
    those in the evaluation context when evaluating isolated formulas.
    
    How to reproduce:
    
    - in a spreadsheet, go to the last row
    - in a cell of the last row, write the formula `=MUNIT(ROW())`
    -> the formula is in #SPILL error because we did not extend the size of
    the spreadsheet
    
    This also cause issues in standalone composers in which the hovered
    result displays an error.
    
    How to reproduce:
    
    - in a standalone composer, write =ROW()
    - hover the ROW function
    -> it displays #ERROR
    
    Task: 5798610

--- src/components/composer/composer/cell_composer_store.ts
+++ src/components/composer/composer/cell_composer_store.ts
@@ -245,7 +245,11 @@ export class CellComposerStore extends AbstractComposerStore {
       return;
     }
 
-    const evaluated = this.getters.evaluateFormula(this.sheetId, content);
+    const evaluated = this.getters.evaluateFormula(this.sheetId, content, {
+      sheetId: this.sheetId,
+      col: this.col,
+      row: this.row,
+    });
     if (!isMatrix(evaluated)) {
       return;
     }
@@ -294,5 +298,13 @@ export class CellComposerStore extends AbstractComposerStore {
       return false;
     }
     return true;
+  }
+
+  protected evaluateCanonicalFormula(canonicalFormula: string) {
+    return this.getters.evaluateFormulaResult(this.sheetId, canonicalFormula, {
+      sheetId: this.sheetId,
+      col: this.col,
+      row: this.row,
+    });
   }
 }
