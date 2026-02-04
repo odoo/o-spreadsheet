6f6cc8b309011318736a0293a6a01d37782e5cea
rmbh-odoo <rmbh@odoo.com>
2026-02-04 10:46:49 +0000

    [IMP] pivot: truncate dimension display name and add title
    Before this commit:
    - Long dimension display names were cropped and not fully readable.
    
    After this commit:
    - Dimension display names are truncated using text-truncate and include
      a title attribute to show the full value on hover.
    - Only the first and last fields are displayed in the format
      `first field > ... > last field`, as these are the most relevant.
    
    Technical:
    - Update the type of dimension props to include displayName to ensure proper
      type safety.
    
    Task: 5895856
    X-original-commit: 269cec2a566725c18a8f09a8380d315c6b11c457

--- src/components/side_panel/pivot/pivot_layout_configurator/pivot_dimension/pivot_dimension.ts
+++ src/components/side_panel/pivot/pivot_layout_configurator/pivot_dimension/pivot_dimension.ts
@@ -1,13 +1,17 @@
+import { collapseHierarchicalDisplayName } from "@odoo/o-spreadsheet-engine/helpers/pivot/pivot_helpers";
+import {
+  PivotDimension as PivotDimensionType,
+  PivotMeasure,
+} from "@odoo/o-spreadsheet-engine/types/pivot";
 import { SpreadsheetChildEnv } from "@odoo/o-spreadsheet-engine/types/spreadsheet_env";
 import { Component } from "@odoo/owl";
-import { PivotCoreDimension, PivotCoreMeasure } from "../../../../..";
 import { TextInput } from "../../../../text_input/text_input";
 import { CogWheelMenu } from "../../../components/cog_wheel_menu/cog_wheel_menu";
 
 interface Props {
-  dimension: PivotCoreDimension | PivotCoreMeasure;
-  onRemoved: (dimension: PivotCoreDimension | PivotCoreMeasure) => void;
-  onNameUpdated?: (dimension: PivotCoreDimension | PivotCoreMeasure, name?: string) => void;
+  dimension: PivotDimensionType | PivotMeasure;
+  onRemoved: (dimension: PivotDimensionType | PivotMeasure) => void;
+  onNameUpdated?: (dimension: PivotDimensionType | PivotMeasure, name?: string) => void;
   type: "row" | "col" | "measure";
 }
 
@@ -26,5 +30,10 @@ export class PivotDimension extends Component<Props, SpreadsheetChildEnv> {
       this.props.dimension,
       name === "" || name.startsWith("=") ? undefined : name
     );
+  }
+
+  get dimensionDisplayName() {
+    const displayName = this.props.dimension.displayName;
+    return collapseHierarchicalDisplayName(displayName);
   }
 }
