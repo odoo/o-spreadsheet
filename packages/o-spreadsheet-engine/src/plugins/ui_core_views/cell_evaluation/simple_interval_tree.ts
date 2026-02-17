104a8d893a5da21fe2f6301f6c16f94d687b21f0
Lucas Lefèvre (lul) <lul@odoo.com>
2026-02-17 12:53:10 +0100

    m

--- packages/o-spreadsheet-engine/src/plugins/ui_core_views/cell_evaluation/simple_interval_tree.ts
+++ packages/o-spreadsheet-engine/src/plugins/ui_core_views/cell_evaluation/simple_interval_tree.ts
@@ -134,6 +134,7 @@ export class IntervalTree {
    * Complexity is O(n log n)
    */
   bulkLoad(intervals: Required<Interval>[]): void {
+    console.time("Building interval tree with " + intervals.length + " intervals");
     const sorted = [...intervals].sort((a, b) => {
       // Sort intervals by their starting point
       if (a.top !== b.top) {
@@ -180,6 +181,7 @@ export class IntervalTree {
     compacted.push(current);
     this.root = this.buildBalancedTree(compacted);
     this.buffer = [];
+    console.timeEnd("Building interval tree with " + intervals.length + " intervals");
   }
 
   /**
