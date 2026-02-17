a236989f2fcdf2505dd0e1904e4a19e6f966bee2
Adrien Minne (adrm) <adrm@odoo.com>
2026-02-17 09:03:08 +0100

    [REF] renderer: decouple rendering logic from getters
    This commit adds a new component `StandaloneGridCanvas` that is able
    to draw any part of a spreadsheet, without relying on the
    `SheetViewPlugin` viewport.
    
    The rendering logic now lose access to the SheetView/Selection getters,
    and is instead passed a `GridRenderingContext` object containing all
    the necessary information.
    
    Task: 5891329

---
+++ src/components/standalone_grid_canvas/standalone_grid_canvas.ts
@@ -0,0 +1,56 @@
+import { GridRenderingContext, UID, Zone } from "@odoo/o-spreadsheet-engine";
+import { SpreadsheetChildEnv } from "@odoo/o-spreadsheet-engine/types/spreadsheet_env";
+import { Component, onWillStart, onWillUpdateProps, useRef } from "@odoo/owl";
+import { Store, useLocalStore } from "../../store_engine";
+import { RendererStore } from "../../stores/renderer_store";
+import { useGridDrawing } from "../helpers/draw_grid_hook";
+import { FigureRendererStore } from "./figure_renderer_store";
+
+interface Props {
+  sheetId: UID;
+  zone: Zone;
+  renderingCtx: Omit<GridRenderingContext, "ctx" | "thinLineWidth">;
+}
+
+export class StandaloneGridCanvas extends Component<Props, SpreadsheetChildEnv> {
+  static template = "o-spreadsheet-StandaloneGridCanvas";
+  static props = {
+    sheetId: String,
+    zone: Object,
+    renderingCtx: Object,
+  };
+
+  canvasRef = useRef("canvas");
+
+  rendererStore!: Store<RendererStore>;
+  figureRendererStore!: Store<FigureRendererStore>;
+
+  setup() {
+    this.rendererStore = useLocalStore(RendererStore, ["Background", "Chart"]);
+    this.figureRendererStore = useLocalStore(FigureRendererStore, this.rendererStore);
+    useGridDrawing({
+      refName: "canvas",
+      renderingCtx: () => this.props.renderingCtx,
+      rendererStore: this.rendererStore,
+      changeCanvasSizeOnZoom: true,
+    });
+    onWillStart(async () => await this.loadImages(this.props));
+    onWillUpdateProps(async (nextProps: Props) => await this.loadImages(nextProps));
+  }
+
+  private async loadImages(props: Props) {
+    const imagePaths = props.renderingCtx.viewports
+      .getVisibleFigures(props.sheetId)
+      .filter((figure) => figure.tag === "image")
+      .map((figure) => this.env.model.getters.getImagePath(figure.id))
+      .filter((path) => path && !this.figureRendererStore.loadedImages[path]);
+
+    await Promise.all(
+      imagePaths.map(async (path) => {
+        const response = await fetch(path);
+        const blob = await response.blob();
+        this.figureRendererStore.addLoadedImage(path, await createImageBitmap(blob));
+      })
+    );
+  }
+}
