8027653b632c7b6148932fc6a250fc69b376e344
Adrien Minne (adrm) <adrm@odoo.com>
2026-02-19 10:16:23 +0100

    wip

--- src/components/pivot_overlay/pivot_overlay/pivot_overlay.ts
+++ src/components/pivot_overlay/pivot_overlay/pivot_overlay.ts
@@ -1,7 +1,9 @@
+import { GridRenderingContext } from "@odoo/o-spreadsheet-engine";
+import { ViewportCollection } from "@odoo/o-spreadsheet-engine/helpers/viewport_collection";
 import { SpreadsheetChildEnv } from "@odoo/o-spreadsheet-engine/types/spreadsheet_env";
 import { Component, useEffect, useRef } from "@odoo/owl";
 import { positionToZone } from "../../../helpers";
-import { cssPropertiesToCss } from "../../helpers";
+import { StandaloneGridCanvas } from "../../standalone_grid_canvas/standalone_grid_canvas";
 import { PivotFacet } from "../pivot_facet/pivot_facet";
 
 interface Props {}
@@ -9,7 +11,7 @@ interface Props {}
 export class PivotOverlay extends Component<Props, SpreadsheetChildEnv> {
   static template = "o-spreadsheet-PivotOverlay";
   static props = { "*": Object }; // ADRM TODO
-  static components = { PivotFacet };
+  static components = { PivotFacet, StandaloneGridCanvas };
 
   setup() {
     const overlayRef = useRef("pivotOverlay");
@@ -18,46 +20,36 @@ export class PivotOverlay extends Component<Props, SpreadsheetChildEnv> {
         return;
       }
       const position = this.pivotFormulaPosition!; // ADRM TODO
+      // const rect = this.env.model.getters.getRect(positionToZone(position));
       const rect = this.env.model.getters.getRect(positionToZone(position));
 
-      let heightOffset = 0;
-      const horizontalOverlays = overlayRef.el.querySelectorAll(
-        ".o-pivot-measures, .o-pivot-columns"
-      );
-      for (const horizontalOverlay of horizontalOverlays) {
-        heightOffset = Math.max(heightOffset, horizontalOverlay.getBoundingClientRect().height);
-      }
+      const heightOffset = 0;
+      // const horizontalOverlays = overlayRef.el.querySelectorAll(
+      //   ".o-pivot-measures, .o-pivot-columns"
+      // );
+      // for (const horizontalOverlay of horizontalOverlays) {
+      //   heightOffset = Math.max(heightOffset, horizontalOverlay.getBoundingClientRect().height);
+      // }
 
-      let widthOffset = 0;
-      const verticalOverlays = overlayRef.el.querySelectorAll(".o-pivot-rows");
-      for (const verticalOverlay of verticalOverlays) {
-        widthOffset = Math.max(widthOffset, verticalOverlay.getBoundingClientRect().width);
-      }
+      const widthOffset = 0;
+      // const verticalOverlays = overlayRef.el.querySelectorAll(".o-pivot-rows");
+      // for (const verticalOverlay of verticalOverlays) {
+      //   widthOffset = Math.max(widthOffset, verticalOverlay.getBoundingClientRect().width);
+      // }
 
-      const y = Math.max(rect.y - heightOffset, 0);
-      const x = Math.max(rect.x - widthOffset, 0);
+      const y = Math.max(rect.y - heightOffset);
+      const x = Math.max(rect.x - widthOffset);
 
       overlayRef.el.style.left = x + "px";
-      overlayRef.el.style.top = y + "px";
+      overlayRef.el.style.top = y - 1 + "px"; // -1 for cell border
     });
   }
 
   get overlayStyle() {
     return "";
-    const position = this.pivotFormulaPosition!; // ADRM TODO
-
-    const rect = this.env.model.getters.getVisibleRect(positionToZone(position));
-
-    const y = Math.max(rect.y - 10, 0); // ADRM TODO: replace 10 by overlay size ?
-    const x = Math.max(rect.x - 10, 0);
-
-    return cssPropertiesToCss({
-      left: x + "px",
-      top: y + "px",
-    });
   }
 
-  get pivotFormulaPosition() {
+  get _pivotFormulaPosition() {
     const sheetId = this.env.model.getters.getActiveSheetId();
     for (const { position, pivotId } of this.env.model.getters.getAllPivotArrayFormulas()) {
       if (position.sheetId === sheetId && pivotId === this.pivotId) {
@@ -65,7 +57,17 @@ export class PivotOverlay extends Component<Props, SpreadsheetChildEnv> {
       }
     }
     return undefined;
-    this.definition.measures;
+  }
+
+  get hasPivotFormula() {
+    return !!this._pivotFormulaPosition;
+  }
+
+  get pivotFormulaPosition() {
+    if (!this._pivotFormulaPosition) {
+      throw new Error("Pivot formula position not found");
+    }
+    return this._pivotFormulaPosition;
   }
 
   get pivotId() {
@@ -74,5 +76,24 @@ export class PivotOverlay extends Component<Props, SpreadsheetChildEnv> {
 
   get definition() {
     return this.env.model.getters.getPivot(this.pivotId).definition;
+  }
+
+  get pivotGridProps(): StandaloneGridCanvas["props"] {
+    const sheetId = this.env.model.getters.getActiveSheetId();
+    const position = this.pivotFormulaPosition;
+    const zone = this.env.model.getters.getSpreadZone(position) || positionToZone(position);
+    const firstRowStart = this.env.model.getters.getRowDimensions(sheetId, zone.top).start;
+    const lastRowEnd = this.env.model.getters.getRowDimensions(sheetId, zone.bottom).end;
+    const firstColStart = this.env.model.getters.getColDimensions(sheetId, zone.left).start;
+    const lastColEnd = this.env.model.getters.getColDimensions(sheetId, zone.right).end;
+
+    const viewports = new ViewportCollection(this.env.model.getters);
+    viewports.sheetViewWidth = lastColEnd - firstColStart;
+    viewports.sheetViewHeight = lastRowEnd - firstRowStart;
+    viewports.setSheetViewOffset(sheetId, firstColStart, firstRowStart);
+
+    const renderingCtx: Partial<GridRenderingContext> = { selectedZones: [], sheetId, viewports };
+
+    return { sheetId, zone, renderingCtx };
   }
 }
