c35a401127e3c76b6f7d9d49d5dba81f594e8fa7
Marceline (matho) <matho@odoo.com>
2026-03-05 13:28:44 +0100

    [FIX] pivot cell style copied twice
    If we copy the top left cell or the all pivot table,
    we should not copy the style of the cell
    
    Task: 5909138

--- tests/pivots/pivot_insert.test.ts
+++ tests/pivots/pivot_insert.test.ts
@@ -1,8 +1,8 @@
 import { PIVOT_INSERT_TABLE_STYLE_ID } from "@odoo/o-spreadsheet-engine/constants";
 import { Model } from "../../src";
 import { toZone } from "../../src/helpers";
-import { insertPivot } from "../test_helpers/commands_helpers";
-import { getCellText, getTable } from "../test_helpers/getters_helpers";
+import { copy, insertPivot, paste } from "../test_helpers/commands_helpers";
+import { getCell, getCellText, getTable } from "../test_helpers/getters_helpers";
 import { setGrid } from "../test_helpers/helpers";
 
 describe("Insert pivot command", () => {
@@ -42,5 +42,44 @@ describe("Insert pivot command", () => {
     });
     insertPivot(model, "A1", "pivot1", "Sheet2");
     expect(model.getters.getPivotCoreDefinition("pivot1")["dataSet"].zone).toEqual(toZone("A1:B2"));
+  });
+});
+
+describe("Check the style when copying and pasting a pivot table", () => {
+  let model: Model;
+  beforeEach(() => {
+    model = new Model({
+      sheets: [
+        {
+          id: "Sheet1",
+          cells: {
+            A1: "1",
+            A2: "2",
+          },
+        },
+      ],
+    });
+    insertPivot(model, "A1", "pivot1", "Sheet2");
+  });
+  test("copy/paste the top left cell", () => {
+    copy(model, "A1");
+    paste(model, "C1");
+    expect(getCell(model, "C1")?.style).toBe(undefined);
+  });
+
+  test("copy/paste the all table", () => {
+    copy(model, "A1:A3");
+    paste(model, "C1");
+    expect(getCell(model, "C1")?.style).toBe(undefined);
+  });
+
+  test("copy/paste a random cell", () => {
+    copy(model, "A2");
+    paste(model, "C1");
+    expect(getCell(model, "C1")?.style).toStrictEqual({
+      bold: true,
+      fillColor: "#6C4E65",
+      textColor: "#FFFFFF",
+    });
   });
 });
