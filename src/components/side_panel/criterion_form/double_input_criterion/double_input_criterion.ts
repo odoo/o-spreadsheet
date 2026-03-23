3673a14535de123a3471143a644183db2d3a371f
Marceline (matho) <matho@odoo.com>
2026-03-23 15:55:32 +0100

    [IMP] date_criterion : added a date picker
    Date criterion input now have a calendar button that opens a date picker.
    The calendar button is a new component.
    
    closes odoo/o-spreadsheet#8166
    
    Task: 5395190
    Signed-off-by: Rémi Rahir (rar) <rar@odoo.com>

--- src/components/side_panel/criterion_form/double_input_criterion/double_input_criterion.ts
+++ src/components/side_panel/criterion_form/double_input_criterion/double_input_criterion.ts
@@ -1,9 +1,10 @@
+import { CalendarButton } from "../calendar_button/calendar_button";
 import { CriterionForm } from "../criterion_form";
 import { CriterionInput } from "../criterion_input/criterion_input";
 
 export class DoubleInputCriterionForm extends CriterionForm {
   static template = "o-spreadsheet-DoubleInputCriterionForm";
-  static components = { CriterionInput };
+  static components = { CriterionInput, CalendarButton };
 
   onFirstValueChanged(value: string) {
     const values = this.props.criterion.values;
@@ -17,5 +18,9 @@ export class DoubleInputCriterionForm extends CriterionForm {
     this.updateCriterion({
       values: [values[0] || "", value],
     });
+  }
+
+  get isDateType() {
+    return ["dateIsBetween", "dateIsNotBetween"].includes(this.props.criterion.type);
   }
 }
