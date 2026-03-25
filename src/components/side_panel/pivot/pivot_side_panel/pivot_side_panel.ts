1759d33ff365d5e2e38b655c984c5376a6bed1a8
Adrien Minne (adrm) <adrm@odoo.com>
2026-03-25 11:26:58 +0100

    [FIX] side panels: fix layout of panels with tabs
    The pivot side panel is wrong since the introduction of its "design"
    tab, it's now the parent that is scrollable and not the config panel
    itself. This causes de "Defer update", which is supposed to be
    sticky at the bottom of the panel, to be at the wrong position.
    
    This commit make the correct panel element scrollable. And it turns
    out that when the child is scrollable instead of the parent, the
    code to set the scroll of the panel manually is not needed.
    
    Task: 6022696

--- src/components/side_panel/pivot/pivot_side_panel/pivot_side_panel.ts
+++ src/components/side_panel/pivot/pivot_side_panel/pivot_side_panel.ts
@@ -1,8 +1,8 @@
 import { SpreadsheetChildEnv } from "@odoo/o-spreadsheet-engine/types/spreadsheet_env";
-import { Component, useEffect, useRef, useState } from "@odoo/owl";
+import { Component, useState } from "@odoo/owl";
 import { getPivotHighlights } from "../../../../helpers/pivot/pivot_highlight";
 import { pivotSidePanelRegistry } from "../../../../helpers/pivot/pivot_side_panel_registry";
-import { Pixel, UID } from "../../../../types";
+import { UID } from "../../../../types";
 import { useHighlights } from "../../../helpers/highlight_hook";
 import { Section } from "../../components/section/section";
 import { PivotLayoutConfigurator } from "../pivot_layout_configurator/pivot_layout_configurator";
@@ -30,23 +30,9 @@ export class PivotSidePanel extends Component<Props, SpreadsheetChildEnv> {
   };
 
   state = useState<State>({ panel: "configuration" });
-  private panelContentRef = useRef<HTMLElement>("panelContent");
-  private scrollPositions: Record<"configuration" | "design", Pixel> = {
-    configuration: 0,
-    design: 0,
-  };
 
   setup() {
     useHighlights(this);
-    useEffect(
-      () => {
-        const el = this.panelContentRef.el as HTMLElement;
-        if (el) {
-          el.scrollTop = this.scrollPositions[this.state.panel];
-        }
-      },
-      () => [this.state.panel]
-    );
   }
 
   get sidePanelEditor() {
@@ -62,10 +48,6 @@ export class PivotSidePanel extends Component<Props, SpreadsheetChildEnv> {
   }
 
   switchPanel(panel: "configuration" | "design") {
-    const el = this.panelContentRef.el as HTMLElement;
-    if (el) {
-      this.scrollPositions[this.state.panel] = el.scrollTop;
-    }
     this.state.panel = panel;
   }
 }
