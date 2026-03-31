588cec13a09fbff597ca71f6464fbed134586d1d
Adrien Minne (adrm) <adrm@odoo.com>
2026-03-31 10:19:30 +0200

    [IMP] tests: update jest to v30
    Update the jest version.
    - some matcher aliases were removed
    - JSDOM was updated, leading a slightly different behaviors
    - snapshot header changed
    
    Task: 5933130
    Part-of: odoo/o-spreadsheet#8361
    Signed-off-by: Adrien Minne (adrm) <adrm@odoo.com>

--- tests/setup/jest.setup.ts
+++ tests/setup/jest.setup.ts
@@ -80,6 +80,7 @@ beforeEach(() => {
       }
       return 0;
     });
+  HTMLElement.prototype.scrollIntoView = () => {};
   jest
     .spyOn(HTMLCanvasElement.prototype, "toBlob")
     .mockImplementation(function (callback: BlobCallback, type?: string) {
