dea7dbca94c2b3712d1089523f33059223ab6fb5
Adrien Minne (adrm) <adrm@odoo.com>
2026-03-31 10:19:30 +0200

    [IMP] tests: update jest to v30
    Update the jest version.
    - some matcher aliases were removed
    - JSDOM was updated, leading a slightly different behaviors
    - snapshot header changed
    
    Task: 5933130

--- tests/evaluation/parser.test.ts
+++ tests/evaluation/parser.test.ts
@@ -182,7 +182,7 @@ describe("parser", () => {
   });
 
   test("Cannot parse empty string", () => {
-    expect(() => parse("")).toThrowError("Invalid expression");
+    expect(() => parse("")).toThrow("Invalid expression");
   });
 
   test("AND", () => {
--- tests/xlsx/xlsx_export.test.ts
+++ tests/xlsx/xlsx_export.test.ts
@@ -1904,7 +1904,7 @@ describe("XML parser", () => {
   });
 
   test("error on the first line", () => {
-    expect(() => parseXML(escapeXml`<hello>Raoul/hello>`)).toThrowError(
+    expect(() => parseXML(escapeXml`<hello>Raoul/hello>`)).toThrow(
       `XML string could not be parsed: 1:19: unclosed tag: hello\n<hello>Raoul/hello>`
     );
   });
@@ -1925,7 +1925,7 @@ describe("XML parser", () => {
         <hello>3</WRONG_TAG>
         <hello>4</hello>
         <hello>5</hello>`;
-    expect(() => parseXML(xmlString)).toThrowError(errorMessage);
+    expect(() => parseXML(xmlString)).toThrow(errorMessage);
   });
 
   test("error at the end", () => {
@@ -1939,6 +1939,6 @@ describe("XML parser", () => {
         <hello>2</hello>
         <hello>3</hello>
       </WRONG_TAG>`;
-    expect(() => parseXML(xmlString)).toThrowError(errorMessage);
+    expect(() => parseXML(xmlString)).toThrow(errorMessage);
   });
 });
