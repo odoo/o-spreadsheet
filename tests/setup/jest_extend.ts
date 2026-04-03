61f6a0e3ddf522939268ed42b67801bf39abe482
Pierre Rousseau <pro@odoo.com>
2026-04-03 06:45:29 +0000

    [FIX] package: update types/jest to match jest version
    closes odoo/o-spreadsheet#8298
    
    Task: 6092447
    X-original-commit: 1a702e5b339e399d00235bdf6f0f06acb0543fe9
    Signed-off-by: Pierre Rousseau (pro) <pro@odoo.com>
    Signed-off-by: Rémi Rahir (rar) <rar@odoo.com>

--- tests/setup/jest_extend.ts
+++ tests/setup/jest_extend.ts
@@ -69,7 +69,7 @@ expect.extend({
       ])
     ) {
       return {
-        pass: this.isNot,
+        pass: !!this.isNot,
         message: () =>
           `Diff: ${this.utils.printDiffOrStringify(
             expected,
@@ -88,7 +88,7 @@ expect.extend({
       if (!this.equals(result, expected, [this.utils.iterableEquality])) {
         const userId = user.getters.getCurrentClient().name;
         return {
-          pass: this.isNot,
+          pass: !!this.isNot,
           message: () =>
             `${userId} does not have the expected value: \nReceived: ${this.utils.printReceived(
               result
@@ -112,7 +112,7 @@ expect.extend({
           const prettyValuesUserA = getPrettyEvaluatedCells(a, sheetId, sheetZone);
           const prettyValuesUserB = getPrettyEvaluatedCells(b, sheetId, sheetZone);
           return {
-            pass: this.isNot,
+            pass: !!this.isNot,
             message: () =>
               `${clientA} and ${clientB} are not synchronized: \n${this.utils.printDiffOrStringify(
                 prettyValuesUserA,
@@ -137,7 +137,7 @@ expect.extend({
         const clientA = a.getters.getCurrentClient().id;
         const clientB = b.getters.getCurrentClient().id;
         return {
-          pass: this.isNot,
+          pass: !!this.isNot,
           message: () =>
             `${clientA} and ${clientB} are not synchronized: \n${this.utils.printDiffOrStringify(
               exportA,
