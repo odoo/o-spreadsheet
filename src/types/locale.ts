42509ce5d765a29776905b0861e45d075e0c3229
Adrien Minne (adrm) <adrm@odoo.com>
2026-04-07 11:43:44 +0200

    [IMP] locale: add digit grouping for thousands separators
    Some locales (eg. India) do not use standard international thousand
    grouping for large numbers, but group the first 3 numbers, then
    group the next numbers by 2.
    
    This commit adds the `digitGrouping` parameter to the locale to
    handle that behaviour.
    
    closes odoo/o-spreadsheet#8319
    
    Task: 4829185
    Signed-off-by: Lucas Lefèvre (lul) <lul@odoo.com>

--- src/types/locale.ts
+++ src/types/locale.ts
@@ -11,6 +11,8 @@ export interface Locale {
   dateFormat: string;
   timeFormat: string;
   formulaArgSeparator: string;
+  /** Describes how to split the digits with the thousand separator. See `getIndexesOfDigitsWithThousandSeparator` for details. */
+  digitGrouping?: string;
 }
 
 export const DEFAULT_LOCALES: Locale[] = [
@@ -34,5 +36,18 @@ export const DEFAULT_LOCALES: Locale[] = [
     timeFormat: "hh:mm:ss",
     formulaArgSeparator: ";",
   },
+  {
+    name: "English (India)",
+    code: "en_IN" as LocaleCode,
+    thousandsSeparator: ",",
+    decimalSeparator: ".",
+    weekStart: 7, // Sunday
+    dateFormat: "dd/mm/yyyy",
+    timeFormat: "hh:mm:ss a",
+    formulaArgSeparator: ",",
+    digitGrouping: "[3,2,0]", // 12,34,56,789.00
+  },
 ];
 export const DEFAULT_LOCALE: Locale = DEFAULT_LOCALES[0];
+
+export const DEFAULT_LOCALE_DIGIT_GROUPING = "[3,0]"; // Group digits 3 by 3
