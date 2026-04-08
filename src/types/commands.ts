e3bb0e35e4c4b3be852ab938f0fc3296bb71f7df
Adrien Minne (adrm) <adrm@odoo.com>
2026-04-08 09:23:08 +0000

    [FIX] cf: add sanity checks on conditional format type/operator
    There was no check at all to see if a commands had a valid conditional
    format type/operator.
    
    closes odoo/o-spreadsheet#8339
    
    Task: 6106092
    X-original-commit: 148bd3c9d8bc6797389146e79752407aca1ecbaa
    Signed-off-by: Lucas Lefèvre (lul) <lul@odoo.com>
    Signed-off-by: Adrien Minne (adrm) <adrm@odoo.com>

--- src/types/commands.ts
+++ src/types/commands.ts
@@ -1298,6 +1298,7 @@ export const enum CommandResult {
   ViewportScrollLimitsReached = "ViewportScrollLimitsReached",
   FigureDoesNotExist = "FigureDoesNotExist",
   InvalidConditionalFormatId = "InvalidConditionalFormatId",
+  InvalidConditionalFormatType = "InvalidConditionalFormatType",
   InvalidCellPopover = "InvalidCellPopover",
   EmptyTarget = "EmptyTarget",
   InvalidFreezeQuantity = "InvalidFreezeQuantity",
