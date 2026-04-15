c79cef99e3ec9a57496a475b47ec9561c428a645
LucasLefebre <lul@odoo.com>
2026-04-15 15:12:06 +0200

    [IMP] config: replace rollup with rolldown
    This commit replaces our bundling tool from rollup to rolldown
    which is a rollup-compatible bundling tool written in rust
    for speed.
    
    For bundling o-spreadsheet, it is roughly 10x faster.
    
    `npm run bundle:iife`
    before: ~3.7s
    after:  ~350ms
    
    While rolldown is still a Release Candidate, I believe it's here to stay.
    
    It's already widely used (12M npm weekly downloads).
    And it's only going grow: `Vite` (75M npm weekly downloads) recently
    replaced their `esbuild`+`rollup` tooling with only `rolldown`.
    
    Before
    ------
    
    Initially:
    - tsc transpiles .ts files to .js files
    - rollup bundles .js files
    
    Watch:
    - tsc watches .ts and re-transpile to .js
    - rollup watches .js files and rebundles them
    
    After
    -----
    
    Initially:
    - rolldown transpiles and bundles .ts files
    - tsc transpiles only to check types
    
    Watch:
    - rolldown watches .ts and rebundles
    - tsc watches .ts files to check types
      -> this could be omitted, do we really care to
         recheck types for hot reload?
    
    Note
    ----
    I removed the .d.ts bundle. The plugin bundling type files
    is not compatible and has no built-in replacement.
    Shipping a bundle file for types is actually useless. We can provide
    the type files as they are.
    
    Backport of bb4cdee
    
    Task: 6119495
    Part-of: odoo/o-spreadsheet#8424
    Signed-off-by: Lucas Lefèvre (lul) <lul@odoo.com>

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,11 @@
-import { nodeResolve } from "@rollup/plugin-node-resolve";
-import terser from "@rollup/plugin-terser";
-import dts from "rollup-plugin-dts";
-import typescript from "rollup-plugin-typescript2";
+import { defineConfig } from "rolldown";
 import { bundle } from "./tools/bundle.cjs";
 
 const outro = bundle.outro();
 
 /**
- * Get the rollup config based on the arguments
+ * Get the rolldown config based on the arguments
  * @param {"esm" | "cjs" | "iife"} format format of the bundle
- * @param {string} generatedFileName generated file name
  * @param {boolean} minified should it be minified
  */
 function getConfigForFormat(format, minified = false) {
@@ -18,67 +14,64 @@ function getConfigForFormat(format, minified = false) {
     format,
     name: "o_spreadsheet",
     extend: true,
-    globals: { "@odoo/owl": "owl" },
+    globals: { "@odoo/owl": "owl", "chart.js": "Chart" },
     outro,
     banner: bundle.jsBanner(),
-    plugins: minified ? [terser()] : [],
+    minify: minified,
   };
 }
 
-export default (commandLineArgs) => {
-  let output = [];
-  let input = "";
-  let plugins = [nodeResolve()];
-  let config = {};
+function onLog(level, log, defaultHandler) {
+  if (level === "warn") {
+    // escalate all warnings to errors
+    defaultHandler("error", log);
+  } else {
+    defaultHandler(level, log);
+  }
+}
 
-  if (commandLineArgs.configDev || commandLineArgs.configDist) {
+export default defineConfig((cliArgs) => {
+  const format = cliArgs.format;
+  if (format) {
     // Only build one version to improve speed
-    input = "build/js/index.js";
-    output = [
-      {
+    return {
+      input: "src/index.ts",
+      external: ["@odoo/owl", "chart.js", "luxon"],
+      checks: {
+        circularDependency: true,
+      },
+      onLog,
+      plugins: [],
+      output: {
         name: "o_spreadsheet",
         extend: true,
         outro,
         banner: bundle.jsBanner(),
-        globals: { "@odoo/owl": "owl" },
+        globals: { "@odoo/owl": "owl", "chart.js": "Chart", luxon: "luxon" },
+        file: `build/o-spreadsheet.${format}.js`,
+        format,
+        sourcemap: true,
       },
-    ];
-    if (commandLineArgs.configDev) {
-      output[0].file = `build/o_spreadsheet.dev.js`;
-      output[0].format = `iife`;
-    } else {
-      output[0].file = `build/o_spreadsheet.js`;
-      output[0].format = `esm`;
-    }
-    config = {
-      input,
-      external: ["@odoo/owl"],
-      output,
-      plugins,
+      watch: {
+        include: ["src/**"],
+      },
     };
-  } else {
-    input = "src/index.ts";
-    output = [
+  }
+
+  // dist build
+  return {
+    input: "src/index.ts",
+    external: ["@odoo/owl", "chart.js", "luxon"],
+    checks: {
+      circularDependency: true,
+    },
+    plugins: [],
+    onLog,
+    output: [
       getConfigForFormat("esm"),
       getConfigForFormat("cjs"),
       getConfigForFormat("iife"),
       getConfigForFormat("iife", true),
-    ];
-    plugins.push(typescript({ useTsconfigDeclarationDir: true }));
-    config = [
-      {
-        input,
-        external: ["@odoo/owl"],
-        output,
-        plugins,
-      },
-      {
-        input: "dist/types/index.d.ts",
-        output: [{ file: "dist/o-spreadsheet.d.ts", format: "es" }],
-        plugins: [dts(), nodeResolve()],
-      },
-    ];
-  }
-
-  return config;
-};
+    ],
+  };
+});
