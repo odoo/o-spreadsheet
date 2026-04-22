801a98804bdf1187c46e5884f840cf7ebea33a03
Marceline (matho) <matho@odoo.com>
2026-04-22 10:39:40 +0200

    [FIX] grid overlay: unhide buttons visibility
    Before this commit:
    When a row/column is frozen and hidden, the unhide buttons were not visible in the header overlay.
    There is no way for the user to unhide these rows/columns.
    
    After this commit:
    The unhide buttons are now visible in the header overlay when a frozen row/column is hidden.
    
    closes odoo/o-spreadsheet#8466
    
    Task: 6127335
    Signed-off-by: Pierre Rousseau (pro) <pro@odoo.com>

--- src/components/headers_overlay/headers_overlay.ts
+++ src/components/headers_overlay/headers_overlay.ts
@@ -518,7 +518,7 @@ export class ColResizer extends AbstractResizer {
     const hiddenGroups = this.env.model.getters.getHiddenColsGroups(this.sheetId);
     const index = hiddenGroups.findIndex((group) => group[0] >= xSplit - 1);
     return {
-      headersGroups: hiddenGroups.slice(index),
+      headersGroups: index === -1 ? [] : hiddenGroups.slice(index),
       offset: this.env.model.getters.getMainViewportCoordinates().x,
       headerRange: { start: left, end: right },
     };
@@ -530,7 +530,7 @@ export class ColResizer extends AbstractResizer {
     const index = hiddenGroups.findIndex((group) => group[0] >= xSplit - 1);
 
     return {
-      headersGroups: hiddenGroups.slice(0, index + 1),
+      headersGroups: index === -1 ? hiddenGroups : hiddenGroups.slice(0, index + 1),
       headerRange: { start: 0, end: xSplit - 1 },
     };
   }
@@ -739,7 +739,7 @@ export class RowResizer extends AbstractResizer {
     const hiddenGroups = this.env.model.getters.getHiddenRowsGroups(this.sheetId);
     const index = hiddenGroups.findIndex((group) => group[0] >= ySplit - 1);
     return {
-      headersGroups: hiddenGroups.slice(index),
+      headersGroups: index === -1 ? [] : hiddenGroups.slice(index),
       offset: this.env.model.getters.getMainViewportCoordinates().y,
       headerRange: { start: top, end: bottom },
     };
@@ -751,7 +751,7 @@ export class RowResizer extends AbstractResizer {
     const index = hiddenGroups.findIndex((group) => group[0] >= ySplit - 1);
 
     return {
-      headersGroups: hiddenGroups.slice(0, index + 1),
+      headersGroups: index === -1 ? hiddenGroups : hiddenGroups.slice(0, index + 1),
       headerRange: { start: 0, end: ySplit - 1 },
     };
   }
