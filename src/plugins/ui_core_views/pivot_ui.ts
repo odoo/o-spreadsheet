d2f8cfea409e3565f52c2c63de29e161e652f6d4
Rémi Rahir (rar) <rar@odoo.com>
2026-04-30 07:05:49 +0000

    [FIX] Pivots: cache `getPivotIdFromPosition` for performance
    the getter `getPivotIdFromPosition` can be heavily called, notably when
    a pivot sidepanel is open. This can become costy as we parse the tokens
    every time to find PIVOT functions.
    
    This revision caches the values, which are only invalidated when a cell
    content is altered.
    
    Task: 6159213
    X-original-commit: a2c27756565b14eaba989c3e341da8bca3948100
    Part-of: odoo/o-spreadsheet#9194
    Signed-off-by: Adrien Minne (adrm) <adrm@odoo.com>

--- src/plugins/ui_core_views/pivot_ui.ts
+++ src/plugins/ui_core_views/pivot_ui.ts
@@ -1,6 +1,7 @@
 import { CompiledFormula } from "../../formulas/compiler";
 import { astToFormula } from "../../formulas/formula_formatter";
 import { toScalar } from "../../functions/helper_matrices";
+import { PositionMap } from "../../helpers/cells/position_map";
 import { deepCopy, deepEquals, getUniqueText, isDefined } from "../../helpers/misc";
 import {
   getNumberOfPivotFunctions,
@@ -63,6 +64,8 @@ export class PivotUIPlugin extends CoreViewPlugin {
   private pivots: Record<UID, Pivot> = {};
   private unusedPivotsInFormulas?: UID[];
   private custom: UIPluginConfig["custom"];
+  private pivotPositionCache: PositionMap<UID[]> = new PositionMap();
+  private shouldInvalidateCache: boolean = false;
 
   constructor(config: CoreViewPluginConfig) {
     super(config);
@@ -80,9 +83,13 @@ export class PivotUIPlugin extends CoreViewPlugin {
 
   handle(cmd: Command) {
     if (invalidateEvaluationCommands.has(cmd.type)) {
+      this.shouldInvalidateCache = true;
       for (const pivotId of this.getters.getPivotIds()) {
         this.setupPivot(pivotId, { recreate: true });
       }
+    }
+    if (cmd.type === "UPDATE_CELL") {
+      this.shouldInvalidateCache = true;
     }
     switch (cmd.type) {
       case "REFRESH_PIVOT":
@@ -132,6 +139,13 @@ export class PivotUIPlugin extends CoreViewPlugin {
     }
   }
 
+  finalize() {
+    if (this.shouldInvalidateCache) {
+      this.pivotPositionCache = new PositionMap();
+      this.shouldInvalidateCache = false;
+    }
+  }
+
   // ---------------------------------------------------------------------
   // Getters
   // ---------------------------------------------------------------------
@@ -148,11 +162,14 @@ export class PivotUIPlugin extends CoreViewPlugin {
    * Get all of the ids of the pivot present in the formula at the given position.
    */
   getPivotIdsFromPosition(position: CellPosition): UID[] {
-    const cell = this.getters.getCorrespondingFormulaCell(position);
-    if (cell && cell.isFormula) {
-      return this.getPivotIdsFromFormula(position.sheetId, cell.compiledFormula);
+    if (!this.pivotPositionCache.has(position)) {
+      const cell = this.getters.getCorrespondingFormulaCell(position);
+      if (cell && cell.isFormula) {
+        const pivotIds = this.getPivotIdsFromFormula(position.sheetId, cell.compiledFormula);
+        this.pivotPositionCache.set(position, pivotIds);
+      }
     }
-    return [];
+    return this.pivotPositionCache.get(position) || [];
   }
 
   private getPivotIdsFromFormula(sheetId: UID, formula: CompiledFormula): UID[] {
