90d8792b2275c14fc0883a97fc27121a50164236
Florian Damhaut <flda@odoo.com>
2026-05-04 15:42:37 +0200

    [IMP] cf: make sheet navigation better with cf sidepanel
    Adapt cf to criterion changes and improve the multi sheet navigation.
    
    Task: 4072640

--- src/components/side_panel/conditional_formatting/cf_preview_list/cf_preview_list.ts
+++ src/components/side_panel/conditional_formatting/cf_preview_list/cf_preview_list.ts
@@ -81,6 +81,7 @@ export class ConditionalFormatPreviewList extends Component<SpreadsheetChildEnv>
           zoneToXc(this.env.model.getters.getUnboundedZone(sheetId, zone))
         ),
       },
+      sheetId,
       isNewCf: true,
     });
   }
