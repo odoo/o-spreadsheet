3a9a588fa6860cf1b1402323a7fd96db70539c47
Adrien Minne (adrm) <adrm@odoo.com>
2026-05-04 10:06:56 +0200

    [FIX] pivot: unused pivot detection with composed formula
    If a formula has two pivots (eg. `=PIVOT(1) + PIVOT(2)`), the
    second pivot will not be detected as used.
    
    closes odoo/o-spreadsheet#8746
    
    Task: 6105894
    Signed-off-by: Lucas Lefèvre (lul) <lul@odoo.com>
    Signed-off-by: Adrien Minne (adrm) <adrm@odoo.com>

--- tests/pivots/pivot_plugin.test.ts
+++ tests/pivots/pivot_plugin.test.ts
@@ -1,5 +1,6 @@
 import { CellErrorType, CommandResult, Model } from "../../src";
 import { FORBIDDEN_SHEETNAME_CHARS } from "../../src/constants";
+import { getPivotHighlights } from "../../src/helpers/pivot/pivot_highlight";
 import { EMPTY_PIVOT_CELL } from "../../src/helpers/pivot/table_spreadsheet_pivot";
 import { toZone } from "../../src/helpers/zones";
 import { getEvaluatedCell } from "../test_helpers";
@@ -485,5 +486,27 @@ describe("Pivot plugin", () => {
       expect(model.getters.isPivotUnused("1")).toBe(false);
       expect(model.getters.isPivotUnused("2")).toBe(false);
     });
+
+    test("Can detect unused pivots with multiple pivot formulas in the same cell", () => {
+      const model = createModelWithPivot("A1:I5");
+      addPivot(model, "A1:I5", { name: "Unused Pivot" }, "2");
+      setCellContent(model, "A40", "=PIVOT(1) + PIVOT(2)");
+
+      expect(model.getters.isPivotUnused("1")).toBe(false);
+      expect(model.getters.isPivotUnused("2")).toBe(false);
+    });
+  });
+
+  test("Pivot highlights works with multiple pivot formulas in the same cell", () => {
+    const model = createModelWithPivot("A1:I5");
+    addPivot(model, "A1:I5", { name: "Unused Pivot" }, "2");
+    setCellContent(model, "A40", "=SUM(PIVOT(1) + PIVOT(2))");
+
+    expect(getPivotHighlights(model.getters, "1")).toMatchObject([
+      { range: { zone: toZone("A40") } },
+    ]);
+    expect(getPivotHighlights(model.getters, "2")).toMatchObject([
+      { range: { zone: toZone("A40") } },
+    ]);
   });
 });
