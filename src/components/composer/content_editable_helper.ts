83e55c9a7224da8a03446889a6ce750dee8a2b07
Adrien Minne (adrm) <adrm@odoo.com>
2026-05-07 07:49:10 +0000

    [FIX] composer: remove forced reflow in content editable
    With the introduction of dark mode, we removed the `toHex` in the
    color comparison of `compareContentToSpanElement`, because it didn't
    handle `light-dark()` colors. But using simple color string comparison
    is wrong, because the browser will often change color formats (e.g.
    hex values will be converted to rgb strings).
    
    The method `compareContentToSpanElement` would also use `innerText` to
    compare text, but `innerText` is style-awayre and might trigger a reflow.
    Which can be a performance bottleneck.
    
    This commit:
    - make `toHex` handle `light-dark()` colors, so we can use it in the
    content editable helper
    - change `innerText` to `textContent` to avoid forced reflow when
    the content actually changed
    
    Benchmark:
    ---------------
    
    Performance in Odoo (that has an expansive style/layout computation)
    on a slow-ish CPU when hovering an autocomplete pivot item in
    the formula `=1+1+1+1+1+1+1+1+1+1+1+PIVOT()`:
    
    Before:
    - Animation frame: ~92ms
    - compareContentToSpanElement: ~79 ms
    
    After:
    - Animation frame: ~4ms
    - compareContentToSpanElement: ~0.35 ms
    
    Task: 6199661
    X-original-commit: dc9a78a73d0d616a13600d2c6145486ef56e425e

--- src/components/composer/content_editable_helper.ts
+++ src/components/composer/content_editable_helper.ts
@@ -1,5 +1,5 @@
 import { NEWLINE } from "../../constants";
-import { deepEquals } from "../../helpers";
+import { deepEquals, toHex } from "../../helpers";
 import {
   getBoundingRectAsPOJO,
   getCurrentSelection,
@@ -274,12 +274,12 @@ export class ContentEditableHelper {
 }
 
 function compareContentToSpanElement(content: HtmlContent, node: HTMLElement): boolean {
-  const contentColor = content.color || "";
-  const nodeColor = node.style?.color || "";
+  const contentColor = content.color ? toHex(content.color) : "";
+  const nodeColor = node.style?.color ? toHex(node.style?.color) : "";
 
   const sameColor = contentColor === nodeColor;
   const sameClass = deepEquals(content.classes, [...node.classList]);
-  const sameContent = node.innerText === content.value;
+  const sameContent = node.textContent === content.value;
   return sameColor && sameClass && sameContent;
 }
 
