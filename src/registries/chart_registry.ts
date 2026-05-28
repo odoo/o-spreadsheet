df0d3a168562b57ac8774b925bfb475c1fc6092a
Adrien Minne (adrm) <adrm@odoo.com>
2026-05-28 12:14:35 +0200

    [FIX] charts: pass `onDataSetHover` to chart config
    The handler `onDataSetHover` of the chart data source was never
    actually passed to the chartJS config.
    
    Also the event handlers were not correctly typed.
    
    Task: 6233312

--- src/registries/chart_registry.ts
+++ src/registries/chart_registry.ts
@@ -1,3 +1,4 @@
+import { CoreChartOptions } from "chart.js";
 import { Getters, Range } from "../types";
 import {
   ChartCreationContext,
@@ -109,20 +110,7 @@ interface ChartDataExtractors {
   extractHierarchicalData(): ChartData;
 }
 
-interface ChartJsEventHandlers {
-  onClick?: (
-    // chartjs internals
-    event: unknown,
-    items: unknown,
-    chartJsChart: unknown
-  ) => void;
-  onHover?: (
-    // chartjs internals
-    event: unknown,
-    items: unknown,
-    chartJsChart: unknown
-  ) => void;
-}
+export type ChartJsEventHandlers = Pick<CoreChartOptions<any>, "onClick" | "onHover">;
 
 export interface ChartTypeRegistry extends Registry<ChartTypeBuilder<any>> {
   add<T extends ChartType>(type: T, builder: ChartTypeBuilder<T>): this;
