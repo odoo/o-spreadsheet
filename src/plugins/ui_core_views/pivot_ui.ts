bbf5de7a8a613aebbc6b06bf107ebfbcf1ad3b2a
Rémi Rahir (rar) <rar@odoo.com>
2026-06-03 08:47:46 +0200

    [FIX] Pivot: runtime definition might not be loaded
    the fix in #8539 relies on the runtime definition of a Pivot to access
    its measures. Unfortunately, such definition is lazy loaded through the
    evaluation process so its not loaded (and will throw a loading error) if
    called when the pivot is not actually used.. which corresponds to the
    situation we try to fix in #8539.
    
    Since the measures exist on the core definition of the pivot, we can
    use the core definition safely.
    
    Note that an integration test is added in Odoo is added as well.
    
    Task: 6267596
    Part-of: odoo/o-spreadsheet#8840
    Signed-off-by: Lucas Lefèvre (lul) <lul@odoo.com>

--- src/plugins/ui_core_views/pivot_ui.ts
+++ src/plugins/ui_core_views/pivot_ui.ts
@@ -356,8 +356,8 @@ export class PivotUIPlugin extends UIPlugin {
     }
 
     for (const pivotId of this.getters.getPivotIds()) {
-      const pivot = this.getters.getPivot(pivotId);
-      for (const measure of pivot.definition.measures) {
+      const pivot = this.getters.getPivotCoreDefinition(pivotId);
+      for (const measure of pivot.measures) {
         if (measure.computedBy) {
           const { sheetId } = measure.computedBy;
           const formula = this.getters.getMeasureCompiledFormula(pivotId, measure);
