odoo/o-spreadsheet

commit 233051cd0a0833f826391d604d24dbf7cd22dc21

[FIX] charts: Do not cast empty values as 'count'
Following the recent refactoring, dataset entries with empty values were
mistakenly treated as text and therefore converted as 'counts' whenever
a part of the dataset contained some text values.

Task: 6146610

src/helpers/figures/charts/chart_data_sources.ts
@@ -293,7 +293,7 @@ function getChartDatasetValues(getters: Getters, dataSets: DataSet[]): DatasetVa
       data.filter(isTextResult).length > 1
     ) {
       // Convert categorical data into counts
-      data = data.map((cell) => (!isErrorResult(cell) ? ONE : EMPTY));
+      data = data.map((cell) => (cell.value && isErrorResult(cell) ? ONE : EMPTY));
     } else if (data.every((cell) => !isNumberResult(cell))) {
       hidden = true;
     }

tests/figures/chart/chart_plugin.test.ts
@@ -1411,6 +1411,25 @@ describe("datasource tests", function () {
       })
     );
   });
+
+  test("empty entries of a dataset are skipped", () => {
+    setCellContent(model, "J1", "test1");
+    setCellContent(model, "J2", "test2");
+    setCellContent(model, "J3", "test3");
+    createChart(
+      model,
+      {
+        ...toChartDataSource({
+          dataSets: [{ dataRange: "J:J", dataSetId: "1" }],
+          dataSetsHaveTitle: false,
+        }),
+        type: "bar",
+      },
+      "1"
+    );
+    const { chartJsConfig } = model.getters.getChartRuntime("1") as BarChartRuntime;
+    expect(chartJsConfig.data.datasets[0].data.length).toBe(0);
+  });
 });
 
 describe("title", function () {

commit d62acc26dbfebbe69fc609b44d12174f79f663b0

[FIX] charts: pie chart shows empty chart for text entries
Before this commit:
- Commit 233051c accidentally changed the condition for counting text values.
  As a result, counting failed for valid text data, causing pie charts with
  only text entries to render empty.

After this commit:
- Fixed the condition for counting text values. Pie charts now correctly
  represent counts of distinct text entries.

Task: 6275525

src/helpers/figures/charts/chart_data_sources.ts
@@ -298,7 +298,7 @@ function getChartDatasetValues(getters: Getters, dataSets: DataSet[]): DatasetVa
       data.filter(isTextResult).length > 1
     ) {
       // Convert categorical data into counts
-      data = data.map((cell) => (cell.value && isErrorResult(cell) ? ONE : EMPTY));
+      data = data.map((cell) => (cell.value && !isErrorResult(cell) ? ONE : EMPTY));
     } else if (data.every((cell) => !isNumberResult(cell))) {
       hidden = true;
     }

tests/figures/chart/chart_plugin.test.ts
@@ -1429,7 +1429,7 @@ describe("datasource tests", function () {
       "1"
     );
     const { chartJsConfig } = model.getters.getChartRuntime("1") as BarChartRuntime;
-    expect(chartJsConfig.data.datasets[0].data.length).toBe(0);
+    expect(chartJsConfig.data.datasets[0].data.length).toBe(3);
   });
 });
 