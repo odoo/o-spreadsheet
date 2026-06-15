6600636c84c27ce078ddcca5dd05f7a8d1c9a50c
Adrien Minne (adrm) <adrm@odoo.com>
2026-06-15 10:40:30 +0000

    [FIX] chart: set point color for radar charts
    The radar charts didn't have a color set for the background of their
    points. By default ChartJS use the same color as the dataset background,
    which is transparent for filled radar charts.
    
    This commit adds an explicit color for the points, so neither the
    points nor the values are half transparent for filled radar charts.
    
    closes odoo/o-spreadsheet#9429
    
    Task: 6474893
    X-original-commit: 8430d2d46d9f32680da0ff0b656a3b4d01145ff1
    Signed-off-by: Pierre Rousseau (pro) <pro@odoo.com>
    Signed-off-by: Adrien Minne (adrm) <adrm@odoo.com>

--- src/helpers/figures/charts/runtime/chartjs_dataset.ts
+++ src/helpers/figures/charts/runtime/chartjs_dataset.ts
@@ -369,6 +369,7 @@ export function getRadarChartDatasets(
       hidden,
       borderColor,
       backgroundColor: borderColor,
+      pointBackgroundColor: borderColor,
       pointRadius: definition.hideDataMarkers ? 0 : LINE_DATA_POINT_RADIUS,
     };
     if (fill) {
