096a94b37925e02d850c2f6aeb56e5f78bbffb03
Marceline (matho) <matho@odoo.com>
2026-07-03 15:56:51 +0200

    [IMP] data_analysis_panel: add general statistics section
    This commit introduces a new section in the data analysis panel
    that provides general statistics for the selected data.
    To start, it is only available for single category data, but it will be extended
    in the future.
    
    Task: 6357486
    Co-authored-by: Marceline Thomas (matho) <matho@odoo.com>
    Co-authored-by: Anthony Hendrickx (anhe) <anhe@odoo.com>

--- src/components/side_panel/data_analysis/data_analysis_store.ts
+++ src/components/side_panel/data_analysis/data_analysis_store.ts
@@ -1,3 +1,6 @@
+import { analyzeColumns } from "../../../helpers/data_analysis";
+import { StatSection } from "../../../helpers/data_statistics/statistics_items";
+import { buildStatSections } from "../../../helpers/data_statistics/statistics_suggestion";
 import {
   ChartSuggestion,
   getChartSuggestions,
@@ -10,6 +13,7 @@ import { Get } from "../../../types/store_engine";
 
 export class DataAnalysisStore extends SpreadsheetStore {
   mutators = [] as const;
+  section: StatSection | undefined = undefined;
   hasData: boolean = false;
   chartSuggestions: ChartSuggestion[] = [];
   private isDirty = false;
@@ -69,5 +73,13 @@ export class DataAnalysisStore extends SpreadsheetStore {
 
     const suggestions = this.hasData ? getChartSuggestions(zones, this.getters) : [];
     this.chartSuggestions = suggestions;
+    if (!this.hasData) {
+      this.section = undefined;
+      return;
+    }
+
+    const cols = analyzeColumns(zones, this.getters);
+    const nonEmpty = cols.filter((c) => c.type !== "empty");
+    this.section = buildStatSections(this.getters, nonEmpty, sheetId);
   }
 }
---
+++ src/components/side_panel/data_analysis/data_statistics.ts
@@ -0,0 +1,30 @@
+import { proxy } from "@odoo/owl";
+import { StatSection } from "../../../helpers/data_statistics/statistics_items";
+import { Component } from "../../../owl3_compatibility_layer";
+import { useLocalStore } from "../../../store_engine/store_hooks";
+import { SpreadsheetChildEnv } from "../../../types/spreadsheet_env";
+import { Store } from "../../../types/store_engine";
+import { Section } from "../components/section/section";
+import { DataAnalysisStore } from "./data_analysis_store";
+
+export class DataStatistics extends Component<SpreadsheetChildEnv> {
+  static template = "o-spreadsheet-DataStatistics";
+  static components = {
+    Section,
+  };
+
+  store!: Store<DataAnalysisStore>;
+  selectedCol = proxy({ index: 0 });
+
+  setup() {
+    this.store = useLocalStore(DataAnalysisStore);
+  }
+
+  get activeColSection(): StatSection | undefined {
+    return this.store.section;
+  }
+
+  onColChange(value: string) {
+    this.selectedCol.index = Number(value);
+  }
+}
