33e5eb7a1239fa2feb535a085f58f0248df198af
Marceline (matho) <matho@odoo.com>
2026-07-06 10:07:14 +0200

    [WIP] side panel data analysis

---
+++ src/components/side_panel/data_analysis/data_analysis_panel.ts
@@ -0,0 +1,19 @@
+import { props, proxy, types } from "@odoo/owl";
+import { Component } from "../../../owl3_compatibility_layer";
+import { useLocalStore } from "../../../store_engine/store_hooks";
+import { SpreadsheetChildEnv } from "../../../types/spreadsheet_env";
+import { Store } from "../../../types/store_engine";
+import { DataAnalysisStore } from "./data_analysis_store";
+
+export class DataAnalysisPanel extends Component<SpreadsheetChildEnv> {
+  static template = "o-spreadsheet-DataAnalysisPanel";
+  protected props = props({ onCloseSidePanel: types.function() });
+
+  store!: Store<DataAnalysisStore>;
+  selectedCol = proxy({ index: 0 });
+  openDescriptionKey = proxy({ value: "" });
+
+  setup() {
+    this.store = useLocalStore(DataAnalysisStore);
+  }
+}
