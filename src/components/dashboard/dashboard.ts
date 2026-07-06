bf809efed2ba0f0e75ff5623ad009ded1af4c50a
Rémi Rahir (rar) <rar@odoo.com>
2026-07-06 13:08:52 +0000

    [FIX] dashboard: clickable cell is not triggered by a right click
    How to reproduce:
    - On a dashboard in Odoo, right-click on a cell that contains a formula
      pointing towards records (ODOO.PIVOT or ODOO.LIST)
    
    => you are redirected to the said records
    
    This is a side-effect from PR #5642 which was meant to detect a
    Ctrl-Click but not a right click.
    
    closes odoo/o-spreadsheet#9236
    
    Task: 6365823
    X-original-commit: fb8ec779322eaddff687ce80c1f1e24e3d2176e2
    Signed-off-by: Pierre Rousseau (pro) <pro@odoo.com>

--- src/components/dashboard/dashboard.ts
+++ src/components/dashboard/dashboard.ts
@@ -122,6 +122,9 @@ export class SpreadsheetDashboard extends Component<SpreadsheetChildEnv> {
   }
 
   selectClickableCell(ev: MouseEvent, clickableCell: ClickableCell) {
+    if (![0, 1].includes(ev.button)) {
+      return;
+    }
     const { position, action } = clickableCell;
     action(position, this.env, isMiddleClickOrCtrlClick(ev));
   }
