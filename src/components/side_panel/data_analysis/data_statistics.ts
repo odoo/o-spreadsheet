c19c314b3d44ca6109c0ed78725bd4861462c534
Marceline (matho) <matho@odoo.com>
2026-07-06 11:19:29 +0200

    [IMP] data_statistics: draganddrop of a statistic
    Drag a statistic from the data analysis panel creates
    a scorecard chart that can be dropped in the spreadsheet.
    
    Task: 6357486
    Co-authored-by: Marceline Thomas (matho) <matho@odoo.com>
    Co-authored-by: Anthony Hendrickx (anhe) <anhe@odoo.com>

--- src/components/side_panel/data_analysis/data_statistics.ts
+++ src/components/side_panel/data_analysis/data_statistics.ts
@@ -1,12 +1,13 @@
 import { proxy } from "@odoo/owl";
+import { CellValue } from "../../..";
 import { StatSection } from "../../../helpers/data_statistics/statistics_items";
 import { Component } from "../../../owl3_compatibility_layer";
 import { useLocalStore } from "../../../store_engine/store_hooks";
 import { SpreadsheetChildEnv } from "../../../types/spreadsheet_env";
 import { Store } from "../../../types/store_engine";
+import { startChartDragAndDrop } from "../../helpers/chart_drag_and_drop";
 import { Section } from "../components/section/section";
 import { DataAnalysisStore } from "./data_analysis_store";
-import { CellValue } from "../../..";
 
 export class DataStatistics extends Component<SpreadsheetChildEnv> {
   static template = "o-spreadsheet-DataStatistics";
@@ -27,6 +28,19 @@ export class DataStatistics extends Component<SpreadsheetChildEnv> {
 
   onColChange(value: string) {
     this.selectedCol.index = Number(value);
+  }
+
+  startDragAndDrop(stat: { name: string; formula: string }, ev: MouseEvent) {
+    const definition = {
+      title: { text: stat.name },
+      type: "scorecard" as const,
+      keyValue: stat.formula,
+      humanize: true,
+      baselineMode: "text" as const,
+      baselineColorUp: "#0F0",
+      baselineColorDown: "#F00",
+    };
+    startChartDragAndDrop(this.env, definition, ev);
   }
 
   async copyFormulaToClipboard(formula: string) {
