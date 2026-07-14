ae830f0ff58ec8267c08f562fc05e1b59c5e638e
rmbh-odoo <rmbh@odoo.com>
2026-07-14 09:14:32 +0000

    [FIX] svg: remove unused commented SVGs
    Remove unused SVGs that were left commented out in the code.
    
    closes odoo/o-spreadsheet#9537
    
    Task: 6317134
    X-original-commit: 2c9e7b34e4ae157291737445375c404cf414adda
    Signed-off-by: Adrien Minne (adrm) <adrm@odoo.com>
    Signed-off-by: Ronakkumar Mukeshbhai Bharadiya (rmbh) <rmbh@odoo.com>

--- src/components/side_panel/components/checkbox/checkbox.ts
+++ src/components/side_panel/components/checkbox/checkbox.ts
@@ -3,12 +3,6 @@ import { SpreadsheetChildEnv } from "../../../../types/spreadsheet_env";
 
 import { Component } from "../../../../owl3_compatibility_layer";
 import { types } from "../../../props_validation";
-// FIXME Encoding version used in css
-// const CHECK_SVG = /*xml*/ `
-// <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20'>
-//   <path fill='none' stroke='#FFF' stroke-linecap='round' stroke-linejoin='round' stroke-width='3' d='m6 10 3 3 6-6'/>
-// </svg>
-// `;
 
 export class Checkbox extends Component<SpreadsheetChildEnv> {
   static template = "o-spreadsheet.Checkbox";
--- src/components/side_panel/components/radio_selection/radio_selection.ts
+++ src/components/side_panel/components/radio_selection/radio_selection.ts
@@ -8,13 +8,6 @@ interface Choice {
   label: string;
 }
 
-// FIXME Encoding version used in css
-// const CIRCLE_SVG = /*xml*/ `
-// <svg xmlns='http://www.w3.org/2000/svg' viewBox='-4 -4 8 8'>
-//   <circle r="2" fill="#FFF"/>
-// </svg>
-// `;
-
 export class RadioSelection extends Component<SpreadsheetChildEnv> {
   static template = "o-spreadsheet.RadioSelection";
 
