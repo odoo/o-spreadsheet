2c9e7b34e4ae157291737445375c404cf414adda
rmbh-odoo <rmbh@odoo.com>
2026-07-14 09:14:32 +0000

    [FIX] svg: remove unused commented SVGs
    Remove unused SVGs that were left commented out in the code.
    
    closes odoo/o-spreadsheet#9534
    
    Task: 6317134
    X-original-commit: e28cb32b53ac5c15219b1b0410363aefb7e9a71e
    Signed-off-by: Adrien Minne (adrm) <adrm@odoo.com>
    Signed-off-by: Ronakkumar Mukeshbhai Bharadiya (rmbh) <rmbh@odoo.com>

--- src/components/side_panel/components/round_color_picker/round_color_picker.ts
+++ src/components/side_panel/components/round_color_picker/round_color_picker.ts
@@ -17,13 +17,6 @@ interface Props {
   disableNoColor?: boolean;
 }
 
-// FIXME Encoding version used in css
-// const TRANSPARENT_BACKGROUND_SVG = /*xml*/ `
-// <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10">
-//   <path fill="#d9d9d9" d="M5 5h5v5H5zH0V0h5"/>
-// </svg>
-// `;
-
 export class RoundColorPicker extends Component<Props, SpreadsheetChildEnv> {
   static template = "o-spreadsheet.RoundColorPicker";
   static components = { Section, ColorPicker };
