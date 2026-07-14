b80626986e0c4fcbb07b35b068135365fcea15d3
rmbh-odoo <rmbh@odoo.com>
2026-07-14 09:14:32 +0000

    [FIX] svg: avoid rjsmin issue caused by nested template literals
    Before this commit:
    - In Odoo, when `debug=assets` is disabled, bundles are minified
      using `rjsmin`.
    - Some SVGs were written as template literals directly inside the
      `css` templates that use them, creating nested backticks.
    - `rjsmin` uses regexes (not a real JS parser) and gets confused by
      these nested backticks, breaking the generated CSS.
    - As a result, some SVGs were not rendering correctly.
    
    After this commit:
    - All raw SVGs are moved to a dedicated file (`raw_svgs.ts`) and
      exported as constants.
    - The templates now reference these constants, so there are no more
      nested backticks.
    - Rolldown keeps them as separate variables instead of inlining them
      back, so the problem does not come back after bundling.
    - `rjsmin` can now minify the bundle correctly and all SVGs render properly.
    
    closes odoo/o-spreadsheet#9531
    
    Task: 6317134
    X-original-commit: 7383e9aa95a09be2bc3f7cf13a61804d94c19164
    Signed-off-by: Adrien Minne (adrm) <adrm@odoo.com>
    Signed-off-by: Ronakkumar Mukeshbhai Bharadiya (rmbh) <rmbh@odoo.com>

--- src/components/side_panel/components/round_color_picker/round_color_picker.ts
+++ src/components/side_panel/components/round_color_picker/round_color_picker.ts
@@ -4,6 +4,7 @@ import { Rect, SpreadsheetChildEnv } from "../../../../types";
 import { ColorPicker } from "../../../color_picker/color_picker";
 import { css, cssPropertiesToCss } from "../../../helpers";
 import { getBoundingRectAsPOJO } from "../../../helpers/dom_helpers";
+import { TRANSPARENT_BACKGROUND_SVG } from "../../../icons/raw_svgs";
 import { Section } from "../section/section";
 
 interface State {
@@ -16,12 +17,6 @@ interface Props {
   title?: string;
   disableNoColor?: boolean;
 }
-
-const TRANSPARENT_BACKGROUND_SVG = /*xml*/ `
-<svg xmlns="http://www.w3.org/2000/svg" width="10" height="10">
-  <path fill="#d9d9d9" d="M5 5h5v5H5zH0V0h5"/>
-</svg>
-`;
 
 css/* scss */ `
   .o-round-color-picker-button {
