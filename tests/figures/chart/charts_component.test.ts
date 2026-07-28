efe4dc1a983cd24a5aad200498422addbd0414c8
rmbh-odoo <rmbh@odoo.com>
2026-07-28 07:03:26 +0000

    [FIX] chart: geoChart uses same d3 projection for main and full screen chart
    Before this commit:
    - For North America, we pass a custom d3 projection to rotate the
      `conicConformal` projection.
    - However, since functions are not deep copied, the same projection object is
      shared between the main and full screen charts.
    - ChartJS updates this projection in place, causing the charts to
      reuse each other's scale and resulting in an incorrect chart size.
    - Other regions are not affected as they pass the projection as a string,
      allowing ChartJS to create a fresh d3 projection for each chart.
    
    After this commit:
    - We now pass `conicConformal` as a string as well and use a ChartJS plugin
      to apply the required 100-degree rotation.
    - This ensures each chart gets its own d3 projection instance and
      prevents the projection state from being shared between the main
      and full screen charts.
    
    Task: 6395379
    X-original-commit: a22013275430c68ed7879c5b996da2bdab1a92c0

--- tests/figures/chart/charts_component.test.ts
+++ tests/figures/chart/charts_component.test.ts
@@ -3353,6 +3353,7 @@ test("ChartJS charts extensions are loaded when mounting a spreadsheet, are only
     "calendar", // Calendar controller
     "zoomWindowPlugin",
     "background",
+    "geoProjection",
   ]);
 
   createChart(model, { type: "line" }, "chart2");
