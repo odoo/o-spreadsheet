a870b1577ac14cae760f4d3bc6e659063232d802
Dhrutik Patel <dhrp@odoo.com>
2026-08-12 14:36:20 +0530

    [IMP] headers: cap row and column sizes
    Oversized headers can exceed browser scroll limits and make parts of
    the grid unreachable. Limit manual resizing, autoresize, and dynamic
    row heights to 2000px.
    
    Task: 6234780

--- src/plugins/evaluation/header_sizes_ui.ts
+++ src/plugins/evaluation/header_sizes_ui.ts
@@ -1,4 +1,4 @@
-import { DEFAULT_CELL_HEIGHT } from "../../constants";
+import { DEFAULT_CELL_HEIGHT, MAX_HEADER_SIZE } from "../../constants";
 import {
   deepCopy,
   getAddHeaderStartIndex,
@@ -216,7 +216,10 @@ export class HeaderSizeUIPlugin
 
     const cell = this.getters.getCell(position);
     const colSize = this.getters.getColSize(position.sheetId, position.col);
-    return getDefaultCellHeight(this.ctx, cell, this.getters.getLocale(), colSize);
+    return Math.min(
+      getDefaultCellHeight(this.ctx, cell, this.getters.getLocale(), colSize),
+      MAX_HEADER_SIZE
+    );
   }
 
   private isInMultiRowMerge(position: CellPosition): boolean {
