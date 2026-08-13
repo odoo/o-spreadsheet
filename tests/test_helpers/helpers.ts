e2d97c48a0c228f00575eb9dbde608ecf0660e74
Adrien Minne (adrm) <adrm@odoo.com>
2026-08-13 09:38:05 +0000

    [FIX] xlsx: fix geo chart xlsx export
    If a geo chart was present when exporting a spreadsheet to xlsx, more
    often than not the exported chart would be empty. It was because we
    didn't wait for the geo json to be loaded before exporting the xlsx.
    
    This commit introduces a new getter `loadUsedGeoJsonFeatures`
    that can be called before any spreadsheet export to ensure that
    all the geo chart data is loaded.
    
    closes odoo/o-spreadsheet#9615
    
    Task: 4632983
    X-original-commit: 67c9f4edeb2b4bfd4fed33fff6200474a740db12
    Signed-off-by: Rémi Rahir (rar) <rar@odoo.com>
    Signed-off-by: Adrien Minne (adrm) <adrm@odoo.com>
    Signed-off-by: Pierre Rousseau (pro) <pro@odoo.com>

--- tests/test_helpers/helpers.ts
+++ tests/test_helpers/helpers.ts
@@ -961,7 +961,7 @@ export const mockChart = (options: any = {}) => {
   return mockChartData;
 };
 
-export const mockGeoJsonService: ModelExternalConfig["geoJsonService"] = {
+export const mockGeoJsonService: NonNullable<ModelExternalConfig["geoJsonService"]> = {
   getAvailableRegions: () => [
     { id: "world", label: "World", defaultProjection: "mercator" },
     { id: "usa", label: "United States", defaultProjection: "albersUsa" },
