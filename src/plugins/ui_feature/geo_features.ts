67c9f4edeb2b4bfd4fed33fff6200474a740db12
Adrien Minne (adrm) <adrm@odoo.com>
2026-08-13 09:38:05 +0000

    [FIX] xlsx: fix geo chart xlsx export
    If a geo chart was present when exporting a spreadsheet to xlsx, more
    often than not the exported chart would be empty. It was because we
    didn't wait for the geo json to be loaded before exporting the xlsx.
    
    This commit introduces a new getter `loadUsedGeoJsonFeatures`
    that can be called before any spreadsheet export to ensure that
    all the geo chart data is loaded.
    
    closes odoo/o-spreadsheet#9606
    
    Task: 4632983
    X-original-commit: 6727ef7e426636bdd576960b71258da8e30939f8
    Signed-off-by: Rémi Rahir (rar) <rar@odoo.com>
    Signed-off-by: Adrien Minne (adrm) <adrm@odoo.com>

--- src/plugins/ui_feature/geo_features.ts
+++ src/plugins/ui_feature/geo_features.ts
@@ -1,3 +1,4 @@
+import { isDefined } from "../../helpers";
 import { GeoChartRegion } from "../../types/chart/geo_chart";
 import { ModelConfig } from "../../types/model";
 import { UIPlugin, UIPluginConfig } from "../ui_plugin";
@@ -7,6 +8,7 @@ export class GeoFeaturePlugin extends UIPlugin {
     "getGeoJsonFeatures",
     "geoFeatureNameToId",
     "getGeoChartAvailableRegions",
+    "loadUsedGeoJsonFeatures",
   ] as const;
 
   private readonly geoJsonService: ModelConfig["external"]["geoJsonService"];
@@ -56,6 +58,36 @@ export class GeoFeaturePlugin extends UIPlugin {
       return;
     }
     return this.geoJsonService.geoFeatureNameToId(region, featureName);
+  }
+
+  loadUsedGeoJsonFeatures(): Promise<void> {
+    const regions = this.getters
+      .getSheetIds()
+      .flatMap((sheetId) =>
+        this.getters.getChartIds(sheetId).map((chartId) => {
+          const definition = this.getters.getChartDefinition(chartId);
+          return definition.type === "geo"
+            ? definition.region || this.getters.getGeoChartAvailableRegions()[0]?.id
+            : undefined;
+        })
+      )
+      .filter(isDefined);
+
+    const uniqueRegions = Array.from(new Set(regions));
+    if (uniqueRegions.length && !this.geoJsonService) {
+      console.error("No geoJsonService provided to the model");
+      return Promise.resolve();
+    }
+
+    for (const region of uniqueRegions) {
+      this.getGeoJsonFeatures(region); // Trigger the loading of the regions
+    }
+
+    const promises = uniqueRegions.map((region) => {
+      const cachedGeoJson = this.geoJsonCache[region];
+      return cachedGeoJson instanceof Promise ? cachedGeoJson : Promise.resolve();
+    });
+    return Promise.all(promises).then(() => {});
   }
 
   private convertToGeoJson(
