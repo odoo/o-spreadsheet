dca36bd5b7c061ea569f243d01f1c0c245fa4209
Pierre Rousseau <pro@odoo.com>
2026-08-14 09:24:07 +0000

    [FIX] filter_menu: take number of headers into account when sorting
    opw-6396073
    
    closes odoo/o-spreadsheet#9383
    
    Task: 6471792
    X-original-commit: 0cc6dc14c7cf1fce1528cfbcc6c52695863769b1
    Signed-off-by: Adrien Minne (adrm) <adrm@odoo.com>
    Signed-off-by: Pierre Rousseau (pro) <pro@odoo.com>

--- src/components/filters/filter_menu/filter_menu.ts
+++ src/components/filters/filter_menu/filter_menu.ts
@@ -165,7 +165,7 @@ export class FilterMenu extends Component<SpreadsheetChildEnv> {
       return;
     }
     const sheetId = this.viewStore.displayedSheetId;
-    const contentZone = { ...tableZone, top: tableZone.top + 1 };
+    const contentZone = { ...tableZone, top: tableZone.top + table.config.numberOfHeaders };
     const sortAnchor = { col: filterPosition.col, row: contentZone.top };
     const sortOptions = { emptyCellAsZero: true, sortHeaders: true };
     interactiveSort(this.env, sheetId, sortAnchor, contentZone, sortDirection, sortOptions);
