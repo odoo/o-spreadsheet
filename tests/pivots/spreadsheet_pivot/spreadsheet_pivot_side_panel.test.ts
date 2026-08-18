ef4304582a7cd39dbeda1f8f6ff00d486dac6cc9
Adrien Minne (adrm) <adrm@odoo.com>
2026-08-18 06:18:22 +0000

    [FIX] pivot: unbounded ranges in the pivot side panel
    We currently do not support unbounded ranges as a pivot dataset,
    but in the side panel we allow selecting an unbounded range, which is
    converted to a bounded range in the commands but not in the UI.
    
    Task: 6478220
    X-original-commit: cbed7348ccba34dfaac263fcb9ca20a7fe4b8456

--- tests/pivots/spreadsheet_pivot/spreadsheet_pivot_side_panel.test.ts
+++ tests/pivots/spreadsheet_pivot/spreadsheet_pivot_side_panel.test.ts
@@ -22,6 +22,7 @@ import {
   getComposerColors,
   keyDown,
   setInputValueAndTrigger,
+  simulateClick,
 } from "../../test_helpers/dom_helper";
 import { getCellText, getCoreTable, getEvaluatedCell } from "../../test_helpers/getters_helpers";
 import {
@@ -73,6 +74,15 @@ describe("Spreadsheet pivot side panel", () => {
     await nextTick();
     setInputValueAndTrigger(".os-pivot-title", "New Pivot Name");
     expect(model.getters.getPivotName("1")).toEqual("New Pivot Name");
+  });
+
+  test("Cannot add an unbounded zone as the pivot dataset", async () => {
+    setInputValueAndTrigger(".o-selection-input input", "A:C");
+    await nextTick();
+    await simulateClick(".o-selection-ok");
+
+    expect(".o-selection-input input").toHaveValue("Sheet1!A1:C100");
+    expect(model.getters.getPivotCoreDefinition("1")["dataSet"].zone).toEqual(toZone("A1:C100"));
   });
 
   test("It should be able to defer updates", async () => {
