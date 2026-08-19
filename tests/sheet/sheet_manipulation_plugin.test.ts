59c2807deeba967457821d358f4ff9f321341286
Rémi Rahir (rar) <rar@odoo.com>
2026-08-19 09:12:50 +0000

    [FIX] grid: hide AddRowFooter when the mainViewport is too small
    How to reproduce:
    - insert a list
    - freeze pane to the lowest visible row
    - start scrolling to get to the bottom of the page
    
    => the full layout is broken
    
    closes odoo/o-spreadsheet#9597
    
    Task: 6103620
    X-original-commit: e8286954de7374584a1cc50d1732b0c69bc5d004
    Signed-off-by: Adrien Minne (adrm) <adrm@odoo.com>
    Signed-off-by: Rémi Rahir (rar) <rar@odoo.com>

--- tests/sheet/sheet_manipulation_plugin.test.ts
+++ tests/sheet/sheet_manipulation_plugin.test.ts
@@ -956,7 +956,7 @@ describe("Rows", () => {
       const newDimensions = model.getters.getMainViewportRect();
       expect(newDimensions).toMatchObject({
         width: DEFAULT_CELL_WIDTH, // sum of col sizes
-        height: 142, // sum of row sizes  + 46px for adding rows footer
+        height: 96, // sum of row sizes
       });
     });
     test("On addition after", () => {
@@ -975,7 +975,7 @@ describe("Rows", () => {
       const newDimensions = model.getters.getMainViewportRect();
       expect(newDimensions).toMatchObject({
         width: DEFAULT_CELL_WIDTH, // sum of col sizes
-        height: 162, // sum of row sizes + 46px for adding rows footer
+        height: 116, // sum of row sizes + 1 correction px
       });
       expect(model.getters.getNumberRows(sheetId)).toBe(6);
     });
@@ -992,7 +992,7 @@ describe("Rows", () => {
       let dimensions = model.getters.getMainViewportRect();
       expect(dimensions).toMatchObject({
         width: DEFAULT_CELL_WIDTH, // sum of col sizes
-        height: 142, // sum of row sizes + 46px for adding rows footer
+        height: 96, // sum of row sizes
       });
       const to = model.getters.getActiveSheetId();
       createSheet(model, { activate: true, sheetId: "42" });
@@ -1000,7 +1000,7 @@ describe("Rows", () => {
       dimensions = model.getters.getMainViewportRect();
       expect(dimensions).toMatchObject({
         width: DEFAULT_CELL_WIDTH, // sum of col sizes
-        height: 142, // sum of row sizes + 46px for adding rows footer
+        height: 96, // sum of row sizes (footer is hidden)
       });
     });
 
