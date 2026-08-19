913ea15b42caff02123434a660c6a9bde28a5173
Adrien Minne (adrm) <adrm@odoo.com>
2026-08-19 13:09:49 +0200

    [FIX] inputs: explicitly define border-style
    The border style for `o-input` class was not explicitly defined. It
    breaks in chrome 151 which somewhy changed the default from `solid` to
    `inset`.
    
    Task: 0

--- src/components/text_input/text_input.ts
+++ src/components/text_input/text_input.ts
@@ -9,6 +9,7 @@ css/* scss */ `
     .os-input {
       box-sizing: border-box;
       border-width: 0 0 1px 0;
+      border-style: solid;
       border-color: transparent;
       outline: none;
       text-overflow: ellipsis;
