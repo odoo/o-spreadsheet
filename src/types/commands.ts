cda4f3f73ffbd4af6266917272e00040dca6f9e4
Adrien Minne (adrm) <adrm@odoo.com>
2026-08-24 10:34:27 +0200

    [FIX] commands: add `UPDATE_COLOR_SCHEME` to readonly commands
    Task: 6497278

--- src/types/commands.ts
+++ src/types/commands.ts
@@ -220,6 +220,8 @@ export const readonlyAllowedCommands = new Set<CommandTypes>([
   "UPDATE_CAROUSEL_ACTIVE_ITEM",
 
   "UPDATE_PIVOT",
+
+  "UPDATE_COLOR_SCHEME",
 ]);
 
 export const lockedSheetAllowedCommands = new Set<Command["type"]>([
