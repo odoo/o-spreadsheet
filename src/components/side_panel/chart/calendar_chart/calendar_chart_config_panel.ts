5aaa5ae35d6b429a83f344a83ab24c24d3fea16a
Adrien Minne (adrm) <adrm@odoo.com>
2026-08-24 11:39:04 +0000

    [FIX] calendar chart: wrong groupBy choice filtering
    In the side panel of the calendar chart, we would filter the available
    granularities based on the chart data. There were a bunch of issues with
    this filtering:
    
    - It's functionally limiting what the user can do. He cannot select
    a year granularity if the data is only for one year, which he might want
    to do to take future data into account.
    - The side panel code would use different logic than the actual
    calendar runtime. Eg. calendar runtime accept non-date-formatted number
    values, the side panel would not.
    - It wouldn't display the current granularity if it was not in the
    filtered list, showing a blank select.
    - It would use private-ish chart helpers that we don't really want
    to use everywhere
    - It would only work for range data, but in future version the side
    panel should be chart datasource-agnostic.
    - It wasn't tested
    
    This commit plainly removes the filtering logic, and instead show all
    available granularity. It's also consistent with what we do with
    pivot date granularities.
    
    Task: 5358625
    X-original-commit: 653c219732e0df9304909ba6d2a763f54406b1b0

--- src/components/side_panel/chart/calendar_chart/calendar_chart_config_panel.ts
+++ src/components/side_panel/chart/calendar_chart/calendar_chart_config_panel.ts
@@ -1,17 +1,9 @@
-import { toJsDate } from "../../../../functions/helpers";
-import { isDateTime } from "../../../../helpers/dates";
-import { SpreadsheetChart } from "../../../../helpers/figures/chart";
-import { getChartData } from "../../../../helpers/figures/charts/chart_data_sources";
-import { getBarChartData } from "../../../../helpers/figures/charts/runtime/chart_data_extractor";
 import { ALL_PERIODS } from "../../../../helpers/pivot/pivot_helpers";
 import {
   CALENDAR_CHART_GRANULARITIES,
   CalendarChartDefinition,
   CalendarChartGranularity,
 } from "../../../../types/chart/calendar_chart";
-import { ChartRangeDataSource } from "../../../../types/chart/chart";
-import { DEFAULT_LOCALE } from "../../../../types/locale";
-import { ValueAndLabel } from "../../../../types/misc";
 import { Select } from "../../../select/select";
 import { GenericChartConfigPanel } from "../building_blocks/generic_side_panel/config_panel";
 import { ChartSidePanelProps } from "../common";
@@ -26,64 +18,6 @@ export class CalendarChartConfigPanel extends GenericChartConfigPanel<
     value,
     label: ALL_PERIODS[value],
   }));
-
-  getGroupByOptions(): ValueAndLabel[] {
-    const sheetId = this.env.model.getters.getFigureSheetId(
-      this.env.model.getters.getFigureIdFromChartId(this.props.chartId)
-    )!;
-    const definition = SpreadsheetChart.fromStrDefinition(
-      this.env.model.getters,
-      sheetId,
-      this.props.definition
-    ).getRangeDefinition() as CalendarChartDefinition;
-    const data = getBarChartData(
-      definition,
-      getChartData(this.env.model.getters, definition.dataSource as ChartRangeDataSource),
-      this.env.model.getters,
-      this.env.model.getters.getSpreadsheetTheme().colorThemeName
-    );
-    const labels = data.labels.filter((l) => isDateTime(l, DEFAULT_LOCALE));
-    if (labels.length === 0) {
-      return [];
-    }
-    const dates = labels.map((label) => toJsDate(label, this.env.model.getters.getLocale()));
-    const uniqueYears = new Set<number>();
-    const uniqueMonths = new Set<number>();
-    const uniqueDays = new Set<number>();
-    const uniqueHours = new Set<number>();
-    const uniqueMinutes = new Set<number>();
-    const uniqueSeconds = new Set<number>();
-    for (const date of dates) {
-      uniqueYears.add(date.getFullYear());
-      uniqueMonths.add(date.getMonth());
-      uniqueDays.add(date.getDate());
-      uniqueHours.add(date.getHours());
-      uniqueMinutes.add(date.getMinutes());
-      uniqueSeconds.add(date.getSeconds());
-    }
-    const groupByPossibilities = this.groupByChoices.filter((groupBy) => {
-      switch (groupBy.value) {
-        case "year":
-          return uniqueYears.size > 1;
-        case "quarter_number":
-        case "month_number":
-          return uniqueMonths.size > 1;
-        case "iso_week_number":
-        case "day_of_month":
-        case "day_of_week":
-          return uniqueDays.size > 1;
-        case "hour_number":
-          return uniqueHours.size > 1;
-        case "minute_number":
-          return uniqueMinutes.size > 1;
-        case "second_number":
-          return uniqueSeconds.size > 1;
-        default:
-          return false;
-      }
-    });
-    return groupByPossibilities;
-  }
 
   getGroupByType(currentAxis: "horizontal" | "vertical"): CalendarChartGranularity {
     const groupBy =
