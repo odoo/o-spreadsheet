0ad6f41bcb43b424ce6e3e31b8e0c787393e54a4
Florian Damhaut <flda@odoo.com>
2026-09-07 11:13:37 +0000

    [FIX] ComposerHighlight: Highlight the correct sheet with `#`
    Highlight the correct sheet when selecting a formula with spill operator
    
    Task: 6527596
    X-original-commit: df01b7dc919e42c6f877c40f0d1e145848d54e00

--- src/components/composer/composer/abstract_composer_store.ts
+++ src/components/composer/composer/abstract_composer_store.ts
@@ -845,7 +845,7 @@ export abstract class AbstractComposerStore extends SpreadsheetStore {
       row: range.zone.top,
     });
     if (spreadZone) {
-      return this.getters.getRangeFromZone(sheetId, spreadZone);
+      return this.getters.getRangeFromZone(range.sheetId, spreadZone);
     }
     return range;
   }
