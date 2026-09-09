2568a7f4858460a5e480627a55052e79fe76de68
Marceline (matho) <matho@odoo.com>
2026-09-09 09:15:21 +0200

    [REF] figure_container, chart_drag_and_drop: small rewrite
    Task: 6537589

--- src/components/figures/figure_container/figure_container.ts
+++ src/components/figures/figure_container/figure_container.ts
@@ -14,7 +14,7 @@ import { UID } from "../../../types/misc";
 import { DOMDimension, Rect } from "../../../types/rendering";
 import { SpreadsheetChildEnv } from "../../../types/spreadsheet_env";
 import { Store } from "../../../types/store_engine";
-import { getCarouselOverlappingChart } from "../../helpers/chart_drag_and_drop";
+import { getOverlappedFigure } from "../../helpers/chart_drag_and_drop";
 import { cssPropertiesToCss } from "../../helpers/css";
 import { isCtrlKey } from "../../helpers/dom_helpers";
 import { startDnd } from "../../helpers/drag_and_drop";
@@ -350,7 +350,7 @@ export class FiguresContainer extends Component<SpreadsheetChildEnv> {
     const draggedFigureId = figureUI.id;
 
     let hasStartedDnd = false;
-    let overlappingChartOrCarousel: FigureUI | undefined = undefined;
+    let overlappedFigure: FigureUI | undefined = undefined;
     const onMouseMove = (ev: MouseEvent) => {
       const currentMousePosition = { x: ev.clientX / zoom, y: ev.clientY / zoom };
 
@@ -371,17 +371,14 @@ export class FiguresContainer extends Component<SpreadsheetChildEnv> {
       );
       const draggedFigure = selectedFigures.find((f) => f.id === draggedFigureId);
 
-      overlappingChartOrCarousel = undefined;
+      overlappedFigure = undefined;
       const otherFigures = this.getOtherFigures(selectedFigures.map((f) => f.id));
       if (draggedFigure && !selectedFigures.find((f) => f.tag !== "chart")) {
-        overlappingChartOrCarousel = getCarouselOverlappingChart(draggedFigure, otherFigures, [
-          "carousel",
-          "chart",
-        ]);
+        overlappedFigure = getOverlappedFigure(draggedFigure, otherFigures, ["carousel", "chart"]);
       }
-      this.chartDragStore.setHighlightedFigure(overlappingChartOrCarousel?.id);
+      this.chartDragStore.setHighlightedFigure(overlappedFigure?.id);
 
-      if (!overlappingChartOrCarousel) {
+      if (!overlappedFigure) {
         const snapReturn = snapForMove(this.env, selectedFigures, otherFigures);
         this.dnd.selectedFigures = snapReturn.snappedFigures;
         this.dnd.selectedRect = this.getDndFigureRect();
@@ -409,7 +406,7 @@ export class FiguresContainer extends Component<SpreadsheetChildEnv> {
         }
         return;
       }
-      if (!overlappingChartOrCarousel) {
+      if (!overlappedFigure) {
         const payloads =
           this.dnd.selectedFigures?.map((f) => {
             return {
@@ -420,15 +417,15 @@ export class FiguresContainer extends Component<SpreadsheetChildEnv> {
           }) || [];
         this.env.model.dispatch("UPDATE_FIGURES", { figures: payloads });
       } else {
-        const overlappingFigureId = overlappingChartOrCarousel.id;
+        const overlappingFigureId = overlappedFigure.id;
         const chartFigureIds = this.dnd.selectedFigures?.map((f) => f.id) || [];
-        if (overlappingChartOrCarousel.tag === "carousel") {
+        if (overlappedFigure.tag === "carousel") {
           this.env.model.dispatch("ADD_FIGURES_CHART_TO_CAROUSEL", {
             sheetId,
             carouselFigureId: overlappingFigureId,
             chartFigureIds: chartFigureIds,
           });
-        } else if (overlappingChartOrCarousel.tag === "chart") {
+        } else if (overlappedFigure.tag === "chart") {
           this.env.model.dispatch("MERGE_CHART_FIGURES_INTO_CAROUSEL", {
             sheetId,
             baseFigureId: overlappingFigureId,
