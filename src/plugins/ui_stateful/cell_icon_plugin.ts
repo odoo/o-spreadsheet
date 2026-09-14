8631952355f864a2dc8a91d340144c83eda5f469
Florian Damhaut <flda@odoo.com>
2026-09-14 16:23:35 +0200

    [REF] UPDATE_CAROUSEL_ACTIVE_ITEM: new handlers

--- src/plugins/ui_stateful/cell_icon_plugin.ts
+++ src/plugins/ui_stateful/cell_icon_plugin.ts
@@ -60,6 +60,7 @@ export class CellIconPlugin extends UIPlugin {
     ADD_NEW_CHART_TO_CAROUSEL: this.clearCellIconsCache,
     ADD_FIGURES_CHART_TO_CAROUSEL: this.clearCellIconsCache,
     DUPLICATE_CAROUSEL_CHART: this.clearCellIconsCache,
+    UPDATE_CAROUSEL_ACTIVE_ITEM: this.clearCellIconsCache,
   };
 
   handle(cmd: Command) {
