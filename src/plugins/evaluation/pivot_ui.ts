9aaead7573c8beed99c1912d95a891a5a2faec61
Florian Damhaut <flda@odoo.com>
2026-09-14 16:54:23 +0200

    [REF] LOCK_SHEET: new handlers

--- src/plugins/evaluation/pivot_ui.ts
+++ src/plugins/evaluation/pivot_ui.ts
@@ -112,6 +112,7 @@ export class PivotUIPlugin extends EvaluationPlugin {
     UPDATE_CAROUSEL: this.invalidateUnusedPivots,
     SET_GRID_LINES_VISIBILITY: this.invalidateUnusedPivots,
     MOVE_SHEET: this.invalidateUnusedPivots,
+    LOCK_SHEET: this.invalidateUnusedPivots,
   };
 
   constructor(config: EvaluationPluginConfig) {
