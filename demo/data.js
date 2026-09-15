5549366441672effc503580574a3fafd917116ba
Adrien Minne (adrm) <adrm@odoo.com>
2026-09-15 06:20:57 +0000

    [FIX] demo: fix scorecard demo definition
    The scorecard in the demo data has a wrong definition.
    "baselineMode: absolute" is not a valid baseline mode in 17.0.
    In some later versions, it's missing a baseline mode altogether.
    
    Task: 6573109
    X-original-commit: b00d61a7f564329f5c4274bca4c5f1d6009ebb0c

--- demo/data.js
+++ demo/data.js
@@ -555,6 +555,7 @@ export const demoData = {
             baselineDescr: {
               text: "Descr",
             },
+            baselineMode: "difference",
             keyValue: "Sheet1!B29",
             humanize: false,
             chartId: "4",
