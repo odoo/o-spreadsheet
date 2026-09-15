a8e6e737fe0743c89fe1adc9e3c9b28042a0d1ed
Adrien Minne (adrm) <adrm@odoo.com>
2026-09-15 09:26:37 +0000

    [IMP] demo: debounce xml template build
    If you change multiple xml files (ie. a checkout), we will build
    the xml templates for each xml file changed. We can debounce the
    build to only do it once instead.
    
    Task: 6574005
    X-original-commit: db1450385b50286af8018b2004040278c7a9eba8

--- tools/bundle_xml/watch_xml_templates.cjs
+++ tools/bundle_xml/watch_xml_templates.cjs
@@ -1,9 +1,13 @@
 const watch = require("node-watch");
 const bundle = require("./bundle_xml_templates.cjs");
 
+let timeOutId = undefined;
 const watcher = watch("./src", { filter: /\.xml$/, recursive: true }, (ev, name) => {
-  console.log(`\n File ${name}: ${ev}`);
-  bundle.writeOwlTemplateBundleToFile("build");
+  console.log(`File ${name}: ${ev}`);
+  clearTimeout(timeOutId);
+  timeOutId = setTimeout(() => {
+    bundle.writeOwlTemplateBundleToFile("build");
+  }, 100);
 });
 
 bundle.writeOwlTemplateBundleToFile("build");
